b93617fb79971afe82bf0a6bd19f640739a4cd84
Christian Kvalheim <christian.kvalheim@xing.com>
2010-01-29 19:10:04 +0100

    finished gridfs example, cleanup of code base

--- examples/gridfs.js
+++ examples/gridfs.js
@@ -13,8 +13,8 @@ var host = process.ENV['MONGO_NODE_DRIVER_HOST'] != null ? process.ENV['MONGO_NO
 var port = process.ENV['MONGO_NODE_DRIVER_PORT'] != null ? process.ENV['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
 sys.debug(">> Connecting to " + host + ":" + port);
-var db = new Db('node-mongo-examples', new Server(host, port, {}), {});
-db.open(function(db) {
+var db1 = new Db('node-mongo-examples', new Server(host, port, {}), {});
+db1.open(function(db) {
   // Write a new file
   var gridStore = new GridStore(db, "foobar", "w");
   gridStore.open(function(gridStore) {    
@@ -36,7 +36,9 @@ db.open(function(db) {
                 gridStore.open(function(gridStore) {
                   gridStore.write(function(gridStore) {
                     gridStore.close(function(result) {
-                      dump(db, 'foobar');          
+                      dump(db, 'foobar', function() {
+                        db.close();                        
+                      });
                     });
                   }, 'hello, sailor!');
                 });
@@ -47,14 +49,17 @@ db.open(function(db) {
       });
     }, "hello world!");
   });
+});
 
+var db2 = new Db('node-mongo-examples', new Server(host, port, {}), {});
+db2.open(function(db) {
   // File existence tests
   var gridStore = new GridStore(db, "foobar2", "w");
   gridStore.open(function(gridStore) {    
     gridStore.write(function(gridStore) {
       gridStore.close(function(result) {
         GridStore.exist(function(result) {
-          sys.debug("File 'foobar' exists: " + result);
+          sys.debug("File 'foobar2' exists: " + result);
         }, db, 'foobar2');
         
         GridStore.exist(function(result) {
@@ -65,7 +70,7 @@ db.open(function(db) {
         GridStore.read(function(data) {
           sys.debug(data);
         }, db, 'foobar2', 6, 7);
-        
+
         // Rewind/seek/tell
         var gridStore2 = new GridStore(db, 'foobar2', 'w');
         gridStore2.open(function(gridStore) {
@@ -79,6 +84,14 @@ db.open(function(db) {
           gridStore.write(function(){}, 'y');
           gridStore.close(function() {
             dump(db, 'foobar2');
+
+            // Unlink file (delete)
+            GridStore.unlink(function(gridStore) {
+              GridStore.exist(function(result) {
+                sys.debug("File 'foobar2' exists: " + result);
+                db.close();
+              }, db, 'foobar2');            
+            }, db, 'foobar2');
           });
         });
       });
@@ -86,8 +99,57 @@ db.open(function(db) {
   });
 });
 
-function dump(db, filename) {
+var db3 = new Db('node-mongo-examples', new Server(host, port, {}), {});
+db3.open(function(db) {
+  // Metadata
+  var gridStore = new GridStore(db, "foobar3", "w");
+  gridStore.open(function(gridStore) {    
+    gridStore.write(function(){}, 'hello, world!');
+    gridStore.close(function() {
+      gridStore = new GridStore(db, 'foobar3', "r");
+      gridStore.open(function(gridStore) {
+        sys.debug("contentType: " + gridStore.contentType);
+        sys.debug("uploadDate: " + gridStore.uploadDate);
+        sys.debug("chunkSize: " + gridStore.chunkSize);
+        sys.debug("metadata: " + gridStore.metadata);          
+      });
+      
+      // Add some metadata
+      gridStore = new GridStore(db, 'foobar3', "w+");
+      gridStore.open(function(gridStore) {
+        gridStore.contentType = 'text/xml';
+        gridStore.metadata = {'a':1};
+        gridStore.close(function() {
+          // Print the metadata
+          gridStore = new GridStore(db, 'foobar3', "r");
+          gridStore.open(function(gridStore) {
+            sys.debug("contentType: " + gridStore.contentType);
+            sys.debug("uploadDate: " + gridStore.uploadDate);
+            sys.debug("chunkSize: " + gridStore.chunkSize);
+            sys.debug("metadata: " + gridStore.metadata);          
+            db.close();
+          });            
+        });
+      });        
+    });
+  });
+  
+  // You can also set meta data when initially writing to a file
+  // setting root means that the file and its chunks are stored in a different root
+  // collection: instead of gridfs.files and gridfs.chunks, here we use
+  // my_files.files and my_files.chunks      
+  var gridStore = new GridStore(db, "foobar3", "w", {'content_type':'text/plain', 
+    'metadata':{'a':1}, 'chunk_size': 1024*4, 'root':'my_files'});
+  gridStore.open(function(gridStore) {    
+    gridStore.write(function(){}, 'hello, world!');
+    gridStore.close(function() {
+    });
+  });
+});
+
+function dump(db, filename, callback) {
   GridStore.read(function(data) {
     sys.debug(data);
+    if(callback != null) callback();
   }, db, filename); 
 }
