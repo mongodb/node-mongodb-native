98f348204cbd7fd9d8e21f33359a795d1a9f3cb5
Christian Amor Kvalheim <christkv@gmail.com>
2011-07-18 15:15:48 +0200

    Small benchmark change

--- benchmark/bson_benchmark.js
+++ benchmark/bson_benchmark.js
@@ -43,20 +43,6 @@ for (i=COUNT; --i>=0; ) {
     },
     subArray: [1,2,3,4,5,6,7,8,9,10],
     anotherString: "another string"
-    
-      // id: new ObjectID(),
-      // string: "Strings are great",
-      // decimal: 3.14159265,
-      // bool: true,
-      // integer: 5,
-      // 
-      // subObject: {
-      //   moreText: "Bacon ipsum dolor sit amet cow pork belly rump ribeye pastrami andouille. Tail hamburger pork belly, drumstick flank salami t-bone sirloin pork chop ribeye ham chuck pork loin shankle. Ham fatback pork swine, sirloin shankle short loin andouille shank sausage meatloaf drumstick. Pig chicken cow bresaola, pork loin jerky meatball tenderloin brisket strip steak jowl spare ribs. Biltong sirloin pork belly boudin, bacon pastrami rump chicken. Jowl rump fatback, biltong bacon t-bone turkey. Turkey pork loin boudin, tenderloin jerky beef ribs pastrami spare ribs biltong pork chop beef.",
-      //   longKeylongKeylongKeylongKeylongKeylongKey: "Pork belly boudin shoulder ribeye pork chop brisket biltong short ribs. Salami beef pork belly, t-bone sirloin meatloaf tail jowl spare ribs. Sirloin biltong bresaola cow turkey. Biltong fatback meatball, bresaola tail shankle turkey pancetta ham ribeye flank bacon jerky pork chop. Boudin sirloin shoulder, salami swine flank jerky t-bone pork chop pork beef tongue. Bresaola ribeye jerky andouille. Ribeye ground round sausage biltong beef ribs chuck, shank hamburger chicken short ribs spare ribs tenderloin meatloaf pork loin."
-      // },
-      // 
-      // subArray: [1,2,3,4,5,6,7,8,9,10],
-      // anotherString: "another string"
   }
 
   objectBSON = BSON.serialize(object, null, true)
@@ -77,21 +63,17 @@ console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/
 // end = new Date
 // console.log("json size (chars): ", objectJSON.length)
 // console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
-// 
-// 
-// console.log(COUNT + " BSON.deserialize(objectBSON)")
-// start = new Date
-// 
-// for (i=COUNT; --i>=0; ) {
-//   x = BSON.deserialize(objectBSON)
-//   // debug("=====================================================================")
-//   // debug(inspect(x))
-//   // x = BSON.deserialize2(objectBSON)
-//   // debug(inspect(x))
-// }
-// 
-// end = new Date
-// console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
+
+
+console.log(COUNT + " BSON.deserialize(objectBSON)")
+start = new Date
+
+for (i=COUNT; --i>=0; ) {
+  x = BSON.deserialize(objectBSON)
+}
+
+end = new Date
+console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
 
 
 // console.log(COUNT + " JSON.parse(objectJSON)")
