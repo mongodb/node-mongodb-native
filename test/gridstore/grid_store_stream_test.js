161dc2d8446aec6b01e1fa0da0141372c13b400d
Christian Amor Kvalheim <christkv@gmail.com>
2011-08-24 16:50:05 +0200

    Fixed variable chunk size error in GridFS code for normal strings and also buffers, nasty bug but only appears if you change the size of the chunks

--- test/gridstore/grid_store_stream_test.js
+++ test/gridstore/grid_store_stream_test.js
@@ -10,6 +10,7 @@ var testCase = require('../../deps/nodeunit').testCase,
   Collection = mongodb.Collection,
   GridStore = mongodb.GridStore,
   Chunk = mongodb.Chunk,
+  Step = require("../../deps/step/lib/step");    
   Server = mongodb.Server;
 
 var MONGODB = 'integration_tests';
@@ -44,15 +45,121 @@ var tests = testCase({
       callback();        
     }      
   },
+  
+  shouldCorrectlyWriteLargeFileStringAndReadBack : function(test) {
+    var db = client;
+    var fileId = new client.bson_serializer.ObjectID();
+    var gridStore = new GridStore(db, fileId, "w", {root:'fs'});
+    gridStore.chunkSize = 5000;
+  
+    gridStore.open(function(err, gridStore) {
+     Step(
+       function writeData() {
+         var group = this.group();
+         var d = '';
+         for(var j = 0; j < 5000;j++) {
+           d = d + '+';
+         }
+  
+         for(var i = 0; i < 15000; i += 5000) {
+           gridStore.write(d, false, group());
+         }   
+       },
+  
+       function readAsStream() {
+         gridStore.close(function(err, result) {
+           var gotEnd = false;           
+           var endLen = 0;
+  
+           var gridStore = new GridStore(db, fileId, "r");
+           gridStore.open(function(err, gridStore) {
+             var stream = gridStore.stream(true);
+  
+             stream.on("data", function(chunk) {
+               endLen += chunk.length
+               // Test length of chunk
+               test.equal(5000, chunk.length);
+               // Check each chunk's data
+               for(var i = 0; i < 5000; i++) test.equal('+', String.fromCharCode(chunk[i]));
+             });
+  
+             stream.on("end", function() {
+               gotEnd = true;
+             });
+  
+             stream.on("close", function() {
+               test.equal(15000, endLen);
+               test.equal(true, gotEnd);
+               test.done();
+             });
+           });           
+         });
+       }
+     )
+    });    
+  },
+
+  shouldCorrectlyWriteLargeFileBufferAndReadBack : function(test) {
+    var db = client;
+    var fileId = new client.bson_serializer.ObjectID();
+    var gridStore = new GridStore(db, fileId, "w", {root:'fs'});
+    gridStore.chunkSize = 5000;
+
+    gridStore.open(function(err, gridStore) {
+      Step(
+       function writeData() {
+         var group = this.group();
+         var d = new Buffer(5000);
+         for(var j = 0; j < 5000;j++) {
+           d[j] = 43;
+         }         
+
+         for(var i = 0; i < 15000; i += 5000) {
+           gridStore.write(d, false, group());
+         }   
+       },
+
+       function readAsStream() {
+         gridStore.close(function(err, result) {
+           var gotEnd = false;           
+           var endLen = 0;
+
+           var gridStore = new GridStore(db, fileId, "r");
+           gridStore.open(function(err, gridStore) {
+             var stream = gridStore.stream(true);
+
+             stream.on("data", function(chunk) {
+               endLen += chunk.length
+               // Test length of chunk
+               test.equal(5000, chunk.length);
+               // Check each chunk's data
+               for(var i = 0; i < 5000; i++) test.equal('+', String.fromCharCode(chunk[i]));
+             });
+
+             stream.on("end", function() {
+               gotEnd = true;
+             });
+
+             stream.on("close", function() {
+               test.equal(15000, endLen);
+               test.equal(true, gotEnd);
+               test.done();
+             });
+           });           
+         });
+       }
+      )
+    });    
+  },
 
   shouldCorrectlyReadFileUsingStream : function(test) {
     var gridStoreR = new GridStore(client, "test_gs_read_stream", "r");
     var gridStoreW = new GridStore(client, "test_gs_read_stream", "w");
     var data = fs.readFileSync("./test/gridstore/test_gs_weird_bug.png", 'binary');
-
+  
     var readLen = 0;
     var gotEnd = 0;
-
+  
     gridStoreW.open(function(err, gs) {
       gs.write(data, function(err, gs) {
         gs.close(function(err, result) {
