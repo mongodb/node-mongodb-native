f871a238fc8e45a4a454edc767c72cae80149f9e
Christian Amor Kvalheim <christkv@gmail.com>
2011-10-06 17:49:09 +0200

    Started new socket connection implementation

---
+++ test/connection/connection_pool_test.js
@@ -0,0 +1,48 @@
+var mongodb = process.env['TEST_NATIVE'] != null ? require('../../lib/mongodb').native() : require('../../lib/mongodb').pure();
+
+var testCase = require('../../deps/nodeunit').testCase,
+  debug = require('util').debug,
+  inspect = require('util').inspect,
+  Buffer = require('buffer').Buffer,
+  gleak = require('../../tools/gleak'),
+  fs = require('fs'),
+  ConnectionPool = require('../../lib/mongodb/connection/connection_pool').ConnectionPool;
+
+var tests = testCase({
+  setUp: function(callback) {
+    callback();        
+  },
+  
+  tearDown: function(callback) {
+    callback();        
+  },
+
+  'Should Correctly create a pool instance with the expected values' : function(test) {
+    var connectionPool = new ConnectionPool('localhost', 2000, 1, {timeout:100, noDelay:true});
+    test.equal(100, connectionPool.socketOptions.timeout);
+    test.equal(true, connectionPool.socketOptions.noDelay);
+    test.equal(null, connectionPool.socketOptions.encoding);
+    test.equal(0, connectionPool.socketOptions.bufferSize);    
+    test.done();
+  },
+  
+  'Should Correctly create a pool of connections and receive an ok when all connections are active' : function(test) {
+    var connectionPool = new ConnectionPool('localhost', 2000, 1, {timeout:100, noDelay:true});
+    // Add event handler that will fire once the pool is ready
+    connectionPool.on("poolReady", function(err, result) {
+      test.done();
+    })
+    
+    // Start the pool
+    connectionPool.start();    
+  },
+  
+  noGlobalsLeaked : function(test) {
+    var leaks = gleak.detectNew();
+    test.equal(0, leaks.length, "global var leak detected: " + leaks.join(', '));
+    test.done();
+  }  
+});
+
+// Assign out tests
+module.exports = tests;
