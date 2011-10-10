d17a85c42814d3b098d659429cf09f75d273e0d2
Christian Amor Kvalheim <christkv@gmail.com>
2011-10-10 12:29:20 +0200

    Updated benchmark for valgrind test

--- benchmark/bson_benchmark.js
+++ benchmark/bson_benchmark.js
@@ -13,24 +13,24 @@ var BSON = require('../lib/mongodb').BSONPure.BSON,
   Binary = require('../lib/mongodb').BSONPure.Binary;
 
 var COUNT = 100000;
-// var COUNT = 1;
+var COUNT = 100;
 
 var object = {
-  // string: "Strings are great",
-  // decimal: 3.14159265,
-  // bool: true,
+  string: "Strings are great",
+  decimal: 3.14159265,
+  bool: true,
   integer: 5,
   long: Long.fromNumber(100),
   bin: new Binary(),
-  // 
-  // subObject: {
-  //   moreText: "Bacon ipsum dolor sit amet cow pork belly rump ribeye pastrami andouille. Tail hamburger pork belly, drumstick flank salami t-bone sirloin pork chop ribeye ham chuck pork loin shankle. Ham fatback pork swine, sirloin shankle short loin andouille shank sausage meatloaf drumstick. Pig chicken cow bresaola, pork loin jerky meatball tenderloin brisket strip steak jowl spare ribs. Biltong sirloin pork belly boudin, bacon pastrami rump chicken. Jowl rump fatback, biltong bacon t-bone turkey. Turkey pork loin boudin, tenderloin jerky beef ribs pastrami spare ribs biltong pork chop beef.",
-  //   longKeylongKeylongKeylongKeylongKeylongKey: "Pork belly boudin shoulder ribeye pork chop brisket biltong short ribs. Salami beef pork belly, t-bone sirloin meatloaf tail jowl spare ribs. Sirloin biltong bresaola cow turkey. Biltong fatback meatball, bresaola tail shankle turkey pancetta ham ribeye flank bacon jerky pork chop. Boudin sirloin shoulder, salami swine flank jerky t-bone pork chop pork beef tongue. Bresaola ribeye jerky andouille. Ribeye ground round sausage biltong beef ribs chuck, shank hamburger chicken short ribs spare ribs tenderloin meatloaf pork loin."
-  // },
-  // 
-  // subArray: [1,2,3,4,5,6,7,8,9,10],
-  // anotherString: "another string",
-  // code: new Code("function() {}", {i:1})
+  
+  subObject: {
+    moreText: "Bacon ipsum dolor sit amet cow pork belly rump ribeye pastrami andouille. Tail hamburger pork belly, drumstick flank salami t-bone sirloin pork chop ribeye ham chuck pork loin shankle. Ham fatback pork swine, sirloin shankle short loin andouille shank sausage meatloaf drumstick. Pig chicken cow bresaola, pork loin jerky meatball tenderloin brisket strip steak jowl spare ribs. Biltong sirloin pork belly boudin, bacon pastrami rump chicken. Jowl rump fatback, biltong bacon t-bone turkey. Turkey pork loin boudin, tenderloin jerky beef ribs pastrami spare ribs biltong pork chop beef.",
+    longKeylongKeylongKeylongKeylongKeylongKey: "Pork belly boudin shoulder ribeye pork chop brisket biltong short ribs. Salami beef pork belly, t-bone sirloin meatloaf tail jowl spare ribs. Sirloin biltong bresaola cow turkey. Biltong fatback meatball, bresaola tail shankle turkey pancetta ham ribeye flank bacon jerky pork chop. Boudin sirloin shoulder, salami swine flank jerky t-bone pork chop pork beef tongue. Bresaola ribeye jerky andouille. Ribeye ground round sausage biltong beef ribs chuck, shank hamburger chicken short ribs spare ribs tenderloin meatloaf pork loin."
+  },
+  
+  subArray: [1,2,3,4,5,6,7,8,9,10],
+  anotherString: "another string",
+  code: new Code("function() {}", {i:1})
 }
 
 // var object = {
@@ -49,66 +49,12 @@ var object = {
 //   // code: new Code("function() {}", {i:1})
 // }
 
-for (i=10000; --i>=0; ) {  
-  objectBSON = BSON.serialize(object, null, true)
-}
-
+// for (i=10000; --i>=0; ) {  
+//   objectBSON = BSON.serialize(object, null, true)
+// }
+// 
 var x, start, end, i
 var objectBSON, objectJSON
-
-console.log(COUNT + "x (objectBSON = BSON.serialize(object))")
-start = new Date
-
-// var object = {
-//   '_id': new ObjectID(),
-//   'x': 1,
-//   'integer':5,
-//   'number':5.05,
-//   'boolean':false,
-//   'array':['test', 'benchmark']
-// }
-
-for (i=COUNT; --i>=0; ) {  
-  // var object = {
-  //   string: "Strings are great",
-  //   decimal: 3.14159265,
-  //   bool: true,
-  //   integer: 5,
-  //   subObject: {
-  //       moreText: "Bacon ipsum dolor sit amet cow pork belly rump ribeye pastrami andouille. Tail hamburger pork belly, drumstick flank salami t-bone sirloin pork chop ribeye ham chuck pork loin shankle. Ham fatback pork swine, sirloin shankle short loin andouille shank sausage meatloaf drumstick. Pig chicken cow bresaola, pork loin jerky meatball tenderloin brisket strip steak jowl spare ribs. Biltong sirloin pork belly boudin, bacon pastrami rump chicken. Jowl rump fatback, biltong bacon t-bone turkey. Turkey pork loin boudin, tenderloin jerky beef ribs pastrami spare ribs biltong pork chop beef.",
-  //       longKeylongKeylongKeylongKeylongKeylongKey: "Pork belly boudin shoulder ribeye pork chop brisket biltong short ribs. Salami beef pork belly, t-bone sirloin meatloaf tail jowl spare ribs. Sirloin biltong bresaola cow turkey. Biltong fatback meatball, bresaola tail shankle turkey pancetta ham ribeye flank bacon jerky pork chop. Boudin sirloin shoulder, salami swine flank jerky t-bone pork chop pork beef tongue. Bresaola ribeye jerky andouille. Ribeye ground round sausage biltong beef ribs chuck, shank hamburger chicken short ribs spare ribs tenderloin meatloaf pork loin."
-  //   },
-  //   subArray: [1,2,3,4,5,6,7,8,9,10],
-  //   anotherString: "another string"
-  // }
-
-  // debug("============== calculateObjectSize :: " + BSON.calculateObjectSize(object))
-  // debug("============== _calculateObjectSize :: " + BSON._calculateObjectSize(object))
-  // BSON._calculateObjectSize(object);
-  // BSON.calculateObjectSize(object);
-
-  objectBSON = BSON.serialize(object, null, true)
-  // objectBSON = BSON._serialize(object, null, true)
-
-  // var b = BSON.deserialize(objectBSON);
-
-  // debug(inspect(b))
-
-  // objectBSON = BSON._serialize(object, null, true)
-
-  // var b = BSON.deserialize(objectBSON);
-
-  // debug(inspect(b))
-}
-
-// debug(inspect(objectBSON))
-
-end = new Date
-// console.log("bson size (bytes): ", objectBSON.length)
-console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
-
-var COUNT = 1000000;
-// var COUNT = 1;
 
 console.log(COUNT + "x (objectBSON = BSON.serialize(object))")
 start = new Date
@@ -121,77 +67,131 @@ start = new Date
 // //   'boolean':false,
 // //   'array':['test', 'benchmark']
 // // }
-
+// 
 for (i=COUNT; --i>=0; ) {  
-  // var object = {
-  //   string: "Strings are great",
-  //   decimal: 3.14159265,
-  //   bool: true,
-  //   integer: 5,
-  //   subObject: {
-  //       moreText: "Bacon ipsum dolor sit amet cow pork belly rump ribeye pastrami andouille. Tail hamburger pork belly, drumstick flank salami t-bone sirloin pork chop ribeye ham chuck pork loin shankle. Ham fatback pork swine, sirloin shankle short loin andouille shank sausage meatloaf drumstick. Pig chicken cow bresaola, pork loin jerky meatball tenderloin brisket strip steak jowl spare ribs. Biltong sirloin pork belly boudin, bacon pastrami rump chicken. Jowl rump fatback, biltong bacon t-bone turkey. Turkey pork loin boudin, tenderloin jerky beef ribs pastrami spare ribs biltong pork chop beef.",
-  //       longKeylongKeylongKeylongKeylongKeylongKey: "Pork belly boudin shoulder ribeye pork chop brisket biltong short ribs. Salami beef pork belly, t-bone sirloin meatloaf tail jowl spare ribs. Sirloin biltong bresaola cow turkey. Biltong fatback meatball, bresaola tail shankle turkey pancetta ham ribeye flank bacon jerky pork chop. Boudin sirloin shoulder, salami swine flank jerky t-bone pork chop pork beef tongue. Bresaola ribeye jerky andouille. Ribeye ground round sausage biltong beef ribs chuck, shank hamburger chicken short ribs spare ribs tenderloin meatloaf pork loin."
-  //   },
-  //   subArray: [1,2,3,4,5,6,7,8,9,10],
-  //   anotherString: "another string"
-  // }
-
-  // debug("============== calculateObjectSize :: " + BSON.calculateObjectSize(object))
-  // debug("============== _calculateObjectSize :: " + BSON._calculateObjectSize(object))
-  // BSON._calculateObjectSize(object);
-  // BSON._calculateObjectSize(object);
-  
-  // objectBSON = BSON.serialize(object, null, true)
-  object = BSON.deserialize(objectBSON);
+//   // var object = {
+//   //   string: "Strings are great",
+//   //   decimal: 3.14159265,
+//   //   bool: true,
+//   //   integer: 5,
+//   //   subObject: {
+//   //       moreText: "Bacon ipsum dolor sit amet cow pork belly rump ribeye pastrami andouille. Tail hamburger pork belly, drumstick flank salami t-bone sirloin pork chop ribeye ham chuck pork loin shankle. Ham fatback pork swine, sirloin shankle short loin andouille shank sausage meatloaf drumstick. Pig chicken cow bresaola, pork loin jerky meatball tenderloin brisket strip steak jowl spare ribs. Biltong sirloin pork belly boudin, bacon pastrami rump chicken. Jowl rump fatback, biltong bacon t-bone turkey. Turkey pork loin boudin, tenderloin jerky beef ribs pastrami spare ribs biltong pork chop beef.",
+//   //       longKeylongKeylongKeylongKeylongKeylongKey: "Pork belly boudin shoulder ribeye pork chop brisket biltong short ribs. Salami beef pork belly, t-bone sirloin meatloaf tail jowl spare ribs. Sirloin biltong bresaola cow turkey. Biltong fatback meatball, bresaola tail shankle turkey pancetta ham ribeye flank bacon jerky pork chop. Boudin sirloin shoulder, salami swine flank jerky t-bone pork chop pork beef tongue. Bresaola ribeye jerky andouille. Ribeye ground round sausage biltong beef ribs chuck, shank hamburger chicken short ribs spare ribs tenderloin meatloaf pork loin."
+//   //   },
+//   //   subArray: [1,2,3,4,5,6,7,8,9,10],
+//   //   anotherString: "another string"
+//   // }
+// 
+//   // debug("============== calculateObjectSize :: " + BSON.calculateObjectSize(object))
+//   // debug("============== _calculateObjectSize :: " + BSON._calculateObjectSize(object))
+//   // BSON._calculateObjectSize(object);
+//   // BSON.calculateObjectSize(object);
+// 
+//   objectBSON = BSON.serialize(object, null, true)
+//   // objectBSON = BSON._serialize(object, null, true)
+// 
+//   // var b = BSON.deserialize(objectBSON);
+// 
+//   // debug(inspect(b))
+// 
+  objectBSON = BSON.serialize(object, null, true)
+// 
+//   // var b = BSON.deserialize(objectBSON);
+// 
+//   // debug(inspect(b))
 }
 
 // // debug(inspect(objectBSON))
 // 
-end = new Date
-// console.log("bson size (bytes): ", objectBSON.length)
-console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
-
-// debug("-------------------------------------------------------------")
-// debug(object)
-
-
-// // var COUNT = 1000000;
+// end = new Date
+// // console.log("bson size (bytes): ", objectBSON.length)
+// console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
 // 
-// // console.log(COUNT + "x (objectJSON = JSON.stringify(object))")
+// var COUNT = 1000000;
+// // var COUNT = 1;
+// 
+// console.log(COUNT + "x (objectBSON = BSON.serialize(object))")
+// start = new Date
+// 
+// // // var object = {
+// // //   '_id': new ObjectID(),
+// // //   'x': 1,
+// // //   'integer':5,
+// // //   'number':5.05,
+// // //   'boolean':false,
+// // //   'array':['test', 'benchmark']
+// // // }
+// 
+// for (i=COUNT; --i>=0; ) {  
+//   // var object = {
+//   //   string: "Strings are great",
+//   //   decimal: 3.14159265,
+//   //   bool: true,
+//   //   integer: 5,
+//   //   subObject: {
+//   //       moreText: "Bacon ipsum dolor sit amet cow pork belly rump ribeye pastrami andouille. Tail hamburger pork belly, drumstick flank salami t-bone sirloin pork chop ribeye ham chuck pork loin shankle. Ham fatback pork swine, sirloin shankle short loin andouille shank sausage meatloaf drumstick. Pig chicken cow bresaola, pork loin jerky meatball tenderloin brisket strip steak jowl spare ribs. Biltong sirloin pork belly boudin, bacon pastrami rump chicken. Jowl rump fatback, biltong bacon t-bone turkey. Turkey pork loin boudin, tenderloin jerky beef ribs pastrami spare ribs biltong pork chop beef.",
+//   //       longKeylongKeylongKeylongKeylongKeylongKey: "Pork belly boudin shoulder ribeye pork chop brisket biltong short ribs. Salami beef pork belly, t-bone sirloin meatloaf tail jowl spare ribs. Sirloin biltong bresaola cow turkey. Biltong fatback meatball, bresaola tail shankle turkey pancetta ham ribeye flank bacon jerky pork chop. Boudin sirloin shoulder, salami swine flank jerky t-bone pork chop pork beef tongue. Bresaola ribeye jerky andouille. Ribeye ground round sausage biltong beef ribs chuck, shank hamburger chicken short ribs spare ribs tenderloin meatloaf pork loin."
+//   //   },
+//   //   subArray: [1,2,3,4,5,6,7,8,9,10],
+//   //   anotherString: "another string"
+//   // }
+// 
+//   // debug("============== calculateObjectSize :: " + BSON.calculateObjectSize(object))
+//   // debug("============== _calculateObjectSize :: " + BSON._calculateObjectSize(object))
+//   // BSON._calculateObjectSize(object);
+//   // BSON._calculateObjectSize(object);
+//   
+//   // objectBSON = BSON.serialize(object, null, true)
+//   object = BSON.deserialize(objectBSON);
+// }
+// 
+// // // debug(inspect(objectBSON))
+// // 
+// end = new Date
+// // console.log("bson size (bytes): ", objectBSON.length)
+// console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
+// 
+// // debug("-------------------------------------------------------------")
+// // debug(object)
+// 
+// 
+// // // var COUNT = 1000000;
+// // 
+// // // console.log(COUNT + "x (objectJSON = JSON.stringify(object))")
+// // // start = new Date
+// // // 
+// // // for (i=COUNT; --i>=0; ) {
+// // //     objectJSON = JSON.stringify(object)
+// // // }
+// // // 
+// // // end = new Date
+// // console.log("json size (chars): ", objectJSON.length)
+// // console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
+// // 
+// // var COUNT = 1000000;
+// // var COUNT = 1;
+// // 
+// // console.log(COUNT + " BSON.deserialize(objectBSON)")
+// // var COUNT = 1000000;
+// // 
 // // start = new Date
 // // 
 // // for (i=COUNT; --i>=0; ) {
-// //     objectJSON = JSON.stringify(object)
+// //   x = BSON.deserialize(objectBSON)
 // // }
 // // 
 // // end = new Date
-// console.log("json size (chars): ", objectJSON.length)
-// console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
+// // console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
+// // 
+// // 
+// // debug(inspect(x))
 // 
-// var COUNT = 1000000;
-// var COUNT = 1;
-// 
-// console.log(COUNT + " BSON.deserialize(objectBSON)")
-// var COUNT = 1000000;
-// 
-// start = new Date
-// 
-// for (i=COUNT; --i>=0; ) {
-//   x = BSON.deserialize(objectBSON)
-// }
-// 
-// end = new Date
-// console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
-// 
-// 
-// debug(inspect(x))
-
-// console.log(COUNT + " JSON.parse(objectJSON)")
-// start = new Date
-// 
-// for (i=COUNT; --i>=0; ) {
-//     x = JSON.parse(objectJSON)
-// }
-// 
-// end = new Date
-// console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
+// // console.log(COUNT + " JSON.parse(objectJSON)")
+// // start = new Date
+// // 
+// // for (i=COUNT; --i>=0; ) {
+// //     x = JSON.parse(objectJSON)
+// // }
+// // 
+// // end = new Date
+// // console.log("time = ", end - start, "ms -", COUNT * 1000 / (end - start), " ops/sec")
