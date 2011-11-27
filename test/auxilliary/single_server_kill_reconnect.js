f63bde0b649dfcec08e85e2ca6a4106b771a9be1
Christian Amor Kvalheim <christkv@gmail.com>
2011-11-27 12:32:07 +0100

    Fixes for single server reconnect functionality, bug fix and tests added

--- test/auxilliary/single_server_kill_reconnect.js
+++ test/auxilliary/single_server_kill_reconnect.js
@@ -61,7 +61,7 @@ var tests = testCase({
             })
           });        
         }, 500);        
-
+  
         // Wait for a second and then kill the server
         setTimeout(function() {
           // Kill server instance
@@ -91,166 +91,58 @@ var tests = testCase({
         }, 3000);
       })      
     });
+  },
 
-    
-    // Step(
-    //   function bootTheServerWithNoAuth() {
-    //     serverManager = new ServerManager({auth:false, purgedirectories:true})
-    //     serverManager.start(true, this);
-    //   },
-    //   
-    //   function openDbs() {
-    //     db1.open(this.parallel());
-    //     db2.open(this.parallel());
-    //     admin.open(this.parallel());
-    //   },
-    //   
-    //   function addAdminUserToDatabase(err, db1, db2, admin) {
-    //     test.equal(null, err);        
-    //     admin.addUser('admin', 'admin', this);
-    //   },
-    //   
-    //   function restartServerInAuthMode(err, result) {
-    //     test.equal(null, err);
-    //     test.equal('7c67ef13bbd4cae106d959320af3f704', result.shift().pwd);
-    // 
-    //     db1.close();
-    //     db2.close();
-    //     admin.close();
-    // 
-    //     serverManager = new ServerManager({auth:true, purgedirectories:false})
-    //     serverManager.start(true, this);
-    //   },
-    //   
-    //   function openDbs() {
-    //     db1.open(this.parallel());
-    //     db2.open(this.parallel());
-    //     admin.open(this.parallel());
-    //   },
-    //   
-    //   function authenticateAdminUser(err) {
-    //     test.equal(null, err);        
-    // 
-    //     admin.authenticate('admin', 'admin', this.parallel());
-    //     db1.admin().authenticate('admin', 'admin', this.parallel());
-    //     db2.admin().authenticate('admin', 'admin', this.parallel());
-    //   },
-    //   
-    //   function addDbUsersForAuthentication(err, result1, result2, result3) {
-    //     test.equal(null, err);
-    //     test.ok(result1);
-    //     test.ok(result2);
-    //     test.ok(result3);
-    //     
-    //     db1.addUser('user1', 'secret', this.parallel());
-    //     db2.addUser('user2', 'secret', this.parallel());
-    //   },
-    //   
-    //   function closeAdminConnection(err, result1, result2) {
-    //     test.ok(err == null);
-    //     test.ok(result1 != null);
-    //     test.ok(result2 != null);
-    //     admin.logout(this.parallel());
-    //     db1.admin().logout(this.parallel());
-    //     db2.admin().logout(this.parallel());
-    //   },
-    //   
-    //   function failAuthenticationWithDbs(err, result) {
-    //     var self = this;
-    // 
-    //     db1.collection('stuff', function(err, collection) {
-    //       collection.insert({a:2}, {safe:true}, self.parallel());
-    //     });        
-    // 
-    //     db2.collection('stuff', function(err, collection) {
-    //       collection.insert({a:2}, {safe:true}, self.parallel());
-    //     });        
-    //   },
-    //   
-    //   function authenticateAgainstDbs(err, result) {
-    //     test.ok(err != null);
-    //             
-    //     db1.authenticate('user1', 'secret', this.parallel());
-    //     db2.authenticate('user2', 'secret', this.parallel());        
-    //   },
-    //   
-    //   function correctlyInsertRowToDbs(err, result1, result2) {
-    //     var self = this;
-    //     test.ok(err == null);
-    //     test.ok(result1);
-    //     test.ok(result2);
-    //     
-    //     db1.collection('stuff', function(err, collection) {
-    //       collection.insert({a:2}, {safe:true}, self.parallel());
-    //     });        
-    //     
-    //     db2.collection('stuff', function(err, collection) {
-    //       collection.insert({a:2}, {safe:true}, self.parallel());
-    //     });                
-    //   },
-    //   
-    //   function validateCorrectInsertsAndBounceServer(err, result1, result2) {
-    //     test.ok(err == null);
-    //     test.ok(result1 != null);
-    //     test.ok(result2 != null);
-    //     
-    //     serverManager = new ServerManager({auth:true, purgedirectories:false})
-    //     serverManager.start(true, this);
-    //   },
-    //   
-    //   function reconnectAndVerifyThatAuthIsAutomaticallyApplied() {
-    //     var self = this;
-    //     db1.collection('stuff', function(err, collection) {
-    //       
-    //       collection.find().toArray(function(err, items) {
-    //         test.ok(err == null);
-    //         test.equal(1, items.length);
-    //         
-    //         db1.collection('stuff', function(err, collection) {
-    //           collection.insert({a:2}, {safe:true}, self.parallel());
-    //         });        
-    //         
-    //         db2.collection('stuff', function(err, collection) {
-    //           collection.insert({a:2}, {safe:true}, self.parallel());
-    //         });                            
-    //       })
-    //     });        
-    //   },
-    //   
-    //   function logoutDb1(err, result1, result2) {
-    //     test.ok(err == null);
-    //     test.ok(result1 != null);
-    //     test.ok(result2 != null);
-    //     
-    //     db1.logout(this);
-    //   },
-    //   
-    //   function insertShouldFail(err, result) {
-    //     var self = this;
-    //     db1.collection('stuff', function(err, collection) {
-    //       collection.insert({a:2}, {safe:true}, self.parallel());
-    //     });                      
-    //   },
-    //   
-    //   function logoutDb2(err, result) {
-    //     test.ok(err != null);      
-    //     db2.logout(this);
-    //   },
-    //   
-    //   function insertShouldFail(err, result) {        
-    //     var self = this;
-    //     db2.collection('stuff', function(err, collection) {
-    //       collection.insert({a:2}, {safe:true}, function(err, result) {
-    //         test.ok(err != null);
-    //         test.done();
-    //         // Close all connections
-    //         db1.close();
-    //         db2.close();
-    //         admin.close();
-    //       });
-    //     });                      
-    //   }
-    // )
+  shouldCorrectlyInsertKillServerFailThenRestartServerAndSucceed : function(test) {
+    var db = new Db('test-single-server-recovery', new Server("127.0.0.1", 27017, {auto_reconnect: true}), {numberOfRetries:3, retryMiliSeconds:500, native_parser: (process.env['TEST_NATIVE'] != null)});
+    // All inserted docs
+    var docs = [];
+    var errs = [];
+    var insertDocs = [];
+
+    // Start server
+    serverManager = new ServerManager({auth:false, purgedirectories:true, journal:true})
+    serverManager.start(true, function() {
+      db.open(function(err, db) {        
+        // Add an error handler
+        db.on("error", function(err) {
+          console.log("----------------------------------------------- received error")
+          console.dir(err)
+          errs.push(err);
+        });
+
+        db.collection('inserts', function(err, collection) {
+          var doc = {timestamp:new Date().getTime(), a:1};
+          collection.insert(doc, {safe:true}, function(err, result) {
+            test.equal(null, err);
+            
+            // Kill server instance
+            serverManager.stop(9, function(err, result) {
+              // Attemp insert (should timeout)
+              var doc = {timestamp:new Date().getTime(), b:1};
+              collection.insert(doc, {safe:true}, function(err, result) {
+                test.ok(err != null);
+                test.equal(null, result);
+                
+                // Restart server
+                serverManager = new ServerManager({auth:false, purgedirectories:false, journal:true});
+                serverManager.start(true, function() {
+                  // Attemp insert again
+                  collection.insert(doc, {safe:true}, function(err, result) {
+                    // Fetch the documents
+                    collection.find({b:1}).toArray(function(err, items) {
+                      test.equal(null, err);
+                      test.equal(1, items[0].b);
+                      test.done();
+                    });                    
+                  });                  
+                });                
+              });              
+            });            
+          })
+        });
+      });
+    });
   },
   
   noGlobalsLeaked : function(test) {
