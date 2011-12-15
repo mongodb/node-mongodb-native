dd99518471fe5f7f5a481ca4f6bc783d7e27f28b
Christian Amor Kvalheim <christkv@gmail.com>
2011-12-15 16:52:26 +0100

    Added start of sharding controller

---
+++ test/tools/sharding_manager.js
@@ -0,0 +1,155 @@
+var debug = require('util').debug,
+  inspect = require('util').inspect,
+  path = require('path'),
+  fs = require('fs'),
+  exec = require('child_process').exec,
+  spawn = require('child_process').spawn,
+  Connection = require('../../lib/mongodb').Connection,
+  Db = require('../../lib/mongodb').Db,
+  Server = require('../../lib/mongodb').Server;
+  
+var ShardingManager = exports.ShardingManager = function(options) {
+  options = options == null ? {} : options;
+  // Basic unpack values
+  this.path = path.resolve("data");
+  this.port = options["start_port"] != null ? options["start_port"] : 27017;  
+  this.number_of_sharding_servers = options["number_of_sharding_servers"] != null ? options["number_of_sharding_servers"] : 2;
+  this.number_of_config_servers = options["number_of_config_servers"] != null ? options["number_of_config_servers"] : 1;
+  this.db_path = getPath(this, "data-" + this.port);
+  this.log_path = getPath(this, "log-" + this.port);
+  this.journal = options["journal"] != null ? options["journal"] : false;   
+  this.auth = options['auth'] != null ? options['auth'] : false; 
+  this.ssl = options['ssl'] != null ? options['ssl'] : false; 
+  this.purgedirectories = options['purgedirectories'] != null ? options['purgedirectories'] : true;
+
+  // Server status values
+  this.up = false;
+  this.pid = null;
+}
+
+// Start up the server instance
+ShardingManager.prototype.start = function(killall, callback) {
+  var self = this;
+  // Unpack callback and variables
+  var args = Array.prototype.slice.call(arguments, 0);
+  callback = args.pop();
+  killall = args.length ? args.shift() : true;  
+  // Create start command
+  var startCmd = generateStartCmd(this, {log_path: self.log_path, 
+    db_path: self.db_path, port: self.port, journal: self.journal, auth:self.auth, ssl:self.ssl});
+    
+  // Purge function for the data directory
+  var purgeFunction = function() {
+    if(self.purgedirectories) {
+      // Remove directory
+      exec("rm -rf " + self.db_path, function(err, stdout, stderr) {
+        if(err != null) return callback(err, null);    
+        // Create directory
+        exec("mkdir -p " + self.db_path, function(err, stdout, stderr) {
+          if(err != null) return callback(err, null);
+          // Start up mongod process
+          var mongodb = exec(startCmd,
+            function (error, stdout, stderr) {
+              // console.log('stdout: ' + stdout);
+              // console.log('stderr: ' + stderr);
+              if (error !== null) {
+                console.log('exec error: ' + error);
+              }
+          });
+
+          // Wait for a half a second then save the pids
+          setTimeout(function() {        
+            // Mark server as running
+            self.up = true;
+            self.pid = fs.readFileSync(path.join(self.db_path, "mongod.lock"), 'ascii').trim();
+            // Callback
+            callback();
+          }, 500);
+        });    
+      });        
+    } else {
+      // Ensure we remove the lock file as we are not purging the directory
+      fs.unlinkSync(path.join(self.db_path, "mongod.lock"));
+      
+      // Start up mongod process
+      var mongodb = exec(startCmd,
+        function (error, stdout, stderr) {
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+      });
+
+      // Wait for a half a second then save the pids
+      setTimeout(function() {        
+        // Mark server as running
+        self.up = true;
+        self.pid = fs.readFileSync(path.join(self.db_path, "mongod.lock"), 'ascii').trim();
+        // Callback
+        callback();
+      }, 5000);      
+    }
+  }
+  
+  // If we specified kill all the instances do that then purge the directories
+  if(killall) {
+    this.killAll(function() {
+      purgeFunction();
+    });
+  } else {
+    purgeFunction();
+  }
+}
+
+ShardingManager.prototype.stop = function(signal, callback) {
+  var self = this;
+  // Unpack callback and variables
+  var args = Array.prototype.slice.call(arguments, 0);
+  callback = args.pop();
+  signal = args.length ? args.shift() : 2;  
+  // Stop the server
+  var command = "kill -" + signal + " " + self.pid;
+  // Kill process
+  exec(command,
+    function (error, stdout, stderr) {
+      // console.log('stdout: ' + stdout);
+      // console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+
+      self.up = false;
+      // Wait for a second
+      setTimeout(callback, 1000);
+  });    
+}
+
+ShardingManager.prototype.killAll = function(callback) {
+	// Kill all mongos instances
+  exec('killall mongos', function(err, stdout, stderr) {
+    // Kill all mongod instances
+    exec('killall mongod', function(err, stdout, stderr) {
+      callback(null, null);
+    });
+  });
+}
+
+// Get absolute path
+var getPath = function(self, name) {
+  return path.join(self.path, name);
+}
+
+// Generate start command
+var generateStartCmd = function(self, options) {
+  // Create boot command
+  var startCmd = "mongod --noprealloc --logpath '" + options['log_path'] + "' " +
+      " --dbpath " + options['db_path'] + " --port " + options['port'] + " --fork";
+  startCmd = options['journal'] ? startCmd + "  --journal" : startCmd;
+  startCmd = options['auth'] ? startCmd + "  --auth" : startCmd;
+  // If we have ssl defined set up with test certificate
+  if(options['ssl']) {
+    var path = getPath(self, '../test/certificates');
+    startCmd = startCmd + " --sslOnNormalPorts --sslPEMKeyFile=" + path + "/mycert.pem --sslPEMKeyPassword=10gen";
+  }
+  // Return start command
+  return startCmd;
+}
