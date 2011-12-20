3a661069334430a3ec07fa8c97dad7c64456e236
Christian Amor Kvalheim <christkv@gmail.com>
2011-12-20 17:17:28 +0100

    Changed mongo reply parser

--- external-libs/bson/test/test_bson.js
+++ external-libs/bson/test/test_bson.js
@@ -375,6 +375,13 @@ var docJSBin = BSONJS.serialize(docJS, false, true, true);
 var docCBin = BSON.serialize(docC, false, true, true);
 assert.equal(docCBin.toString('hex'), docJSBin.toString('hex'));
 
+// // Complex document serialization
+// doc = {"DateTime": "Tue Nov 40 2011 17:27:55 GMT+0000 (WEST)","isActive": true,"Media": {"URL": "http://videos.sapo.pt/Tc85NsjaKjj8o5aV7Ubb"},"Title": "Lisboa fecha a ganhar 0.19%","SetPosition": 60,"Type": "videos","Thumbnail": [{"URL": "http://rd3.videos.sapo.pt/Tc85NsjaKjj8o5aV7Ubb/pic/320x240","Dimensions": {"Height": 240,"Width": 320}}],"Source": {"URL": "http://videos.sapo.pt","SetID": "1288","SourceID": "http://videos.sapo.pt/tvnet/rss2","SetURL": "http://noticias.sapo.pt/videos/tv-net_1288/","ItemID": "Tc85NsjaKjj8o5aV7Ubb","Name": "SAPO VÃ­deos"},"Category": "Tec_ciencia","Description": "Lisboa fecha a ganhar 0.19%","GalleryID": new ObjectID("4eea2a634ce8573200000000"),"InternalRefs": {"RegisterDate": "Thu Dec 15 2011 17:12:51 GMT+0000 (WEST)","ChangeDate": "Thu Dec 15 2011 17:12:51 GMT+0000 (WEST)","Hash": 332279244514},"_id": new ObjectID("4eea2a96e52778160000003a")}
+// var docJSBin = BSONJS.serialize(docJS, false, true, true);
+// var docCBin = BSON.serialize(docC, false, true, true);
+// 
+// 
+
 // Force garbage collect
 global.gc();
 
