246101c1897b179705b6d22ed6423cd6fdcaf82e
Christian Amor Kvalheim <christkv@gmail.com>
2012-01-04 12:53:41 +0100

    Added Code for sanity check benchmarking of gridfs vs mongolian

--- benchmark/grid_fs_write_benchmark.js
+++ benchmark/grid_fs_write_benchmark.js
@@ -1,11 +1,7 @@
 var Db = require('../lib/mongodb').Db,
   Server = require('../lib/mongodb').Server,
   ObjectID = require('../lib/mongodb').ObjectID,
-  Cursor = require('../lib/mongodb').Cursor,
-  Collection = require('../lib/mongodb').Collection,
-  Buffer = require('buffer').Buffer,
-  GridStore = require('../lib/mongodb').GridStore,
-  sys = require('util');
+  GridStore = require('../lib/mongodb').GridStore;
 
 var simulated_buffer = new Buffer(1024*1000*10).toString();
 
@@ -18,14 +14,11 @@ new Db('grid_fs_write_benchmark', new Server("127.0.0.1", 27017, {auto_reconnect
         var gridStore = new GridStore(client, "foobar" + i, "w");
         gridStore.open(function(err, gridStore) {    
           gridStore.write(simulated_buffer.toString(), function(err, gridStore) {
-            // sys.puts("========================== wrote file: " + "foobar" + i);              
             gridStore.close(function(err, result) {
               client.close();
-              // sys.puts("========================== close file: " + "foobar" + i);              
             });
           });
         });    
-        // write_files.push(writeFile)        
       });    
     }
   })  
