1bfac4f6e56650ff1d0ff868ead959d9f690ee45
Christian Kvalheim <christkv@gmail.com>
2012-03-17 00:33:42 +0100

    added manual test

---
+++ test/manual_tests/replicaset_manual_kill_test.js
@@ -0,0 +1,60 @@
+var http            = require('http'),
+    os              = require('os'),
+    mongodb         = require('../../lib/mongodb'),
+    Server          = mongodb.Server,
+    ReplSetServers  = mongodb.ReplSetServers,
+    Db              = mongodb.Db;
+
+    console.log('launching simple mongo application...');
+
+    //open replicaset
+    var replSet = new ReplSetServers([
+            new Server('127.0.0.1', 30000, { auto_reconnect: true }),
+            new Server('127.0.0.1', 30001, { auto_reconnect: true }),
+            new Server('127.0.0.1', 30002, { auto_reconnect: true })
+        ],
+        {
+            rs_name: 'testappset',
+            read_secondary: true
+        }
+    );
+
+    //opens the database
+    var db = new Db('testapp', replSet);
+    db.open(function(err) {
+        if (err) return console.log('database open error %o', err);
+        console.log('database opened');
+
+        db.collection('stats', function(statsErr, stats) {
+            if (statsErr) return console.log('error opening stats %o', err);
+
+            //create server
+            http.createServer(function (req, res) {
+                if (req.url !== '/') {
+                    res.end();
+                    return console.log('invalid request performed');
+                }
+
+                //get amount of requests done
+                stats.findOne({name: 'reqcount'}, function(err, reqstat) {
+                    if (err | !reqstat) {
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.end('Hello World, from server node: ' + os.hostname() + '...\nError #' + err + ', reqstat ' + reqstat);
+                        return console.log('reqstat is null!');
+                    }
+                    var reqcount = reqstat.value;
+
+                    //write to client
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.end('Hello World, from server node: ' + os.hostname() + '...\nThis is visit #' + reqcount);
+                });
+
+                //increment amount of requests
+                console.log('incrementing request by 1!');
+                stats.update({name: 'reqcount'}, {'$inc': {value: 1}}, {upsert: true});
+
+            }).listen(8000);
+
+            console.log('Server running at port 8000');
+        });
+    });
