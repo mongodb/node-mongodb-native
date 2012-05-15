9e16df31183ecf33611be1dcf0ec96b898d121a0
Christian Kvalheim <christkv@gmail.com>
2012-05-15 18:39:53 +0200

    whitespace formating

--- test/gridstore/gridstore_direct_streaming_test.js
+++ test/gridstore/gridstore_direct_streaming_test.js
@@ -55,44 +55,44 @@ exports.tearDown = function(callback) {
  * @ignore
  */
 exports.shouldCorrectlyStreamWriteToGridStoreObject = function(test) {
-	// Set up gridStore
+  // Set up gridStore
   var gridStore = new GridStore(client, "test_stream_write", "w");
   // Create a file reader stream to an object
   var fileStream = fs.createReadStream("./test/gridstore/test_gs_working_field_read.pdf");
-	gridStore.on("close", function(err) {
-		// Just read the content and compare to the raw binary
-		GridStore.read(client, "test_stream_write", function(err, gridData) {
-			var fileData = fs.readFileSync("./test/gridstore/test_gs_working_field_read.pdf");
-			test.deepEqual(fileData, gridData);
-			test.done();
-		})
-	});
+  gridStore.on("close", function(err) {
+    // Just read the content and compare to the raw binary
+    GridStore.read(client, "test_stream_write", function(err, gridData) {
+      var fileData = fs.readFileSync("./test/gridstore/test_gs_working_field_read.pdf");
+      test.deepEqual(fileData, gridData);
+      test.done();
+    })
+  });
 
-	// Pipe it through to the gridStore
-	fileStream.pipe(gridStore);
+  // Pipe it through to the gridStore
+  fileStream.pipe(gridStore);
 }
 
 /**
  * @ignore
  */
 exports.shouldCorrectlyStreamReadFromGridStoreObject = function(test) {
-	// Set up gridStore
+  // Set up gridStore
   var gridStore = new GridStore(client, "test_stream_write_2", "w");
-	gridStore.writeFile("./test/gridstore/test_gs_working_field_read.pdf", function(err, result) {		
-		// Open a readable gridStore
-		gridStore = new GridStore(client, "test_stream_write_2", "r");		
+  gridStore.writeFile("./test/gridstore/test_gs_working_field_read.pdf", function(err, result) {		
+    // Open a readable gridStore
+    gridStore = new GridStore(client, "test_stream_write_2", "r");		
     // Create a file write stream
     var fileStream = fs.createWriteStream("./test_stream_write_2.tmp");
-		fileStream.on("close", function(err) {			
-			// Read the temp file and compare
-			var compareData = fs.readFileSync("./test_stream_write_2.tmp");
-			var originalData = fs.readFileSync("./test/gridstore/test_gs_working_field_read.pdf");
-			test.deepEqual(originalData, compareData);			
-			test.done();			
-		})
+    fileStream.on("close", function(err) {			
+      // Read the temp file and compare
+      var compareData = fs.readFileSync("./test_stream_write_2.tmp");
+      var originalData = fs.readFileSync("./test/gridstore/test_gs_working_field_read.pdf");
+      test.deepEqual(originalData, compareData);			
+      test.done();			
+    })
     // Pipe out the data
     gridStore.pipe(fileStream);
-	});
+  });
 }
 
 /**
