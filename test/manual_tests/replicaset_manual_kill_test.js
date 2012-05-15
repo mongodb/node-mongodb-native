ea577cd90f6b82fc2e41cef813a48d491c1675a4
Christian Kvalheim <christkv@gmail.com>
2012-05-15 17:18:17 +0200

    added automatic startup for manual test

--- test/manual_tests/replicaset_manual_kill_test.js
+++ test/manual_tests/replicaset_manual_kill_test.js
@@ -2,68 +2,73 @@ var http            = require('http'),
     os              = require('os'),
     mongodb         = require('../../lib/mongodb'),
     Server          = mongodb.Server,
+	  ReplicaSetManager = require('../tools/replica_set_manager').ReplicaSetManager,
     ReplSetServers  = mongodb.ReplSetServers,
     Db              = mongodb.Db;
 
-    console.log('launching simple mongo application...');
+console.log('launching simple mongo application...');
 
-    //open replicaset
-    var replSet = new ReplSetServers([
-            new Server('127.0.0.1', 30000, { auto_reconnect: true }),
-            new Server('127.0.0.1', 30001, { auto_reconnect: true }),
-            new Server('127.0.0.1', 30002, { auto_reconnect: true })
-        ],
-        {
-            rs_name: 'testappset',
-            read_secondary: true,
-            ha:true
-        }
-    );
+//open replicaset
+var replSet = new ReplSetServers([
+        new Server('127.0.0.1', 30000, { auto_reconnect: true }),
+        new Server('127.0.0.1', 30001, { auto_reconnect: true }),
+        new Server('127.0.0.1', 30002, { auto_reconnect: true })
+    ], {
+      rs_name: 'testappset',
+      read_secondary: true,
+      // ha:true
+    }
+);
 
-    //opens the database
-    var db = new Db('testapp', replSet);
-    db.open(function(err) {
-        if (err) return console.log('database open error %o', err);
-        console.log('database opened');
+RS = new ReplicaSetManager({name:"testappset", retries:120, secondary_count:2, passive_count:0, arbiter_count:0});
+RS.startSet(true, function(err, result) {      
+  if(err != null) throw err;
 
-        db.collection('stats', function(statsErr, stats) {
-            if (statsErr) return console.log('error opening stats %o', err);
-            stats.remove({}, {safe:true}, function(err, result) {
+  //opens the database
+  var db = new Db('testapp', replSet);
+  db.open(function(err) {
+      if (err) return console.log('database open error %o', err);
+      console.log('database opened');
+
+      db.collection('stats', function(statsErr, stats) {
+          if (statsErr) return console.log('error opening stats %o', err);
+          stats.remove({}, {safe:true}, function(err, result) {
+            console.log("================================================================")
+            console.dir(err)
+            
+            stats.insert({name:'reqcount', value:0}, {safe:true}, function(err, result) {
               console.log("================================================================")
               console.dir(err)
-              
-              stats.insert({name:'reqcount', value:0}, {safe:true}, function(err, result) {
-                console.log("================================================================")
-                console.dir(err)
-                //create server
-                http.createServer(function (req, res) {
-                    if (req.url !== '/') {
-                        res.end();
-                        return console.log('invalid request performed');
-                    }
+              //create server
+              http.createServer(function (req, res) {
+                  if (req.url !== '/') {
+                      res.end();
+                      return console.log('invalid request performed');
+                  }
 
-                    //get amount of requests done
-                    stats.findOne({name: 'reqcount'}, function(err, reqstat) {
-                        if(err) {
-                          res.writeHead(200, {'Content-Type': 'text/plain'});
-                          res.end('Hello World, from server node: ' + os.hostname() + '...\nError #' + err + ', reqstat ' + reqstat);
-                          return console.log('reqstat is null!');
-                        }
-                        var reqcount = reqstat.value;
-
-                        //write to client
+                  //get amount of requests done
+                  stats.findOne({name: 'reqcount'}, function(err, reqstat) {
+                      if(err) {
                         res.writeHead(200, {'Content-Type': 'text/plain'});
-                        res.end('Hello World, from server node: ' + os.hostname() + '...\nThis is visit #' + reqcount);
-                    });
+                        res.end('Hello World, from server node: ' + os.hostname() + '...\nError #' + err + ', reqstat ' + reqstat);
+                        return console.log('reqstat is null!');
+                      }
+                      var reqcount = reqstat.value;
 
-                    //increment amount of requests
-                    console.log('incrementing request by 1!');
-                    stats.update({name: 'reqcount'}, {'$inc': {value: 1}}, {upsert: true});
+                      //write to client
+                      res.writeHead(200, {'Content-Type': 'text/plain'});
+                      res.end('Hello World, from server node: ' + os.hostname() + '...\nThis is visit #' + reqcount);
+                  });
 
-                }).listen(8000);                
-              });              
-            });
+                  //increment amount of requests
+                  console.log('incrementing request by 1!');
+                  stats.update({name: 'reqcount'}, {'$inc': {value: 1}}, {upsert: true});
+              }).listen(8000);                
+            });              
+          });
 
-            console.log('Server running at port 8000');
-        });
-    });
+          console.log('Server running at port 8000');
+      });
+  });
+});      
+
