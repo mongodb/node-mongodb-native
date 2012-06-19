4eb9260a305c1ac99adf241de0e6b1e5140e9319
Christian Kvalheim <christkv@gmail.com>
2012-06-19 14:57:07 +0200

    Authentication tests now passing correctly

--- test/auxilliary/authentication_test.js
+++ test/auxilliary/authentication_test.js
@@ -1,6 +1,7 @@
 var mongodb = process.env['TEST_NATIVE'] != null ? require('../../lib/mongodb').native() : require('../../lib/mongodb').pure();
 
 var testCase = require('nodeunit').testCase,
+  async = require('async'),
   debug = require('util').debug,
   inspect = require('util').inspect,
   nodeunit = require('nodeunit'),
@@ -36,6 +37,124 @@ exports.tearDown = function(callback) {
   // serverManager.killAll(function(err, result) {
     callback();
   // });
+}
+
+exports.shouldCorrectlyAuthenticateWithHorribleBananaCode = function(test) {
+  var db1 = new Db('mongo-ruby-test-auth1', new Server("127.0.0.1", 27017, {auto_reconnect: true}), {native_parser: (process.env['TEST_NATIVE'] != null)});
+  var db2 = new Db('mongo-ruby-test-auth2', new Server("127.0.0.1", 27017, {auto_reconnect: true}), {native_parser: (process.env['TEST_NATIVE'] != null)});
+  var admin = new Db('admin', new Server("127.0.0.1", 27017, {auto_reconnect: true}), {native_parser: (process.env['TEST_NATIVE'] != null)});
+
+  serverManager = new ServerManager({auth:false, purgedirectories:true})
+  serverManager.start(true, function(err, result) {
+    db1.open(function(err, result) {
+      db2.open(function(err, result) {
+        admin.open(function(err, result) {
+          admin.addUser('admin', 'admin', function(err, result) {
+            test.equal(null, err);
+            test.equal('7c67ef13bbd4cae106d959320af3f704', result.shift().pwd);
+            
+            serverManager = new ServerManager({auth:true, purgedirectories:false})
+            serverManager.start(true, function(err, result) {                            
+              admin.authenticate('admin', 'admin', function(err, result1) {
+                
+                db1.admin().authenticate('admin', 'admin', function(err, result2) {
+          
+                  db2.admin().authenticate('admin', 'admin', function(err, result3) {
+                    test.equal(true, result1);
+                    test.equal(true, result2);
+                    test.equal(true, result3);
+                
+                    db1.addUser('user1', 'secret', function(err, result1) {
+                      
+                      db2.addUser('user2', 'secret', function(err, result2) {
+                        test.ok(result1 != null);
+                        test.ok(result2 != null);
+                    
+                        admin.logout(function(err, _result1) {
+                          
+                          db1.admin().logout(function(err, _result2) {
+
+                            db2.admin().logout(function(err, _result3) {
+                              test.equal(true, _result1);
+                              test.equal(true, _result2);
+                              test.equal(true, _result3);
+                              
+                              var col1 = db1.collection('stuff');
+                              var col2 = db2.collection('stuff');      
+                              
+                              col1.insert({a:2}, {safe:{j:true}}, function(err1, result) {
+                                
+                                col2.insert({a:2}, {safe:{j:true}}, function(err2, result) {
+                                  test.ok(err1 != null);
+                                  test.ok(err2 != null);
+
+                                  db1.authenticate('user1', 'secret', function(err, result1) {
+                                    
+                                    db2.authenticate('user2', 'secret', function(err, result2) {
+                                      test.ok(result1);
+                                      test.ok(result2);                                      
+                                      
+                                      col1.insert({a:2}, {safe:{j:true}}, function(err1, result) {
+
+                                        col2.insert({a:2}, {safe:{j:true}}, function(err2, result) {
+                                          test.equal(null, err1);
+                                          test.equal(null, err2);
+                                      
+                                          serverManager = new ServerManager({auth:true, purgedirectories:false})
+                                          serverManager.start(true, function(err, result) {
+                                            
+                                            col1.find({}).toArray(function(err, items) {
+                                              test.ok(err == null);
+                                              test.equal(1, items.length);
+                                              
+                                              col1.insert({a:2}, {safe:{j:true}}, function(err1, result) {
+                                                col2.insert({a:2}, {safe:{j:true}}, function(err2, result) {
+                                                  db1.logout(function(err, result) {
+                                                    test.ok(err == null);
+                                                    test.ok(result);
+                                                    
+                                                    col1.insert({a:2}, {safe:{j:true}}, function(err, result) {
+                                                      test.ok(err != null);
+
+                                                      db2.logout(function(err, result) {
+                                                        test.ok(err == null);
+                                                        test.ok(result);
+
+                                                        col2.insert({a:2}, {safe:{j:true}}, function(err, result) {
+                                                          test.ok(err != null);
+
+                                                          test.done();
+                                                          db1.close();
+                                                          db2.close();
+                                                          admin.close();
+                                                        });
+                                                      });
+                                                    });
+                                                  });
+                                                });
+                                              });
+                                            })
+                                          });
+                                        });
+                                      });
+                                    });                                    
+                                  });
+                                })        
+                              })
+                            })
+                          });
+                        });
+                      });        
+                    });
+                  });
+                });
+              });              
+            });
+          });          
+        })
+      })
+    })
+  });
 }
 
 exports.shouldCorrectlyAuthenticate = function(test) {
