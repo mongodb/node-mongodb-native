958d782a7e8a7cf0aca24636be58167015e1a2af
Christian Kvalheim <christkv@gmail.com>
2012-06-27 11:03:12 +0200

    Added tests, fixed cursor.js typo using this

---
+++ test/replicaset/read_preferences_spec.js
@@ -0,0 +1,189 @@
+var mongodb = process.env['TEST_NATIVE'] != null ? require('../../lib/mongodb').native() : require('../../lib/mongodb').pure();
+var noReplicasetStart = process.env['NO_REPLICASET_START'] != null ? true : false;
+
+var testCase = require('nodeunit').testCase,
+  debug = require('util').debug,
+  inspect = require('util').inspect,
+  gleak = require('../../dev/tools/gleak'),
+  ReplicaSetManager = require('../tools/replica_set_manager').ReplicaSetManager,
+  Db = mongodb.Db,
+  ReplSetServers = mongodb.ReplSetServers,
+  PingStrategy = require('../../lib/mongodb/connection/strategies/ping_strategy').PingStrategy,
+  StatisticsStrategy = require('../../lib/mongodb/connection/strategies/statistics_strategy').StatisticsStrategy,
+  Server = mongodb.Server;
+
+// Keep instance of ReplicaSetManager
+var serversUp = false;
+var retries = 120;
+var RS = RS == null ? null : RS;
+
+var ensureConnection = function(test, numberOfTries, callback) {
+  // Replica configuration
+  var replSet = new ReplSetServers( [ 
+      new Server( RS.host, RS.ports[1], { auto_reconnect: true } ),
+      new Server( RS.host, RS.ports[0], { auto_reconnect: true } ),
+      new Server( RS.host, RS.ports[2], { auto_reconnect: true } )
+    ], 
+    {rs_name:RS.name}
+  );
+  
+  if(numberOfTries <= 0) return callback(new Error("could not connect correctly"), null);
+
+  var db = new Db('integration_test_', replSet);
+  // Print any errors
+  db.on("error", function(err) {
+    console.log("============================= ensureConnection caught error")
+    console.dir(err)
+    if(err != null && err.stack != null) console.log(err.stack)
+    db.close();
+  })
+  
+  // Open the db
+  db.open(function(err, p_db) {    
+    db.close();
+
+    if(err != null) {
+      // Wait for a sec and retry
+      setTimeout(function() {
+        numberOfTries = numberOfTries - 1;
+        ensureConnection(test, numberOfTries, callback);
+      }, 3000);
+    } else {
+      return callback(null);
+    }    
+  })            
+}
+
+var waitForReplicaset = function(callback) {    
+  // Replica configuration
+  var replSet = new ReplSetServers([ 
+      new Server( RS.host, RS.ports[1], { auto_reconnect: true } ),
+      new Server( RS.host, RS.ports[0], { auto_reconnect: true } ),
+      new Server( RS.host, RS.ports[2], { auto_reconnect: true } )
+    ], {});
+    
+  var db = new Db('integration_test_', replSet);    
+  replSet.on("fullsetup", function() {
+    db.close();
+    callback();
+  });
+  
+  db.open(function(err, p_db) {
+    db = p_db;
+  });
+}
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ * 
+ * @ignore
+ */
+exports.setUp = function(callback) {
+  // Create instance of replicaset manager but only for the first call
+  if(!serversUp && !noReplicasetStart) {
+    serversUp = true;
+    RS = new ReplicaSetManager({retries:120, passive_count:0, secondary_count:2, tags:[{"dc1":"ny"}, {"dc1":"ny"}, {"dc2":"sf"}]});    
+    RS.startSet(true, function(err, result) {      
+      if(err != null) throw err;
+      waitForReplicaset(callback);      
+    });      
+  } else {    
+    RS.restartKilledNodes(function(err, result) {
+      if(err != null) throw err;
+      waitForReplicaset(callback);
+    })
+  }
+}
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ * 
+ * @ignore
+ */
+exports.tearDown = function(callback) {
+  numberOfTestsRun = numberOfTestsRun - 1;
+  if(numberOfTestsRun == 0) {
+    // Finished kill all instances
+    RS.killAll(function() {
+      callback();              
+    })
+  } else {
+    callback();            
+  }  
+}
+
+exports['Should Correctly Checkout Readers'] = function(test) {
+  // Replica configuration
+  var replSet = new ReplSetServers([ 
+      new Server( RS.host, RS.ports[1], { auto_reconnect: true } ),
+      new Server( RS.host, RS.ports[0], { auto_reconnect: true } ),
+      new Server( RS.host, RS.ports[2], { auto_reconnect: true } )
+    ], 
+    {}
+  );
+  
+  // Open the database
+  var db = new Db('integration_test_', replSet, {recordQueryStats:true});
+  // Trigger test once whole set is up
+  replSet.on("fullsetup", function() {
+    
+    //
+    // Read using PRIMARY
+    // Grab a connection
+    var connection = db.serverConfig.checkoutReader(Server.PRIMARY);
+    // Locate connection
+    test.ok(locateConnection(connection, db.serverConfig._state.master.allRawConnections()));
+    
+    // // Read Preferences
+    // Server.PRIMARY = 'primary';
+    // Server.PRIMARY_PREFERRED = 'primaryPreferred';
+    // Server.SECONDARY = 'secondaryOnly';
+    // Server.SECONDARY_PREFERRED = 'secondary';
+    // Server.NEAREST = 'nearest'    
+    
+    //
+    // Read using PRIMARY_PREFERRED
+    var connection = db.serverConfig.checkoutReader(Server.PRIMARY_PREFERRED);
+    
+        
+    test.done();
+    db.close();
+  });
+
+  db.open(function(err, p_db) {
+    db = p_db;
+  })    
+}
+
+var locateConnection = function(connection, connections) {
+  // Locate one
+  for(var i = 0; i < connections.length; i++) {
+    if(connections[i].id == connection.id) {
+      return true;
+    }
+  }  
+  
+  return false;
+}
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ * 
+ * @ignore
+ */
+exports.noGlobalsLeaked = function(test) {
+  var leaks = gleak.detectNew();
+  test.equal(0, leaks.length, "global var leak detected: " + leaks.join(', '));
+  test.done();
+}
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ * 
+ * @ignore
+ */
+var numberOfTestsRun = Object.keys(this).length - 2;
