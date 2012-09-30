5f1fde6824b6227faa2327c293e9ad0a306ce591
Christian Kvalheim <christkv@gmail.com>
2012-09-30 23:49:35 +0200

    Working on more test running stuff

--- test/tools/rcover.js
+++ test/tools/rcover.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 var fs = require('fs'),
     path = require('path'),
-    nodeunit = require('nodeunit');
+    nodeunit = require('nodeunit'),
+    jade = require('jade');
+
 // Default options for nodeunit
 var options = {
     "error_prefix": "\u001B[31m",
@@ -16,23 +18,31 @@ var options = {
 
 var RCover = function RCover() {  
   this.testrunner = nodeunit.reporters.default;
+  this.dataDirectory = __dirname + "/../../rcover_html"; 
   this.results = {};
 }
 
 RCover.prototype.load = function load(files) { 
   this.files = files;
+  this.finalFiles = [];
+  this.totalNumberOfFiles = 0;
 
   // For each entry load the file and extract all the test methods available
-  // for(var i = 0; i < this.files.length; i++) {
   for(var i = 0; i < this.files.length; i++) {
     var file = this.files[i];
-
     // Stat the object
     var stat = fs.statSync(__dirname + "/../../" + file);
     if(stat.isFile()) {
-      // Run the test we passed in
-      this.run(__dirname + "/../../" + file);
+      this.finalFiles.push({file: file, path: __dirname + "/../../" + file});
     }
+  }
+
+  // Total number of files that need to be processed
+  this.totalNumberOfFiles = this.finalFiles.length;
+  // Only run tests verified as files
+  for(var i = 0; i < this.finalFiles.length; i++) {
+    this.run(this.finalFiles[i]);
+    // this.run(this.finalFile[i]);
   }
 }
 
@@ -52,26 +62,33 @@ RCover.prototype.run = function run(testfile, functionName) {
       // console.log("------------------------------------------------ testStart")
     },
     testDone: function (name, assertions) {
+      // console.log("================================================= testDone :: " + name)
+      // console.dir(Object.keys(_$jscoverage))
+      // console.dir(_$jscoverage)
       // Get all the coverage files
       var keys = Object.keys(_$jscoverage);
       // Iterate over all the files
       for(var i = 0; i < keys.length; i++) {
         var file = keys[i];
-
+        // console.log("-------------------------------------------- 0")
+        // console.dir(Object.keys(_$jscoverage))
+        // console.dir(_$jscoverage[file])
         if(!self.results[file]) self.results[file] = [];
+        // console.log("-------------------------------------------- 1")
         // Get all the sources for the result
         var source = _$jscoverage[file].source;
+        // console.log("-------------------------------------------- 2")
         // Iterate over all the coverage
         for(var j = 0; j < _$jscoverage[file].length; j++) {
-          if(_$jscoverage[file][j] == 1) {
-
+          if(_$jscoverage[file][j] >= 1) {
+            // console.log("================================= file :: " + testfile.file)
             // No existing record
             if(!self.results[file][j]) {
-              self.results[file][j] = {source: source[j], files:[{file: testfile, test:functionName}]}
+              self.results[file][j] = {source: source[j], files:[{file: testfile.file, test:name}]}
             } else {
-              self.results[file][j].files.push({file:testfile, test:functionName});
+              self.results[file][j].files.push({file:testfile, test:name});
             }
-          } else if(_$jscoverage[file][j] != 1) {
+          } else if(isNaN(parseInt(_$jscoverage[file][j], 10))) {
             if(!self.results[file][j]) {
               self.results[file][j] = {source: source[j], files:[]};
             }
@@ -79,26 +96,68 @@ RCover.prototype.run = function run(testfile, functionName) {
         }
       }
 
+      // console.dir(_$jscoverage['mongodb/connection/connection.js']  )
+
       // Clear out the js coverage variable state recorded by the last test
       // Ensuring we calculate pr test coverage
       for(var i = 0; i < keys.length; i++) {
+        // for(var j = 0; j < _$jscoverage[keys[i]].length; j++) {
+        //   if(!isNaN(parseInt(_$jscoverage[keys[i]][j]), 10)) {
+        //     _$jscoverage[keys[i]][j] = 0;
+        //   }
+        // }
         var newEmptyStateArray = new Array(_$jscoverage[keys[i]].length);
         newEmptyStateArray.source = _$jscoverage[keys[i]].source;
         _$jscoverage[keys[i]] = newEmptyStateArray;
       }
     },
     done: function (assertions) {
+
+      self.totalNumberOfFiles = self.totalNumberOfFiles - 1;
+      // Perform reporting
+      if(self.totalNumberOfFiles == 0) {
+        self.report();
+      }
     }
   };
-  // console.log("-------------------------------------------------------- 9")
-  // console.dir(self.results)
-
   // Run the file with options
-  nodeunit.runFiles([testfile], opts);
+  nodeunit.runFiles([testfile.path], opts);
+}
+
+RCover.prototype.report = function() {
+  // console.log("-------------------------------------------------------- 9")
+  // console.dir(self.results['mongodb/collection.js'])
+  var self = this;
+  // console.dir(self.results['mongodb/connection/connection.js'])
+  // Just generate a html report for now
+  // Create the output directory
+  if(!fs.existsSync(self.dataDirectory)) {
+    // Create directory
+    fs.mkdirSync(self.dataDirectory, "0755");
+  }
+
+  // For each result file render a html file
+  var keys = Object.keys(self.results);
+  for(var i = 0; i < keys.length; i++) {
+    var key = keys[i].replace(/\/|\./g, '_');
+    // console.dir(self.results[keys[i]])
+    // Render the template
+    jade.renderFile(__dirname + "/templates/file_results.jade", 
+      { pretty: true, 
+        debug: false, 
+        compileDebug: false,
+        results: self.results[keys[i]]
+      }, function(err, str){
+      if (err) throw err;
+      fs.writeFileSync(self.dataDirectory + "/" + key + ".html", str, 'ascii');
+    });
+  }
 }
 
 // Create instance and load specific file
 new RCover().load([
-  "test/insert_test.js",
-  "test/connection/message_parser_test.js"
+  // "test/insert_test.js",
+  // "test/find_test.js",
+  "test/connection/message_parser_test.js",
+  // "test/connection/message_parser_2_test.js"
   ]);
