6fbec6b65b14a7f88699d70071276bb29d6f5e99
Christian Kvalheim <christkv@gmail.com>
2012-11-07 15:18:41 +0100

    Fixed up _writeConcern error handling

--- examples/capped.js
+++ examples/capped.js
@@ -1,26 +1,24 @@
-var Db = require('../lib/mongodb').Db,
-  Connection = require('../lib/mongodb').Connection,
-  Server = require('../lib/mongodb').Server;
+var Db = require('../lib/mongodb').Db
+  , Connection = require('../lib/mongodb').Connection
+  , Server = require('../lib/mongodb').Server
+  , format = require('util').format;
 
 var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
 var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
 console.log("Connecting to " + host + ":" + port);
-var db = new Db('node-mongo-examples', new Server(host, port, {}), {native_parser:true});
-db.open(function(err, db) {
+Db.connect(format("mongodb://%s:%s/node-mongo-examples?w=1", host, port), function(err, db) {
   db.dropCollection('test', function(err, result) {
     // A capped collection has a max size and optionally a max number of records.
     // Old records get pushed out by new ones once the size or max num records is
     // reached.
     db.createCollection('test', {'capped':true, 'size':1024, 'max':12}, function(err, collection) {      
-      for(var i = 0; i < 100; i++) { collection.insert({'a':i}); }
+      for(var i = 0; i < 100; i++) { collection.insert({'a':i}, {safe:{w:0}}); }
       
       // We will only see the last 12 records
-      collection.find(function(err, cursor) {
-        cursor.toArray(function(err, items) {
-          console.log("The number of records: " + items.length);
-          db.close();
-        })
+      collection.find().toArray(function(err, items) {
+        console.log("The number of records: " + items.length);
+        db.close();
       })
     });    
   });
