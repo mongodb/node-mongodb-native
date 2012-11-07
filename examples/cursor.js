9aecca60579f4d4bd24a89ff4499f2654cadf568
Christian Kvalheim <christkv@gmail.com>
2012-11-07 16:46:03 +0100

    Added support for w, journal, fsync at the crud level of operations

--- examples/cursor.js
+++ examples/cursor.js
@@ -1,20 +1,20 @@
-var Db = require('../lib/mongodb').Db,
-  Connection = require('../lib/mongodb').Connection,
-  Server = require('../lib/mongodb').Server;
+var Db = require('../lib/mongodb').Db
+  , Connection = require('../lib/mongodb').Connection
+  , Server = require('../lib/mongodb').Server
+  , format = require('util').format;
 
 var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
 var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
 console.log("Connecting to " + host + ":" + port);
-var db = new Db('node-mongo-examples', new Server(host, port, {}), {native_parser:true});
-db.open(function(err, db) {
+Db.connect(format("mongodb://%s:%s/node-mongo-examples?w=1", host, port), function(err, db) {
   db.collection('test', function(err, collection) {
     // Erase all records from collection, if any
     collection.remove(function(err, result) {
       
       // Insert 3 records
       for(var i = 0; i < 3; i++) {
-        collection.insert({'a':i});
+        collection.insert({'a':i}, {safe:{w:0}});
       }
       
       // Cursors don't run their queries until you actually attempt to retrieve data
---
+++ test/write_preferences_test.js
@@ -0,0 +1,169 @@
+var mongodb = process.env['TEST_NATIVE'] != null ? require('../lib/mongodb').native() : require('../lib/mongodb').pure();
+var useSSL = process.env['USE_SSL'] != null ? true : false;
+var native_parser = (process.env['TEST_NATIVE'] != null);
+
+var testCase = require('nodeunit').testCase,
+  debug = require('util').debug,
+  inspect = require('util').inspect,
+  nodeunit = require('nodeunit'),
+  gleak = require('../dev/tools/gleak'),
+  Db = mongodb.Db,
+  Cursor = mongodb.Cursor,
+  Collection = mongodb.Collection,
+  Server = mongodb.Server;
+
+var MONGODB = 'integration_tests';
+var client = null;
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ *
+ * @ignore
+ */
+exports.setUp = function(callback) {
+  var self = exports;
+  client = new Db(MONGODB, new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL}), {safe:false, native_parser: (process.env['TEST_NATIVE'] != null)});
+  client.open(function(err, db_p) {
+    if(numberOfTestsRun == (Object.keys(self).length)) {
+      // If first test drop the db
+      client.dropDatabase(function(err, done) {
+        callback();
+      });
+    } else {
+      return callback();
+    }
+  });
+}
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ *
+ * @ignore
+ */
+exports.tearDown = function(callback) {
+  var self = this;
+  numberOfTestsRun = numberOfTestsRun - 1;
+  // Close connection
+  client.close();
+  callback();
+}
+
+exports['insert with w=1 db level'] = function(test) {
+  var db = new Db(MONGODB, 
+    new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL})
+    , {w:1, native_parser: (process.env['TEST_NATIVE'] != null)});
+  db.open(function(err, db) {
+    db.collection('insert_with_w_1').update({a:1}, {a:1}, {upsert:true}, function(err, result) {
+      test.equal(null, err);
+      test.equal(1, result);
+      test.done();
+      db.close();
+    });
+  });
+}
+
+exports['insert with w=1 collection level'] = function(test) {
+  var db = new Db(MONGODB, 
+    new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL})
+    , {w:0, native_parser: (process.env['TEST_NATIVE'] != null)});
+  db.open(function(err, db) {
+    db.collection('insert_with_w_1', {w:1}).update({a:1}, {a:1}, {upsert:true}, function(err, result) {
+      test.equal(null, err);
+      test.equal(1, result);
+      test.done();
+      db.close();
+    });
+  });
+}
+
+exports['insert with w=1 operation level'] = function(test) {
+  var db = new Db(MONGODB, 
+    new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL})
+    , {w:0, native_parser: (process.env['TEST_NATIVE'] != null)});
+  db.open(function(err, db) {
+    db.collection('insert_with_w_1').update({a:1}, {a:1}, {upsert:true, w:1}, function(err, result) {
+      test.equal(null, err);
+      test.equal(1, result);
+      test.done();
+      db.close();
+    });
+  });
+}
+
+exports['insert with journal db level'] = function(test) {
+  var db = new Db(MONGODB, 
+    new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL})
+    , {journal:true, native_parser: (process.env['TEST_NATIVE'] != null)});
+  db.open(function(err, db) {
+    db.collection('insert_with_w_1').update({a:1}, {a:1}, {upsert:true}, function(err, result) {
+      test.equal(null, err);
+      test.equal(1, result);
+      test.done();
+      db.close();
+    });
+  });
+}
+
+exports['insert with journal collection level'] = function(test) {
+  var db = new Db(MONGODB, 
+    new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL})
+    , {w:0, native_parser: (process.env['TEST_NATIVE'] != null)});
+  db.open(function(err, db) {
+    db.collection('insert_with_w_1', {journal:true}).update({a:1}, {a:1}, {upsert:true}, function(err, result) {
+      test.equal(null, err);
+      test.equal(1, result);
+      test.done();
+      db.close();
+    });
+  });
+}
+
+exports['insert with journal collection insert level'] = function(test) {
+  var db = new Db(MONGODB, 
+    new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL})
+    , {w:0, native_parser: (process.env['TEST_NATIVE'] != null)});
+  db.open(function(err, db) {
+    db.collection('insert_with_w_1').update({a:1}, {a:1}, {upsert:true, journal:true}, function(err, result) {
+      test.equal(null, err);
+      test.equal(1, result);
+      test.done();
+      db.close();
+    });
+  });
+}
+
+exports['insert with journal and w == 1 at db level'] = function(test) {
+  var db = new Db(MONGODB, 
+    new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 4, ssl:useSSL})
+    , {w:1, journal:true, wtimeout:1000, native_parser: (process.env['TEST_NATIVE'] != null)});
+  db.open(function(err, db) {
+    db.collection('insert_with_w_1').update({a:1}, {a:1}, {upsert:true}, function(err, result) {
+      test.equal(null, err);
+      test.equal(1, result);
+      test.done();
+      db.close();
+    });
+  });
+}
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ *
+ * @ignore
+ */
+exports.noGlobalsLeaked = function(test) {
+  var leaks = gleak.detectNew();
+  test.equal(0, leaks.length, "global var leak detected: " + leaks.join(', '));
+  test.done();
+}
+
+/**
+ * Retrieve the server information for the current
+ * instance of the db client
+ *
+ * @ignore
+ */
+var numberOfTestsRun = Object.keys(this).length - 2;
