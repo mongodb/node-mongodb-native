090abcc72d66171dc793e84bbfae19ccce4c9762
Christian Kvalheim <christkv@gmail.com>
2013-01-14 14:35:06 +0100

    Working on adding some benchmarking code

--- performance/benchmark.js
+++ performance/benchmark.js
@@ -1,5 +1,6 @@
 var read_all_tests = require('./util').read_all_tests
-  , run_test = require('./util').run_test;
+  , run_test = require('./util').run_test
+  , RunningStats = require('./util').RunningStats;
 
 // Load all the tests
 var tests = read_all_tests(__dirname + "/test");
@@ -21,14 +22,20 @@ run_test(default_url, tests[0], run_number_of_times, concurrent, function(err, r
     // Calculate the averages
     var result = results[key];
     var total_time = 0;
+    var stats = new RunningStats();
+
+    // Iterate over all the items
     for(var i = 0; i < result.results.length; i++) {
-      total_time = total_time + result.results[i].time;
+      // total_time = total_time + result.results[i].time;
+      stats.push(result.results[i].time);
     }
     // Calculate the average
     var average = total_time / result.results.length;
     console.log("= test: " + key);
-    console.log("  tot :: " + total_time);
-    console.log("  avg :: " + average);
+    console.log("  num      :: " + stats.numDataValues);
+    console.log("  avg      :: " + stats.mean);
+    console.log("  variance :: " + stats.variance);
+    console.log("  std      :: " + stats.standardDeviation);
   }
 
 
--- performance/util.js
+++ performance/util.js
@@ -102,6 +102,65 @@ var exceute_test_serially = function(url, func_name, test, number_of_times, resu
   });
 }
 
+/**
+ * Internal statistics object used for calculating average and standard devitation on
+ * running queries
+ * @ignore
+ */
+var RunningStats = function() {
+  var self = this;
+  this.m_n = 0;
+  this.m_oldM = 0.0;
+  this.m_oldS = 0.0;
+  this.m_newM = 0.0;
+  this.m_newS = 0.0;
+
+  // Define getters
+  Object.defineProperty(this, "numDataValues", { enumerable: true
+    , get: function () { return this.m_n; }
+  });
+
+  Object.defineProperty(this, "mean", { enumerable: true
+    , get: function () { return (this.m_n > 0) ? this.m_newM : 0.0; }
+  });
+
+  Object.defineProperty(this, "variance", { enumerable: true
+    , get: function () { return ((this.m_n > 1) ? this.m_newS/(this.m_n - 1) : 0.0); }
+  });
+
+  Object.defineProperty(this, "standardDeviation", { enumerable: true
+    , get: function () { return Math.sqrt(this.variance); }
+  });
+
+  Object.defineProperty(this, "sScore", { enumerable: true
+    , get: function () {
+      var bottom = this.mean + this.standardDeviation;
+      if(bottom == 0) return 0;
+      return ((2 * this.mean * this.standardDeviation)/(bottom));
+    }
+  });
+}
+
+/**
+ * @ignore
+ */
+RunningStats.prototype.push = function(x) {
+  // Update the number of samples
+  this.m_n = this.m_n + 1;
+  // See Knuth TAOCP vol 2, 3rd edition, page 232
+  if(this.m_n == 1) {
+    this.m_oldM = this.m_newM = x;
+    this.m_oldS = 0.0;
+  } else {
+    this.m_newM = this.m_oldM + (x - this.m_oldM) / this.m_n;
+    this.m_newS = this.m_oldS + (x - this.m_oldM) * (x - this.m_newM);
+
+    // set up for next iteration
+    this.m_oldM = this.m_newM;
+    this.m_oldS = this.m_newS;
+  }
+}
 
 exports.read_all_tests = read_all_tests;
 exports.run_test = run_test;
+exports.RunningStats = RunningStats;
