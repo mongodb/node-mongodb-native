925568fc82e54d785b4d4dce813f6532557cca7d
Christian Kvalheim <christkv@gmail.com>
2013-03-22 17:30:46 +0100

    GSSAPI Working for single connection

--- test/tests/kerberos/kdc_tests.js
+++ test/tests/kerberos/kdc_tests.js
@@ -19,9 +19,26 @@ exports['Should Correctly Authenticate using different user source database and 
 
   // Let's write the actual connection code
   MongoClient.connect(format("mongodb://%s@%s/test?authMechanism=GSSAPI&maxPoolSize=1", urlEncodedPrincipal, server), function(err, db) {
-    console.log("+++++++++++++++++++++++++++++++++++++++++++");
-    console.dir(err);
-    console.dir(db)
-    test.done();
+    test.equal(null, err);
+    test.ok(db != null);
+
+    // Attempt an operation
+    db.admin().command({listDatabases:1}, function(err, docs) {
+      test.equal(null, err);
+      console.log("+++++++++++++++++++++++++++++++++++++++++++");
+      console.dir(err);
+      console.dir(docs.documents[0].databases)
+      test.equal(null, err);
+      test.ok(docs.documents[0].databases);
+
+      db.close();
+      test.done();
+    });
+
+    // console.log("+++++++++++++++++++++++++++++++++++++++++++");
+    // console.dir(err);
+    // console.dir(db)
+    // db.close();
+    // test.done();
   });
 }
