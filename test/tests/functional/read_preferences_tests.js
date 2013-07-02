87b98515b2ea00cbc3958a00e1d58e13174bfb3c
Christian Kvalheim <christkv@gmail.com>
2013-07-02 08:27:05 +0200

    Ensure not breaking previous behavior while also supporting out: {inline:1} #1016

--- test/tests/functional/read_preferences_tests.js
+++ test/tests/functional/read_preferences_tests.js
@@ -166,6 +166,81 @@ exports['Should correctly apply collection level read Preference to mapReduce'] 
 /**
  * @ignore
  */
+exports['Should correctly apply collection level read Preference to mapReduce backward compatibility'] = function(configuration, test) {
+  var mongo = configuration.getMongoPackage()
+    , ReadPreference = mongo.ReadPreference;
+
+  var db = configuration.newDbInstance({w:1}, {poolSize:1});
+
+  db.open(function(err, db) {
+    test.equal(null, err);
+    // Set read preference
+    var collection = db.collection('read_pref_1', {readPreference:ReadPreference.SECONDARY_PREFERRED});
+    // Save checkout function
+    var checkout = db.serverConfig.checkoutReader;
+    // Set up our checker method
+    db.serverConfig.checkoutReader = function() {
+      var args = Array.prototype.slice.call(arguments, 0);
+      test.equal(ReadPreference.SECONDARY_PREFERRED, args[0]);
+      return checkout.apply(db.serverConfig, args);
+    }
+
+    // Map function
+    var map = function() { emit(this.user_id, 1); };
+    // Reduce function
+    var reduce = function(k,vals) { return 1; };
+
+    // Peform the map reduce
+    collection.mapReduce(map, reduce, {out: 'inline'}, function(err, collection) {
+      db.serverConfig.checkoutReader = checkout;
+
+      db.close();
+      test.done();
+    });
+  });  
+}
+
+/**
+ * @ignore
+ */
+exports['Should fail due to not using mapreduce inline with read preference'] = function(configuration, test) {
+  var mongo = configuration.getMongoPackage()
+    , ReadPreference = mongo.ReadPreference;
+
+  var db = configuration.newDbInstance({w:1}, {poolSize:1});
+
+  db.open(function(err, db) {
+    test.equal(null, err);
+    // Set read preference
+    var collection = db.collection('read_pref_1', {readPreference:ReadPreference.SECONDARY_PREFERRED});
+    // // Save checkout function
+    // var checkout = db.serverConfig.checkoutReader;
+    // // Set up our checker method
+    // db.serverConfig.checkoutReader = function() {
+    //   var args = Array.prototype.slice.call(arguments, 0);
+    //   test.equal(ReadPreference.SECONDARY_PREFERRED, args[0]);
+    //   return checkout.apply(db.serverConfig, args);
+    // }
+
+    // Map function
+    var map = function() { emit(this.user_id, 1); };
+    // Reduce function
+    var reduce = function(k,vals) { return 1; };
+
+    try {
+      // Peform the map reduce
+      collection.mapReduce(map, reduce, {out: {append: "test"}}, function(err, collection) {});      
+      test.fail();
+    } catch(err) {
+      db.close();
+      test.done();      
+    }
+  });  
+}
+
+/**
+ * @ignore
+ */
 exports['Should correctly apply collection level read Preference to aggregate'] = function(configuration, test) {
   var mongo = configuration.getMongoPackage()
     , ReadPreference = mongo.ReadPreference;
