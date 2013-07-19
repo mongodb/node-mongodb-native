e7d198522fc7f0d9f6bf6e623b61eba9adccefaf
Christian Kvalheim <christkv@gmail.com>
2013-07-19 14:47:59 +0200

    Hardened collection name and namespace handling throwing errors if there are null characters in db name or collection name

--- test/tests/functional/insert_tests.js
+++ test/tests/functional/insert_tests.js
@@ -1456,6 +1456,32 @@ exports.shouldCorrectlyInsertSimpleUTF8Regexp = function(configuration, test) {
   });    
 }
 
+exports.shouldCorrectlyThrowDueToIllegalCollectionName = function(configuration, test) {
+  var db = configuration.db();
+
+  var k = new Buffer(15);
+  for (var i = 0; i < 15; i++)
+    k[i] = 0;
+
+  k.write("hello");
+  k[6] = 0x06;
+  k.write("world", 10);
+
+
+  try {
+    var collection = db.collection(k.toString());
+    test.fail(false);
+  } catch (err) {
+  }
+
+  var collection = db.collection('test');
+  collection.collectionName = k.toString();
+  collection.insert({'b':1}, {w:1}, function(err, ids) {
+    test.ok(err != null);
+    test.done();
+  });
+}
+
 // exports.shouldCorrectlyPullDoc = function(configuration, test) {
 //   var client = configuration.db();
 
