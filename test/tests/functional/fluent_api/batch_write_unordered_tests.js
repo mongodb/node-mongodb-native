8230e0b6fdf16125fc9771abb06e188a95d420b6
Christian Kvalheim <christkv@gmail.com>
2013-11-13 10:24:38 +0100

    Finished up refactoring to make the new batch api be backward compatible with pre 2.6 servers

--- test/tests/functional/fluent_api/batch_write_unordered_tests.js
+++ test/tests/functional/fluent_api/batch_write_unordered_tests.js
@@ -1,6 +1,75 @@
 var fs = require('fs')
 	, stream = require('stream');
 
+exports['Should Correctly Execute Unordered Batch of Write Operations with remove taking out existing writes'] = {
+  // Add a tag that our runner can trigger on
+  // in this case we are setting that node needs to be higher than 0.10.X to run
+  requires: {serverType: 'Server'},
+  // requires: {mongodb: ">2.5.3"},
+  
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var db = configuration.newDbInstance({w:1}, {poolSize:1, auto_reconnect:false});
+    db.open(function(err, db) {
+      // Get the collection
+      var col = db.collection('batch_write_unordered_ops_0');
+      // Insert some records to allow for removal
+      col.insert([{d:1, e:1}, {d:2, e:1}], function(err, docs) {
+        test.equal(err, null);
+  
+        // Add unique index on b field causing all updates to fail
+        col.ensureIndex({b:1}, {unique:true, sparse:true}, function(err, result) {
+          test.equal(err, null);
+
+          // Initialize the unOrdered Batch
+          var batch = col.initializeBulkOp();
+          // Perform some operations
+          for(var i = 0; i < 2; i++) {
+            batch.insert({a:i, b:10});
+          }
+
+          // Illegal remove
+          batch.find({e: 1}).remove();
+
+          // Perform some operations
+          for(var i = 0; i < 2; i++) {
+            batch.find({c:i}).upsert().update({$set: {b: 11}});
+          }
+
+          // Execute the batch
+          batch.execute(function(err, result) {
+            test.equal(err, null);
+            test.equal(99999, result.code);
+            test.equal('batch op errors occurred', result.errmsg);
+            test.equal(0, result.ok);
+            test.equal(4, result.n);
+            test.equal(2, result.errDetails.length);
+            
+            test.equal(1, result.errDetails[0].index);
+            test.equal(11000, result.errDetails[0].code);
+            test.equal('string', typeof result.errDetails[0].errmsg);
+            
+            test.equal(4, result.errDetails[1].index);
+            test.equal(11000, result.errDetails[1].code);
+            test.equal('string', typeof result.errDetails[1].errmsg);
+
+            test.equal(1, result.upserted.length);
+            test.equal(3, result.upserted[0].index);
+            test.ok(result.upserted[0]._id != null);
+
+            col.find({e:1}).toArray(function(err, docs) {
+              test.equal(null, err);
+              test.equal(0, docs.length);
+              db.close();
+              test.done();
+            });
+          });
+        });
+      });
+    });
+  }
+}
+
 exports['Should Correctly Execute Unordered Batch of Write Operations with duplicate key errors on updates bypassing the upsert issue'] = {
   // Add a tag that our runner can trigger on
   // in this case we are setting that node needs to be higher than 0.10.X to run
@@ -174,63 +243,3 @@ exports['Should Correctly perform upsert with update, updateOne and replaceOne u
     });
   }
 }
-
-// exports['Should Correctly Execute Unordered Batch of Write Operations with duplicate key errors on updates'] = {
-//   // Add a tag that our runner can trigger on
-//   // in this case we are setting that node needs to be higher than 0.10.X to run
-//   requires: {serverType: 'Server'},
-//   requires: {mongodb: ">2.5.3"},
-  
-//   // The actual test we wish to run
-//   test: function(configuration, test) {
-//     var db = configuration.newDbInstance({w:1}, {poolSize:1, auto_reconnect:false});
-//     db.open(function(err, db) {
-//       // Get the collection
-//       var col = db.collection('batch_write_unordered_ops_4');
-//       // Add unique index on b field causing all updates to fail
-//       col.ensureIndex({b:1}, {unique:true, sparse:true}, function(err, result) {
-//         test.equal(err, null);
-
-//         // Initialize the unOrdered Batch
-//         var batch = col.initializeBulkOp();
-//         // Perform some operations
-//         for(var i = 0; i < 2; i++) {
-//           batch.insert({a:i});
-//         }
-
-//         // Perform some operations
-//         for(var i = 0; i < 2; i++) {
-//           batch.find({a:i}).upsert().update({$set: {b: 10}});
-//         }
-
-//         // Remove a couple of operations
-//         for(var i = 0; i < 2; i++) {
-//           batch.find({a:i}).removeOne()
-//         }
-
-//         // Execute the batch
-//         batch.execute(function(err, result) {
-//           console.log("=====================================================")
-//           console.dir(err)
-//           console.dir(result)
-
-//           // test.equal(err, null);
-//           // test.equal(false, result.ok);
-//           // test.equal(5, result.n);
-//           // test.equal(0, result.upserted);
-//           // test.equal(99999, result.code);
-//           // test.equal('batch op errors occurred', result.errMessage);
-//           // test.equal('', result.errmsg);
-//           // test.equal(1, result.errDetails.length);
-//           // test.equal(3, result.errDetails[0].index);
-//           // test.equal(11000, result.errDetails[0].code);
-//           // test.ok(result.errDetails[0].errMessage.indexOf('Update validation failed: DuplicateKey E11000 duplicate') != -1);
-
-//           db.close();
-//           test.done();
-//         });
-//       });
-//     });
-//   }
-// }
-
