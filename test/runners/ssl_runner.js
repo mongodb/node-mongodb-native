3f7e18bf4d9cf2b6f58d18acd24a589c379c243f
Christian Kvalheim <christkv@gmail.com>
2014-01-22 12:59:15 +0100

    Added fips tests to runner

--- test/runners/ssl_runner.js
+++ test/runners/ssl_runner.js
@@ -24,6 +24,7 @@ module.exports = function(configurations) {
       [
           '/test/tests/ssl/mongoclient_tests.js'
         , '/test/tests/ssl/ssl_validation_tests.js'
+        , '/test/tests/ssl/ssl_fips_validation_tests.js'
         , '/test/tests/ssl/x509_validation_tests.js'
       ]
     );
