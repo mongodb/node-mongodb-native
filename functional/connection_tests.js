1e6663b420035d4946217570cfebaa8eb0b7234b
Christian Kvalheim <christkv@gmail.com>
2014-03-21 17:18:29 +0100

    Connection allows for simple execution of command

--- functional/connection_tests.js
+++ functional/connection_tests.js
@@ -2,13 +2,15 @@ exports['Should correctly connect to single server'] = {
   metadata: {},
 
   test: function(configuration, test) {
-    var Connection = configuration.require.Connection;
+    var Connection = configuration.require.Connection
+      , bson = require('bson').BSONPure.BSON;
 
     // Attempt to connect
     var connection = new Connection({
         id: 1
       , host: configuration.host
       , port: configuration.port
+      , bson: bson
     })
 
     // Add event listeners
@@ -26,22 +28,68 @@ exports['Should fail connect to single server'] = {
   metadata: {},
 
   test: function(configuration, test) {
-    var Connection = configuration.require.Connection;
+    var Connection = configuration.require.Connection
+      , bson = require('bson').BSONPure.BSON;
 
     // Attempt to connect
     var connection = new Connection({
         id: 1
       , host: configuration.host
       , port: 23343
+      , bson: bson
     })
 
     // Add event listeners
     connection.on('error', function(err) {
-      // console.dir(err)
       test.ok(err instanceof configuration.require.MongoError)
       connection.destroy();
       test.done();
     })
+
+    // Start connection
+    connection.connect();
+  }
+}
+
+exports['Should correctly execute ismaster on single server'] = {
+  metadata: {},
+
+  test: function(configuration, test) {
+    var Connection = configuration.require.Connection
+      , Query = configuration.require.Query
+      , bson = require('bson').BSONPure.BSON;
+
+    // Attempt to connect
+    var connection = new Connection({
+        id: 1
+      , host: configuration.host
+      , port: configuration.port
+      , bson: bson
+    })
+
+    // Add event listeners
+    connection.on('connect', function(_connection) {
+      // Create a query command
+      var query = new Query(bson, 'admin.$cmd', {ismaster:true}, {
+        numberToReturn: -1
+      });
+
+      // Write it out to the connection
+      _connection.write(query);
+    });
+
+    connection.on('message', function(message) {
+      test.equal(true, message.documents[0].ismaster);
+      test.done();
+    });
+
+    connection.on('close', function(err) {
+      test.fail(0);
+    });
+
+    connection.on('close', function(err) {
+      test.fail(0);
+    });
 
     // Start connection
     connection.connect();
