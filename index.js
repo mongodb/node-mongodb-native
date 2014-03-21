d082d147d0c470f8578223a8eb9d1e193a0cf148
Christian Kvalheim <christkv@gmail.com>
2014-03-21 17:18:29 +0100

    Connection allows for simple execution of command

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ module.exports = {
     MongoError: require('./error')
   , Connection: require('./connection/connection')
   , Pool: require('./connection/pool')
+  , Query: require('./connection/commands').Query
+  , GetMore: require('./connection/commands').GetMore
+  , Response: require('./connection/commands').Response
 }
