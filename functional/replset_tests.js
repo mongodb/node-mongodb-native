bd5afb2781e7680554698e63dc04520e048b4cb9
Christian Kvalheim <christkv@gmail.com>
2014-04-02 20:04:32 +0200

    Basic HA working for existing member going offline and online

--- functional/replset_tests.js
+++ functional/replset_tests.js
@@ -1,30 +1,92 @@
 var f = require('util').format
   , Long = require('bson').Long;
 
-exports['Should correctly connect using replset object'] = {
-  metadata: {},
+// exports['Should correctly connect using replset object'] = {
+//   metadata: {},
 
-  test: function(configuration, test) {
-    var ReplSet = configuration.require.ReplSet;
+//   test: function(configuration, test) {
+//     var ReplSet = configuration.require.ReplSet;
 
-    // Attempt to connect
-    var server = new ReplSet([{
-        host: configuration.host
-      , port: configuration.port
-    }])
+//     // Attempt to connect
+//     var server = new ReplSet([{
+//         host: configuration.host
+//       , port: configuration.port
+//     }])
 
-    // Add event listeners
-    server.on('connect', function(_server) {
-      _server.destroy();
-      test.done();
-    })
+//     // Add event listeners
+//     server.on('connect', function(_server) {
+//       _server.destroy();
+//       test.done();
+//     })
 
-    // Start connection
-    server.connect();
-  }
-}
+//     // Start connection
+//     server.connect();
+//   }
+// }
 
-exports['Should correctly execute command using replset'] = {
+// exports['Should correctly execute command using replset'] = {
+//   metadata: {},
+
+//   test: function(configuration, test) {
+//     var ReplSet = configuration.require.ReplSet;
+
+//     // Attempt to connect
+//     var server = new ReplSet([{
+//         host: configuration.host
+//       , port: configuration.port
+//     }]);
+
+//     // Add event listeners
+//     server.on('connect', function(_server) {
+//       // Execute the command
+//       _server.command("system.$cmd", {ismaster: true}, {readPreference: 'primary'}, function(err, result) {
+//         test.equal(null, err);
+//         test.equal(true, result.result.ismaster);
+//         // Destroy the connection
+//         _server.destroy();
+//         // Finish the test
+//         test.done();
+//       });      
+//     });
+
+//     // Start connection
+//     server.connect();
+//   }
+// }
+
+// exports['Should correctly execute write using replset'] = {
+//   metadata: {},
+
+//   test: function(configuration, test) {
+//     var ReplSet = configuration.require.ReplSet;
+
+//     // Attempt to connect
+//     var server = new ReplSet([{
+//         host: configuration.host
+//       , port: configuration.port
+//     }]);
+
+//     // Add event listeners
+//     server.on('connect', function(_server) {
+//       // Execute the write
+//       _server.insert(f("%s.inserts_repl1", configuration.db), [{a:1}], {
+//         writeConcern: {w:1}, ordered:true
+//       }, function(err, results) {
+//         test.equal(null, err);
+//         test.equal(1, results.result.n);
+//         // Destroy the connection
+//         _server.destroy();
+//         // Finish the test
+//         test.done();
+//       });
+//     })
+
+//     // Start connection
+//     server.connect();
+//   }
+// }
+
+exports['Should correctly fail secondary server'] = {
   metadata: {},
 
   test: function(configuration, test) {
@@ -36,48 +98,32 @@ exports['Should correctly execute command using replset'] = {
       , port: configuration.port
     }]);
 
-    // Add event listeners
-    server.on('connect', function(_server) {
-      // Execute the command
-      _server.command("system.$cmd", {ismaster: true}, {readPreference: 'primary'}, function(err, result) {
-        test.equal(null, err);
-        test.equal(true, result.result.ismaster);
-        // Destroy the connection
-        _server.destroy();
-        // Finish the test
-        test.done();
-      });      
-    });
-
-    // Start connection
-    server.connect();
-  }
-}
-
-exports['Should correctly execute write using replset'] = {
-  metadata: {},
-
-  test: function(configuration, test) {
-    var ReplSet = configuration.require.ReplSet;
-
-    // Attempt to connect
-    var server = new ReplSet([{
-        host: configuration.host
-      , port: configuration.port
-    }]);
+    console.log("######################################################")
+    console.log("######################################################")
+    console.log("######################################################")
 
     // Add event listeners
     server.on('connect', function(_server) {
       // Execute the write
-      _server.insert(f("%s.inserts", configuration.db), [{a:1}], {
+      _server.insert(f("%s.inserts_repl2", configuration.db), [{a:1}], {
         writeConcern: {w:1}, ordered:true
       }, function(err, results) {
         test.equal(null, err);
         test.equal(1, results.result.n);
-        // Destroy the connection
-        _server.destroy();
-        // Finish the test
-        test.done();
+
+        // Execute find
+        var cursor = _server.cursor(f("%s.inserts_repl2", configuration.db), {
+            find: f("%s.inserts_repl2", configuration.db)
+          , query: {}
+        });
+
+        // Execute next
+        cursor.next(function(err, d) {
+          // // Destroy the connection
+          // _server.destroy();
+          // // Finish the test
+          // test.done();
+        });
       });
     })
 
