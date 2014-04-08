9942385bd0054784f6123f8057f449162c733724
Christian Kvalheim <christkv@gmail.com>
2014-04-08 16:37:33 +0200

    Added some more support for logging

--- test/functional_test_runner.js
+++ test/functional_test_runner.js
@@ -149,7 +149,8 @@ runner.on('exit', function(errors, results) {
 // Set Logger level for driver
 Logger.setLevel('debug');
 // Logger.filter('class', ['ReplSet', 'Server', 'Connection']);
-Logger.filter('class', ['Connection']);
+// Logger.filter('class', ['Connection']);
+Logger.filter('class', ['ReplSet']);
 
 // Run the tests
 runner.run(StandaloneConfiguration({
