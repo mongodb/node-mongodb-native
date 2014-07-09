mongodb/node-mongodb-native

commit 927692e43c90d16832f8b1b3789c472b5df4ddfd

Fix correctly inheriting db level readPreference from MongoClient NODE-169 and NODE-168

lib/mongodb/connection/url_parser.js
@@ -171,7 +171,7 @@ exports.parse = function(url, options) {
         replSetServersOptions.socketOptions.socketTimeoutMS = parseInt(value, 10);
         break;
       case 'w':
-        dbOptions.w = parseInt(value, 10);
+        dbOptions.w = value;
         break;
       case 'authSource':
         dbOptions.authSource = value;
@@ -204,13 +204,15 @@ exports.parse = function(url, options) {
         dbOptions.authMechanism = value;
         break;
       case 'wtimeoutMS':
-        dbOptions.wtimeoutMS = parseInt(value, 10);
+        dbOptions.wtimeout = parseInt(value, 10);
         break;
       case 'readPreference':
         if(!ReadPreference.isValid(value)) throw new Error("readPreference must be either primary/primaryPreferred/secondary/secondaryPreferred/nearest");
         dbOptions.read_preference = value;
         break;
       case 'readPreferenceTags':
+        // Decode the value
+        value = decodeURIComponent(value);
         // Contains the tag object
         var tagObject = {};
         if(value == null || value == '') {

lib/mongodb/collection.js
@@ -51,10 +51,16 @@ function Collection (db, collectionName, pkFactory, options) {
   this.serializeFunctions = options == null || options.serializeFunctions == null ? db.serializeFunctions : options.serializeFunctions;
   this.raw = options == null || options.raw == null ? db.raw : options.raw;
 
-  this.readPreference = options == null || options.readPreference == null ? db.serverConfig.options.readPreference : options.readPreference;
-  this.readPreference = this.readPreference == null ? 'primary' : this.readPreference;
+  // Assign the right collection level readPreference
+  if(options && options.readPreference) {
+    this.readPreference = options.readPreference;
+  } else if(this.db.options.readPreference) {
+    this.readPreference = this.db.options.readPreference;
+  } else if(this.db.serverConfig.options.readPreference) {
+    this.readPreference = this.db.serverConfig.options.readPreference;
+  }
 
-
+  // Set custom primary key factory if provided
   this.pkFactory = pkFactory == null
     ? ObjectID
     : pkFactory;

lib/mongodb/collection/query.js
@@ -131,6 +131,9 @@ var find = function find () {
   if(o["read"] != null) o["readPreference"] = o["read"];
   // If no readPreference specified set the collection level readPreference
   o.readPreference = o.readPreference ? o.readPreference : this.readPreference;
+  // If still no readPreference specified set the db level
+  o.readPreference = o.readPreference ? o.readPreference : this.db.options.readPreference;
+  // Set slaveok if needed
   if(o.readPreference == "secondary" || o.read == "secondaryOnly") o.slaveOk = true;
 
   // Set the selector

lib/mongodb/connection/mongos.js
@@ -499,9 +499,9 @@ Mongos.prototype.checkoutReader = function(read) {
   // If we have a read preference object unpack it
   if(read != null && typeof read == 'object' && read['_type'] == 'ReadPreference') {
     // Validate if the object is using a valid mode
-    if(!read.isValid()) throw new Error("Illegal readPreference mode specified, " + read.mode);
+    if(!read.isValid()) throw new Error("Illegal readPreference mode specified, " + JSON.stringify(read));
   } else if(!ReadPreference.isValid(read)) {
-    throw new Error("Illegal readPreference mode specified, " + read);
+    throw new Error("Illegal readPreference mode specified, " + JSON.stringify(read));
   }
 
   // Checkout a writer

lib/mongodb/connection/read_preference.js
@@ -28,7 +28,7 @@ ReadPreference.isValid = function(_mode) {
   return (_mode == ReadPreference.PRIMARY || _mode == ReadPreference.PRIMARY_PREFERRED
     || _mode == ReadPreference.SECONDARY || _mode == ReadPreference.SECONDARY_PREFERRED
     || _mode == ReadPreference.NEAREST
-    || _mode == true || _mode == false);
+    || _mode == true || _mode == false || _mode == null);
 }
 
 /**

lib/mongodb/connection/repl_set/options.js
@@ -49,7 +49,7 @@ Options.prototype.init = function() {
     if(readPreference != ReadPreference.PRIMARY && readPreference != ReadPreference.PRIMARY_PREFERRED
       && readPreference != ReadPreference.SECONDARY && readPreference != ReadPreference.SECONDARY_PREFERRED
       && readPreference != ReadPreference.NEAREST && typeof readPreference != 'object' && readPreference['_type'] != 'ReadPreference') {
-      throw new Error("Illegal readPreference mode specified, " + readPreference);
+      throw new Error("Illegal readPreference mode specified, " + JSON.stringify(readPreference));
     }
 
     this.readPreference = readPreference;

lib/mongodb/connection/repl_set/repl_set.js
@@ -549,7 +549,7 @@ ReplSet.prototype.checkoutReader = function(readPreference, tags) {
   // If we have a read preference object unpack it
   if(typeof readPreference == 'object' && readPreference['_type'] == 'ReadPreference') {
     // Validate if the object is using a valid mode
-    if(!readPreference.isValid()) throw new Error("Illegal readPreference mode specified, " + readPreference.mode);
+    if(!readPreference.isValid()) throw new Error("Illegal readPreference mode specified, " + JSON.stringify(readPreference.mode));
     // Set the tag
     tags = readPreference.tags;
     readPreference = readPreference.mode;
@@ -563,7 +563,7 @@ ReplSet.prototype.checkoutReader = function(readPreference, tags) {
   // Ensure we unpack a reference
   if(finalReadPreference != null && typeof finalReadPreference == 'object' && finalReadPreference['_type'] == 'ReadPreference') {
     // Validate if the object is using a valid mode
-    if(!finalReadPreference.isValid()) throw new Error("Illegal readPreference mode specified, " + finalReadPreference.mode);
+    if(!finalReadPreference.isValid()) throw new Error("Illegal readPreference mode specified, " + JSON.stringify(finalReadPreference.mode));
     // Set the tag
     tags = finalReadPreference.tags;
     readPreference = finalReadPreference.mode;

lib/mongodb/db.js
@@ -1653,9 +1653,9 @@ var __executeQueryCommand = function(self, db_command, options, callback) {
   // Correct readPreference preference to default primary if set to false, null or primary
   if(!(typeof readPreference == 'object') && readPreference._type == 'ReadPreference') {
     readPreference = (readPreference == null || readPreference == 'primary' || readPreference == false) ? ReadPreference.PRIMARY : readPreference;
-    if(!ReadPreference.isValid(readPreference)) return callback(new Error("Illegal readPreference mode specified, " + readPreference));
+    if(!ReadPreference.isValid(readPreference)) return callback(new Error("Illegal readPreference mode specified, " + JSON.stringify(readPreference)));
   } else if(typeof readPreference == 'object' && readPreference._type == 'ReadPreference') {
-    if(!readPreference.isValid()) return callback(new Error("Illegal readPreference mode specified, " + readPreference.mode));
+    if(!readPreference.isValid()) return callback(new Error("Illegal readPreference mode specified, " + JSON.stringify(readPreference)));
   }
 
   // If we have a read preference set and we are a mongos pass the read preference on to the mongos instance,

commit 5c517f87bbaf4318f4a329eef247b2ae1b09a447

Fixed parsing of write concern option in URL
The issue was introduced in 927692e and a test added in #1191.

lib/mongodb/connection/url_parser.js
@@ -178,7 +178,7 @@ exports.parse = function(url, options) {
         replSetServersOptions.socketOptions.socketTimeoutMS = parseInt(value, 10);
         break;
       case 'w':
-        dbOptions.w = value;
+        dbOptions.w = parseInt(value, 10);
         break;
       case 'authSource':
         dbOptions.authSource = value;