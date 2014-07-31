3d9cb78f6e5e55f4360ad3bb4df23d64e66ecee2
Christian Kvalheim <christkv@gmail.com>
2014-07-31 22:51:11 +0200

    Fixes for filters

--- test/runners/shared/filters.js
+++ test/runners/shared/filters.js
@@ -1,35 +1,70 @@
 var Filters = require('integra').Filters;
 
 // Get environmental variables that are known
-var node_version_array = process
-    .version
-    .replace(/v/g, '')
-    .split('.')
-    .map(function(x) { return parseInt(x, 10) });
+var node_version = process.version.replace(/v/g, '');
 
 // Check if we have a valid node.js method
-var validVersions = function(compare_version, version_required) {
-  var comparator = version_required.slice(0, 1)
-  var version_array = version_required
-      .slice(1).split(/\./).map(function(x) { return parseInt(x, 10); });
+var validVersion = function(compare_version, version_required) {
+  var comparator = version_required.substr(0, 1)
 
-  // Slice the arrays
-  var compare_version = compare_version.slice(0, 3);
-  var version_array = version_array.slice(0, 3);
-  // Convert to actual number
-  var cnumber = compare_version[0] * 100 + compare_version[1] * 10 + compare_version[2];
-  var ver = version_array[0] * 100 + version_array[1] * 10 + version_array[2];
-
-  // Comparator
-  if(comparator == '>') {
-    if(cnumber > ver) return true;
-  } else if(comparator == '<') {
-    if(cnumber < ver) return true;
-  } else if(comparator == '=') {
-    if(cnumber == ver) return true;
+  // Figure out the comparator
+  if(version_required.indexOf('>=') != -1 || version_required.indexOf('<=') != -1) {
+    comparator = version_required.substr(0, 2);
+    version_required = version_required.substr(2);
+  } else if(version_required.indexOf('>') != -1 || version_required.indexOf('<') != -1 || version_required.indexOf('=') != -1) {
+    version_required = version_required.substr(1);
   }
-  
-  // No valid version
+
+  var v1parts = compare_version.split('.');
+  var v2parts = version_required.split('.');
+
+  function isValidPart(x) {
+    return (/^\d+$/).test(x);
+  }
+
+  if(!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+    return false;
+  }
+
+  // Map all parts to numbers
+  v1parts = v1parts.map(Number);
+  v2parts = v2parts.map(Number);
+
+  // Comparison function
+  var compare = function(v1, v2) {
+    for(var i = 0; i < v1.length; ++i) {
+      if(v2.length == i) {
+        return 1;
+      }
+
+      if(v1[i] == v2[i]) {
+        continue;
+      } else if (v1[i] > v2[i]) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+
+    if(v1.length != v2.length) {
+      return -1;
+    }
+
+    return 0;    
+  }
+
+  // calculate comparison
+  var result = compare(v1parts, v2parts)
+
+  // Return if it's valid depending on the passed in comparator
+  if((comparator == '=' || comparator == '>=' || comparator == '<=') && result == 0) {
+    return true;
+  } else if(comparator == '>' && result == -1) {
+    return true;
+  } else if(comparator == '<' && result == 1) {
+    return true;
+  }
+
   return false;
 }
 
@@ -40,7 +75,7 @@ var MongoDBVersionFilter = function() {
     configuration.newDbInstance({w:1}).open(function(err, db) {
       db.command({buildInfo:true}, function(err, result) {
         if(err) throw err;
-        mongodb_version = result.versionArray;
+        mongodb_version = result.versionArray.join('.');
         db.close();
         callback();
       });
@@ -48,24 +83,15 @@ var MongoDBVersionFilter = function() {
   }
 
   this.filter = function(test) {
-    if(typeof test != 'function') {      
-      if(test.requires && test.requires.mongodb) {
-        return validVersions(mongodb_version, test.requires.mongodb);
-      }
-
-      if(test.requires 
-        && test.requires.serverType 
-        && test.requires.serverType.toLowerCase() != 'server') {
-        return false;
-      }
-    }
-
-    return true
+    if(test.requires == null) return true;
+    if(test.requires.mongodb == null) return true;
+    return !validVersion(mongodb_version, test.requires.mongodb);
   }  
 }
 
 var NodeVersionFilter = function() {
-  var nodeVersionArray = process
+  // Get environmental variables that are known
+  var node_version_array = process
       .version
       .replace(/v/g, '')
       .split('.')
@@ -75,19 +101,10 @@ var NodeVersionFilter = function() {
     callback(null, null);
   }
 
-  this.filter = function(test) {    
-    if(typeof test != 'function') {      
-      if(test.requires && test.requires.node) 
-        return validVersions(node_version_array, test.requires.node);
-
-      if(test.requires 
-        && test.requires.serverType 
-        && test.requires.serverType.toLowerCase() != 'server') {
-        return false;
-      }
-    }
-
-    return true
+  this.filter = function(test) {
+    if(test.requires == null) return true;
+    if(test.requires.node == null) return true;
+    return !validVersion(node_version, test.requires.node);
   }  
 }
 
