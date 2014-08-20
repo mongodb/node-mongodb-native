1d617abddcba97d8c303d8138974ff8ba8e70ef0
Kenneth Powers <ken@kenpowers.net>
2014-08-19 22:23:24 -0400

    Add failing test for Cursor export.

--- test/tests/functional/collection_tests.js
+++ test/tests/functional/collection_tests.js
@@ -1487,3 +1487,28 @@ exports['Should correctly execute remove with $elemMatch field in selector'] = f
     });
   });
 }
+
+/**
+ * @ignore
+ */
+exports['Should return Cursor object constructed from exposed Constructor with original prototype'] = function(configuration, test) {
+  // Get refernce to Cursor constructor
+  var Cursor = require('../../../').Cursor;
+  // Get db instance
+  var db = configuration.newDbInstance({w:0}, {poolSize:1});
+
+  // DOC_LINE var db = new Db('test', new Server('localhost', 27017));
+  // DOC_START
+  // Establish connection to db
+  db.open(function(err, db) {
+
+    // Fetch the collection
+    var collection = db.collection('foo');
+    // Find the saved document
+    var cursor = collection.find();
+    test.equal(cursor.constructor, Cursor);
+    test.equal(cursor.constructor.prototype, Cursor.prototype);
+    test.done();
+  });
+  // DOC_END
+}
