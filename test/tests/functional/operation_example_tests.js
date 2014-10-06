9cc22821c1b276dcc10c45c32993773a9c952249
Christian Kvalheim <christkv@gmail.com>
2014-10-06 12:57:50 +0200

    Added sharding example for docs

--- test/tests/functional/operation_example_tests.js
+++ test/tests/functional/operation_example_tests.js
@@ -595,3 +595,277 @@ exports['Example of command using ReplSet instance'] = {
     // END
   }
 }
+
+/**************************************************************************
+ *
+ * MONGOS TESTS
+ *
+ *************************************************************************/
+
+/**
+ * Correctly insert a document using the Mongos insert method
+ *
+ * @example-class Mongos
+ * @example-method insert
+ * @ignore
+ */
+exports['Example of simple insert into db using Mongos'] = {
+  metadata: {
+    requires: {
+      topology: "mongos"
+    }
+  },
+
+  test: function(configuration, test) {
+    var Mongos = configuration.require.Mongos;
+
+    // Attempt to connect
+    var server = new Mongos([{
+        host: configuration.host
+      , port: configuration.port
+    }]);
+
+    // LINE var Mongos = configuration.require.Mongos,
+    // LINE   test = require('assert');
+    // LINE var server = new Mongos([{host: 'localhost', port:50000}]);
+    // REMOVE-LINE test.done();
+    // BEGIN
+    // Add event listeners
+    server.on('connect', function(_server) {
+      // Execute the insert
+      _server.insert('integration_tests.inserts_example_mongos_1', [{a:1}], {
+        writeConcern: {w:1}, ordered:true
+      }, function(err, results) {
+        test.equal(null, err);
+        test.equal(1, results.result.n);
+
+        _server.destroy();
+        test.done();
+      });
+    });
+
+    // Start connection
+    server.connect();
+    // END
+  }
+}
+
+/**
+ * Correctly update a document using the Mongos update method
+ *
+ * @example-class Mongos
+ * @example-method update
+ * @ignore
+ */
+exports['Example of update using ReplSet instance'] = {
+  metadata: {
+    requires: {
+      topology: "mongos"
+    }
+  },
+
+  test: function(configuration, test) {
+    var Mongos = configuration.require.Mongos;
+
+    // Attempt to connect
+    var server = new Mongos([{
+        host: configuration.host
+      , port: configuration.port
+    }]);
+
+    // LINE var Mongos = configuration.require.Mongos,
+    // LINE   test = require('assert');
+    // LINE var server = new Mongos([{host: 'localhost', port:50000}]);
+    // REMOVE-LINE test.done();
+    // BEGIN
+    // Add event listeners
+    server.on('connect', function(_server) {
+      // Execute the insert
+      _server.insert('integration_tests.inserts_example_mongos_2', [{a:1}], {
+        writeConcern: {w:1}, ordered:true
+      }, function(err, results) {
+        test.equal(null, err);
+        test.equal(1, results.result.n);
+
+        // Execute the write
+        _server.update('integration_tests.inserts_example_mongos_2', [{
+          q: {a: 1}, u: {'$set': {b:1}}
+        }], {
+          writeConcern: {w:1}, ordered:true
+        }, function(err, results) {
+          test.equal(null, err);
+          test.equal(1, results.result.n);
+
+          _server.destroy();
+          test.done();
+        });
+      });
+    });
+
+    // Start connection
+    server.connect();
+    // END
+  }
+}
+
+/**
+ * Correctly remove a document using the Mongos remove method
+ *
+ * @example-class Mongos
+ * @example-method remove
+ * @ignore
+ */
+exports['Example of remove using Mongos instance'] = {
+  metadata: {
+    requires: {
+      topology: "mongos"
+    }
+  },
+
+  test: function(configuration, test) {
+    var Mongos = configuration.require.Mongos;
+
+    // Attempt to connect
+    var server = new Mongos([{
+        host: configuration.host
+      , port: configuration.port
+    }]);
+
+    // LINE var Mongos = configuration.require.Mongos,
+    // LINE   test = require('assert');
+    // LINE var server = new Mongos([{host: 'localhost', port:50000}]);
+    // REMOVE-LINE test.done();
+    // BEGIN
+    // Add event listeners
+    server.on('connect', function(_server) {
+      // Execute the insert
+      _server.insert('integration_tests.inserts_example_mongos_3', [{a:1}], {
+        writeConcern: {w:1}, ordered:true
+      }, function(err, results) {
+        test.equal(null, err);
+        test.equal(1, results.result.n);
+
+        // Execute the write
+        _server.remove('integration_tests.inserts_example_mongos_3', [{
+          q: {a: 1}, limit: 1
+        }], {
+          writeConcern: {w:1}, ordered:true
+        }, function(err, results) {
+          test.equal(null, err);
+          test.equal(1, results.result.n);
+
+          _server.destroy();
+          test.done();
+        });
+      });
+    });
+
+    // Start connection
+    server.connect();
+    // END
+  }
+}
+
+/**
+ * Correctly find a document on the Mongos using the cursor
+ *
+ * @example-class Mongos
+ * @example-method cursor
+ * @ignore
+ */
+exports['Example of cursor using Mongos instance'] = {
+  metadata: {
+    requires: {
+      topology: "mongos"
+    }
+  },
+
+  test: function(configuration, test) {
+    var Mongos = configuration.require.Mongos;
+
+    // Attempt to connect
+    var server = new Mongos([{
+        host: configuration.host
+      , port: configuration.port
+    }]);
+
+    // LINE var Mongos = configuration.require.Mongos,
+    // LINE   test = require('assert');
+    // LINE var server = new Mongos([{host: 'localhost', port:50000}]);
+    // REMOVE-LINE test.done();
+    // BEGIN
+    // Add event listeners
+    server.on('connect', function(_server) {
+      // Execute the insert
+      _server.insert('integration_tests.inserts_example_mongos_4', [{a:1}], {
+        writeConcern: {w:1}, ordered:true
+      }, function(err, results) {
+        test.equal(null, err);
+        test.equal(1, results.result.n);
+
+        // Execute the write
+        var cursor = _server.cursor('integration_tests.inserts_example_mongos_4', {
+            find: 'integration_tests.example4'
+          , query: {a:1}
+        });
+
+        // Get the first document
+        cursor.next(function(err, doc) {
+          test.equal(null, err);
+          test.equal(1, doc.a);
+
+          _server.destroy();
+          test.done();
+        });
+      });
+    });
+
+    // Start connection
+    server.connect();
+    // END
+  }
+}
+
+/**
+ * Correctly execute ismaster command on the Mongos using the cursor
+ *
+ * @example-class Mongos
+ * @example-method command
+ * @ignore
+ */
+exports['Example of command using Mongos instance'] = {
+  metadata: {
+    requires: {
+      topology: "mongos"
+    }
+  },
+
+  test: function(configuration, test) {
+    var Mongos = configuration.require.Mongos;
+
+    // Attempt to connect
+    var server = new Mongos([{
+        host: configuration.host
+      , port: configuration.port
+    }]);
+
+    // LINE var Mongos = configuration.require.Mongos,
+    // LINE   test = require('assert');
+    // LINE var server = new Mongos([{host: 'localhost', port:50000}]);
+    // REMOVE-LINE test.done();
+    // BEGIN
+    // Add event listeners
+    server.on('connect', function(_server) {
+      // Execute the command
+      _server.command("system.$cmd", {ismaster: true}, function(err, result) {
+        test.equal(null, err)
+        _server.destroy();
+        test.done();
+      });
+    });
+
+    // Start connection
+    server.connect();
+    // END
+  }
+}
