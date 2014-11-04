0a81679b88a4964cb7127c5fdcf8eacaad15c0c1
Christian Kvalheim <christkv@gmail.com>
2014-11-04 14:14:41 +0100

    cleaning up unused code

--- topologies/session.js
+++ topologies/session.js
@@ -9,80 +9,83 @@ var inherits = require('util').inherits
  * @param {{Server}|{ReplSet}|{Mongos}} topology The topology instance underpinning the session
  */
 var Session = function(options, topology) {
+  this.options = options;
+  this.topology = topology;
+  
   // Add event listener
   EventEmitter.call(this);
-
-  /**
-   * Execute a command
-   * @method
-   * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
-   * @param {object} cmd The command hash
-   * @param {object} [options.readPreference] Specify read preference if command supports it
-   * @param {object} [options.connection] Specify connection object to execute command against
-   * @param {opResultCallback} callback A callback function
-   */
-  this.command = function(ns, cmd, options, callback) {
-    topology.command(ns, cmd, options, callback);
-  }
-
-  /**
-   * Insert one or more documents
-   * @method
-   * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
-   * @param {array} ops An array of documents to insert
-   * @param {boolean} [options.ordered=true] Execute in order or out of order
-   * @param {object} [options.writeConcern={}] Write concern for the operation
-   * @param {opResultCallback} callback A callback function
-   */
-  this.insert = function(ns, ops, options, callback) {
-    topology.insert(ns, ops, options, callback);
-  }
-
-  /**
-   * Perform one or more update operations
-   * @method
-   * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
-   * @param {array} ops An array of updates
-   * @param {boolean} [options.ordered=true] Execute in order or out of order
-   * @param {object} [options.writeConcern={}] Write concern for the operation
-   * @param {opResultCallback} callback A callback function
-   */
-  this.update = function(ns, ops, options, callback) {
-    topology.update(ns, ops, options, callback);
-  }
-
-  /**
-   * Perform one or more remove operations
-   * @method
-   * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
-   * @param {array} ops An array of removes
-   * @param {boolean} [options.ordered=true] Execute in order or out of order
-   * @param {object} [options.writeConcern={}] Write concern for the operation
-   * @param {opResultCallback} callback A callback function
-   */
-  this.remove = function(ns, ops, options, callback) {
-    topology.remove(ns, ops, options, callback);
-  }
-
-  /**
-   * Perform one or more remove operations
-   * @method
-   * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
-   * @param {{object}|{Long}} cmd Can be either a command returning a cursor or a cursorId
-   * @param {object} [options.batchSize=0] Batchsize for the operation
-   * @param {array} [options.documents=[]] Initial documents list for cursor
-   * @param {boolean} [options.tailable=false] Tailable flag set
-   * @param {boolean} [options.oplogReply=false] oplogReply flag set
-   * @param {boolean} [options.awaitdata=false] awaitdata flag set
-   * @param {boolean} [options.exhaust=false] exhaust flag set
-   * @param {boolean} [options.partial=false] partial flag set
-   * @param {opResultCallback} callback A callback function
-   */
-  this.cursor = function(ns, cmd, options) {
-    return topology.cursor(ns, cmd, options);
-  }  
 }
 
 inherits(Session, EventEmitter);
+
+/**
+ * Execute a command
+ * @method
+ * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
+ * @param {object} cmd The command hash
+ * @param {object} [options.readPreference] Specify read preference if command supports it
+ * @param {object} [options.connection] Specify connection object to execute command against
+ * @param {opResultCallback} callback A callback function
+ */
+Session.prototype.command = function(ns, cmd, options, callback) {
+  this.topology.command(ns, cmd, options, callback);
+}
+
+/**
+ * Insert one or more documents
+ * @method
+ * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
+ * @param {array} ops An array of documents to insert
+ * @param {boolean} [options.ordered=true] Execute in order or out of order
+ * @param {object} [options.writeConcern={}] Write concern for the operation
+ * @param {opResultCallback} callback A callback function
+ */
+Session.prototype.insert = function(ns, ops, options, callback) {
+  this.topology.insert(ns, ops, options, callback);
+}
+
+/**
+ * Perform one or more update operations
+ * @method
+ * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
+ * @param {array} ops An array of updates
+ * @param {boolean} [options.ordered=true] Execute in order or out of order
+ * @param {object} [options.writeConcern={}] Write concern for the operation
+ * @param {opResultCallback} callback A callback function
+ */
+Session.prototype.update = function(ns, ops, options, callback) {
+  this.topology.update(ns, ops, options, callback);
+}
+
+/**
+ * Perform one or more remove operations
+ * @method
+ * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
+ * @param {array} ops An array of removes
+ * @param {boolean} [options.ordered=true] Execute in order or out of order
+ * @param {object} [options.writeConcern={}] Write concern for the operation
+ * @param {opResultCallback} callback A callback function
+ */
+Session.prototype.remove = function(ns, ops, options, callback) {
+  this.topology.remove(ns, ops, options, callback);
+}
+
+/**
+ * Perform one or more remove operations
+ * @method
+ * @param {string} ns The MongoDB fully qualified namespace (ex: db1.collection1)
+ * @param {{object}|{Long}} cmd Can be either a command returning a cursor or a cursorId
+ * @param {object} [options.batchSize=0] Batchsize for the operation
+ * @param {array} [options.documents=[]] Initial documents list for cursor
+ * @param {boolean} [options.tailable=false] Tailable flag set
+ * @param {boolean} [options.oplogReply=false] oplogReply flag set
+ * @param {boolean} [options.awaitdata=false] awaitdata flag set
+ * @param {boolean} [options.exhaust=false] exhaust flag set
+ * @param {boolean} [options.partial=false] partial flag set
+ * @param {opResultCallback} callback A callback function
+ */
+Session.prototype.cursor = function(ns, cmd, options) {
+  return this.topology.cursor(ns, cmd, options);
+}  
 
 module.exports = Session;
