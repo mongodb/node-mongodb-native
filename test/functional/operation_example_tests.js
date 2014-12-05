fd54ea71c64ba0a991555dc1e4e4d871ecd418f3
Christian Kvalheim <christkv@gmail.com>
2014-12-05 17:12:25 +0100

    Added readable streams support

--- test/functional/operation_example_tests.js
+++ test/functional/operation_example_tests.js
@@ -8,121 +8,121 @@ var f = require('util').format;
  *
  *************************************************************************/
 
-// /**
-//  * Correctly call the aggregation framework using a pipeline in an Array.
-//  *
-//  * @_class collection
-//  * @_function aggregate
-//  * @ignore
-//  */
-// exports.aggregationExample1 = {
-//   // Add a tag that our runner can trigger on
-//   // in this case we are setting that node needs to be higher than 0.10.X to run
-//   metadata: { requires: { mongodb:">2.1.0", topology: ['single', 'replicaset', 'ssl', 'heap', 'wiredtiger'] } },  
+/**
+ * Correctly call the aggregation framework using a pipeline in an Array.
+ *
+ * @_class collection
+ * @_function aggregate
+ * @ignore
+ */
+exports.aggregationExample1 = {
+  // Add a tag that our runner can trigger on
+  // in this case we are setting that node needs to be higher than 0.10.X to run
+  metadata: { requires: { mongodb:">2.1.0", topology: ['single', 'replicaset', 'ssl', 'heap', 'wiredtiger'] } },  
   
-//   // The actual test we wish to run
-//   test: function(configure, test) {
-//     var db = configure.newDbInstance({w:1}, {poolSize:1});
+  // The actual test we wish to run
+  test: function(configure, test) {
+    var db = configure.newDbInstance({w:1}, {poolSize:1});
 
-//     // DOC_LINE var db = new Db('test', new Server('localhost', 27017));
-//     // DOC_START
-//     db.open(function(err, db) {
-//       // Some docs for insertion
-//       var docs = [{
-//           title : "this is my title", author : "bob", posted : new Date() ,
-//           pageViews : 5, tags : [ "fun" , "good" , "fun" ], other : { foo : 5 },
-//           comments : [
-//             { author :"joe", text : "this is cool" }, { author :"sam", text : "this is bad" }
-//           ]}];
+    // DOC_LINE var db = new Db('test', new Server('localhost', 27017));
+    // DOC_START
+    db.open(function(err, db) {
+      // Some docs for insertion
+      var docs = [{
+          title : "this is my title", author : "bob", posted : new Date() ,
+          pageViews : 5, tags : [ "fun" , "good" , "fun" ], other : { foo : 5 },
+          comments : [
+            { author :"joe", text : "this is cool" }, { author :"sam", text : "this is bad" }
+          ]}];
 
-//       // Create a collection
-//       var collection = db.collection('aggregationExample1');
-//       // Insert the docs
-//       collection.insert(docs, {w: 1}, function(err, result) {
+      // Create a collection
+      var collection = db.collection('aggregationExample1');
+      // Insert the docs
+      collection.insert(docs, {w: 1}, function(err, result) {
 
-//         // Execute aggregate, notice the pipeline is expressed as an Array
-//         collection.aggregate([
-//             { $project : {
-//               author : 1,
-//               tags : 1
-//             }},
-//             { $unwind : "$tags" },
-//             { $group : {
-//               _id : {tags : "$tags"},
-//               authors : { $addToSet : "$author" }
-//             }}
-//           ], function(err, result) {
-//             test.equal(null, err);
-//             test.equal('good', result[0]._id.tags);
-//             test.deepEqual(['bob'], result[0].authors);
-//             test.equal('fun', result[1]._id.tags);
-//             test.deepEqual(['bob'], result[1].authors);
+        // Execute aggregate, notice the pipeline is expressed as an Array
+        collection.aggregate([
+            { $project : {
+              author : 1,
+              tags : 1
+            }},
+            { $unwind : "$tags" },
+            { $group : {
+              _id : {tags : "$tags"},
+              authors : { $addToSet : "$author" }
+            }}
+          ], function(err, result) {
+            test.equal(null, err);
+            test.equal('good', result[0]._id.tags);
+            test.deepEqual(['bob'], result[0].authors);
+            test.equal('fun', result[1]._id.tags);
+            test.deepEqual(['bob'], result[1].authors);
 
-//             db.close();
-//             test.done();
-//         });
-//       });
-//     });
-//     // DOC_END
-//   }
-// }
+            db.close();
+            test.done();
+        });
+      });
+    });
+    // DOC_END
+  }
+}
 
-// /**
-//  * Correctly call the aggregation using a cursor
-//  *
-//  * @_class collection
-//  * @_function aggregate
-//  * @ignore
-//  */
-// exports.aggregationExample2 = {
-//   // Add a tag that our runner can trigger on
-//   // in this case we are setting that node needs to be higher than 0.10.X to run
-//   metadata: { requires: { mongodb:">2.1.0", topology: ['single', 'replicaset', 'ssl', 'heap', 'wiredtiger'] } },  
+/**
+ * Correctly call the aggregation using a cursor
+ *
+ * @_class collection
+ * @_function aggregate
+ * @ignore
+ */
+exports.aggregationExample2 = {
+  // Add a tag that our runner can trigger on
+  // in this case we are setting that node needs to be higher than 0.10.X to run
+  metadata: { requires: { mongodb:">2.1.0", topology: ['single', 'replicaset', 'ssl', 'heap', 'wiredtiger'] } },  
   
-//   // The actual test we wish to run
-//   test: function(configure, test) {
-//     var db = configure.newDbInstance({w:1}, {poolSize:1});
+  // The actual test we wish to run
+  test: function(configure, test) {
+    var db = configure.newDbInstance({w:1}, {poolSize:1});
 
-//     // DOC_LINE var db = new Db('test', new Server('localhost', 27017));
-//     // DOC_START
-//     db.open(function(err, db) {
-//       // Some docs for insertion
-//       var docs = [{
-//           title : "this is my title", author : "bob", posted : new Date() ,
-//           pageViews : 5, tags : [ "fun" , "good" , "fun" ], other : { foo : 5 },
-//           comments : [
-//             { author :"joe", text : "this is cool" }, { author :"sam", text : "this is bad" }
-//           ]}];
+    // DOC_LINE var db = new Db('test', new Server('localhost', 27017));
+    // DOC_START
+    db.open(function(err, db) {
+      // Some docs for insertion
+      var docs = [{
+          title : "this is my title", author : "bob", posted : new Date() ,
+          pageViews : 5, tags : [ "fun" , "good" , "fun" ], other : { foo : 5 },
+          comments : [
+            { author :"joe", text : "this is cool" }, { author :"sam", text : "this is bad" }
+          ]}];
 
-//       // Create a collection
-//       var collection = db.collection('aggregationExample2');
-//       // Insert the docs
-//       collection.insert(docs, {w: 1}, function(err, result) {
+      // Create a collection
+      var collection = db.collection('aggregationExample2');
+      // Insert the docs
+      collection.insert(docs, {w: 1}, function(err, result) {
 
-//         // Execute aggregate, notice the pipeline is expressed as an Array
-//         var cursor = collection.aggregate([
-//             { $project : {
-//               author : 1,
-//               tags : 1
-//             }},
-//             { $unwind : "$tags" },
-//             { $group : {
-//               _id : {tags : "$tags"},
-//               authors : { $addToSet : "$author" }
-//             }}
-//           ], { cursor: { batchSize: 1 } });
-//         // Get all the aggregation results
-//         cursor.get(function(err, docs) {
-//           test.equal(null, err);
-//           test.equal(2, docs.length);
-//           test.done();
-//           db.close();
-//         });
-//       });
-//     });
-//     // DOC_END
-//   }
-// }
+        // Execute aggregate, notice the pipeline is expressed as an Array
+        var cursor = collection.aggregate([
+            { $project : {
+              author : 1,
+              tags : 1
+            }},
+            { $unwind : "$tags" },
+            { $group : {
+              _id : {tags : "$tags"},
+              authors : { $addToSet : "$author" }
+            }}
+          ], { cursor: { batchSize: 1 } });
+        // Get all the aggregation results
+        cursor.get(function(err, docs) {
+          test.equal(null, err);
+          test.equal(2, docs.length);
+          test.done();
+          db.close();
+        });
+      });
+    });
+    // DOC_END
+  }
+}
 
 /**
  * Correctly call the aggregation using a cursor and toArray
