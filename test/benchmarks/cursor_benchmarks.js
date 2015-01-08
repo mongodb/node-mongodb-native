7e721492725ae9ef4a65b4ae6ce989832426771d
Christian Kvalheim <christkv@gmail.com>
2015-01-08 23:51:10 +0100

    NODE-346 adaptions to work with cursor changes in mongodb-core

--- test/benchmarks/cursor_benchmarks.js
+++ test/benchmarks/cursor_benchmarks.js
@@ -42,62 +42,100 @@ var simple_100_document_toArray = function(connection_string) {
   }
 }
 
-// var A = function() {
-//   this.execute = function() {    
-//   }
-// }
+var simple_2_document_limit_toArray = function(connection_string) {
+  return function() {
+    return {
+      db: null,
 
-// var B = function() {}
-// B.prototype.execute = function() {}
+      // Setup function, called once before tests are run
+      setup: function(callback) {
+        var self = this;
 
-// var simple_public_method_bench = function(connection_string) {
-//   return function() {
-//     return {
-//       db: null,
+        MongoClient.connect(connection_string, function(err, db) {
+          if(err) return callback(err);
+          self.db = db;
+    
+          // Drop the collection
+          db.collection('simple_2_document_limit_toArray').drop(function(err, result) {
+            // Create 100 documents
+            var docs = [];
+            for(var i = 0; i < 1000; i++) docs.push({a:1, b:'hello world', c:1});
+            // Setup the 100 documents
+            db.collection('simple_2_document_limit_toArray').insert(docs, {w:1}, callback);  
+          });
+        });
+      },
 
-//       // Setup function, called once before tests are run
-//       setup: function(callback) {
-//         callback()
-//       },
+      // Setup function, called once after test are run
+      teardown: function(callback) {
+        if(this.db != null) this.db.close(callback);
+      },
 
-//       // Setup function, called once after test are run
-//       teardown: function(callback) {
-//         callback()
-//       },
+      // Actual operation we are measuring
+      test: function(callback) {
+        this.db.collection('simple_100_document_toArray').find({}, {limit:2}).toArray(callback);
+      }
+    }
+  }
+}
 
-//       // Actual operation we are measuring
-//       test: function(callback) {
-//         new A().execute();
-//         callback()
-//       }
-//     }
-//   }
-// }
+var A = function() {
+  this.execute = function() {    
+  }
+}
 
-// var simple_protected_method_bench = function(connection_string) {
-//   return function() {
-//     return {
-//       db: null,
+var B = function() {}
+B.prototype.execute = function() {}
 
-//       // Setup function, called once before tests are run
-//       setup: function(callback) {
-//         callback()
-//       },
+var simple_public_method_bench = function(connection_string) {
+  return function() {
+    return {
+      db: null,
 
-//       // Setup function, called once after test are run
-//       teardown: function(callback) {
-//         callback()
-//       },
+      // Setup function, called once before tests are run
+      setup: function(callback) {
+        callback()
+      },
 
-//       // Actual operation we are measuring
-//       test: function(callback) {
-//         new B().execute();
-//         callback()
-//       }
-//     }
-//   }
-// }
+      // Setup function, called once after test are run
+      teardown: function(callback) {
+        callback()
+      },
 
-exports.simple_100_document_toArray = simple_100_document_toArray;
+      // Actual operation we are measuring
+      test: function(callback) {
+        new A().execute();
+        callback()
+      }
+    }
+  }
+}
+
+var simple_protected_method_bench = function(connection_string) {
+  return function() {
+    return {
+      db: null,
+
+      // Setup function, called once before tests are run
+      setup: function(callback) {
+        callback()
+      },
+
+      // Setup function, called once after test are run
+      teardown: function(callback) {
+        callback()
+      },
+
+      // Actual operation we are measuring
+      test: function(callback) {
+        new B().execute();
+        callback()
+      }
+    }
+  }
+}
+
+// exports.simple_100_document_toArray = simple_100_document_toArray;
+exports.simple_2_document_limit_toArray = simple_2_document_limit_toArray;
 // exports.simple_protected_method_bench = simple_protected_method_bench;
 // exports.simple_public_method_bench = simple_public_method_bench;
