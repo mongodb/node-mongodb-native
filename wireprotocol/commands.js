5ff371c3bcf30007dca2f26ec796aeac5a080193
Christian Kvalheim <christkv@gmail.com>
2015-06-16 22:10:38 +0200

    Refactored all wire protocol messages to leverage the benefits of the new bson serializer

--- wireprotocol/commands.js
+++ wireprotocol/commands.js
@@ -37,90 +37,92 @@ var Insert = function(requestId, ismaster, bson, ns, documents, options) {
 
 // To Binary
 Insert.prototype.toBin = function() {
-  // Calculate total length of the document
-  var length = 4 + Buffer.byteLength(this.ns) + 1 + (4 * 4);
+  // Contains all the buffers to be written
+  var buffers = [];
 
-  // Calculate the size of all documents
+  // Header buffer
+  var header = new Buffer(
+    4 * 4 // Header
+    + 4   // Flags
+    + Buffer.byteLength(this.ns) + 1 // namespace
+  );
+
+  // Add header to buffers
+  buffers.push(header);
+
+  // Total length of the message
+  var totalLength = header.length;
+
+  // Serialize all the documents
   for(var i = 0; i < this.documents.length; i++) {
-    var docsize = this.bson.calculateObjectSize(this.documents[i], this.serializeFunctions, true);
+    var buffer = this.bson.serialize(this.documents[i]
+      , this.checkKeys
+      , true
+      , this.serializeFunctions);
 
     // Document is larger than maxBsonObjectSize, terminate serialization
-    if(docsize > this.ismaster.maxBsonObjectSize) {
-      throw new MongoError("Document exceeds maximum allowed bson size of " + this.ismaster.maxBsonObjectSize + " bytes");        
+    if(buffer.length > this.ismaster.maxBsonObjectSize) {
+      throw new MongoError("Document exceeds maximum allowed bson size of " + this.ismaster.maxBsonObjectSize + " bytes");
     }
 
-    // Add to total command size
-    length += docsize;
+    // Add to total length of wire protocol message
+    totalLength = totalLength + buffer.length;
+    // Add to buffer
+    buffers.push(buffer);
   }
 
   // Command is larger than maxMessageSizeBytes terminate serialization
-  if(length > this.ismaster.maxBsonObjectSize) {
+  if(totalLength > this.ismaster.maxMessageSizeBytes) {
     throw new MongoError("Command exceeds maximum message size of " + this.ismaster.maxMessageSizeBytes + " bytes");
   }
 
-  // Create command buffer
-  var buffer = new Buffer(length);
+  // Add all the metadata
   var index = 0;
-  
+
   // Write header length
-  buffer[index + 3] = (length >> 24) & 0xff;
-  buffer[index + 2] = (length >> 16) & 0xff;
-  buffer[index + 1] = (length >> 8) & 0xff;
-  buffer[index] = (length) & 0xff;
+  header[index + 3] = (totalLength >> 24) & 0xff;
+  header[index + 2] = (totalLength >> 16) & 0xff;
+  header[index + 1] = (totalLength >> 8) & 0xff;
+  header[index] = (totalLength) & 0xff;
   index = index + 4;
 
   // Write header requestId
-  buffer[index + 3] = (this.requestId >> 24) & 0xff;
-  buffer[index + 2] = (this.requestId >> 16) & 0xff;
-  buffer[index + 1] = (this.requestId >> 8) & 0xff;
-  buffer[index] = (this.requestId) & 0xff;
+  header[index + 3] = (this.requestId >> 24) & 0xff;
+  header[index + 2] = (this.requestId >> 16) & 0xff;
+  header[index + 1] = (this.requestId >> 8) & 0xff;
+  header[index] = (this.requestId) & 0xff;
   index = index + 4;
 
   // No flags
-  buffer[index + 3] = (0 >> 24) & 0xff;
-  buffer[index + 2] = (0 >> 16) & 0xff;
-  buffer[index + 1] = (0 >> 8) & 0xff;
-  buffer[index] = (0) & 0xff;
+  header[index + 3] = (0 >> 24) & 0xff;
+  header[index + 2] = (0 >> 16) & 0xff;
+  header[index + 1] = (0 >> 8) & 0xff;
+  header[index] = (0) & 0xff;
   index = index + 4;
 
   // Operation
-  buffer[index + 3] = (OP_INSERT >> 24) & 0xff;
-  buffer[index + 2] = (OP_INSERT >> 16) & 0xff;
-  buffer[index + 1] = (OP_INSERT >> 8) & 0xff;
-  buffer[index] = (OP_INSERT) & 0xff;
+  header[index + 3] = (OP_INSERT >> 24) & 0xff;
+  header[index + 2] = (OP_INSERT >> 16) & 0xff;
+  header[index + 1] = (OP_INSERT >> 8) & 0xff;
+  header[index] = (OP_INSERT) & 0xff;
   index = index + 4;
 
   // Flags
-  buffer[index + 3] = (this.flags >> 24) & 0xff;
-  buffer[index + 2] = (this.flags >> 16) & 0xff;
-  buffer[index + 1] = (this.flags >> 8) & 0xff;
-  buffer[index] = (this.flags) & 0xff;
+  header[index + 3] = (this.flags >> 24) & 0xff;
+  header[index + 2] = (this.flags >> 16) & 0xff;
+  header[index + 1] = (this.flags >> 8) & 0xff;
+  header[index] = (this.flags) & 0xff;
   index = index + 4;
 
   // Write collection name
-  index = index + buffer.write(this.ns, index, 'utf8') + 1;
-  buffer[index - 1] = 0;
+  index = index + header.write(this.ns, index, 'utf8') + 1;
+  header[index - 1] = 0;
 
-  // Write all the bson documents to the buffer at the index offset
-  for(var i = 0; i < this.documents.length; i++) {
-    // Serialize the entry
-    var newIndex = this.bson.serializeWithBufferAndIndex(this.documents[i], this.checkKeys, buffer, index, this.serializeFunctions);
-    var docSize = newIndex - index + 1;
-    // Write the doc size
-    buffer[index + 3] = (docSize >> 24) & 0xff;
-    buffer[index + 2] = (docSize >> 16) & 0xff;
-    buffer[index + 1] = (docSize >> 8) & 0xff;
-    buffer[index] = (docSize) & 0xff;
-    // Adjust index
-    index = index + docSize;
-    // Add terminating 0 for the object
-    buffer[index - 1] = 0;      
-  }
-
-  return buffer;
+  // Return the buffers
+  return buffers;
 }
 
-var Update = function(requestId, ismaster, bson, ns, update, options) {  
+var Update = function(requestId, ismaster, bson, ns, update, options) {
   // Basic options needed to be passed in
   if(ns == null) throw new MongoError("ns must be specified for query");
 
@@ -150,88 +152,94 @@ var Update = function(requestId, ismaster, bson, ns, update, options) {
 
 // To Binary
 Update.prototype.toBin = function() {
-  // Calculate total length of the document
-  var length = (4 * 4) + 4 + Buffer.byteLength(this.ns) + 1 + 4;
+  // Contains all the buffers to be written
+  var buffers = [];
 
-  // Calculate the two object sizes
-  var qSize = this.bson.calculateObjectSize(this.q, this.serializeFunctions, true);
-  var uSize = this.bson.calculateObjectSize(this.u, this.serializeFunctions, true);
+  // Header buffer
+  var header = new Buffer(
+    4 * 4 // Header
+    + 4   // ZERO
+    + Buffer.byteLength(this.ns) + 1 // namespace
+    + 4   // Flags
+  );
 
-  // Update the length
-  length = length + qSize + uSize;
-  
-  // Create command buffer
-  var buffer = new Buffer(length);
+  // Add header to buffers
+  buffers.push(header);
+
+  // Total length of the message
+  var totalLength = header.length;
+
+  // Serialize the selector
+  var selector = this.bson.serialize(this.q
+    , this.checkKeys
+    , true
+    , this.serializeFunctions);
+  buffers.push(selector);
+  totalLength = totalLength + selector.length;
+
+  // Serialize the update
+  var update = this.bson.serialize(this.u
+    , this.checkKeys
+    , true
+    , this.serializeFunctions);
+  buffers.push(update);
+  totalLength = totalLength + update.length;
+
+  // Index in header buffer
   var index = 0;
 
   // Write header length
-  buffer[index + 3] = (length >> 24) & 0xff;
-  buffer[index + 2] = (length >> 16) & 0xff;
-  buffer[index + 1] = (length >> 8) & 0xff;
-  buffer[index] = (length) & 0xff;
+  header[index + 3] = (totalLength >> 24) & 0xff;
+  header[index + 2] = (totalLength >> 16) & 0xff;
+  header[index + 1] = (totalLength >> 8) & 0xff;
+  header[index] = (totalLength) & 0xff;
   index = index + 4;
 
   // Write header requestId
-  buffer[index + 3] = (this.requestId >> 24) & 0xff;
-  buffer[index + 2] = (this.requestId >> 16) & 0xff;
-  buffer[index + 1] = (this.requestId >> 8) & 0xff;
-  buffer[index] = (this.requestId) & 0xff;
+  header[index + 3] = (this.requestId >> 24) & 0xff;
+  header[index + 2] = (this.requestId >> 16) & 0xff;
+  header[index + 1] = (this.requestId >> 8) & 0xff;
+  header[index] = (this.requestId) & 0xff;
   index = index + 4;
 
   // No flags
-  buffer[index + 3] = (0 >> 24) & 0xff;
-  buffer[index + 2] = (0 >> 16) & 0xff;
-  buffer[index + 1] = (0 >> 8) & 0xff;
-  buffer[index] = (0) & 0xff;
+  header[index + 3] = (0 >> 24) & 0xff;
+  header[index + 2] = (0 >> 16) & 0xff;
+  header[index + 1] = (0 >> 8) & 0xff;
+  header[index] = (0) & 0xff;
   index = index + 4;
 
   // Operation
-  buffer[index + 3] = (OP_UPDATE >> 24) & 0xff;
-  buffer[index + 2] = (OP_UPDATE >> 16) & 0xff;
-  buffer[index + 1] = (OP_UPDATE >> 8) & 0xff;
-  buffer[index] = (OP_UPDATE) & 0xff;
+  header[index + 3] = (OP_UPDATE >> 24) & 0xff;
+  header[index + 2] = (OP_UPDATE >> 16) & 0xff;
+  header[index + 1] = (OP_UPDATE >> 8) & 0xff;
+  header[index] = (OP_UPDATE) & 0xff;
   index = index + 4;
 
   // Write ZERO
-  buffer[index + 3] = (0 >> 24) & 0xff;
-  buffer[index + 2] = (0 >> 16) & 0xff;
-  buffer[index + 1] = (0 >> 8) & 0xff;
-  buffer[index] = (0) & 0xff;
+  header[index + 3] = (0 >> 24) & 0xff;
+  header[index + 2] = (0 >> 16) & 0xff;
+  header[index + 1] = (0 >> 8) & 0xff;
+  header[index] = (0) & 0xff;
   index = index + 4;
 
   // Write collection name
-  index = index + buffer.write(this.ns, index, 'utf8') + 1;
-  buffer[index - 1] = 0;
+  index = index + header.write(this.ns, index, 'utf8') + 1;
+  header[index - 1] = 0;
 
   // Flags
-  buffer[index + 3] = (this.flags >> 24) & 0xff;
-  buffer[index + 2] = (this.flags >> 16) & 0xff;
-  buffer[index + 1] = (this.flags >> 8) & 0xff;
-  buffer[index] = (this.flags) & 0xff;
+  header[index + 3] = (this.flags >> 24) & 0xff;
+  header[index + 2] = (this.flags >> 16) & 0xff;
+  header[index + 1] = (this.flags >> 8) & 0xff;
+  header[index] = (this.flags) & 0xff;
   index = index + 4;
 
-  // Serialize the selector
-  var length = this.bson.serializeWithBufferAndIndex(this.q, this.checkKeys, buffer, index, this.serializeFunctions) - index + 1;
-  buffer[index + 3] = (length >> 24) & 0xff;
-  buffer[index + 2] = (length >> 16) & 0xff;
-  buffer[index + 1] = (length >> 8) & 0xff;
-  buffer[index] = (length) & 0xff;
-  index = index + length;
-
-  // Serialize the update statement
-  length = this.bson.serializeWithBufferAndIndex(this.u, false, buffer, index, this.serializeFunctions) - index + 1;
-  buffer[index + 3] = (length >> 24) & 0xff;
-  buffer[index + 2] = (length >> 16) & 0xff;
-  buffer[index + 1] = (length >> 8) & 0xff;
-  buffer[index] = (length) & 0xff;  
-  index = index + length;
-
-  // Return the buffer
-  return buffer;
+  // Return the buffers
+  return buffers;
 }
 
 
-var Remove = function(requestId, ismaster, bson, ns, remove, options) {  
+var Remove = function(requestId, ismaster, bson, ns, remove, options) {
   // Basic options needed to be passed in
   if(ns == null) throw new MongoError("ns must be specified for query");
 
@@ -258,74 +266,82 @@ var Remove = function(requestId, ismaster, bson, ns, remove, options) {
 
 // To Binary
 Remove.prototype.toBin = function() {
-  // Calculate total length of the document
-  var length = (4 * 4) + 4 + Buffer.byteLength(this.ns) + 1 + 4;
+  // Contains all the buffers to be written
+  var buffers = [];
 
-  // Calculate the two object sizes
-  var qSize = this.bson.calculateObjectSize(this.q, this.serializeFunctions, true);
+  // Header buffer
+  var header = new Buffer(
+    4 * 4 // Header
+    + 4   // ZERO
+    + Buffer.byteLength(this.ns) + 1 // namespace
+    + 4   // Flags
+  );
 
-  // Update the length
-  length = length + qSize;
-  // Create command buffer
-  var buffer = new Buffer(length);
+  // Add header to buffers
+  buffers.push(header);
+
+  // Total length of the message
+  var totalLength = header.length;
+
+  // Serialize the selector
+  var selector = this.bson.serialize(this.q
+    , this.checkKeys
+    , true
+    , this.serializeFunctions);
+  buffers.push(selector);
+  totalLength = totalLength + selector.length;
+
+  // Index in header buffer
   var index = 0;
 
   // Write header length
-  buffer[index + 3] = (length >> 24) & 0xff;
-  buffer[index + 2] = (length >> 16) & 0xff;
-  buffer[index + 1] = (length >> 8) & 0xff;
-  buffer[index] = (length) & 0xff;
+  header[index + 3] = (totalLength >> 24) & 0xff;
+  header[index + 2] = (totalLength >> 16) & 0xff;
+  header[index + 1] = (totalLength >> 8) & 0xff;
+  header[index] = (totalLength) & 0xff;
   index = index + 4;
 
   // Write header requestId
-  buffer[index + 3] = (this.requestId >> 24) & 0xff;
-  buffer[index + 2] = (this.requestId >> 16) & 0xff;
-  buffer[index + 1] = (this.requestId >> 8) & 0xff;
-  buffer[index] = (this.requestId) & 0xff;
+  header[index + 3] = (this.requestId >> 24) & 0xff;
+  header[index + 2] = (this.requestId >> 16) & 0xff;
+  header[index + 1] = (this.requestId >> 8) & 0xff;
+  header[index] = (this.requestId) & 0xff;
   index = index + 4;
 
   // No flags
-  buffer[index + 3] = (0 >> 24) & 0xff;
-  buffer[index + 2] = (0 >> 16) & 0xff;
-  buffer[index + 1] = (0 >> 8) & 0xff;
-  buffer[index] = (0) & 0xff;
+  header[index + 3] = (0 >> 24) & 0xff;
+  header[index + 2] = (0 >> 16) & 0xff;
+  header[index + 1] = (0 >> 8) & 0xff;
+  header[index] = (0) & 0xff;
   index = index + 4;
 
   // Operation
-  buffer[index + 3] = (OP_DELETE >> 24) & 0xff;
-  buffer[index + 2] = (OP_DELETE >> 16) & 0xff;
-  buffer[index + 1] = (OP_DELETE >> 8) & 0xff;
-  buffer[index] = (OP_DELETE) & 0xff;
+  header[index + 3] = (OP_DELETE >> 24) & 0xff;
+  header[index + 2] = (OP_DELETE >> 16) & 0xff;
+  header[index + 1] = (OP_DELETE >> 8) & 0xff;
+  header[index] = (OP_DELETE) & 0xff;
   index = index + 4;
 
   // Write ZERO
-  buffer[index + 3] = (0 >> 24) & 0xff;
-  buffer[index + 2] = (0 >> 16) & 0xff;
-  buffer[index + 1] = (0 >> 8) & 0xff;
-  buffer[index] = (0) & 0xff;
+  header[index + 3] = (0 >> 24) & 0xff;
+  header[index + 2] = (0 >> 16) & 0xff;
+  header[index + 1] = (0 >> 8) & 0xff;
+  header[index] = (0) & 0xff;
   index = index + 4;
 
   // Write collection name
-  index = index + buffer.write(this.ns, index, 'utf8') + 1;
-  buffer[index - 1] = 0;
+  index = index + header.write(this.ns, index, 'utf8') + 1;
+  header[index - 1] = 0;
 
   // Write ZERO
-  buffer[index + 3] = (this.flags >> 24) & 0xff;
-  buffer[index + 2] = (this.flags >> 16) & 0xff;
-  buffer[index + 1] = (this.flags >> 8) & 0xff;
-  buffer[index] = (this.flags) & 0xff;
+  header[index + 3] = (this.flags >> 24) & 0xff;
+  header[index + 2] = (this.flags >> 16) & 0xff;
+  header[index + 1] = (this.flags >> 8) & 0xff;
+  header[index] = (this.flags) & 0xff;
   index = index + 4;
 
-  // Serialize the selector
-  var length = this.bson.serializeWithBufferAndIndex(this.q, this.checkKeys, buffer, index, this.serializeFunctions) - index + 1;
-  buffer[index + 3] = (length >> 24) & 0xff;
-  buffer[index + 2] = (length >> 16) & 0xff;
-  buffer[index + 1] = (length >> 8) & 0xff;
-  buffer[index] = (length) & 0xff;  
-  index = index + length;
-
-  // Return the buffer
-  return buffer;
+  // Return the buffers
+  return buffers;
 }
 
 module.exports = {
