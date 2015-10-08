c06f14931e3e90806f81355ea7148648136c50c2
Christian Kvalheim <christkv@gmail.com>
2015-10-08 11:52:34 +0200

    added nested document insert made of references

--- test/functional/insert_tests.js
+++ test/functional/insert_tests.js
@@ -2191,6 +2191,46 @@ exports['Correctly allow forceServerObjectId for insertMany'] = {
   }
 }
 
+exports['Insert document including sub documents'] = {
+  metadata: { requires: { topology: ['single'] } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var db = configuration.newDbInstance({w:1}, {poolSize:1, auto_reconnect:false});
+    db.open(function(err, db) {
+      test.equal(null, err);
+
+      var shipment = {
+        shipment1: 'a'
+      }
+
+      var supplier = {
+        shipments: [shipment]
+      }
+
+      var product = {
+        suppliers: [supplier]
+      }
+
+      var doc = {
+        a: 1, products: [product]
+      }
+
+      db.collection('sub_documents').insertOne(doc, function(err, r) {
+        test.equal(null, err);
+        
+        db.collection('sub_documents').find({}).next(function(err, v) {
+          test.equal(null, err);
+          test.equal('a', v.products[0].suppliers[0].shipments[0].shipment1);
+
+          db.close();
+          test.done();
+        });
+      });
+    });
+  }
+}
+
 
 
 
