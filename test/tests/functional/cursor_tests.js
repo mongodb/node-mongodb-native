024375781b7c8992644d1d22e2e75c5c8fe562e0
Valeri Karpov <valkar207@gmail.com>
2015-10-27 14:49:29 -0400

    repro; Automattic/mongoose#3493

--- test/tests/functional/cursor_tests.js
+++ test/tests/functional/cursor_tests.js
@@ -39,7 +39,7 @@ exports['Should iterate cursor'] = {
               test.equal(null, err);
               test.equal(2, d.a);
               test.equal(0, cursor.bufferedCount());
-              // Destroy the server connection        
+              // Destroy the server connection
               _server.destroy();
               // Finish the test
               test.done();
@@ -94,7 +94,7 @@ exports['Should iterate cursor but readBuffered'] = {
               test.equal(null, err);
               test.equal(null, d);
 
-              // Destroy the server connection        
+              // Destroy the server connection
               _server.destroy();
               // Finish the test
               test.done();
@@ -145,7 +145,7 @@ exports['Should callback exhausted cursor with error'] = {
               cursor.next(function(err, d) {
                 test.ok(err);
                 test.equal(null, d);
-                // Destroy the server connection        
+                // Destroy the server connection
                 _server.destroy();
                 // Finish the test
                 test.done();
@@ -154,6 +154,63 @@ exports['Should callback exhausted cursor with error'] = {
           });
         });
       })
+
+      // Start connection
+      server.connect();
+    });
+  }
+};
+
+exports['Handles overflow sort error when exhausting cursor'] = {
+  metadata: {
+    requires: {
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    configuration.newTopology(function(err, server) {
+      var ns = f("%s.cursor4", configuration.db);
+      // Add event listeners
+      server.on('connect', function(_server) {
+        var totalCount = 1024;
+        var numOutstanding = totalCount;
+        for (var i = 0; i < totalCount; ++i) {
+          var doc = { f: require('crypto').pseudoRandomBytes(32 * totalCount) };
+          _server.insert(ns, doc, { w: 1 }, function(error) {
+            test.equal(error, null);
+            if (!--totalCount) {
+              doQuery();
+            }
+          });
+        }
+
+        var numInCursor = 0;
+        var exhaust = function(cursor, callback) {
+          cursor.next(function(error, res) {
+            if (error) {
+              return callback(error);
+            }
+            if (!res) {
+              return callback(null);
+            }
+            exhaust(cursor, callback);
+          });
+        };
+
+        var doQuery = function() {
+          var sort = { f: 1, _id: 1 }
+          var cursor = _server.cursor(ns,
+            { find: ns, query: {}, batchSize: totalCount, sort: sort });
+          exhaust(cursor, function(error) {
+            test.ok(error);
+            var message = error.toString();
+            test.ok(message.indexOf('Overflow sort stage') !== -1);
+            _server.destroy();
+            test.done();
+          });
+        };
+      });
 
       // Start connection
       server.connect();
