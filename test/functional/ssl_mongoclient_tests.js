66f5d6efbe03df90c0173b88bcce407a79eaf644
Christian Kvalheim <christkv@gmail.com>
2015-12-17 10:56:19 +0100

    updated default detection MongoClient timeouts to 120 sec

--- test/functional/ssl_mongoclient_tests.js
+++ test/functional/ssl_mongoclient_tests.js
@@ -763,3 +763,111 @@ exports['should correctly connect using ssl with sslValidation turned off'] = {
     });
   }
 }
+
+/**
+ * @ignore
+ */
+exports['should correctly connect using SSL to replicaset with requireSSL'] = {
+  metadata: { requires: { topology: 'ssl' } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var ReplSetManager = require('mongodb-topology-manager').ReplSet
+      , MongoClient = configuration.require.MongoClient;
+
+    // All inserted docs
+    var docs = [];
+    var errs = [];
+    var insertDocs = [];
+
+    // Read the ca
+    var ca = [fs.readFileSync(__dirname + "/ssl/ca.pem")];
+    var cert = fs.readFileSync(__dirname + "/ssl/client.pem");
+    var key = fs.readFileSync(__dirname + "/ssl/client.pem");
+    var replicasetManager = new ReplSetManager('mongod', [{
+      options: {
+        bind_ip: 'server', port: 31000,
+        dbpath: f('%s/../db/31000', __dirname),
+        sslPEMKeyFile: __dirname + "/ssl/server.pem",
+        sslMode: 'requireSSL'
+      }
+    }, {
+      options: {
+        bind_ip: 'server', port: 31001,
+        dbpath: f('%s/../db/31001', __dirname),
+        sslPEMKeyFile: __dirname + "/ssl/server.pem",
+        sslMode: 'requireSSL'
+      }
+    }, {
+      options: {
+        bind_ip: 'server', port: 31002,
+        dbpath: f('%s/../db/31002', __dirname),
+        sslPEMKeyFile: __dirname + "/ssl/server.pem",
+        sslMode: 'requireSSL'
+      }
+    }], {
+      replSet: 'rs', ssl:true, rejectUnauthorized: false, ca: ca, host: 'server'
+    });
+
+    replicasetManager.purge().then(function() {
+      // Start the server
+      replicasetManager.start().then(function() {
+        setTimeout(function() {
+          // Connect and validate the server certificate
+          MongoClient.connect("mongodb://server:31000,server:31001,server:31002/test?replicaSet=rs", {
+            replSet: {
+                ssl:true
+              , sslKey: key
+              , sslCert: cert
+              , sslCA:ca
+            }
+          }, function(err, db) {
+            if(err) console.dir(err)
+            test.equal(null, err);
+            test.ok(db != null);
+            var sets = [{}];
+
+            var interval = setInterval(function() {
+              db.command({ismaster:true}, {readPreference:'nearest', full:true}, function(e, r) {
+                // Add seen servers to list
+                if(r) {
+                  sets[sets.length - 1][r.connection.port] = true;
+                }
+              });
+            }, 100)
+
+            setTimeout(function() {
+              // Force a reconnect of a server
+              var secondary = db.serverConfig.s.replset.state.secondaries[0]
+              secondary.destroy(true, false);
+              sets.push({});
+
+              db.serverConfig.once('joined', function(t, o, s) {
+                setTimeout(function() {
+                  clearInterval(interval);
+
+                  test.ok(sets[0][31000]);
+                  test.ok(sets[0][31001]);
+                  test.ok(sets[0][31002]);
+
+                  test.ok(sets[1][31000]);
+                  test.ok(sets[1][31001]);
+                  test.ok(sets[1][31002]);
+
+                  db.close();
+
+                  replicasetManager.stop().then(function() {
+                    test.done();
+                  });
+                }, 1000);
+              });
+            }, 2000)
+          });
+        });
+      }, 10000).catch(function(err) {
+        console.dir(err)
+        process.exit(0)
+      });
+    });
+  }
+}
