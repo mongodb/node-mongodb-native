1cb0583848390f4117079acd20be0064b16b0bd0
Christian Kvalheim <christkv@gmail.com>
2016-01-28 11:04:13 +0100

    fixes to make 2.4 work with worker queue

--- functional/pool_growth_shrink_tests.js
+++ functional/pool_growth_shrink_tests.js
@@ -20,11 +20,12 @@ exports['Example of simple parallel insert into db'] = {
       , port: configuration.port
       , reconnect: true
       , reconnectInterval: 50
+      , size: 3
     })
 
     // Number of operations done
     var numberOfOpsDone = 0;
-    var numberOfPoolConnectionExpansion = 0;
+    var numberOfPoolConnectionExpansion = 1;
 
     // LINE var Server = require('mongodb-core').Server,
     // LINE   test = require('assert');
@@ -33,55 +34,27 @@ exports['Example of simple parallel insert into db'] = {
     // BEGIN
     // Add event listeners
     server.on('connect', function(_server) {
-      _server.s.pool.once('connection', function() {
+      _server.s.pool.on('connection', function() {
         numberOfPoolConnectionExpansion = numberOfPoolConnectionExpansion + 1;
       });
 
+      var left = 100;
+      for(var i = 0; i < 100; i++) {
+        // Execute the insert
+        _server.insert('integration_tests.inserts_example1', [{a:i}], {
+          writeConcern: {w:1}, ordered:true
+        }, function(err, results) {
+          test.equal(null, err);
+          test.equal(1, results.result.n);
 
-      // Execute the insert
-      _server.insert('integration_tests.inserts_example1', [{a:1}], {
-        writeConcern: {w:1}, ordered:true
-      }, function(err, results) {
-        test.equal(null, err);
-        test.equal(1, results.result.n);
-        numberOfOpsDone = numberOfOpsDone + 1;
-
-        if(numberOfOpsDone == 3) {
-          test.ok(numberOfPoolConnectionExpansion > 0);
-          _server.destroy();
-          test.done();          
-        }
-      });
-
-      // Execute the insert
-      _server.insert('integration_tests.inserts_example1', [{a:1}], {
-        writeConcern: {w:1}, ordered:true
-      }, function(err, results) {
-        test.equal(null, err);
-        test.equal(1, results.result.n);
-        numberOfOpsDone = numberOfOpsDone + 1;
-
-        if(numberOfOpsDone == 3) {
-          test.ok(numberOfPoolConnectionExpansion > 0);
-          _server.destroy();
-          test.done();          
-        }
-      });
-
-      // Execute the insert
-      _server.insert('integration_tests.inserts_example1', [{a:1}], {
-        writeConcern: {w:1}, ordered:true
-      }, function(err, results) {
-        test.equal(null, err);
-        test.equal(1, results.result.n);
-        numberOfOpsDone = numberOfOpsDone + 1;
-
-        if(numberOfOpsDone == 3) {
-          test.ok(numberOfPoolConnectionExpansion > 0);
-          _server.destroy();
-          test.done();          
-        }
-      });
+          left = left - 1;
+          if(left == 0) {
+            test.equal(3, numberOfPoolConnectionExpansion);
+            _server.destroy();
+            test.done();
+          }
+        });        
+      }
     });
 
     // Start connection
