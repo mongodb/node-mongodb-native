b21d877870ac2c3b0ca2d836699139633a2ad7ac
Christian Kvalheim <christkv@gmail.com>
2016-01-28 15:41:47 +0100

    working on single socket failures not causing full connection shutdown

--- functional/pool_growth_shrink_tests.js
+++ functional/pool_growth_shrink_tests.js
@@ -3,7 +3,7 @@
 /**
  * Correctly cause pool to grow
  */
-exports['Example of simple parallel insert into db'] = {
+exports['Poolsize should grown as concurrent operations are executed'] = {
   metadata: {
     requires: {
       topology: "single"
@@ -54,6 +54,76 @@ exports['Example of simple parallel insert into db'] = {
             test.done();
           }
         });        
+      }
+    });
+
+    // Start connection
+    server.connect();
+    // END
+  }
+}
+
+/**
+ * Correctly cause pool to grow
+ */
+exports['Destroyed connection should only affect operations on the particular connection'] = {
+  metadata: {
+    requires: {
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    var Server = configuration.require.Server
+      , ReadPreference = configuration.require.ReadPreference;
+
+    // Attempt to connect
+    var server = new Server({
+        host: configuration.host
+      , port: configuration.port
+      , reconnect: true
+      , reconnectInterval: 50
+      , size: 3
+    })
+
+    // Number of operations done
+    var numberOfOpsDone = 0;
+    var numberOfPoolConnectionExpansion = 1;
+
+    // LINE var Server = require('mongodb-core').Server,
+    // LINE   test = require('assert');
+    // LINE var server = new Server({host: 'localhost', port: 27017});
+    // REMOVE-LINE test.done();
+    // BEGIN
+    // Add event listeners
+    server.on('connect', function(_server) {
+      _server.s.pool.on('connection', function() {
+        numberOfPoolConnectionExpansion = numberOfPoolConnectionExpansion + 1;
+      });
+
+      var left = 10;
+      for(var i = 0; i < 10; i++) {
+        // Execute the insert
+        _server.insert('integration_tests.inserts_example1', [{a:i}], {
+          writeConcern: {w:1}, ordered:true
+        }, function(err, results) {
+          console.log("------------- op returned")
+          console.dir(err)
+          // test.equal(null, err);
+          // test.equal(1, results.result.n);
+
+          left = left - 1;
+          if(left == 0) {
+            _server.destroy();
+            test.done();
+          }
+        });
+
+        // Destroy a connection
+        if(i == 2) {
+          var connections = _server.s.pool.getAll();
+          connections[0].connection.write('§§12§3e1e213123213123')
+        }
       }
     });
 
