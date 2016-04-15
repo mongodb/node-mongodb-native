6adf29f9597cd94e3ec01bec15f38b52ebb3e8c5
Christian Kvalheim <christkv@gmail.com>
2016-04-15 09:46:40 +0200

    Better handling of unique createIndex error

--- test/functional/index_tests.js
+++ test/functional/index_tests.js
@@ -929,6 +929,38 @@ exports['should correctly create index using . keys'] = {
   }
 }
 
+/**
+ * @ignore
+ */
+exports['error on duplicate key index'] = {
+  metadata: { requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var db = configuration.newDbInstance(configuration.writeConcernMax(), {poolSize:1});
+    db.open(function(err, db) {
+      test.equal(null, err);
+      var collection = db.collection('embedded_key_indes_2');
+      collection.insertMany([{
+        key: { external_id: 1, type: 1}
+      }, {
+        key: { external_id: 1, type: 1}
+      }], function(err, r) {
+        test.equal(null, err);
+        collection.createIndex(
+            { 'key.external_id': 1, 'key.type': 1 },
+            { unique: true, sparse: true, name: 'indexname'},
+        function(err, r) {
+          test.equal(11000, err.code);
+
+          db.close();
+          test.done();
+        });
+      });
+    });
+  }
+}
+
 // /**
 //  * @ignore
 //  */
