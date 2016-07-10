5a5f155cbf9d29103cd25263bdc1943d79ba2c83
Christian Kvalheim <christkv@gmail.com>
2016-07-10 18:27:08 +0200

    handle situation where user is providing seedlist names that do not match host list. fix allows for a single full discovery connection sweep before erroring out

--- functional/replset_state_tests.js
+++ functional/replset_state_tests.js
@@ -15,7 +15,10 @@ exports['Should correctly execute state machine tests'] = {
       return x.indexOf('.json') != -1;
     })
     // .filter(function(x) {
-    //   return x.indexOf('primary_changes_set_name.json') != -1;
+    //   return x.indexOf('use_setversion_without_electionid.json') != -1;
+    // });
+    // .filter(function(x) {
+    //   return x.indexOf('use_setversion_without_electionid.json') == -1;
     // });
     // console.dir(entries)
 
@@ -146,11 +149,11 @@ function executePhase(test, state, phase) {
 
     // test.deepEqual(outcome.servers[name], state.set[name]);
   } catch(e) {
-    // console.log("========================== 0")
-    // console.dir(outcome.servers)
-    // console.log("========================== 1")
-    // console.dir(state.set)
-    // process.exit(0)
+    console.log("========================== 0")
+    console.dir(outcome.servers)
+    console.log("========================== 1")
+    console.dir(state.set)
+    process.exit(0)
   }
     // console.log("========================== 2")
   }
