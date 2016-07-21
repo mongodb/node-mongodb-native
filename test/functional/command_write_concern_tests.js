59522f8cea868e1b5de14a2251b0a328ed7523bc
Christian Kvalheim <christkv@gmail.com>
2016-07-21 12:23:28 +0200

    Added addUser and removeUser write concern mock tests

--- test/functional/command_write_concern_tests.js
+++ test/functional/command_write_concern_tests.js
@@ -883,3 +883,249 @@ exports['Successfully pass through writeConcern to dropIndexes command'] = {
     });
   }
 }
+
+exports['Successfully pass through writeConcern to createUser command'] = {
+  metadata: {
+    requires: {
+      generators: true,
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    var MongoClient = configuration.require.MongoClient,
+      ObjectId = configuration.require.ObjectId,
+      ReadPreference = configuration.require.ReadPreference,
+      Long = configuration.require.Long,
+      Code = configuration.require.Code,
+      co = require('co'),
+      mockupdb = require('../mock');
+
+    // Contain mock server
+    var primaryServer = null;
+    var firstSecondaryServer = null;
+    var arbiterServer = null;
+    var running = true;
+    var electionIds = [new ObjectId(), new ObjectId()];
+
+    // Default message fields
+    var defaultFields = {
+      "setName": "rs", "setVersion": 1, "electionId": electionIds[0],
+      "maxBsonObjectSize" : 16777216, "maxMessageSizeBytes" : 48000000,
+      "maxWriteBatchSize" : 1000, "localTime" : new Date(), "maxWireVersion" : 5,
+      "minWireVersion" : 0, "ok" : 1, "hosts": ["localhost:32000", "localhost:32001", "localhost:32002"], "arbiters": ["localhost:32002"]
+    }
+
+    // Primary server states
+    var primary = [extend(defaultFields, {
+      "ismaster":true, "secondary":false, "me": "localhost:32000", "primary": "localhost:32000", "tags" : { "loc" : "ny" }
+    })];
+
+    // Primary server states
+    var firstSecondary = [extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32001", "primary": "localhost:32000", "tags" : { "loc" : "sf" }
+    })];
+
+    // Primary server states
+    var arbiter = [extend(defaultFields, {
+      "ismaster":false, "secondary":false, "arbiterOnly": true, "me": "localhost:32002", "primary": "localhost:32000"
+    })];
+
+    // Boot the mock
+    co(function*() {
+      primaryServer = yield mockupdb.createServer(32000, 'localhost');
+      firstSecondaryServer = yield mockupdb.createServer(32001, 'localhost');
+      arbiterServer = yield mockupdb.createServer(32002, 'localhost');
+
+      // Primary state machine
+      co(function*() {
+        while(running) {
+          var request = yield primaryServer.receive();
+          var doc = request.document;
+          // console.log("========================== cmd")
+          // console.dir(doc)
+
+          if(doc.ismaster) {
+            request.reply(primary[0]);
+          } else if(doc.createUser) {
+            commandResult = doc;
+            request.reply({ok:1});
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.stack);
+      });
+
+      // First secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield firstSecondaryServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(firstSecondary[0]);
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.stack);
+      });
+
+      // Second secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield arbiterServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(arbiter[0]);
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.stack);
+      });
+    });
+
+    var commandResult = null;
+
+    // Connect to the mocks
+    MongoClient.connect('mongodb://localhost:32000,localhost:32001,localhost:32002/test?replicaSet=rs', function(err, db) {
+      test.equal(null, err);
+
+      db.admin().addUser('kay:kay', 'abc123', {w:2, wtimeout:1000}, function(err, result) {
+        test.equal(null, err);
+        test.deepEqual({ w: 2, wtimeout: 1000 }, commandResult.writeConcern);
+
+        primaryServer.destroy();
+        firstSecondaryServer.destroy();
+        arbiterServer.destroy();
+        running = false;
+
+        db.close();
+        test.done();
+      });
+    });
+  }
+}
+
+exports['Successfully pass through writeConcern to dropUser command'] = {
+  metadata: {
+    requires: {
+      generators: true,
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    var MongoClient = configuration.require.MongoClient,
+      ObjectId = configuration.require.ObjectId,
+      ReadPreference = configuration.require.ReadPreference,
+      Long = configuration.require.Long,
+      Code = configuration.require.Code,
+      co = require('co'),
+      mockupdb = require('../mock');
+
+    // Contain mock server
+    var primaryServer = null;
+    var firstSecondaryServer = null;
+    var arbiterServer = null;
+    var running = true;
+    var electionIds = [new ObjectId(), new ObjectId()];
+
+    // Default message fields
+    var defaultFields = {
+      "setName": "rs", "setVersion": 1, "electionId": electionIds[0],
+      "maxBsonObjectSize" : 16777216, "maxMessageSizeBytes" : 48000000,
+      "maxWriteBatchSize" : 1000, "localTime" : new Date(), "maxWireVersion" : 5,
+      "minWireVersion" : 0, "ok" : 1, "hosts": ["localhost:32000", "localhost:32001", "localhost:32002"], "arbiters": ["localhost:32002"]
+    }
+
+    // Primary server states
+    var primary = [extend(defaultFields, {
+      "ismaster":true, "secondary":false, "me": "localhost:32000", "primary": "localhost:32000", "tags" : { "loc" : "ny" }
+    })];
+
+    // Primary server states
+    var firstSecondary = [extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32001", "primary": "localhost:32000", "tags" : { "loc" : "sf" }
+    })];
+
+    // Primary server states
+    var arbiter = [extend(defaultFields, {
+      "ismaster":false, "secondary":false, "arbiterOnly": true, "me": "localhost:32002", "primary": "localhost:32000"
+    })];
+
+    // Boot the mock
+    co(function*() {
+      primaryServer = yield mockupdb.createServer(32000, 'localhost');
+      firstSecondaryServer = yield mockupdb.createServer(32001, 'localhost');
+      arbiterServer = yield mockupdb.createServer(32002, 'localhost');
+
+      // Primary state machine
+      co(function*() {
+        while(running) {
+          var request = yield primaryServer.receive();
+          var doc = request.document;
+          // console.log("========================== cmd")
+          // console.dir(doc)
+
+          if(doc.ismaster) {
+            request.reply(primary[0]);
+          } else if(doc.dropUser) {
+            commandResult = doc;
+            request.reply({ok:1});
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.stack);
+      });
+
+      // First secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield firstSecondaryServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(firstSecondary[0]);
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.stack);
+      });
+
+      // Second secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield arbiterServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(arbiter[0]);
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.stack);
+      });
+    });
+
+    var commandResult = null;
+
+    // Connect to the mocks
+    MongoClient.connect('mongodb://localhost:32000,localhost:32001,localhost:32002/test?replicaSet=rs', function(err, db) {
+      test.equal(null, err);
+
+      db.admin().removeUser('kay:kay', {w:2, wtimeout:1000}, function(err, result) {
+        test.equal(null, err);
+        test.deepEqual({ w: 2, wtimeout: 1000 }, commandResult.writeConcern);
+
+        primaryServer.destroy();
+        firstSecondaryServer.destroy();
+        arbiterServer.destroy();
+        running = false;
+
+        db.close();
+        test.done();
+      });
+    });
+  }
+}
