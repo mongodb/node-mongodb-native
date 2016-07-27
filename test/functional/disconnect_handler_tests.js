64992e1be383dee266e04346547e3e1f1341d726
Christian Kvalheim <christkv@gmail.com>
2016-07-27 21:29:47 +0200

    added some minor tests for single server/replicaset for disconnect

---
+++ test/functional/disconnect_handler_tests.js
@@ -0,0 +1,33 @@
+/**
+ * @ignore
+ */
+exports['Should correctly recover when bufferMaxEntries: -1 and restart'] = {
+  metadata: { requires: { topology: ['single', 'replicaset'] }, ignore: { travis:true } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var MongoClient = configuration.require.MongoClient
+      , f = require('util').format;
+
+    var done = false;
+
+    MongoClient.connect(configuration.url(), {}, function(err, db) {
+      test.equal(null, err);
+
+      configuration.manager.stop(9).then(function() {
+        db.collection('disconnect_handler_tests').update({a:1}, {$set: {b:1}}, function(err, r) {
+          test.equal(null, err);
+          test.equal(0, r.result.nModified);
+
+          db.close();
+          test.done();
+        });
+
+        setTimeout(function() {
+          configuration.manager.restart(9, {waitMS: 5000}).then(function() {
+          });
+        }, 5000)
+      });
+    });
+  }
+}
