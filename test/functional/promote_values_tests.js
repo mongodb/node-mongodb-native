cd783aa975bb1a324a1e0ac9ee21d95612c9f2b6
Christian Kvalheim <christkv@gmail.com>
2016-08-25 10:56:09 +0200

    added tags for promoteValues/promoteLong/raw to find/findOne and aggregate

--- test/functional/promote_values_tests.js
+++ test/functional/promote_values_tests.js
@@ -147,3 +147,40 @@ exports['should correctly honor promoteValues at cursor find level'] = {
     });
   }
 }
+
+exports['should correctly honor promoteValues at aggregate level'] = {
+  // Add a tag that our runner can trigger on
+  // in this case we are setting that node needs to be higher than 0.10.X to run
+  metadata: { requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var Long = configuration.require.Long,
+      Int32 = configuration.require.Int32,
+      Double = configuration.require.Double,
+      MongoClient = configuration.require.MongoClient;
+
+    MongoClient.connect(configuration.url(), {
+    }, function(err, db) {
+      db.collection('shouldCorrectlyHonorPromoteValues2').insert({
+            doc: Long.fromNumber(10)
+          , int: 10
+          , double: 2.2222
+          , array: [[Long.fromNumber(10)]]
+        }, function(err, doc) {
+          test.equal(null, err);
+
+          db.collection('shouldCorrectlyHonorPromoteValues2').aggregate([{$match: {}}], {promoteValues: false}).next(function(err, doc) {
+            test.equal(null, err);
+
+            test.deepEqual(Long.fromNumber(10), doc.doc);
+            test.deepEqual(new Int32(10), doc.int);
+            test.deepEqual(new Double(2.2222), doc.double);
+
+            db.close();
+            test.done();
+          });
+      });
+    });
+  }
+}
