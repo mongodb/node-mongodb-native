9554f5969978f4d3470d1967e7aab1c261fc766b
Christian Kvalheim <christkv@gmail.com>
2016-08-29 11:59:27 +0200

    minor fix for promoteBuffers

--- test/functional/promote_buffers_tests.js
+++ test/functional/promote_buffers_tests.js
@@ -129,7 +129,7 @@ exports['should correctly honor promoteBuffers at cursor find level'] = {
 exports['should correctly honor promoteBuffers at aggregate level'] = {
   // Add a tag that our runner can trigger on
   // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: { requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] } },
+  metadata: { requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'], mongodb:">=2.4.0" } },
 
   // The actual test we wish to run
   test: function(configuration, test) {
