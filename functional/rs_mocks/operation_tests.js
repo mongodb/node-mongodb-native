dfdb17fd9af75585ab7b9870ef902211c773fc01
isayme <isaymeorg@gmail.com>
2016-10-12 23:18:09 +0800

    NODE-798 Driver hangs on count command in replica set with one member

--- functional/rs_mocks/operation_tests.js
+++ functional/rs_mocks/operation_tests.js
@@ -103,3 +103,93 @@ exports['Correctly execute count command against replicaset with a single member
     }, 100)
   }
 }
+
+exports['Correctly execute count command against replicaset with a single member and secondaryPreferred'] = {
+  metadata: {
+    requires: {
+      generators: true,
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    var ReplSet = configuration.require.ReplSet,
+      ObjectId = configuration.require.BSON.ObjectId,
+      ReadPreference = configuration.require.ReadPreference,
+      Connection = require('../../../../lib/connection/connection'),
+      Long = configuration.require.BSON.Long,
+      co = require('co'),
+      mockupdb = require('../../../mock');
+
+    // Contain mock server
+    var primaryServer = null;
+    var firstSecondaryServer = null;
+    var secondSecondaryServer = null;
+    var arbiterServer = null;
+    var running = true;
+    var currentIsMasterIndex = 0;
+
+    // Default message fields
+    var defaultFields = {
+      "setName": "rs", "setVersion": 1, "electionId": new ObjectId(),
+      "maxBsonObjectSize" : 16777216, "maxMessageSizeBytes" : 48000000,
+      "maxWriteBatchSize" : 1000, "localTime" : new Date(), "maxWireVersion" : 4,
+      "minWireVersion" : 0, "ok" : 1, "hosts": ["localhost:32000"]
+    }
+
+    // Primary server states
+    var primary = [extend(defaultFields, {
+      "ismaster":true, "secondary":false, "me": "localhost:32000", "primary": "localhost:32000", "tags" : { "loc" : "ny" }
+    })];
+
+    // Boot the mock
+    co(function*() {
+      primaryServer = yield mockupdb.createServer(32000, 'localhost');
+
+      // Primary state machine
+      co(function*() {
+        while(running) {
+          var request = yield primaryServer.receive();
+          var doc = request.document;
+          // console.log("======== doc")
+          // console.dir(doc)
+
+          if(doc.ismaster) {
+            request.reply(primary[currentIsMasterIndex]);
+          } else if(doc.count) {
+            request.reply({ok:1, n:1});
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.stack);
+      });
+    });
+
+    Connection.enableConnectionAccounting();
+    // Attempt to connect
+    var server = new ReplSet([{ host: 'localhost', port: 32000 }], {
+        setName: 'rs',
+        connectionTimeout: 3000,
+        socketTimeout: 0,
+        haInterval: 2000,
+        size: 1,
+        disconnectHandler: {
+          add: function() {}, execute: function() {}
+        }
+    });
+
+    server.on('connect', function(server) {
+      server.command('test.test', {count: 'test'}, {readPreference: ReadPreference.secondaryPreferred}, function() {
+        primaryServer.destroy();
+        server.destroy();
+        running = false;
+        test.done();
+      });
+    });
+
+    // Gives proxies a chance to boot up
+    setTimeout(function() {
+      server.connect();
+    }, 100)
+  }
+}
