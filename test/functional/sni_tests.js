c5e8f20f231be0fe870396897246a8539592aa53
Christian Kvalheim <christkv@gmail.com>
2016-11-18 18:33:42 +0100

    fixed sni tests to work correctly

--- test/functional/sni_tests.js
+++ test/functional/sni_tests.js
@@ -10,25 +10,34 @@ exports['Should correct connect to snitest1.10gen.cc'] = {
   test: function(configuration, test) {
     var MongoClient = configuration.require.MongoClient
 
-    console.log("====================================================== 0")
     // Let's write the actual connection code
-    // MongoClient.connect("mongodb://snitest1.10gen.cc:27777/tests?ssl=true&maxPoolSize=1", {
     MongoClient.connect("mongodb://snitest2.mongodb.com:27777/?ssl=true", {
-    // MongoClient.connect("mongodb://snitest2.cloudmanager.mongodb.com:27777/tests?ssl=true&maxPoolSize=1", {
-      servername: 'cloudmanager.mongodb.com',
+      servername: 'snitest1.10gen.cc',
     }, function(err, db) {
-      console.log("====================================================== 1")
-      console.dir(err)
       test.equal(null, err);
-      // test.ok(db != null);
+      db.close();
+      test.done();
+    });
+  }
+}
 
-      // db.collection('test').find().toArray(function(err, docs) {
-      //   test.equal(null, err);
+/**
+ * @ignore
+ */
+exports['Should correct connect to snitest2.mongodb.com'] = {
+  metadata: { requires: { topology: 'sni', os: "!win32"  } },
 
-      //   test.ok(true, docs[0].kerberos);
-        db.close();
-        test.done();
-      // });
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var MongoClient = configuration.require.MongoClient
+
+    // Let's write the actual connection code
+    MongoClient.connect("mongodb://snitest2.mongodb.com:27777/?ssl=true", {
+      servername: 'snitest2.mongodb.com',
+    }, function(err, db) {
+      test.equal(null, err);
+      db.close();
+      test.done();
     });
   }
 }
