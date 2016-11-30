2a8cf95b51e77ebda225776efce9ba1e44229c61
Christian Kvalheim <christkv@gmail.com>
2016-11-30 14:42:48 +0100

    fixed BSON export

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// module.exports = {
-//     MongoError: require('./lib/error')
-//   , Server: require('./lib/topologies/server')
-//   , ReplSet: require('./lib/topologies/replset')
-//   , Mongos: require('./lib/topologies/mongos')
-//   , Logger: require('./lib/connection/logger')
-//   , Cursor: require('./lib/cursor')
-//   , ReadPreference: require('./lib/topologies/read_preference')
-//   , BSON: require('bson')
-//   // Raw operations
-//   , Query: require('./lib/connection/commands').Query
-//   // Auth mechanisms
-//   , MongoCR: require('./lib/auth/mongocr')
-//   , X509: require('./lib/auth/x509')
-//   , Plain: require('./lib/auth/plain')
-//   , GSSAPI: require('./lib/auth/gssapi')
-//   , ScramSHA1: require('./lib/auth/scram')
-// }
+var BSON = require('bson');
+
+try {
+  try { BSON = require('bson-ext'); } catch(err) {
+    BSON = require_optional('bson-ext');
+  }
+} catch(err) {}
 
 module.exports = {
     MongoError: require('./lib/error')
@@ -26,7 +15,7 @@ module.exports = {
   , Logger: require('./lib/connection/logger')
   , Cursor: require('./lib/cursor')
   , ReadPreference: require('./lib/topologies/read_preference')
-  , BSON: require('bson')
+  , BSON: BSON
   // Raw operations
   , Query: require('./lib/connection/commands').Query
   // Auth mechanisms
