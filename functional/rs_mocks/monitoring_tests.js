98de739540e0984ac43e188ebb5f0ddb6aa12402
Christian Kvalheim <christkv@gmail.com>
2017-04-21 17:05:24 +0200

    NODE-987 Moved filtering to pingServer method and added test case

--- functional/rs_mocks/monitoring_tests.js
+++ functional/rs_mocks/monitoring_tests.js
@@ -1,3 +1,11 @@
+var timeoutPromise = function(timeout) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve();
+    }, timeout);
+  });
+}
+
 exports['Should correctly connect to a replicaset where the primary hangs causing monitoring thread to hang'] = {
   metadata: {
     requires: {
@@ -58,14 +66,6 @@ exports['Should correctly connect to a replicaset where the primary hangs causin
     }), extend(defaultFields, {
       "ismaster":false, "secondary":true, "me": "localhost:32002", "primary": "localhost:32001"
     })];
-
-    var timeoutPromise = function(timeout) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          resolve();
-        }, timeout);
-      });
-    }
 
     // Joined servers
     var joinedPrimaries = {};
@@ -211,6 +211,149 @@ exports['Should correctly connect to a replicaset where the primary hangs causin
     })
 
     // console.log("==================================== schedule -1")
+    // Gives proxies a chance to boot up
+    setTimeout(function() {
+      server.connect();
+    }, 100)
+  }
+}
+
+exports['Should correctly prune intervalIds array'] = {
+  metadata: {
+    requires: {
+      generators: true,
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    var ReplSet = configuration.require.ReplSet,
+      ObjectId = configuration.require.BSON.ObjectId,
+      Connection = require('../../../../lib/connection/connection'),
+      co = require('co'),
+      mockupdb = require('../../../mock');
+
+    // Contain mock server
+    var primaryServer = null;
+    var firstSecondaryServer = null;
+    var secondSecondaryServer = null;
+    var running = true;
+    var electionIds = [new ObjectId(), new ObjectId()];
+    // Current index for the ismaster
+    var currentIsMasterState = 0;
+    // Primary stop responding
+    var stopRespondingPrimary = false;
+
+    // Extend the object
+    var extend = function(template, fields) {
+      for(var name in template) fields[name] = template[name];
+      return fields;
+    }
+
+    // Default message fields
+    var defaultFields = {
+      "setName": "rs", "setVersion": 1, "electionId": electionIds[currentIsMasterState],
+      "maxBsonObjectSize" : 16777216, "maxMessageSizeBytes" : 48000000,
+      "maxWriteBatchSize" : 1000, "localTime" : new Date(), "maxWireVersion" : 3,
+      "minWireVersion" : 0, "ok" : 1, "hosts": ["localhost:32000", "localhost:32001", "localhost:32002"]
+    }
+
+    // Primary server states
+    var primary = [extend(defaultFields, {
+      "ismaster":true, "secondary":false, "me": "localhost:32000", "primary": "localhost:32000"
+    }), extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32000", "primary": "localhost:32001"
+    })];
+
+    // Primary server states
+    var firstSecondary = [extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32001", "primary": "localhost:32000"
+    }), extend(defaultFields, {
+      "ismaster":true, "secondary":false, "me": "localhost:32001", "primary": "localhost:32001"
+    })];
+
+    // Primary server states
+    var secondSecondary = [extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32002", "primary": "localhost:32000"
+    }), extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32002", "primary": "localhost:32001"
+    })];
+
+    // Boot the mock
+    co(function*() {
+      primaryServer = yield mockupdb.createServer(32000, 'localhost');
+      firstSecondaryServer = yield mockupdb.createServer(32001, 'localhost');
+      secondSecondaryServer = yield mockupdb.createServer(32002, 'localhost');
+
+      // Primary state machine
+      co(function*() {
+        while(running) {
+          var request = yield primaryServer.receive();
+
+          // Get the document
+          var doc = request.document;
+          if(doc.ismaster && currentIsMasterState == 0) {
+            request.reply(primary[currentIsMasterState]);
+          }
+        }
+      });
+
+      // First secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield firstSecondaryServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(firstSecondary[currentIsMasterState]);
+          }
+        }
+      });
+
+      // Second secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield secondSecondaryServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(secondSecondary[currentIsMasterState]);
+          }
+        }
+      });
+    });
+
+    // Attempt to connect
+    var server = new ReplSet([
+      { host: 'localhost', port: 32000 },
+      { host: 'localhost', port: 32001 },
+      { host: 'localhost', port: 32002 }], {
+        setName: 'rs',
+        connectionTimeout: 5000,
+        socketTimeout: 60000,
+        haInterval: 200,
+        size: 1
+    });
+
+    // Add event listeners
+    server.on('connect', function(_server) {
+      setTimeout(function() {
+        console.dir(_server.intervalIds.length)
+        test.ok(_server.intervalIds.length < 5);
+
+        // Destroy mock
+        primaryServer.destroy();
+        firstSecondaryServer.destroy();
+        secondSecondaryServer.destroy();
+        server.destroy();
+        running = false;
+
+        test.equal(0, _server.intervalIds.length);
+
+        test.done();
+      }, 1000);
+    });
+
     // Gives proxies a chance to boot up
     setTimeout(function() {
       server.connect();
