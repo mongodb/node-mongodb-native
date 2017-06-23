7532fcbd6e415424340773eda411f2ade12f93f9
Christian Amor Kvalheim <christkv@users.noreply.github.com>
2017-06-23 15:18:46 +0200

    NODE-1049 only include primary server if there are no secondary serve… (#194)
    * NODE-1049 only include primary server if there are no secondary servers for readPrefrence secondaryPreferred
    
    * minor correction to fix test error
    
    * removed commented out code and stray 2
    
    * minor tweak to test case to make less brittle

--- functional/rs_mocks/read_preferences_tests.js
+++ functional/rs_mocks/read_preferences_tests.js
@@ -1353,3 +1353,171 @@ exports['Should correctly connect connect to single server replicaset and peform
     });
   }
 }
+
+exports['Should only read from secondaries when read preference secondaryPreferred is specified'] = {
+  metadata: {
+    requires: {
+      generators: true,
+      topology: "single"
+    }
+  },
+
+  test: function(configuration, test) {
+    var ReplSet = configuration.require.ReplSet,
+      ObjectId = configuration.require.BSON.ObjectId,
+      Connection = require('../../../../lib/connection/connection'),
+      ReadPreference = configuration.require.ReadPreference,
+      Long = configuration.require.BSON.Long,
+      co = require('co'),
+      mockupdb = require('../../../mock');
+
+    // Contain mock server
+    var primaryServer = null;
+    var firstSecondaryServer = null;
+    var secondSecondaryServer = null;
+    var running = true;
+    var electionIds = [new ObjectId(), new ObjectId()];
+
+    // Extend the object
+    var extend = function(template, fields) {
+      for(var name in template) fields[name] = template[name];
+      return fields;
+    }
+
+    // Default message fields
+    var defaultFields = {
+      "setName": "rs", "setVersion": 1, "electionId": electionIds[0],
+      "maxBsonObjectSize" : 16777216, "maxMessageSizeBytes" : 48000000,
+      "maxWriteBatchSize" : 1000, "localTime" : new Date(), "maxWireVersion" : 4,
+      "minWireVersion" : 0, "ok" : 1, "hosts": ["localhost:32000", "localhost:32001", "localhost:32002"]
+    }
+
+    // Primary server states
+    var primary = [extend(defaultFields, {
+      "ismaster":true, "secondary":false, "me": "localhost:32000", "primary": "localhost:32000", "tags" : { "loc" : "ny" }
+    })];
+
+    // Primary server states
+    var firstSecondary = [extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32001", "primary": "localhost:32000", "tags" : { "loc" : "sf" }
+    })];
+
+    // Primary server states
+    var secondSecondary = [extend(defaultFields, {
+      "ismaster":false, "secondary":true, "me": "localhost:32002", "primary": "localhost:32000", "tags" : { "loc" : "dc" }
+    })];
+
+    // Boot the mock
+    co(function*() {
+      primaryServer = yield mockupdb.createServer(32000, 'localhost');
+      firstSecondaryServer = yield mockupdb.createServer(32001, 'localhost');
+      secondSecondaryServer = yield mockupdb.createServer(32002, 'localhost');
+
+      // Primary state machine
+      co(function*() {
+        while(running) {
+          var request = yield primaryServer.receive();
+          // Get the document
+          var doc = request.document;
+          if(doc.ismaster) {
+            request.reply(primary[0]);
+          } else if(doc.count) {
+            request.reply({ "waitedMS" : Long.ZERO, "n" : 1, "ok" : 1});
+          }
+        }
+      });
+
+      // First secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield firstSecondaryServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(firstSecondary[0]);
+          } else if(doc.count) {
+            request.reply({ "waitedMS" : Long.ZERO, "n" : 1, "ok" : 1});
+          }
+        }
+      }).catch(function(err) {
+        // console.log(err.stack);
+      });
+
+      // Second secondary state machine
+      co(function*() {
+        while(running) {
+          var request = yield secondSecondaryServer.receive();
+          var doc = request.document;
+
+          if(doc.ismaster) {
+            request.reply(secondSecondary[0]);
+          } else if(doc.count) {
+            request.reply({ "waitedMS" : Long.ZERO, "n" : 1, "ok" : 1});
+          }
+        }
+      }).catch(function(err) {
+        // console.log(err.stack);
+      });
+
+      Connection.enableConnectionAccounting();
+      // Attempt to connect
+      var server = new ReplSet([
+        { host: 'localhost', port: 32000 },
+        { host: 'localhost', port: 32001 },
+        { host: 'localhost', port: 32002 }], {
+          setName: 'rs',
+          connectionTimeout: 3000,
+          socketTimeout: 0,
+          haInterval: 2000,
+          size: 1
+      });
+
+      // Add event listeners
+      server.on('all', function(_server) {
+        // Execute more operations than there is servers connected
+        setTimeout(function() {
+          var count = 50;
+          var portsSeen = {};
+
+          for(var i = 0; i < 50; i++) {
+            // Perform a find
+            _server.command('test.test', {
+                count: 'test.test'
+              , batchSize: 2
+            }, {
+              readPreference: new ReadPreference('secondaryPreferred')
+            }, function(err, r) {
+              count = count - 1;
+              test.equal(null, err);
+
+              // Add the port to the portsSeen
+              portsSeen[r.connection.port] = true;
+
+              // Finished up
+              if(count === 0) {
+                // Should not contain the primary
+                test.equal(null, portsSeen['32000']);
+
+                // Shut down mocks
+                primaryServer.destroy();
+                firstSecondaryServer.destroy();
+                secondSecondaryServer.destroy();
+                server.destroy();
+                running = false; 
+
+                setTimeout(function() {
+                  test.done();
+                }, 500);
+              }
+            });
+          }
+        }, 1000);
+      });
+
+      // Gives proxies a chance to boot up
+      setTimeout(function() {
+        server.connect();
+      }, 100)
+    });
+  }
+}
