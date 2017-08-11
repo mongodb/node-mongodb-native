9e4082f61b5baba463fe56ad176c10beac954332
Marcus Todd <malexander.todd@gmail.com>
2017-08-11 18:40:42 -0400

    test(tailable_cursor_tests): convert tailable_cursor_tests to run with mocha
    NODE-1096

--- functional/tailable_cursor_tests.js
+++ functional/tailable_cursor_tests.js
@@ -1,64 +1,65 @@
-"use strict";
+'use strict';
 
-var f = require('util').format;
+var expect = require('chai').expect,
+    f = require('util').format;
 
-exports['Should correctly perform awaitdata'] = {
-  metadata: {
-    requires: { topology: ["single", "replicaset", "sharded"] }
-  },
+describe('Tailable cursor tests', function() {
+  it('should correctly perform awaitdata', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded'] }
+    },
 
-  test: function(configuration, test) {
-    configuration.newTopology(function(err, server) {
-      var ns = f("%s.cursor_tailable", configuration.db);
-      // Add event listeners
-      server.on('connect', function(_server) {
-        // Create a capped collection
-        _server.command(f("%s.$cmd", configuration.db), {create: 'cursor_tailable', capped:true, size: 10000}, function(err, r) {
-          // Execute the write
-          _server.insert(ns, [{a:1}], {
-            writeConcern: {w:1}, ordered:true
-          }, function(err, results) {
-            test.equal(null, err);
-            test.equal(1, results.result.n);
+    test: function(done) {
+      var self = this;
 
-            // Execute find
-            var cursor = _server.cursor(ns, {
-                find: ns
-              , query: {}
-              , batchSize: 2
-              , tailable: true
-              , awaitData: true
-            });
+      this.configuration.newTopology(function(err, server) {
+        var ns = f('%s.cursor_tailable', self.configuration.db);
+        // Add event listeners
+        server.on('connect', function(_server) {
+          // Create a capped collection
+          _server.command(f('%s.$cmd', self.configuration.db), {create: 'cursor_tailable', capped: true, size: 10000}, function(cmdErr, cmdRes) {
+            // Execute the write
+            _server.insert(ns, [{a: 1}], {
+              writeConcern: {w: 1}, ordered: true
+            }, function(insertErr, results) {
+              expect(insertErr).to.be.null;
+              expect(results.result.n).to.equal(1);
 
-            // Execute next
-            cursor.next(function(err, d) {
-              // console.log("==================================== Should correctly perform awaitdata 0")
-              test.equal(null, err);
-              var s = new Date();
-
-              cursor.next(function(err, d) {
-                var e = new Date();
-                // console.log("==================================== Should correctly perform awaitdata")
-                // console.log((e.getTime() - s.getTime()));
-
-                test.ok((e.getTime() - s.getTime()) >= 300);
-
-                // Destroy the server connection
-                _server.destroy();
-                // Finish the test
-                test.done();
+              // Execute find
+              var cursor = _server.cursor(ns, {
+                find: ns,
+                query: {},
+                batchSize: 2,
+                tailable: true,
+                awaitData: true
               });
 
-              setTimeout(function() {
-                cursor.kill();
-              }, 300)
+              // Execute next
+              cursor.next(function(cursorErr, cursorD) {
+                expect(cursorErr).to.be.null;
+                var s = new Date();
+
+                cursor.next(function(secondCursorErr, secondCursorD) {
+                  var e = new Date();
+                  expect(e.getTime() - s.getTime()).to.be.at.least(300);
+
+                  // Destroy the server connection
+                  _server.destroy();
+                  // Finish the test
+                  done();
+                });
+
+                setTimeout(function() {
+                  cursor.kill();
+                }, 300);
+              });
             });
           });
         });
-      })
 
-      // Start connection
-      server.connect();
-    });
-  }
-}
+        // Start connection
+        server.connect();
+      });
+    }
+  });
+});
