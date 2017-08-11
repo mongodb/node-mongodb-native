54dbb601bf804df99301baee8f784bf700a7ee0e
Marcus Todd <malexander.todd@gmail.com>
2017-08-11 18:28:23 -0400

    test(client_metadata_tests): convert client_metadata_tests to run with mocha
    NODE-1096

--- functional/client_metadata_tests.js
+++ functional/client_metadata_tests.js
@@ -1,72 +1,72 @@
-"use strict";
+'use strict';
 
-var f = require('util').format
-  , Long = require('bson').Long;
+var expect = require('chai').expect,
+    Server = require('../../../lib/topologies/server'),
+    Bson = require('bson'),
+    Mongos = require('../../../lib/topologies/mongos');
 
-exports['Should correctly pass the configuration settings to server'] = {
-  metadata: { requires: { topology: "single" } },
+describe('Client metadata tests', function() {
+  it('should correctly pass the configuration settings to server', {
+    metadata: { requires: { topology: 'single' } },
 
-  test: function(configuration, test) {
-    var Server = require('../../../lib/topologies/server')
-      , bson = require('bson');
-
-    // Attempt to connect
-    var server = new Server({
-        host: configuration.host
-      , port: configuration.port
-      , bson: new bson()
-      , appname: 'My application name'
-    })
-
-    test.equal('My application name', server.clientInfo.application.name);
-    test.done();
-  }
-}
-
-exports['Should correctly pass the configuration settings to replset'] = {
-  metadata: { requires: { topology: "replicaset" } },
-
-  test: function(configuration, test) {
-    var ReplSet = configuration.require.ReplSet,
-      manager = configuration.manager;
-
-    // Get the primary server
-    manager.primary().then(function(manager) {
+    test: function(done) {
       // Attempt to connect
-      var server = new ReplSet([{
-          host: manager.host
-        , port: manager.port
+      var server = new Server({
+        host: this.configuration.host,
+        port: this.configuration.port,
+        bson: new Bson(),
+        appname: 'My application name'
+      });
+
+      expect(server.clientInfo.application.name).to.equal('My application name');
+      done();
+    }
+  });
+
+  it('should correctly pass the configuration settings to replset', {
+    metadata: { requires: { topology: 'replicaset' } },
+
+    test: function(done) {
+      var self = this;
+
+      var ReplSet = this.configuration.mongo.ReplSet,
+          manager = this.configuration.manager;
+
+      // Get the primary server
+      manager.primary().then(function(_manager) {
+        // Attempt to connect
+        var server = new ReplSet([{
+          host: _manager.host,
+          port: _manager.port
+        }], {
+          setName: self.configuration.setName, appname: 'My application name'
+        });
+
+        server.on('connect', function(_server) {
+          _server.s.replicaSetState.allServers().forEach(function(x) {
+            // console.dir(x.clientInfo)
+            expect(x.clientInfo.application.name).to.equal('My application name');
+            expect(x.clientInfo.platform.split('mongodb-core').length).to.equal(2);
+          });
+
+          _server.destroy();
+          done();
+        });
+
+        server.connect();
+      });
+    }
+  });
+
+  it('should correctly pass the configuration settings to mongos', {
+    metadata: { requires: { topology: 'sharded' } },
+
+    test: function(done) {
+      // Attempt to connect
+      var _server = new Mongos([{
+        host: 'localhost',
+        port: 51000
       }], {
-        setName: configuration.setName, appname: 'My application name'
-      });
-
-      server.on('connect', function(_server) {
-        _server.s.replicaSetState.allServers().forEach(function(x) {
-          // console.dir(x.clientInfo)
-          test.equal('My application name', x.clientInfo.application.name);
-          test.equal(2, x.clientInfo.platform.split('mongodb-core').length);
-        })
-
-        _server.destroy();
-        test.done();
-      });
-
-      server.connect();
-    });
-  }
-}
-
-exports['Should correctly pass the configuration settings to mongos'] = {
-  metadata: { requires: { topology: "sharded" } },
-
-  test: function(configuration, test) {
-    var Mongos = require('../../../lib/topologies/mongos')
-      , bson = require('bson');
-
-      // Attempt to connect
-      var _server = new Mongos([
-          { host: 'localhost', port: 51000 },
-        ], {
         appname: 'My application name'
       });
 
@@ -74,14 +74,15 @@ exports['Should correctly pass the configuration settings to mongos'] = {
       _server.once('connect', function(server) {
         server.connectedProxies.forEach(function(x) {
           // console.dir(x.clientInfo)
-          test.equal('My application name', x.clientInfo.application.name);
-          test.equal(2, x.clientInfo.platform.split('mongodb-core').length);
-        })
+          expect(x.clientInfo.application.name).to.equal('My application name');
+          expect(x.clientInfo.platform.split('mongodb-core').length).to.equal(2);
+        });
 
         server.destroy();
-        test.done();
+        done();
       });
 
       _server.connect();
-  }
-}
+    }
+  });
+});
