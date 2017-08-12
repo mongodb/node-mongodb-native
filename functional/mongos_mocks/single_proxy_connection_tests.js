1f15c8fe354a0eed043b92e530cf786ceacd3e46
Matt Broadstone <mbroadst@gmail.com>
2017-08-12 17:34:16 -0400

    test(mongos_mocks): converted `single_proxy_connection` tests to mocha
    NODE-1096

--- functional/mongos_mocks/single_proxy_connection_tests.js
+++ functional/mongos_mocks/single_proxy_connection_tests.js
@@ -1,265 +1,242 @@
-"use strict";
-var f = require('util').format;
-var assign = require('../../../../lib/utils').assign;
+'use strict';
+var expect = require('chai').expect,
+    f = require('util').format,
+    co = require('co'),
+    assign = require('../../../../lib/utils').assign;
 
-exports['Should correctly timeout mongos socket operation and then correctly re-execute'] = {
-  metadata: {
-    requires: {
-      generators: true,
-      topology: "single"
-    }
-  },
+var timeoutPromise = function(timeout) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() { resolve(); }, timeout);
+  });
+};
 
-  test: function(configuration, test) {
-    // console.log("--------------------------------------- -1")
-    var Mongos = configuration.require.Mongos,
-      ObjectId = configuration.require.BSON.ObjectId,
-      co = require('co'),
-      mockupdb = require('../../../mock');
+describe('Mongos Single Proxy Connection (mocks)', function() {
+  it('Should correctly timeout mongos socket operation and then correctly re-execute', {
+    metadata: {
+      requires: {
+        generators: true,
+        topology: 'single'
+      }
+    },
 
-    // Contain mock server
-    var server = null;
-    var running = true;
-    // Current index for the ismaster
-    var currentStep = 0;
-    // Primary stop responding
-    var stopRespondingPrimary = false;
+    test: function(done) {
+      var Mongos = this.configuration.mongo.Mongos,
+          mockupdb = require('../../../mock');
 
-    // Default message fields
-    var defaultFields = {
-      "ismaster" : true,
-      "msg" : "isdbgrid",
-      "maxBsonObjectSize" : 16777216,
-      "maxMessageSizeBytes" : 48000000,
-      "maxWriteBatchSize" : 1000,
-      "localTime" : new Date(),
-      "maxWireVersion" : 3,
-      "minWireVersion" : 0,
-      "ok" : 1
-    }
+      // Contain mock server
+      var server = null;
+      var running = true;
+      // Current index for the ismaster
+      var currentStep = 0;
+      // Primary stop responding
+      var stopRespondingPrimary = false;
 
-    // Primary server states
-    var serverIsMaster = [ assign({}, defaultFields) ];
-    var timeoutPromise = function(timeout) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          resolve();
-        }, timeout);
-      });
-    }
+      // Default message fields
+      var defaultFields = {
+        'ismaster': true,
+        'msg': 'isdbgrid',
+        'maxBsonObjectSize': 16777216,
+        'maxMessageSizeBytes': 48000000,
+        'maxWriteBatchSize': 1000,
+        'localTime': new Date(),
+        'maxWireVersion': 3,
+        'minWireVersion': 0,
+        'ok': 1
+      };
 
-    // Boot the mock
-    co(function*() {
-      server = yield mockupdb.createServer(52017, 'localhost');
+      // Primary server states
+      var serverIsMaster = [ assign({}, defaultFields) ];
 
-      // Primary state machine
+      // Boot the mock
       co(function*() {
-        while(running) {
-          var request = yield server.receive();
+        server = yield mockupdb.createServer(52017, 'localhost');
 
-          // Get the document
-          var doc = request.document;
+        // Primary state machine
+        co(function*() {
+          while (running) {
+            var request = yield server.receive();
 
-          if(doc.ismaster && currentStep == 0) {
-            request.reply(serverIsMaster[0]);
-            currentStep += 1;
-          } else if(doc.insert && currentStep == 1) {
-            // Stop responding to any calls (emulate dropping packets on the floor)
-            if(stopRespondingPrimary) {
+            // Get the document
+            var doc = request.document;
+
+            if (doc.ismaster && currentStep === 0) {
+              request.reply(serverIsMaster[0]);
               currentStep += 1;
-              stopRespondingPrimary = false;
-              // Timeout after 1500 ms
-              yield timeoutPromise(1500);
-              request.connection.destroy();
+            } else if (doc.insert && currentStep === 1) {
+              // Stop responding to any calls (emulate dropping packets on the floor)
+              if (stopRespondingPrimary) {
+                currentStep += 1;
+                stopRespondingPrimary = false;
+                // Timeout after 1500 ms
+                yield timeoutPromise(1500);
+                request.connection.destroy();
+              }
+            } else if (doc.ismaster) {
+              request.reply(serverIsMaster[0]);
+            } else if (doc.insert && currentStep === 2) {
+              request.reply({ok: 1, n: doc.documents, lastOp: new Date()});
             }
-          } else if(doc.ismaster) {
-            request.reply(serverIsMaster[0]);
-          } else if(doc.insert && currentStep == 2) {
-            request.reply({ok:1, n:doc.documents, lastOp: new Date()});
           }
-        }
+        }).catch(function(err) {
+        });
+
+        // Start dropping the packets
+        setTimeout(function() {
+          stopRespondingPrimary = true;
+        }, 500);
       }).catch(function(err) {
       });
 
-      // Start dropping the packets
-      setTimeout(function() {
-        stopRespondingPrimary = true;
-      }, 500);
-    }).catch(function(err) {
-    });
-
-    // console.log("--------------------------------------- 0")
-    // Attempt to connect
-    var _server = new Mongos([
-        { host: 'localhost', port: 52017 },
+      // Attempt to connect
+      var _server = new Mongos([
+        { host: 'localhost', port: 52017 }
       ], {
-      connectionTimeout: 3000,
-      socketTimeout: 1000,
-      haInterval: 500,
-      size: 1
-    });
+        connectionTimeout: 3000,
+        socketTimeout: 1000,
+        haInterval: 500,
+        size: 1
+      });
 
-    // Are we done
-    var done = false;
+      // Are we done
+      var finished = false;
 
-    // console.log("--------------------------------------- 1")
-    // Add event listeners
-    _server.once('connect', function() {
-      // console.log("--------------------------------------- 3")
-      // Run an interval
-      var intervalId = setInterval(function() {
-        // console.log("--------------------------------------- 4")
-        _server.insert('test.test', [{created:new Date()}], function(err, r) {
-          // console.log("--------------------------------------- 5")
-          // console.dir(err)
-          // if(r) console.dir(r.connection)
-          // console.log("-------------------------------- insert attempt")
-          // console.dir(err)
-          if(r && !done) {
-            done = true;
-            clearInterval(intervalId);
-            test.equal(52017, r.connection.port);
+      // Add event listeners
+      _server.once('connect', function() {
+        // Run an interval
+        var intervalId = setInterval(function() {
+          _server.insert('test.test', [{ created: new Date() }], function(err, r) {
+            if (r && !finished) {
+              finished = true;
+              clearInterval(intervalId);
+              expect(r.connection.port).to.equal(52017);
+              running = false;
+              server.destroy();
+              done();
+            }
+          });
+        }, 500);
+      });
+
+      _server.on('error', done);
+      _server.connect();
+    }
+  });
+
+  it('Should not fail due to available connections equal to 0 during ha process', {
+    metadata: {
+      requires: {
+        generators: true,
+        topology: 'single'
+      }
+    },
+
+    test: function(done) {
+      var Mongos = this.configuration.mongo.Mongos,
+          Long = this.configuration.mongo.BSON.Long,
+          ObjectId = this.configuration.mongo.BSON.ObjectId,
+          mockupdb = require('../../../mock');
+
+      // Contain mock server
+      var server = null;
+      var running = true;
+
+      // Default message fields
+      var defaultFields = {
+        'ismaster': true,
+        'msg': 'isdbgrid',
+        'maxBsonObjectSize': 16777216,
+        'maxMessageSizeBytes': 48000000,
+        'maxWriteBatchSize': 1000,
+        'localTime': new Date(),
+        'maxWireVersion': 4,
+        'minWireVersion': 0,
+        'ok': 1
+      };
+
+      // Primary server states
+      var serverIsMaster = [ assign({}, defaultFields) ];
+
+      // Boot the mock
+      co(function*() {
+        server = yield mockupdb.createServer(52018, 'localhost');
+
+        // Primary state machine
+        co(function*() {
+          while (running) {
+            var request = yield server.receive();
+
+            // Get the document
+            var doc = request.document;
+
+            if (doc.ismaster) {
+              request.reply(serverIsMaster[0]);
+            } else if (doc.find) {
+              yield timeoutPromise(600);
+              // Reply with first batch
+              request.reply({
+                'cursor': {
+                  'id': Long.fromNumber(1),
+                  'ns': f('%s.cursor1', 'test'),
+                  'firstBatch': [
+                    { _id: new ObjectId(), a: 1 }
+                  ]
+                },
+                'ok': 1
+              });
+            } else if (doc.getMore) {
+              // Reply with first batch
+              request.reply({
+                'cursor': {
+                  'id': Long.fromNumber(1),
+                  'ns': f('%s.cursor1', 'test'),
+                  'nextBatch': [
+                    { _id: new ObjectId(), a: 1 }
+                  ]
+                },
+                'ok': 1
+              });
+            }
+          }
+        }).catch(function(err) {
+        });
+      }).catch(function(err) {
+      });
+
+      // Attempt to connect
+      var _server = new Mongos([
+        { host: 'localhost', port: 52018 }
+      ], {
+        connectionTimeout: 30000,
+        socketTimeout: 30000,
+        haInterval: 500,
+        size: 1
+      });
+
+      // Add event listeners
+      _server.once('connect', function() {
+        // Execute find
+        var cursor = _server.cursor('test.test', {
+          find: 'test',
+          query: {},
+          batchSize: 2
+        });
+
+        // Execute next
+        cursor.next(function(err, d) {
+          expect(err).to.not.exist;
+
+          cursor.next(function(_err, _d) {
+            expect(_err).to.not.exist;
+
             running = false;
             server.destroy();
-            test.done();
-          }
-        });
-      }, 500);
-    });
-
-    // console.log("--------------------------------------- 2")
-    _server.on('error', function(){});
-    _server.connect();
-  }
-}
-
-exports['Should not fail due to available connections equal to 0 during ha process'] = {
-  metadata: {
-    requires: {
-      generators: true,
-      topology: "single"
-    }
-  },
-
-  test: function(configuration, test) {
-    var Mongos = configuration.require.Mongos,
-      Long = configuration.require.BSON.Long,
-      ObjectId = configuration.require.BSON.ObjectId,
-      co = require('co'),
-      mockupdb = require('../../../mock');
-
-    // Contain mock server
-    var server = null;
-    var running = true;
-    // Primary stop responding
-    var stopRespondingPrimary = false;
-
-    // Default message fields
-    var defaultFields = {
-      "ismaster" : true,
-      "msg" : "isdbgrid",
-      "maxBsonObjectSize" : 16777216,
-      "maxMessageSizeBytes" : 48000000,
-      "maxWriteBatchSize" : 1000,
-      "localTime" : new Date(),
-      "maxWireVersion" : 4,
-      "minWireVersion" : 0,
-      "ok" : 1
-    }
-
-    // Primary server states
-    var serverIsMaster = [ assign({}, defaultFields) ];
-    var timeoutPromise = function(timeout) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          resolve();
-        }, timeout);
-      });
-    }
-
-    // Boot the mock
-    co(function*() {
-      server = yield mockupdb.createServer(52018, 'localhost');
-
-      // Primary state machine
-      co(function*() {
-        while(running) {
-          var request = yield server.receive();
-
-          // Get the document
-          var doc = request.document;
-
-          if(doc.ismaster) {
-            request.reply(serverIsMaster[0]);
-          } else if(doc.find) {
-            yield timeoutPromise(600);
-            // Reply with first batch
-            request.reply({
-              "cursor" : {
-                "id" : Long.fromNumber(1),
-                "ns" : f("%s.cursor1", 'test'),
-                "firstBatch" : [
-                  { _id: new ObjectId(), a:1}
-                ]
-              },
-              "ok" : 1
-            });
-          } else if(doc.getMore) {
-            // Reply with first batch
-            request.reply({
-              "cursor" : {
-                "id" : Long.fromNumber(1),
-                "ns" : f("%s.cursor1", 'test'),
-                "nextBatch" : [
-                  { _id: new ObjectId(), a:1}
-                ]
-              },
-              "ok" : 1
-            });
-          }
-        }
-      }).catch(function(err) {
-      });
-    }).catch(function(err) {
-    });
-
-    // Attempt to connect
-    var _server = new Mongos([
-        { host: 'localhost', port: 52018 },
-      ], {
-      connectionTimeout: 30000,
-      socketTimeout: 30000,
-      haInterval: 500,
-      size: 1
-    });
-
-    // Are we done
-    var done = false;
-
-    // Add event listeners
-    _server.once('connect', function() {
-      // Execute find
-      var cursor = _server.cursor('test.test', {
-          find: 'test'
-        , query: {}
-        , batchSize: 2
-      });
-
-      // Execute next
-      cursor.next(function(err, d) {
-        test.equal(null, err);
-
-        cursor.next(function(err, d) {
-          test.equal(null, err);
-
-          running = false;
-          server.destroy();
-          test.done();
+            done();
+          });
         });
       });
-    });
 
-    _server.on('error', function(){});
-    setTimeout(function() { _server.connect(); }, 100);
-  }
-}
+      _server.on('error', done);
+      setTimeout(function() { _server.connect(); }, 100);
+    }
+  });
+});
