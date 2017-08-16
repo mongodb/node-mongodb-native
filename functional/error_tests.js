618fef98b74103f44879f06604f28e98abe3ecf1
Marcus Todd <malexander.todd@gmail.com>
2017-08-16 16:48:57 -0400

    test(error_tests): convert error_tests to run with mocha
    NODE-1096

--- functional/error_tests.js
+++ functional/error_tests.js
@@ -1,99 +1,104 @@
 'use strict';
 
-var f = require('util').format;
+var expect = require('chai').expect,
+    f = require('util').format;
 
-exports['should return helpful error when geoHaystack fails'] = {
-  metadata: {
-    requires: { topology: ["single", "replicaset"] }
-  },
+describe.only('Error tests', function() {
+  it('should return helpful error when geoHaystack fails',  {
+    metadata: {
+      requires: { topology: ['single', 'replicaset'] }
+    },
 
-  test: function(configuration, test) {
-    configuration.newTopology(function(err, server) {
-      var ns = f('%s.geohaystack1', configuration.db);
-      server.on('connect', function(_server) {
-        _server.command('system.$cmd', {geoNear: ns}, {}, function(err, result) {
-          test.ok(/can\'t find ns/.test(err));
-          _server.destroy();
-          test.done();
+    test: function(done) {
+      var self = this;
+
+      self.configuration.newTopology(function(err, server) {
+        var ns = f('%s.geohaystack1', self.configuration.db);
+        server.on('connect', function(_server) {
+          _server.command('system.$cmd', {geoNear: ns}, {}, function(_err, result) {
+            expect(/can\'t find ns/.test(_err)).to.be.ok;
+            _server.destroy();
+            done();
+          });
         });
+
+        // Start connection
+        server.connect();
       });
+    }
+  });
 
-      // Start connection
-      server.connect();
-    });
-  }
-};
+  it('should create a MongoError from string',  {
+    metadata: {
+      requires: { topology: ['single'] }
+    },
 
-exports['should create a MongoError from string'] = {
-  metadata: {
-    requires: { topology: ["single"] }
-  },
+    test: function(done) {
+      var MongoError = require('../../../lib/error.js').MongoError;
 
-  test: function(configuration, test) {
-    var MongoError = require('../../../lib/error.js').MongoError;
+      var errorMessage = 'A test error';
+      var err = new MongoError(errorMessage);
+      expect(err).to.be.an.instanceof(Error);
+      expect(err.name).to.equal('MongoError');
+      expect(err.message).to.equal(errorMessage);
 
-    var errorMessage = 'A test error';
-    var err = new MongoError(errorMessage);
-    test.ok(err instanceof Error);
-    test.equal(err.name, 'MongoError');
-    test.equal(err.message, errorMessage);
+      done();
+    }
+  });
 
-    test.done();
-  }
-};
+  it('should create a MongoError from Error',  {
+    metadata: {
+      requires: { topology: ['single'] }
+    },
 
-exports['should create a MongoError from Error'] = {
-  metadata: {
-    requires: { topology: ["single"] }
-  },
+    test: function(done) {
+      var MongoError = require('../../../lib/error.js').MongoError;
 
-  test: function(configuration, test) {
-    var MongoError = require('../../../lib/error.js').MongoError;
+      var errorMessage = 'A test error';
+      var err = new MongoError(new Error(errorMessage));
+      expect(err).to.be.an.instanceof(Error);
+      expect(err.name).to.equal('MongoError');
+      expect(err.message).to.equal(errorMessage);
 
-    var errorMessage = 'A test error';
-    var err = new MongoError(new Error(errorMessage));
-    test.ok(err instanceof Error);
-    test.equal(err.name, 'MongoError');
-    test.equal(err.message, errorMessage);
+      done();
+    }
+  });
 
-    test.done();
-  }
-};
+  it('should create a MongoError from object',  {
+    metadata: {
+      requires: { topology: ['single'] }
+    },
 
-exports['should create a MongoError from object'] = {
-  metadata: {
-    requires: { topology: ["single"] }
-  },
+    test: function(done) {
+      var MongoError = require('../../../lib/error.js').MongoError;
 
-  test: function(configuration, test) {
-    var MongoError = require('../../../lib/error.js').MongoError;
+      var errorMessage = 'A test error';
+      var err = new MongoError({message: errorMessage, someData: 12345});
+      expect(err).to.be.an.instanceof(Error);
+      expect(err.name).to.equal('MongoError');
+      expect(err.message).to.equal(errorMessage);
+      expect(err.someData).to.equal(12345);
 
-    var errorMessage = 'A test error';
-    var err = new MongoError({message: errorMessage, someData: 12345});
-    test.ok(err instanceof Error);
-    test.equal(err.name, 'MongoError');
-    test.equal(err.message, errorMessage);
-    test.equal(err.someData, 12345);
+      done();
+    }
+  });
 
-    test.done();
-  }
-};
+  it('should create a MongoNetworkError',  {
+    metadata: {
+      requires: { topology: ['single'] }
+    },
 
-exports['should create a MongoNetworkError'] = {
-  metadata: {
-    requires: { topology: ["single"] }
-  },
+    test: function(done) {
+      var errors = require('../../../lib/error');
 
-  test: function(configuration, test) {
-    var errors = require('../../../lib/error');
+      var errorMessage = 'A test error';
+      var err = new errors.MongoNetworkError(errorMessage);
+      expect(err).to.be.an.instanceof(Error);
+      expect(err).to.be.an.instanceof(errors.MongoError);
+      expect(err.name).to.equal('MongoNetworkError');
+      expect(err.message).to.equal(errorMessage);
 
-    var errorMessage = 'A test error';
-    var err = new errors.MongoNetworkError(errorMessage);
-    test.ok(err instanceof Error);
-    test.ok(err instanceof errors.MongoError);
-    test.equal(err.name, 'MongoNetworkError');
-    test.equal(err.message, errorMessage);
-
-    test.done();
-  }
-};
+      done();
+    }
+  });
+});
