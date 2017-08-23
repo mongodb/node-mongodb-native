74d32da2ee8ccce4884da4b3ab498ca06b38b1d4
Matt Broadstone <mbroadst@gmail.com>
2017-08-23 16:54:27 -0400

    test(views): convert `view` tests to mocha style
    NODE-1095

--- test/functional/view_tests.js
+++ test/functional/view_tests.js
@@ -1,103 +1,91 @@
-// Extend the object
-var extend = function(template, fields) {
-  var object = {};
-  for (var name in template) {
-    object[name] = template[name];
-  }
+'use strict';
+var expect = require('chai').expect,
+  assign = require('../../lib/utils').assign;
 
-  for (var name in fields) {
-    object[name] = fields[name];
-  }
+describe('Views', function() {
+  it('should successfully pass through collation to findAndModify command', {
+    metadata: { requires: { generators: true, topology: 'single' } },
 
-  return object;
-};
+    test: function(done) {
+      var self = this,
+        MongoClient = self.configuration.mongo.MongoClient,
+        co = require('co'),
+        Long = self.configuration.mongo.Long,
+        mockupdb = require('../mock');
 
-exports['Successfully pass through collation to findAndModify command'] = {
-  metadata: { requires: { generators: true, topology: 'single' } },
+      // Contain mock server
+      var singleServer = null;
+      var running = true;
 
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient,
-      co = require('co'),
-      Long = configuration.require.Long,
-      mockupdb = require('../mock');
+      // Default message fields
+      var defaultFields = {
+        ismaster: true,
+        maxBsonObjectSize: 16777216,
+        maxMessageSizeBytes: 48000000,
+        maxWriteBatchSize: 1000,
+        localTime: new Date(),
+        maxWireVersion: 5,
+        minWireVersion: 0,
+        ok: 1
+      };
 
-    // Contain mock server
-    var singleServer = null;
-    var running = true;
-
-    // Default message fields
-    var defaultFields = {
-      ismaster: true,
-      maxBsonObjectSize: 16777216,
-      maxMessageSizeBytes: 48000000,
-      maxWriteBatchSize: 1000,
-      localTime: new Date(),
-      maxWireVersion: 5,
-      minWireVersion: 0,
-      ok: 1
-    };
-
-    // Primary server states
-    var primary = [extend(defaultFields, {})];
-    var commandResult = null;
-
-    // Boot the mock
-    co(function*() {
-      singleServer = yield mockupdb.createServer(32000, 'localhost');
-
-      // Primary state machine
-      co(function*() {
-        while (running) {
-          var request = yield singleServer.receive();
-          var doc = request.document;
-          // console.log("========================== cmd")
-          // console.dir(doc)
-
-          if (doc.ismaster) {
-            request.reply(primary[0]);
-          } else if (doc.listCollections) {
-            request.reply({
-              ok: 1,
-              cursor: {
-                id: Long.fromNumber(0),
-                ns: 'test.cmd$.listCollections',
-                firstBatch: []
-              }
-            });
-          } else if (doc.create) {
-            commandResult = doc;
-            request.reply({ ok: 1 });
-          }
-        }
-      }).catch(function(err) {
-        // console.log(err.stack);
-      });
-
+      // Primary server states
+      var primary = [assign({}, defaultFields)];
       var commandResult = null;
 
-      // Connect to the mocks
-      MongoClient.connect('mongodb://localhost:32000/test', function(err, client) {
-        test.equal(null, err);
-        var db = client.db(configuration.database);
+      // Boot the mock
+      co(function*() {
+        singleServer = yield mockupdb.createServer(32000, 'localhost');
 
-        // Simple findAndModify command returning the new document
-        db.createCollection('test', { viewOn: 'users', pipeline: [{ $match: {} }] }, function(
-          err,
-          r
-        ) {
-          test.equal(null, err);
-          test.deepEqual(
-            { create: 'test', viewOn: 'users', pipeline: [{ $match: {} }] },
-            commandResult
-          );
+        // Primary state machine
+        co(function*() {
+          while (running) {
+            var request = yield singleServer.receive();
+            var doc = request.document;
 
-          singleServer.destroy();
-          running = false;
+            if (doc.ismaster) {
+              request.reply(primary[0]);
+            } else if (doc.listCollections) {
+              request.reply({
+                ok: 1,
+                cursor: {
+                  id: Long.fromNumber(0),
+                  ns: 'test.cmd$.listCollections',
+                  firstBatch: []
+                }
+              });
+            } else if (doc.create) {
+              commandResult = doc;
+              request.reply({ ok: 1 });
+            }
+          }
+        }).catch(function(err) {
+          // console.log(err.stack);
+        });
 
-          client.close();
-          test.done();
+        var commandResult = null;
+
+        // Connect to the mocks
+        MongoClient.connect('mongodb://localhost:32000/test', function(err, client) {
+          expect(err).to.not.exist;
+          var db = client.db(self.configuration.db);
+
+          // Simple findAndModify command returning the new document
+          db.createCollection('test', { viewOn: 'users', pipeline: [{ $match: {} }] }, function(
+            err,
+            r
+          ) {
+            expect(err).to.not.exist;
+            expect(commandResult).to.eql({ create: 'test', viewOn: 'users', pipeline: [{ $match: {} }] });
+
+            singleServer.destroy();
+            running = false;
+
+            client.close();
+            done();
+          });
         });
       });
-    });
-  }
-};
+    }
+  });
+});
