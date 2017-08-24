258d4e9258995d8b91db44f931a681cc245d35b1
Matt Broadstone <mbroadst@gmail.com>
2017-08-24 14:20:35 -0400

    test(byo-promises): convert `byo_promises` to mocha style
    NODE-1095

--- test/functional/byo_promises_tests.js
+++ test/functional/byo_promises_tests.js
@@ -1,31 +1,33 @@
 'use strict';
+var expect = require('chai').expect;
 
-var f = require('util').format;
+describe('Promises', function() {
+  it('should Correctly Use Blurbird promises library', {
+    metadata: {
+      requires: {
+        topology: ['single', 'ssl', 'wiredtiger']
+      }
+    },
 
-exports['Should Correctly Use Blurbird promises library'] = {
-  metadata: {
-    requires: {
-      topology: ['single', 'ssl', 'wiredtiger']
-    }
-  },
+    // The actual test we wish to run
+    test: function(done) {
+      var self = this;
+      var MongoClient = self.configuration.require.MongoClient;
+      var Promise = require('bluebird');
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient,
-      Promise = require('bluebird');
+      MongoClient.connect(self.configuration.url(), {
+        promiseLibrary: Promise,
+        server: { sslValidate: false }
+      }).then(function(client) {
+        var db = client.db(self.configuration.db);
+        var promise = db.collection('test').insert({ a: 1 });
+        expect(promise).to.be.an.instanceOf(Promise);
 
-    MongoClient.connect(configuration.url(), {
-      promiseLibrary: Promise,
-      server: { sslValidate: false }
-    }).then(function(client) {
-      var db = client.db(configuration.database);
-      var promise = db.collection('test').insert({ a: 1 });
-      test.ok(promise instanceof Promise);
-
-      promise.then(function() {
-        client.close();
-        test.done();
+        promise.then(function() {
+          client.close();
+          done();
+        });
       });
-    });
-  }
-};
+    }
+  });
+});
