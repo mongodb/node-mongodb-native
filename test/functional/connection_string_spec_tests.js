1a4ae4e0085c97d13a9650ea603fb438e7aeb632
Matt Broadstone <mbroadst@gmail.com>
2017-08-24 14:28:19 -0400

    test(connection-string): convert `connection_string` tests to mocha
    NODE-1095

--- test/functional/connection_string_spec_tests.js
+++ test/functional/connection_string_spec_tests.js
@@ -1,65 +1,42 @@
 'use strict';
 
+var parse = require('../../lib/url_parser');
 var fs = require('fs'),
   f = require('util').format;
 
-/**
- * @ignore
- */
-exports['Should run all connection string tests'] = {
-  metadata: { requires: { topology: 'single' } },
+describe('Connection String', function() {
+  var testFiles = fs
+    .readdirSync(f('%s/connection-string', __dirname))
+    .filter(function(x) {
+      return x.indexOf('.json') != -1;
+    })
+    .map(function(x) {
+      return JSON.parse(fs.readFileSync(f('%s/connection-string/%s', __dirname, x)));
+    });
 
-  // The actual test we wish to run
-  test: function(configuration, assert) {
-    var testFiles = fs
-      .readdirSync(f('%s/connection-string', __dirname))
-      .filter(function(x) {
-        return x.indexOf('.json') != -1;
-      })
-      .map(function(x) {
-        return JSON.parse(fs.readFileSync(f('%s/connection-string/%s', __dirname, x)));
+  // Execute the tests
+  for (var i = 0; i < testFiles.length; i++) {
+    var testFile = testFiles[i];
+
+    // Get each test
+    for (var j = 0; j < testFile.tests.length; j++) {
+      var test = testFile.tests[j];
+
+      it(test.description, {
+        metadata: { requires: { topology: 'single' } },
+        test: function(done) {
+          var valid = test.valid;
+
+          try {
+            parse(test.uri);
+            if (valid == false) done('should not have been able to parse');
+          } catch (err) {
+            if (valid == true) done(err);
+          }
+
+          done();
+        }
       });
-
-    var parser = require('../../lib/url_parser');
-
-    // Execute the tests
-    for (var i = 0; i < testFiles.length; i++) {
-      var testFile = testFiles[i];
-
-      // Get each test
-      for (var j = 0; j < testFile.tests.length; j++) {
-        var test = testFile.tests[j];
-        console.log(f('  %s', test.description));
-        // console.dir(test)
-
-        // Unpack the test
-        var auth = test.auth;
-        var description = test.description;
-        var hosts = test.hosts;
-        var options = test.options;
-        var uri = test.uri;
-        var valid = test.valid;
-        var warning = test.warning;
-
-        // Test state
-        var success = true;
-
-        // Parse the test
-        try {
-          var result = parser(test.uri);
-          if (valid == false) success = false;
-        } catch (err) {
-          // console.log(err.stack)
-          if (valid == true) success = false;
-        }
-
-        // If we were unsuccessful
-        if (!success) {
-          throw test;
-        }
-      }
     }
-
-    assert.done();
   }
-};
+});
