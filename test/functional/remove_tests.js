0dae7ead09d02dc41e9b82311502cfc0c6e821e6
Matt Broadstone <mbroadst@gmail.com>
2017-08-25 08:52:31 -0400

    test(remove): convert `remove` tests to mocha style
    NODE-1095

--- test/functional/remove_tests.js
+++ test/functional/remove_tests.js
@@ -1,46 +1,58 @@
 'use strict';
+var test = require('./shared').assert;
+var setupDatabsae = require('./shared').setupDatabase;
 
-/**
- * @ignore
- */
-exports.shouldCorrectlyClearOutCollection = {
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
+describe('Remove', function() {
+  before(function() {
+    return setupDatabsae(this.configuration);
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      test.equal(null, err);
+  /**
+   * @ignore
+   */
+  it('should correctly clear out collection', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
 
-      db.createCollection('test_clear', function(err, r) {
+    // The actual test we wish to run
+    test: function(done) {
+      var self = this;
+      var client = self.configuration.newClient(self.configuration.writeConcernMax(), {
+        poolSize: 1
+      });
+
+      client.connect(function(err, client) {
+        var db = client.db(self.configuration.db);
         test.equal(null, err);
 
-        db.collection('test_clear', function(err, collection) {
+        db.createCollection('test_clear', function(err) {
           test.equal(null, err);
 
-          collection.insert({ i: 1 }, { w: 1 }, function(err, ids) {
+          db.collection('test_clear', function(err, collection) {
             test.equal(null, err);
 
-            collection.insert({ i: 2 }, { w: 1 }, function(err, ids) {
+            collection.insert({ i: 1 }, { w: 1 }, function(err) {
               test.equal(null, err);
 
-              collection.count(function(err, count) {
+              collection.insert({ i: 2 }, { w: 1 }, function(err) {
                 test.equal(null, err);
-                test.equal(2, count);
-                // Clear the collection
-                collection.remove({}, { w: 1 }, function(err, r) {
-                  test.equal(null, err);
-                  test.equal(2, r.result.n);
 
-                  collection.count(function(err, count) {
+                collection.count(function(err, count) {
+                  test.equal(null, err);
+                  test.equal(2, count);
+                  // Clear the collection
+                  collection.remove({}, { w: 1 }, function(err, r) {
                     test.equal(null, err);
-                    test.equal(0, count);
-                    // Let's close the db
-                    client.close();
-                    test.done();
+                    test.equal(2, r.result.n);
+
+                    collection.count(function(err, count) {
+                      test.equal(null, err);
+                      test.equal(0, count);
+                      // Let's close the db
+                      client.close();
+                      done();
+                    });
                   });
                 });
               });
@@ -48,92 +60,97 @@ exports.shouldCorrectlyClearOutCollection = {
           });
         });
       });
-    });
-  }
-};
+    }
+  });
 
-/**
- * @ignore
- */
-exports.shouldCorrectlyRemoveDocumentUsingRegExp = {
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
+  /**
+   * @ignore
+   */
+  it('should correctly remove document using RegExp', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      test.equal(null, err);
+    // The actual test we wish to run
+    test: function(done) {
+      var self = this;
+      var client = self.configuration.newClient(self.configuration.writeConcernMax(), {
+        poolSize: 1
+      });
 
-      db.createCollection('test_remove_regexp', function(err, r) {
+      client.connect(function(err, client) {
+        var db = client.db(self.configuration.db);
         test.equal(null, err);
 
-        db.collection('test_remove_regexp', function(err, collection) {
+        db.createCollection('test_remove_regexp', function(err) {
           test.equal(null, err);
 
-          collection.insert({ address: '485 7th ave new york' }, { w: 1 }, function(err, ids) {
+          db.collection('test_remove_regexp', function(err, collection) {
             test.equal(null, err);
 
-            // Clear the collection
-            collection.remove({ address: /485 7th ave/ }, { w: 1 }, function(err, r) {
-              test.equal(1, r.result.n);
+            collection.insert({ address: '485 7th ave new york' }, { w: 1 }, function(err) {
+              test.equal(null, err);
 
-              collection.count(function(err, count) {
-                test.equal(0, count);
-                // Let's close the db
-                client.close();
-                test.done();
+              // Clear the collection
+              collection.remove({ address: /485 7th ave/ }, { w: 1 }, function(err, r) {
+                test.equal(1, r.result.n);
+
+                collection.count(function(err, count) {
+                  test.equal(0, count);
+                  // Let's close the db
+                  client.close();
+                  done();
+                });
               });
             });
           });
         });
       });
-    });
-  }
-};
+    }
+  });
 
-/**
- * @ignore
- */
-exports.shouldCorrectlyRemoveOnlyFirstDocument = {
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
+  /**
+   * @ignore
+   */
+  it('should correctly remove only first document', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      test.equal(null, err);
+    // The actual test we wish to run
+    test: function(done) {
+      var self = this;
+      var client = self.configuration.newClient(self.configuration.writeConcernMax(), {
+        poolSize: 1
+      });
 
-      db.createCollection('shouldCorrectlyRemoveOnlyFirstDocument', function(err, r) {
+      client.connect(function(err, client) {
+        var db = client.db(self.configuration.db);
         test.equal(null, err);
 
-        db.collection('shouldCorrectlyRemoveOnlyFirstDocument', function(err, collection) {
+        db.createCollection('shouldCorrectlyRemoveOnlyFirstDocument', function(err) {
           test.equal(null, err);
 
-          collection.insert([{ a: 1 }, { a: 1 }, { a: 1 }, { a: 1 }], { w: 1 }, function(
-            err,
-            result
-          ) {
+          db.collection('shouldCorrectlyRemoveOnlyFirstDocument', function(err, collection) {
             test.equal(null, err);
 
-            // Remove the first
-            collection.remove({ a: 1 }, { w: 1, single: true }, function(err, r) {
-              test.equal(1, r.result.n);
+            collection.insert([{ a: 1 }, { a: 1 }, { a: 1 }, { a: 1 }], { w: 1 }, function(err) {
+              test.equal(null, err);
 
-              collection.find({ a: 1 }).count(function(err, result) {
-                test.equal(3, result);
-                client.close();
-                test.done();
+              // Remove the first
+              collection.remove({ a: 1 }, { w: 1, single: true }, function(err, r) {
+                test.equal(1, r.result.n);
+
+                collection.find({ a: 1 }).count(function(err, result) {
+                  test.equal(3, result);
+                  client.close();
+                  done();
+                });
               });
             });
           });
         });
       });
-    });
-  }
-};
+    }
+  });
+});
