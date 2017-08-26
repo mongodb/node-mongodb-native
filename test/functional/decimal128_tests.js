84ad2fa3931cf8ba511aa99062280dbdba6e087c
Matt Broadstone <mbroadst@gmail.com>
2017-08-26 08:27:01 -0400

    test(decimal128): convert `decimal128` tests to mocha style
    NODE-1095

--- test/functional/decimal128_tests.js
+++ test/functional/decimal128_tests.js
@@ -1,42 +1,53 @@
-/**
- * @ignore
- */
-exports['should correctly insert decimal128 value'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    requires: {
-      mongodb: '>=3.3.6',
-      topology: ['single']
-    }
-  },
+'use strict';
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Decimal128 = configuration.require.Decimal128;
+describe('Decimal128', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      var object = {
-        id: 1,
-        value: Decimal128.fromString('1')
-      };
+  /**
+   * @ignore
+   */
+  it('should correctly insert decimal128 value', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      requires: {
+        mongodb: '>=3.3.6',
+        topology: ['single']
+      }
+    },
 
-      db.collection('decimal128').insertOne(object, function(err, r) {
-        test.equal(null, err);
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var Decimal128 = configuration.require.Decimal128;
 
-        db.collection('decimal128').findOne({
-          id: 1
-        }, function(err, doc) {
+      var client = configuration.newClient(configuration.writeConcernMax(), { poolSize: 1 });
+      client.connect(function(err, client) {
+        var db = client.db(configuration.db);
+        var object = {
+          id: 1,
+          value: Decimal128.fromString('1')
+        };
+
+        db.collection('decimal128').insertOne(object, function(err) {
           test.equal(null, err);
-          test.ok(doc.value instanceof Decimal128);
-          test.equal('1', doc.value.toString());
 
-          client.close();
-          test.done();
+          db.collection('decimal128').findOne({
+            id: 1
+          }, function(err, doc) {
+            test.equal(null, err);
+            test.ok(doc.value instanceof Decimal128);
+            test.equal('1', doc.value.toString());
+
+            client.close();
+            done();
+          });
         });
       });
-    });
-  }
-};
+    }
+  });
+});
