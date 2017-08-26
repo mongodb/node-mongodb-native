794ca2a8b069969b2ccdf8240b2b4b4ba77e33c6
Matt Broadstone <mbroadst@gmail.com>
2017-08-26 17:08:50 -0400

    test(promises): convert `promises_cursor` tests to mocha style
    NODE-1095

--- test/functional/promises_cursor_tests.js
+++ test/functional/promises_cursor_tests.js
@@ -1,33 +1,41 @@
 'use strict';
-
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 var f = require('util').format;
 
-exports['Should correctly execute Collection.prototype.insertOne as promise'] = {
-  metadata: {
-    requires: {
-      promises: true,
-      node: '>0.8.0',
-      topology: ['single']
-    }
-  },
+describe('Promises (Cursor)', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
-    var url = configuration.url();
-    url =
-      url.indexOf('?') != -1
-        ? f('%s&%s', url, 'maxPoolSize=100')
-        : f('%s?%s', url, 'maxPoolSize=100');
+  it('Should correctly execute Collection.prototype.insertOne as promise', {
+    metadata: {
+      requires: {
+        promises: true,
+        node: '>0.8.0',
+        topology: ['single']
+      }
+    },
 
-    MongoClient.connect(url).then(function(client) {
-      var db = client.db(configuration.database);
-      test.equal(1, client.topology.connections().length);
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+      var url = configuration.url();
+      url =
+        url.indexOf('?') != -1
+          ? f('%s&%s', url, 'maxPoolSize=100')
+          : f('%s?%s', url, 'maxPoolSize=100');
 
-      db.collection('insertOne').insertOne({ a: 1 }).then(function(r) {
-        client.close();
-        test.done();
+      MongoClient.connect(url).then(function(client) {
+        var db = client.db(configuration.db);
+        test.equal(1, client.topology.connections().length);
+
+        db.collection('insertOne').insertOne({ a: 1 }).then(function() {
+          client.close();
+          done();
+        });
       });
-    });
-  }
-};
+    }
+  });
+});
