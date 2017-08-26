98bc20618936cec5c3a2a80c55d5d526961b0c81
Matt Broadstone <mbroadst@gmail.com>
2017-08-26 08:29:46 -0400

    test(domain): convert `domain` tests to mocha style
    NODE-1095

--- test/functional/domain_tests.js
+++ test/functional/domain_tests.js
@@ -1,63 +1,34 @@
 'use strict';
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 
-/**
- * @ignore
- */
-exports.shouldStayInCorrectDomainForReadCommand = {
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
+describe('Decimal128', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Domain = require('domain');
-    var domainInstance = Domain.create();
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), {
-      poolSize: 1,
-      domainsEnabled: true
-    });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      test.ok(!err);
-      var collection = db.collection('test');
+  /**
+   * @ignore
+   */
+  it('shouldStayInCorrectDomainForReadCommand', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
 
-      domainInstance.run(function() {
-        collection.count({}, function(err) {
-          test.ok(!err);
-          test.ok(domainInstance === process.domain);
-          domainInstance.exit();
-          domainInstance.dispose();
-          client.close();
-          test.done();
-        });
-      });
-    });
-  }
-};
-
-/**
- * @ignore
- */
-exports.shouldStayInCorrectDomainForReadCommandUsingMongoClient = {
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
-    var Domain = require('domain');
-    var domainInstance = Domain.create();
-
-    MongoClient.connect(
-      configuration.url(),
-      {
+    // The actual test we wish to run
+    test: function(done) {
+      var Domain = require('domain');
+      var domainInstance = Domain.create();
+      var configuration = this.configuration;
+      var client = configuration.newClient(configuration.writeConcernMax(), {
+        poolSize: 1,
         domainsEnabled: true
-      },
-      function(err, client) {
+      });
+      client.connect(function(err, client) {
+        var db = client.db(configuration.db);
         test.ok(!err);
-        var db = client.db(configuration.database);
         var collection = db.collection('test');
+
         domainInstance.run(function() {
           collection.count({}, function(err) {
             test.ok(!err);
@@ -65,123 +36,165 @@ exports.shouldStayInCorrectDomainForReadCommandUsingMongoClient = {
             domainInstance.exit();
             domainInstance.dispose();
             client.close();
-            test.done();
+            done();
           });
         });
-      }
-    );
-  }
-};
-
-/**
- * @ignore
- */
-exports.shouldStayInCorrectDomainForWriteCommand = {
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Domain = require('domain');
-    var domainInstance = Domain.create();
-    var client = configuration.newDbInstance(
-      { w: 1 },
-      { poolSize: 1, auto_reconnect: true, domainsEnabled: true }
-    );
-
-    client.connect(function(err, client) {
-      test.ok(!err);
-      var db = client.db(configuration.database);
-      var collection = db.collection('test');
-      domainInstance.run(function() {
-        collection.insert({ field: 123 }, function(err) {
-          test.ok(!err);
-          test.ok(domainInstance === process.domain);
-          domainInstance.exit();
-          domainInstance.dispose();
-          client.close();
-          test.done();
-        });
       });
-    });
-  }
-};
-
-/**
- * @ignore
- */
-exports.shouldStayInCorrectDomainForQueuedReadCommand = {
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Domain = require('domain');
-    var domainInstance = Domain.create();
-    var client = configuration.newDbInstance(
-      { w: 0, bufferMaxEntries: 0 },
-      { poolSize: 1, auto_reconnect: true, domainsEnabled: true }
-    );
-
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      var connection = client.topology.connections()[0];
-      var collection = db.collection('test');
-      connection.destroy();
-
-      domainInstance.run(function() {
-        collection.count({}, function(err, c) {
-          test.ok(err != null);
-          test.ok(process.domain === domainInstance);
-          domainInstance.exit();
-          domainInstance.dispose();
-          client.close();
-          test.done();
-        });
-      });
-    });
-  }
-};
-
-/**
- * @ignore
- */
-exports.shouldStayInCorrectDomainForQueuedWriteCommand = {
-  metadata: {
-    requires: {
-      node: '>=0.10.x',
-      topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger']
     }
-  },
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Domain = require('domain');
-    var domainInstance = Domain.create();
-    var client = configuration.newDbInstance(
-      { w: 1, bufferMaxEntries: 0 },
-      { poolSize: 1, auto_reconnect: true, domainsEnabled: true }
-    );
+  /**
+   * @ignore
+   */
+  it('shouldStayInCorrectDomainForReadCommandUsingMongoClient', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
 
-    client.connect(function(err, client) {
-      test.ok(!err);
-      var db = client.db(configuration.database);
-      var connection = client.topology.connections()[0];
-      var collection = db.collection('test');
-      connection.destroy();
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+      var Domain = require('domain');
+      var domainInstance = Domain.create();
 
-      domainInstance.run(function() {
-        collection.insert({ field: 123 }, function(err) {
-          test.ok(err != null);
-          test.ok(process.domain === domainInstance);
-          domainInstance.exit();
-          domainInstance.dispose();
-          client.close();
-          test.done();
+      MongoClient.connect(
+        configuration.url(),
+        {
+          domainsEnabled: true
+        },
+        function(err, client) {
+          test.ok(!err);
+          var db = client.db(configuration.db);
+          var collection = db.collection('test');
+          domainInstance.run(function() {
+            collection.count({}, function(err) {
+              test.ok(!err);
+              test.ok(domainInstance === process.domain);
+              domainInstance.exit();
+              domainInstance.dispose();
+              client.close();
+              done();
+            });
+          });
+        }
+      );
+    }
+  });
+
+  /**
+   * @ignore
+   */
+  it('shouldStayInCorrectDomainForWriteCommand', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var Domain = require('domain');
+      var domainInstance = Domain.create();
+      var configuration = this.configuration;
+      var client = configuration.newClient(
+        { w: 1 },
+        { poolSize: 1, auto_reconnect: true, domainsEnabled: true }
+      );
+
+      client.connect(function(err, client) {
+        test.ok(!err);
+        var db = client.db(configuration.db);
+        var collection = db.collection('test');
+        domainInstance.run(function() {
+          collection.insert({ field: 123 }, function(err) {
+            test.ok(!err);
+            test.ok(domainInstance === process.domain);
+            domainInstance.exit();
+            domainInstance.dispose();
+            client.close();
+            done();
+          });
         });
       });
-    });
-  }
-};
+    }
+  });
+
+  /**
+   * @ignore
+   */
+  it('shouldStayInCorrectDomainForQueuedReadCommand', {
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var Domain = require('domain');
+      var domainInstance = Domain.create();
+      var configuration = this.configuration;
+      var client = configuration.newClient(
+        { w: 0, bufferMaxEntries: 0 },
+        { poolSize: 1, auto_reconnect: true, domainsEnabled: true }
+      );
+
+      client.connect(function(err, client) {
+        var db = client.db(configuration.db);
+        var connection = client.topology.connections()[0];
+        var collection = db.collection('test');
+        connection.destroy();
+
+        domainInstance.run(function() {
+          collection.count({}, function(err) {
+            test.ok(err != null);
+            test.ok(process.domain === domainInstance);
+            domainInstance.exit();
+            domainInstance.dispose();
+            client.close();
+            done();
+          });
+        });
+      });
+    }
+  });
+
+  /**
+   * @ignore
+   */
+  it('shouldStayInCorrectDomainForQueuedWriteCommand', {
+    metadata: {
+      requires: {
+        node: '>=0.10.x',
+        topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger']
+      }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var Domain = require('domain');
+      var domainInstance = Domain.create();
+      var configuration = this.configuration;
+      var client = configuration.newClient(
+        { w: 1, bufferMaxEntries: 0 },
+        { poolSize: 1, auto_reconnect: true, domainsEnabled: true }
+      );
+
+      client.connect(function(err, client) {
+        test.ok(!err);
+        var db = client.db(configuration.db);
+        var connection = client.topology.connections()[0];
+        var collection = db.collection('test');
+        connection.destroy();
+
+        domainInstance.run(function() {
+          collection.insert({ field: 123 }, function(err) {
+            test.ok(err != null);
+            test.ok(process.domain === domainInstance);
+            domainInstance.exit();
+            domainInstance.dispose();
+            client.close();
+            done();
+          });
+        });
+      });
+    }
+  });
+});
