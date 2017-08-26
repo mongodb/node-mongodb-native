33486c4f533e4386a67da81087e665bccbda88f3
Matt Broadstone <mbroadst@gmail.com>
2017-08-26 10:36:34 -0400

    test(sdam): convert `sdam` tests to mocha style
    NODE-1095

--- test/functional/sdam_tests.js
+++ test/functional/sdam_tests.js
@@ -1,153 +1,161 @@
-exports['Should correctly emit all Replicaset SDAM operations'] = {
-  metadata: { requires: { topology: 'replicaset' } },
+'use strict';
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
-    var operations = {
-      serverDescriptionChanged: [],
-      serverHeartbeatStarted: [],
-      serverHeartbeatSucceeded: [],
-      serverOpening: [],
-      serverClosed: [],
-      topologyOpening: [],
-      topologyDescriptionChanged: [],
-      topologyClosed: []
-    };
+describe('SDAM', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-    var client = new MongoClient(configuration.url());
-    var events = [
-      'serverDescriptionChanged',
-      'serverHeartbeatStarted',
-      'serverHeartbeatSucceeded',
-      'serverOpening',
-      'serverClosed',
-      'topologyOpening',
-      'topologyDescriptionChanged',
-      'topologyClosed'
-    ];
-    events.forEach(function(e) {
-      client.on(e, function(result) {
-        operations[e].push(result);
+  it('Should correctly emit all Replicaset SDAM operations', {
+    metadata: { requires: { topology: 'replicaset' } },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+      var operations = {
+        serverDescriptionChanged: [],
+        serverHeartbeatStarted: [],
+        serverHeartbeatSucceeded: [],
+        serverOpening: [],
+        serverClosed: [],
+        topologyOpening: [],
+        topologyDescriptionChanged: [],
+        topologyClosed: []
+      };
+
+      var client = new MongoClient(configuration.url());
+      var events = [
+        'serverDescriptionChanged',
+        'serverHeartbeatStarted',
+        'serverHeartbeatSucceeded',
+        'serverOpening',
+        'serverClosed',
+        'topologyOpening',
+        'topologyDescriptionChanged',
+        'topologyClosed'
+      ];
+      events.forEach(function(e) {
+        client.on(e, function(result) {
+          operations[e].push(result);
+        });
       });
-    });
 
-    client.on('fullsetup', function(topology) {
-      topology.close(true);
+      client.on('fullsetup', function(topology) {
+        topology.close(true);
 
-      // console.log(JSON.stringify(operations.topologyDescriptionChanged, null, 2));
-
-      for (var name in operations) {
-        test.ok(operations[name].length > 0);
-      }
-
-      test.done();
-    });
-
-    client.connect(function(err, db) {
-      test.equal(null, err);
-    });
-  }
-};
-
-exports['Should correctly emit all Mongos SDAM operations'] = {
-  metadata: { requires: { topology: 'sharded' } },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
-    var operations = {
-      serverDescriptionChanged: [],
-      serverHeartbeatStarted: [],
-      serverHeartbeatSucceeded: [],
-      serverOpening: [],
-      serverClosed: [],
-      topologyOpening: [],
-      topologyDescriptionChanged: [],
-      topologyClosed: []
-    };
-
-    var client = new MongoClient(configuration.url(), { haInterval: 500 });
-    var events = [
-      'serverDescriptionChanged',
-      'serverHeartbeatStarted',
-      'serverHeartbeatSucceeded',
-      'serverOpening',
-      'serverClosed',
-      'topologyOpening',
-      'topologyDescriptionChanged',
-      'topologyClosed'
-    ];
-    events.forEach(function(e) {
-      client.on(e, function(result) {
-        operations[e].push(result);
-      });
-    });
-
-    client.on('fullsetup', function(topology) {
-      setTimeout(function() {
-        topology.close();
-
-        // console.log(JSON.stringify(operations, null, 2));
+        // console.log(JSON.stringify(operations.topologyDescriptionChanged, null, 2));
 
         for (var name in operations) {
           test.ok(operations[name].length > 0);
         }
 
-        test.done();
-      }, 1000);
-    });
-
-    client.connect(function(err, db) {
-      test.equal(null, err);
-    });
-  }
-};
-
-exports['Should correctly emit all Server SDAM operations'] = {
-  metadata: { requires: { topology: 'single' } },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
-    var operations = {
-      serverDescriptionChanged: [],
-      serverOpening: [],
-      serverClosed: [],
-      topologyOpening: [],
-      topologyDescriptionChanged: [],
-      topologyClosed: []
-    };
-
-    var client = new MongoClient(configuration.url());
-    var events = [
-      'serverDescriptionChanged',
-      'serverOpening',
-      'serverClosed',
-      'topologyOpening',
-      'topologyDescriptionChanged',
-      'topologyClosed'
-    ];
-    events.forEach(function(e) {
-      client.on(e, function(result) {
-        operations[e].push(result);
+        done();
       });
-    });
 
-    client.connect(function(err, client) {
-      test.equal(null, err);
-      client.close(true);
+      client.connect(function(err) {
+        test.equal(null, err);
+      });
+    }
+  });
 
-      // console.log("########################################")
-      // console.dir(Object.keys(operations))
+  it('Should correctly emit all Mongos SDAM operations', {
+    metadata: { requires: { topology: 'sharded' } },
 
-      for (var name in operations) {
-        // console.log("" + name + " :: " + operations[name].length)
-        test.ok(operations[name].length > 0);
-      }
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+      var operations = {
+        serverDescriptionChanged: [],
+        serverHeartbeatStarted: [],
+        serverHeartbeatSucceeded: [],
+        serverOpening: [],
+        serverClosed: [],
+        topologyOpening: [],
+        topologyDescriptionChanged: [],
+        topologyClosed: []
+      };
 
-      // console.log(JSON.stringify(operations, null, 2));
-      test.done();
-    });
-  }
-};
+      var client = new MongoClient(configuration.url(), { haInterval: 500 });
+      var events = [
+        'serverDescriptionChanged',
+        'serverHeartbeatStarted',
+        'serverHeartbeatSucceeded',
+        'serverOpening',
+        'serverClosed',
+        'topologyOpening',
+        'topologyDescriptionChanged',
+        'topologyClosed'
+      ];
+      events.forEach(function(e) {
+        client.on(e, function(result) {
+          operations[e].push(result);
+        });
+      });
+
+      client.on('fullsetup', function(topology) {
+        setTimeout(function() {
+          topology.close();
+
+          // console.log(JSON.stringify(operations, null, 2));
+
+          for (var name in operations) {
+            test.ok(operations[name].length > 0);
+          }
+
+          done();
+        }, 1000);
+      });
+
+      client.connect(function(err) {
+        test.equal(null, err);
+      });
+    }
+  });
+
+  it('Should correctly emit all Server SDAM operations', {
+    metadata: { requires: { topology: 'single' } },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+      var operations = {
+        serverDescriptionChanged: [],
+        serverOpening: [],
+        serverClosed: [],
+        topologyOpening: [],
+        topologyDescriptionChanged: [],
+        topologyClosed: []
+      };
+
+      var client = new MongoClient(configuration.url());
+      var events = [
+        'serverDescriptionChanged',
+        'serverOpening',
+        'serverClosed',
+        'topologyOpening',
+        'topologyDescriptionChanged',
+        'topologyClosed'
+      ];
+      events.forEach(function(e) {
+        client.on(e, function(result) {
+          operations[e].push(result);
+        });
+      });
+
+      client.connect(function(err, client) {
+        test.equal(null, err);
+        client.close(true);
+
+        for (var name in operations) {
+          test.ok(operations[name].length > 0);
+        }
+
+        done();
+      });
+    }
+  });
+});
