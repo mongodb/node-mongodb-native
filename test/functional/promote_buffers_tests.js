4072f22f3e263d74e09196e383c9a6e885c8293c
Matt Broadstone <mbroadst@gmail.com>
2017-08-26 10:33:45 -0400

    test(promote-buffers): convert `promote_buffers` tests to mocha style
    NODE-1095

--- test/functional/promote_buffers_tests.js
+++ test/functional/promote_buffers_tests.js
@@ -1,193 +1,191 @@
+'use strict';
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 var Buffer = require('buffer').Buffer;
 
-exports['should correctly honor promoteBuffers when creating an instance using Db'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
+describe('Promote Buffers', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Long = configuration.require.Long,
-      Int32 = configuration.require.Int32,
-      Double = configuration.require.Double;
+  it('should correctly honor promoteBuffers when creating an instance using Db', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
 
-    var o = configuration.writeConcernMax();
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), {
-      poolSize: 1,
-      promoteBuffers: true
-    });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      db.collection('shouldCorrectlyHonorPromoteBuffer1').insert({
-        doc: new Buffer(256)
-      }, function(err, doc) {
-        test.equal(null, err);
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var client = configuration.newClient(configuration.writeConcernMax(), {
+        poolSize: 1,
+        promoteBuffers: true
+      });
 
-        db.collection('shouldCorrectlyHonorPromoteBuffer1').findOne(function(err, doc) {
+      client.connect(function(err, client) {
+        var db = client.db(configuration.db);
+        db.collection('shouldCorrectlyHonorPromoteBuffer1').insert({
+          doc: new Buffer(256)
+        }, function(err) {
           test.equal(null, err);
-          test.ok(doc.doc instanceof Buffer);
 
-          client.close();
-          test.done();
+          db.collection('shouldCorrectlyHonorPromoteBuffer1').findOne(function(err, doc) {
+            test.equal(null, err);
+            test.ok(doc.doc instanceof Buffer);
+
+            client.close();
+            done();
+          });
         });
       });
-    });
-  }
-};
-
-exports['should correctly honor promoteBuffers when creating an instance using MongoClient'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Long = configuration.require.Long,
-      Int32 = configuration.require.Int32,
-      Double = configuration.require.Double,
-      MongoClient = configuration.require.MongoClient;
-
-    MongoClient.connect(
-      configuration.url(),
-      {
-        promoteBuffers: true
-      },
-      function(err, client) {
-        var db = client.db(configuration.database);
-
-        db.collection('shouldCorrectlyHonorPromoteBuffer2').insert({
-          doc: new Buffer(256)
-        }, function(err, doc) {
-          test.equal(null, err);
-
-          db.collection('shouldCorrectlyHonorPromoteBuffer2').findOne(function(err, doc) {
-            test.equal(null, err);
-            test.ok(doc.doc instanceof Buffer);
-
-            client.close();
-            test.done();
-          });
-        });
-      }
-    );
-  }
-};
-
-exports['should correctly honor promoteBuffers at cursor level'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Long = configuration.require.Long,
-      Int32 = configuration.require.Int32,
-      Double = configuration.require.Double,
-      MongoClient = configuration.require.MongoClient;
-
-    MongoClient.connect(
-      configuration.url(),
-      {
-        promoteBuffers: true
-      },
-      function(err, client) {
-        var db = client.db(configuration.database);
-
-        db.collection('shouldCorrectlyHonorPromoteBuffer3').insert({
-          doc: new Buffer(256)
-        }, function(err, doc) {
-          test.equal(null, err);
-
-          db.collection('shouldCorrectlyHonorPromoteBuffer3').find().next(function(err, doc) {
-            test.equal(null, err);
-            test.ok(doc.doc instanceof Buffer);
-
-            client.close();
-            test.done();
-          });
-        });
-      }
-    );
-  }
-};
-
-exports['should correctly honor promoteBuffers at cursor find level'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Long = configuration.require.Long,
-      Int32 = configuration.require.Int32,
-      Double = configuration.require.Double,
-      MongoClient = configuration.require.MongoClient;
-
-    MongoClient.connect(configuration.url(), {}, function(err, client) {
-      var db = client.db(configuration.database);
-      db.collection('shouldCorrectlyHonorPromoteBuffer4').insert({
-        doc: new Buffer(256)
-      }, function(err, doc) {
-        test.equal(null, err);
-
-        db
-          .collection('shouldCorrectlyHonorPromoteBuffer4')
-          .find({}, {}, { promoteBuffers: true })
-          .next(function(err, doc) {
-            test.equal(null, err);
-            test.ok(doc.doc instanceof Buffer);
-
-            client.close();
-            test.done();
-          });
-      });
-    });
-  }
-};
-
-exports['should correctly honor promoteBuffers at aggregate level'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    requires: {
-      topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'],
-      mongodb: '>=2.4.0'
     }
-  },
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Long = configuration.require.Long,
-      Int32 = configuration.require.Int32,
-      Double = configuration.require.Double,
-      MongoClient = configuration.require.MongoClient;
+  it('should correctly honor promoteBuffers when creating an instance using MongoClient', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
 
-    MongoClient.connect(configuration.url(), {}, function(err, client) {
-      var db = client.db(configuration.database);
-      db.collection('shouldCorrectlyHonorPromoteBuffer5').insert({
-        doc: new Buffer(256)
-      }, function(err, doc) {
-        test.equal(null, err);
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
 
-        db
-          .collection('shouldCorrectlyHonorPromoteBuffer5')
-          .aggregate([{ $match: {} }], { promoteBuffers: true })
-          .next(function(err, doc) {
+      MongoClient.connect(
+        configuration.url(),
+        {
+          promoteBuffers: true
+        },
+        function(err, client) {
+          var db = client.db(configuration.db);
+
+          db.collection('shouldCorrectlyHonorPromoteBuffer2').insert({
+            doc: new Buffer(256)
+          }, function(err) {
             test.equal(null, err);
-            test.ok(doc.doc instanceof Buffer);
 
-            client.close();
-            test.done();
+            db.collection('shouldCorrectlyHonorPromoteBuffer2').findOne(function(err, doc) {
+              test.equal(null, err);
+              test.ok(doc.doc instanceof Buffer);
+
+              client.close();
+              done();
+            });
           });
+        }
+      );
+    }
+  });
+
+  it('should correctly honor promoteBuffers at cursor level', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+
+      MongoClient.connect(
+        configuration.url(),
+        {
+          promoteBuffers: true
+        },
+        function(err, client) {
+          var db = client.db(configuration.db);
+
+          db.collection('shouldCorrectlyHonorPromoteBuffer3').insert({
+            doc: new Buffer(256)
+          }, function(err) {
+            test.equal(null, err);
+
+            db.collection('shouldCorrectlyHonorPromoteBuffer3').find().next(function(err, doc) {
+              test.equal(null, err);
+              test.ok(doc.doc instanceof Buffer);
+
+              client.close();
+              done();
+            });
+          });
+        }
+      );
+    }
+  });
+
+  it('should correctly honor promoteBuffers at cursor find level', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+
+      MongoClient.connect(configuration.url(), {}, function(err, client) {
+        var db = client.db(configuration.db);
+        db.collection('shouldCorrectlyHonorPromoteBuffer4').insert({
+          doc: new Buffer(256)
+        }, function(err) {
+          test.equal(null, err);
+
+          db
+            .collection('shouldCorrectlyHonorPromoteBuffer4')
+            .find({}, {}, { promoteBuffers: true })
+            .next(function(err, doc) {
+              test.equal(null, err);
+              test.ok(doc.doc instanceof Buffer);
+
+              client.close();
+              done();
+            });
+        });
       });
-    });
-  }
-};
+    }
+  });
+
+  it('should correctly honor promoteBuffers at aggregate level', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      requires: {
+        topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'],
+        mongodb: '>=2.4.0'
+      }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+
+      MongoClient.connect(configuration.url(), {}, function(err, client) {
+        var db = client.db(configuration.db);
+        db.collection('shouldCorrectlyHonorPromoteBuffer5').insert({
+          doc: new Buffer(256)
+        }, function(err) {
+          test.equal(null, err);
+
+          db
+            .collection('shouldCorrectlyHonorPromoteBuffer5')
+            .aggregate([{ $match: {} }], { promoteBuffers: true })
+            .next(function(err, doc) {
+              test.equal(null, err);
+              test.ok(doc.doc instanceof Buffer);
+
+              client.close();
+              done();
+            });
+        });
+      });
+    }
+  });
+});
