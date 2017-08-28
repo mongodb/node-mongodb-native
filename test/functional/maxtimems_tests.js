fef9bad706a464ef9dae104deb1836aa99dc6fc0
Matt Broadstone <mbroadst@gmail.com>
2017-08-28 16:36:41 -0400

    test(maxtimems): convert `maxtimems` tests to mocha style
    NODE-1095

--- test/functional/maxtimems_tests.js
+++ test/functional/maxtimems_tests.js
@@ -1,127 +1,138 @@
 'use strict';
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 
-exports['Should Correctly respect the maxtimeMs property on count'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    disabled: true,
-    requires: {
-      mongodb: '>2.5.5',
-      topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger']
-    }
-  },
+describe('Unicode', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      var col = db.collection('max_time_ms');
+  it('Should Correctly respect the maxtimeMs property on count', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      disabled: true,
+      requires: {
+        mongodb: '>2.5.5',
+        topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger']
+      }
+    },
 
-      // Insert a couple of docs
-      var docs_1 = [{ agg_pipe: 1 }];
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var client = configuration.newClient(configuration.writeConcernMax(), { poolSize: 1 });
+      client.connect(function(err, client) {
+        var db = client.db(configuration.db);
+        var col = db.collection('max_time_ms');
 
-      // Simple insert
-      col.insert(docs_1, { w: 1 }, function(err, result) {
-        test.equal(null, err);
+        // Insert a couple of docs
+        var docs_1 = [{ agg_pipe: 1 }];
 
-        // Execute a find command
-        col.find({ $where: 'sleep(100) || true' }).maxTimeMS(50).count(function(err, count) {
-          test.ok(err != null);
-          client.close();
-          test.done();
-        });
-      });
-    });
-  }
-};
+        // Simple insert
+        col.insert(docs_1, { w: 1 }, function(err) {
+          test.equal(null, err);
 
-exports['Should Correctly respect the maxtimeMs property on toArray'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    disabled: true,
-
-    requires: {
-      topology: ['single', 'replicaset'],
-      mongodb: '>2.5.5'
-    }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      var col = db.collection('max_time_ms_2');
-
-      // Insert a couple of docs
-      var docs_1 = [{ agg_pipe: 1 }];
-
-      // Simple insert
-      col.insert(docs_1, { w: 1 }, function(err, result) {
-        test.equal(null, err);
-
-        // Execute a find command
-        col.find({ $where: 'sleep(100) || true' }).maxTimeMS(50).toArray(function(err, items) {
-          console.dir(err);
-          test.ok(err != null);
-          client.close();
-          test.done();
-        });
-      });
-    });
-  }
-};
-
-exports['Should Correctly fail with maxTimeMS error'] = {
-  // Add a tag that our runner can trigger on
-  // in this case we are setting that node needs to be higher than 0.10.X to run
-  metadata: {
-    requires: {
-      topology: ['single', 'replicaset'],
-      mongodb: '>2.5.5'
-    }
-  },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var client = configuration.newDbInstance(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect(function(err, client) {
-      var db = client.db(configuration.database);
-      var col = db.collection('max_time_ms_5');
-
-      // Insert a couple of docs
-      var docs_1 = [{ agg_pipe: 10 }];
-
-      // Simple insert
-      col.insert(docs_1, { w: 1 }, function(err, result) {
-        test.equal(null, err);
-
-        db
-          .admin()
-          .command({ configureFailPoint: 'maxTimeAlwaysTimeOut', mode: 'alwaysOn' }, function(
-            err,
-            result
-          ) {
-            test.equal(true, result.ok);
-
-            col.find({}).maxTimeMS(10).toArray(function(err, docs) {
-              test.ok(err != null);
-
-              db
-                .admin()
-                .command({ configureFailPoint: 'maxTimeAlwaysTimeOut', mode: 'off' }, function(
-                  err,
-                  result
-                ) {
-                  test.equal(true, result.ok);
-                  client.close();
-                  test.done();
-                });
-            });
+          // Execute a find command
+          col.find({ $where: 'sleep(100) || true' }).maxTimeMS(50).count(function(err) {
+            test.ok(err != null);
+            client.close();
+            done();
           });
+        });
       });
-    });
-  }
-};
+    }
+  });
+
+  it('Should Correctly respect the maxtimeMs property on toArray', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      disabled: true,
+      requires: {
+        topology: ['single', 'replicaset'],
+        mongodb: '>2.5.5'
+      }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var client = configuration.newClient(configuration.writeConcernMax(), { poolSize: 1 });
+      client.connect(function(err, client) {
+        var db = client.db(configuration.db);
+        var col = db.collection('max_time_ms_2');
+
+        // Insert a couple of docs
+        var docs_1 = [{ agg_pipe: 1 }];
+
+        // Simple insert
+        col.insert(docs_1, { w: 1 }, function(err) {
+          test.equal(null, err);
+
+          // Execute a find command
+          col.find({ $where: 'sleep(100) || true' }).maxTimeMS(50).toArray(function(err) {
+            test.ok(err != null);
+            client.close();
+            done();
+          });
+        });
+      });
+    }
+  });
+
+  it('Should Correctly fail with maxTimeMS error', {
+    // Add a tag that our runner can trigger on
+    // in this case we are setting that node needs to be higher than 0.10.X to run
+    metadata: {
+      requires: {
+        topology: ['single', 'replicaset'],
+        mongodb: '>2.5.5'
+      }
+    },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var client = configuration.newClient(configuration.writeConcernMax(), { poolSize: 1 });
+      client.connect(function(err, client) {
+        var db = client.db(configuration.db);
+        var col = db.collection('max_time_ms_5');
+
+        // Insert a couple of docs
+        var docs_1 = [{ agg_pipe: 10 }];
+
+        // Simple insert
+        col.insert(docs_1, { w: 1 }, function(err) {
+          test.equal(null, err);
+
+          db
+            .admin()
+            .command({ configureFailPoint: 'maxTimeAlwaysTimeOut', mode: 'alwaysOn' }, function(
+              err,
+              result
+            ) {
+              test.equal(null, err);
+              test.equal(1, result.ok);
+
+              col.find({}).maxTimeMS(10).toArray(function(err) {
+                test.ok(err != null);
+
+                db
+                  .admin()
+                  .command({ configureFailPoint: 'maxTimeAlwaysTimeOut', mode: 'off' }, function(
+                    err,
+                    result
+                  ) {
+                    test.equal(null, err);
+                    test.equal(1, result.ok);
+                    client.close();
+                    done();
+                  });
+              });
+            });
+        });
+      });
+    }
+  });
+});
