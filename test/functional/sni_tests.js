c607066712c7c5244a8935677946625e496231db
Matt Broadstone <mbroadst@gmail.com>
2017-08-30 15:53:54 -0400

    test(sni): convert `sni` tests to mocha style
    NODE-1095

--- test/functional/sni_tests.js
+++ test/functional/sni_tests.js
@@ -1,52 +1,56 @@
 'use strict';
+var test = require('./shared').assert;
 
-/**
- * @ignore
- */
-exports['Should correct connect to snitest1.10gen.cc'] = {
-  metadata: { requires: { topology: 'sni', os: '!win32' } },
+describe('SNI', function() {
+  /**
+   * @ignore
+   */
+  it('Should correct connect to snitest1.10gen.cc', {
+    metadata: { requires: { topology: 'sni', os: '!win32' } },
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
 
-    // Let's write the actual connection code
-    MongoClient.connect(
-      'mongodb://snitest2.mongodb.com:27777/?ssl=true',
-      {
-        // servername: 'snitest1.10gen.cc',
-      },
-      function(err, client) {
-        console.dir(err);
-        test.equal(null, err);
-        client.close();
-        test.done();
-      }
-    );
-  }
-};
+      // Let's write the actual connection code
+      MongoClient.connect(
+        'mongodb://snitest2.mongodb.com:27777/?ssl=true',
+        {
+          // servername: 'snitest1.10gen.cc',
+        },
+        function(err, client) {
+          test.equal(null, err);
+          client.close();
+          done();
+        }
+      );
+    }
+  });
 
-/**
- * @ignore
- */
-exports['Should correct connect to snitest2.mongodb.com'] = {
-  metadata: { requires: { topology: 'sni', os: '!win32' } },
+  /**
+   * @ignore
+   */
+  it('Should correct connect to snitest2.mongodb.com', {
+    metadata: { requires: { topology: 'sni', os: '!win32' } },
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
 
-    // Let's write the actual connection code
-    MongoClient.connect(
-      'mongodb://snitest2.mongodb.com:27777/?ssl=true',
-      {
-        // servername: 'snitest2.mongodb.com',
-      },
-      function(err, client) {
-        test.equal(null, err);
-        client.close();
-        test.done();
-      }
-    );
-  }
-};
+      // Let's write the actual connection code
+      MongoClient.connect(
+        'mongodb://snitest2.mongodb.com:27777/?ssl=true',
+        {
+          // servername: 'snitest2.mongodb.com',
+        },
+        function(err, client) {
+          test.equal(null, err);
+          client.close();
+          done();
+        }
+      );
+    }
+  });
+});
