1ae2b1ab40efb997a7f5b87b966da5fc902ae4d9
Matt Broadstone <mbroadst@gmail.com>
2017-08-30 14:47:24 -0400

    test(scram): convert `scram` tests to mocha style
    NODE-1095

--- test/functional/scram_tests.js
+++ test/functional/scram_tests.js
@@ -1,72 +1,79 @@
 'use strict';
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 
-/**
- * rm -rf data; mkdir data; mongod --dbpath=./data --setParameter authenticationMechanisms=SCRAM-SHA-1 --auth
- * @ignore
- */
-exports['Should correctly authenticate against scram'] = {
-  metadata: { requires: { topology: 'scram', mongodb: '>=3.2.0' } },
+describe('SCRAM', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var Db = configuration.require.Db,
-      MongoClient = configuration.require.MongoClient,
-      Server = configuration.require.Server;
+  /**
+   * rm -rf data; mkdir data; mongod --dbpath=./data --setParameter authenticationMechanisms=SCRAM-SHA-1 --auth
+   * @ignore
+   */
+  it('Should correctly authenticate against scram', {
+    metadata: { requires: { topology: 'scram', mongodb: '>=3.2.0' } },
 
-    // User and password
-    var user = 'test';
-    var password = 'test';
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
 
-    // Connect to the server
-    MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
-      test.equal(null, err);
+      // User and password
+      var user = 'test';
+      var password = 'test';
 
-      // Create an admin user
-      db.admin().addUser(user, password, function(err, result) {
+      // Connect to the server
+      MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
         test.equal(null, err);
-        db.close();
 
-        // Attempt to reconnect authenticating against the admin database
-        MongoClient.connect(
-          'mongodb://test:test@localhost:27017/test?authMechanism=SCRAM-SHA-1&authSource=admin&maxPoolSize=5',
-          function(err, db) {
-            test.equal(null, err);
+        // Create an admin user
+        db.admin().addUser(user, password, function(err) {
+          test.equal(null, err);
+          db.close();
 
-            db.collection('test').insert({ a: 1 }, function(err, r) {
+          // Attempt to reconnect authenticating against the admin database
+          MongoClient.connect(
+            'mongodb://test:test@localhost:27017/test?authMechanism=SCRAM-SHA-1&authSource=admin&maxPoolSize=5',
+            function(err, client) {
               test.equal(null, err);
-              test.ok(r != null);
 
-              // Wait for a reconnect to happen
-              client.topology.once('reconnect', function() {
-                // Perform an insert after reconnect
-                db.collection('test').insert({ a: 1 }, function(err, r) {
-                  test.equal(null, err);
-                  test.ok(r != null);
+              db.collection('test').insert({ a: 1 }, function(err, r) {
+                test.equal(null, err);
+                test.ok(r != null);
 
-                  // Attempt to reconnect authenticating against the admin database
-                  MongoClient.connect(
-                    'mongodb://test:test@localhost:27017/test?authMechanism=SCRAM-SHA-1&authSource=admin&maxPoolSize=5',
-                    function(err, db2) {
-                      test.equal(null, err);
+                // Wait for a reconnect to happen
+                client.topology.once('reconnect', function() {
+                  // Perform an insert after reconnect
+                  db.collection('test').insert({ a: 1 }, function(err, r) {
+                    test.equal(null, err);
+                    test.ok(r != null);
 
-                      // Remove the user
-                      db.admin().removeUser(user, function(err, r) {
+                    // Attempt to reconnect authenticating against the admin database
+                    MongoClient.connect(
+                      'mongodb://test:test@localhost:27017/test?authMechanism=SCRAM-SHA-1&authSource=admin&maxPoolSize=5',
+                      function(err) {
                         test.equal(null, err);
 
-                        db.close();
-                        test.done();
-                      });
-                    }
-                  );
-                });
-              });
+                        // Remove the user
+                        db.admin().removeUser(user, function(err) {
+                          test.equal(null, err);
 
-              // Attempt disconnect again
-              client.topology.connections()[0].destroy();
-            });
-          }
-        );
+                          db.close();
+                          done();
+                        });
+                      }
+                    );
+                  });
+                });
+
+                // Attempt disconnect again
+                client.topology.connections()[0].destroy();
+              });
+            }
+          );
+        });
       });
-    });
-  }
-};
+    }
+  });
+});
