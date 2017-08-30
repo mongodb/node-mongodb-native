b7b221425c9997fc26142b3a8b71a12a43e2c8ab
Matt Broadstone <mbroadst@gmail.com>
2017-08-30 14:44:36 -0400

    test(sharding): convert `sharding_connection` tests to mocha style
    NODE-1095

--- test/functional/sharding_connection_tests.js
+++ test/functional/sharding_connection_tests.js
@@ -1,163 +1,171 @@
 'use strict';
-
 var f = require('util').format;
+var test = require('./shared').assert;
+var setupDatabase = require('./shared').setupDatabase;
 
-/**
- * @ignore
- */
-exports['Should connect to mongos proxies using connectiong string and options'] = {
-  metadata: { requires: { topology: 'sharded' } },
+describe('Sharding (Connection)', function() {
+  before(function() {
+    return setupDatabase(this.configuration);
+  });
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
+  /**
+   * @ignore
+   */
+  it('Should connect to mongos proxies using connectiong string and options', {
+    metadata: { requires: { topology: 'sharded' } },
 
-    var url = f(
-      'mongodb://%s:%s,%s:%s/sharded_test_db?w=1&readPreference=secondaryPreferred&readPreferenceTags=sf%3A1&readPreferenceTags=',
-      configuration.host,
-      configuration.port,
-      configuration.host,
-      configuration.port + 1
-    );
-    MongoClient.connect(
-      url,
-      {
-        mongos: {
-          haInterval: 500
-        }
-      },
-      function(err, client) {
-        test.equal(null, err);
-        test.equal(500, client.topology.haInterval);
-        var db = client.db(configuration.database);
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
 
-        db
-          .collection('replicaset_mongo_client_collection')
-          .update({ a: 1 }, { b: 1 }, { upsert: true }, function(err, result) {
-            test.equal(null, err);
-            test.equal(1, result.result.n);
+      var url = f(
+        'mongodb://%s:%s,%s:%s/sharded_test_db?w=1&readPreference=secondaryPreferred&readPreferenceTags=sf%3A1&readPreferenceTags=',
+        configuration.host,
+        configuration.port,
+        configuration.host,
+        configuration.port + 1
+      );
+      MongoClient.connect(
+        url,
+        {
+          mongos: {
+            haInterval: 500
+          }
+        },
+        function(err, client) {
+          test.equal(null, err);
+          test.equal(500, client.topology.haInterval);
+          var db = client.db(configuration.db);
 
-            // Perform fetch of document
-            db.collection('replicaset_mongo_client_collection').findOne(function(err, d) {
+          db
+            .collection('replicaset_mongo_client_collection')
+            .update({ a: 1 }, { b: 1 }, { upsert: true }, function(err, result) {
               test.equal(null, err);
+              test.equal(1, result.result.n);
 
-              client.close();
-              test.done();
+              // Perform fetch of document
+              db.collection('replicaset_mongo_client_collection').findOne(function(err) {
+                test.equal(null, err);
+
+                client.close();
+                done();
+              });
             });
-          });
-      }
-    );
-  }
-};
-
-/**
- * @ignore
- */
-exports['Should correctly connect with a missing mongos'] = {
-  metadata: { requires: { topology: 'sharded' } },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
-
-    var url = f(
-      'mongodb://%s:%s,%s:%s,localhost:50002/sharded_test_db?w=1',
-      configuration.host,
-      configuration.port,
-      configuration.host,
-      configuration.port + 1
-    );
-
-    MongoClient.connect(url, {}, function(err, client) {
-      setTimeout(function() {
-        test.equal(null, err);
-        client.close();
-        test.done();
-      }, 2000);
-    });
-  }
-};
-
-/**
- * @ignore
- */
-exports['Should exercise all options on mongos topology'] = {
-  metadata: { requires: { topology: 'sharded' } },
-
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var MongoClient = configuration.require.MongoClient;
-
-    var url = f(
-      'mongodb://%s:%s,%s:%s/sharded_test_db?w=1&readPreference=secondaryPreferred&readPreferenceTags=sf%3A1&readPreferenceTags=',
-      configuration.host,
-      configuration.port,
-      configuration.host,
-      configuration.port + 1
-    );
-    MongoClient.connect(
-      url,
-      {
-        mongos: {
-          haInterval: 500
         }
-      },
-      function(err, client) {
-        test.equal(null, err);
-        test.equal(500, client.topology.haInterval);
-        test.ok(client.topology.capabilities() != null);
-        test.equal(true, client.topology.isConnected());
-        test.ok(client.topology.lastIsMaster() != null);
-        test.ok(client.topology.connections() != null);
-        test.ok(client.topology.isMasterDoc != null);
-        test.ok(client.topology.bson != null);
+      );
+    }
+  });
 
-        client.close();
-        test.done();
-      }
-    );
-  }
-};
+  /**
+   * @ignore
+   */
+  it('Should correctly connect with a missing mongos', {
+    metadata: { requires: { topology: 'sharded' } },
 
-/**
- * @ignore
- */
-exports['Should correctly modify the server reconnectTries for all sharded proxy instances'] = {
-  metadata: { requires: { topology: 'sharded' } },
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
 
-  // The actual test we wish to run
-  test: function(configuration, test) {
-    var mongo = configuration.require,
-      MongoClient = mongo.MongoClient,
-      Db = configuration.require.Db,
-      CoreServer = configuration.require.CoreServer,
-      CoreConnection = configuration.require.CoreConnection;
+      var url = f(
+        'mongodb://%s:%s,%s:%s,localhost:50002/sharded_test_db?w=1',
+        configuration.host,
+        configuration.port,
+        configuration.host,
+        configuration.port + 1
+      );
 
-    var url = f(
-      'mongodb://%s:%s,%s:%s/sharded_test_db?w=1&readPreference=secondaryPreferred&readPreferenceTags=sf%3A1&readPreferenceTags=',
-      configuration.host,
-      configuration.port,
-      configuration.host,
-      configuration.port + 1
-    );
+      MongoClient.connect(url, {}, function(err, client) {
+        setTimeout(function() {
+          test.equal(null, err);
+          client.close();
+          done();
+        }, 2000);
+      });
+    }
+  });
 
-    MongoClient.connect(
-      url,
-      {
-        reconnectTries: 10
-      },
-      function(err, client) {
-        test.equal(null, err);
-        test.ok(client != null);
+  /**
+   * @ignore
+   */
+  it('Should exercise all options on mongos topology', {
+    metadata: { requires: { topology: 'sharded' } },
 
-        var servers = client.topology.s.mongos.connectedProxies;
-        for (var i = 0; i < servers.length; i++) {
-          test.equal(10, servers[i].s.pool.options.reconnectTries);
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var MongoClient = configuration.require.MongoClient;
+
+      var url = f(
+        'mongodb://%s:%s,%s:%s/sharded_test_db?w=1&readPreference=secondaryPreferred&readPreferenceTags=sf%3A1&readPreferenceTags=',
+        configuration.host,
+        configuration.port,
+        configuration.host,
+        configuration.port + 1
+      );
+      MongoClient.connect(
+        url,
+        {
+          mongos: {
+            haInterval: 500
+          }
+        },
+        function(err, client) {
+          test.equal(null, err);
+          test.equal(500, client.topology.haInterval);
+          test.ok(client.topology.capabilities() != null);
+          test.equal(true, client.topology.isConnected());
+          test.ok(client.topology.lastIsMaster() != null);
+          test.ok(client.topology.connections() != null);
+          test.ok(client.topology.isMasterDoc != null);
+          test.ok(client.topology.bson != null);
+
+          client.close();
+          done();
         }
+      );
+    }
+  });
 
-        client.close();
-        test.done();
-      }
-    );
-  }
-};
+  /**
+   * @ignore
+   */
+  it('Should correctly modify the server reconnectTries for all sharded proxy instances', {
+    metadata: { requires: { topology: 'sharded' } },
+
+    // The actual test we wish to run
+    test: function(done) {
+      var configuration = this.configuration;
+      var mongo = configuration.require,
+        MongoClient = mongo.MongoClient;
+
+      var url = f(
+        'mongodb://%s:%s,%s:%s/sharded_test_db?w=1&readPreference=secondaryPreferred&readPreferenceTags=sf%3A1&readPreferenceTags=',
+        configuration.host,
+        configuration.port,
+        configuration.host,
+        configuration.port + 1
+      );
+
+      MongoClient.connect(
+        url,
+        {
+          reconnectTries: 10
+        },
+        function(err, client) {
+          test.equal(null, err);
+          test.ok(client != null);
+
+          var servers = client.topology.s.mongos.connectedProxies;
+          for (var i = 0; i < servers.length; i++) {
+            test.equal(10, servers[i].s.pool.options.reconnectTries);
+          }
+
+          client.close();
+          done();
+        }
+      );
+    }
+  });
+});
