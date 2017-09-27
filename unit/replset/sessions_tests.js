1d709608f8b33fe563a74e2cc5e43d6968582999
Matt Broadstone <mbroadst@gmail.com>
2017-09-27 13:30:06 -0400

    refactor(session-tests): move session timeout tests to sessions
    NODE-1088

--- unit/replset/sessions_tests.js
+++ unit/replset/sessions_tests.js
@@ -7,21 +7,80 @@ var expect = require('chai').expect,
 
 const test = new ReplSetFixture();
 describe('Sessions (ReplSet)', function() {
-  describe('$clusterTime', function() {
-    afterEach(() => mock.cleanup());
-    beforeEach(() => test.setup());
+  afterEach(() => mock.cleanup());
+  beforeEach(() => test.setup());
 
-    it('should track the highest `clusterTime` seen in a replica set', {
+  it('should track the highest `clusterTime` seen in a replica set', {
+    metadata: { requires: { topology: 'single' } },
+    test: function(done) {
+      const clusterTime = genClusterTime(Date.now()),
+        futureClusterTime = genClusterTime(Date.now() + 10 * 60 * 1000);
+
+      test.primaryStates[0].$clusterTime = clusterTime;
+      test.firstSecondaryStates[0].$clusterTime = futureClusterTime;
+      test.arbiterStates[0].$clusterTime = futureClusterTime;
+
+      var replset = new ReplSet(
+        [test.primaryServer.address(), test.firstSecondaryServer.address()],
+        {
+          setName: 'rs',
+          connectionTimeout: 3000,
+          socketTimeout: 0,
+          haInterval: 100,
+          size: 1
+        }
+      );
+
+      let serverCount = 0;
+      replset.on('joined', () => {
+        serverCount++;
+        if (serverCount === 3) {
+          expect(replset.clusterTime).to.eql(futureClusterTime);
+          replset.destroy();
+          done();
+        }
+      });
+
+      replset.on('error', done);
+      replset.connect();
+    }
+  });
+
+  it('should set `logicalSessionTimeoutMinutes` to `null` if any incoming server is `null`', {
+    metadata: { requires: { topology: 'single' } },
+    test: function(done) {
+      const replset = new ReplSet(
+        [test.primaryServer.address(), test.firstSecondaryServer.address()],
+        {
+          setName: 'rs',
+          connectionTimeout: 3000,
+          socketTimeout: 0,
+          haInterval: 100,
+          size: 1
+        }
+      );
+
+      replset.on('error', done);
+      replset.once('connect', () => {
+        expect(replset.logicalSessionTimeoutMinutes).to.equal(null);
+        replset.destroy();
+        done();
+      });
+
+      replset.connect();
+    }
+  });
+
+  it(
+    'should track `logicalSessionTimeoutMinutes` for replset topology, choosing the lowest value',
+    {
       metadata: { requires: { topology: 'single' } },
       test: function(done) {
-        const clusterTime = genClusterTime(Date.now()),
-          futureClusterTime = genClusterTime(Date.now() + 10 * 60 * 1000);
+        test.primaryStates[0].logicalSessionTimeoutMinutes = 426;
+        test.firstSecondaryStates[0].logicalSessionTimeoutMinutes = 1;
+        test.arbiterStates[0].logicalSessionTimeoutMinutes = 32;
 
-        test.primaryStates[0].$clusterTime = clusterTime;
-        test.firstSecondaryStates[0].$clusterTime = futureClusterTime;
-        test.arbiterStates[0].$clusterTime = futureClusterTime;
-
-        var replset = new ReplSet(
+        const replset = new ReplSet(
           [test.primaryServer.address(), test.firstSecondaryServer.address()],
           {
             setName: 'rs',
@@ -32,11 +91,12 @@ describe('Sessions (ReplSet)', function() {
           }
         );
 
-        let serverCount = 0;
+        let joinCount = 0;
         replset.on('joined', () => {
-          serverCount++;
-          if (serverCount === 3) {
-            expect(replset.clusterTime).to.eql(futureClusterTime);
+          joinCount++;
+
+          if (joinCount === 3) {
+            expect(replset.logicalSessionTimeoutMinutes).to.equal(1);
             replset.destroy();
             done();
           }
@@ -45,6 +105,6 @@ describe('Sessions (ReplSet)', function() {
         replset.on('error', done);
         replset.connect();
       }
-    });
-  });
+    }
+  );
 });
--- unit/single/sessions_tests.js
+++ unit/single/sessions_tests.js
@@ -7,81 +7,110 @@ var Server = require('../../../../lib/topologies/server'),
 
 const test = {};
 describe('Sessions (Single)', function() {
-  describe('$clusterTime', function() {
-    afterEach(() => mock.cleanup());
-    beforeEach(() => {
-      return mock.createServer(37019, 'localhost').then(mockServer => {
-        test.server = mockServer;
-      });
+  afterEach(() => mock.cleanup());
+  beforeEach(() => {
+    return mock.createServer(37019, 'localhost').then(mockServer => {
+      test.server = mockServer;
     });
+  });
 
-    it('should recognize and set `clusterTime` on the topology', {
-      metadata: { requires: { topology: 'single' } },
-      test: function(done) {
-        const clusterTime = genClusterTime(Date.now());
-        test.server.setMessageHandler(request => {
+  it('should recognize and set `clusterTime` on the topology', {
+    metadata: { requires: { topology: 'single' } },
+    test: function(done) {
+      const clusterTime = genClusterTime(Date.now());
+      test.server.setMessageHandler(request => {
+        request.reply(
+          assign({}, mock.DEFAULT_ISMASTER, {
+            $clusterTime: clusterTime
+          })
+        );
+      });
+
+      const client = new Server(test.server.address());
+      client.on('error', done);
+      client.once('connect', () => {
+        expect(client.clusterTime).to.eql(clusterTime);
+        client.destroy();
+        done();
+      });
+
+      client.connect();
+    }
+  });
+
+  it('should track the highest `$clusterTime` seen', {
+    metadata: { requires: { topology: 'single' } },
+    test: function(done) {
+      const clusterTime = genClusterTime(Date.now()),
+        futureClusterTime = genClusterTime(Date.now() + 10 * 60 * 1000);
+
+      test.server.setMessageHandler(request => {
+        const doc = request.document;
+        if (doc.ismaster) {
           request.reply(
             assign({}, mock.DEFAULT_ISMASTER, {
               $clusterTime: clusterTime
             })
           );
-        });
+        } else if (doc.insert) {
+          request.reply({
+            ok: 1,
+            n: [],
+            lastOp: new Date(),
+            $clusterTime: futureClusterTime
+          });
+        }
+      });
 
-        const client = new Server({ host: test.server.host, port: test.server.port });
-        client.on('error', done);
-        client.once('connect', () => {
-          expect(client.clusterTime).to.eql(clusterTime);
+      const client = new Server(test.server.address());
+      client.on('error', done);
+      client.once('connect', () => {
+        expect(client.clusterTime).to.exist;
+        expect(client.clusterTime).to.eql(clusterTime);
+
+        client.insert('test.test', [{ created: new Date() }], function(err) {
+          expect(err).to.not.exist;
+          expect(client.clusterTime).to.exist;
+          expect(client.clusterTime).to.not.eql(clusterTime);
+          expect(client.clusterTime).to.eql(futureClusterTime);
+
           client.destroy();
           done();
         });
+      });
 
-        client.connect();
-      }
-    });
+      client.connect();
+    }
+  });
 
-    it('should track the highest `$clusterTime` seen', {
-      metadata: { requires: { topology: 'single' } },
-      test: function(done) {
-        const clusterTime = genClusterTime(Date.now()),
-          futureClusterTime = genClusterTime(Date.now() + 10 * 60 * 1000);
+  it('should default `logicalSessionTimeoutMinutes` to `null`', {
+    metadata: { requires: { topology: 'single' } },
+    test: function() {
+      const single = new Server();
+      expect(single.logicalSessionTimeoutMinutes).to.equal(null);
+    }
+  });
 
-        test.server.setMessageHandler(request => {
-          const doc = request.document;
-          if (doc.ismaster) {
-            request.reply(
-              assign({}, mock.DEFAULT_ISMASTER, {
-                $clusterTime: clusterTime
-              })
-            );
-          } else if (doc.insert) {
-            request.reply({
-              ok: 1,
-              n: [],
-              lastOp: new Date(),
-              $clusterTime: futureClusterTime
-            });
-          }
-        });
+  it('should track `logicalSessionTimeoutMinutes`', {
+    metadata: { requires: { topology: 'single' } },
+    test: function(done) {
+      test.server.setMessageHandler(request => {
+        request.reply(
+          assign({}, mock.DEFAULT_ISMASTER, {
+            logicalSessionTimeoutMinutes: 10
+          })
+        );
+      });
 
-        const client = new Server({ host: test.server.host, port: test.server.port });
-        client.on('error', done);
-        client.once('connect', () => {
-          expect(client.clusterTime).to.exist;
-          expect(client.clusterTime).to.eql(clusterTime);
+      var client = new Server(test.server.address());
+      client.on('error', done);
+      client.once('connect', () => {
+        expect(client.logicalSessionTimeoutMinutes).to.equal(10);
+        client.destroy();
+        done();
+      });
 
-          client.insert('test.test', [{ created: new Date() }], function(err) {
-            expect(err).to.not.exist;
-            expect(client.clusterTime).to.exist;
-            expect(client.clusterTime).to.not.eql(clusterTime);
-            expect(client.clusterTime).to.eql(futureClusterTime);
-
-            client.destroy();
-            done();
-          });
-        });
-
-        client.connect();
-      }
-    });
+      client.connect();
+    }
   });
 });
