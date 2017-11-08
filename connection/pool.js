ceb301bcea19a870cdfacb127bd216939a23b58e
Matt Broadstone <mbroadst@gmail.com>
2017-11-08 16:24:00 -0500

    refactor(pool): temporarily disable topology checks

--- connection/pool.js
+++ connection/pool.js
@@ -1166,6 +1166,7 @@ Pool.prototype.write = function(commands, options, cb) {
     }
 
     if (operation.session) {
+      // TODO: reenable when sessions development is complete
       // if (operation.session.topology !== this.topology) {
       //   return cb(
       //     new MongoError('Sessions may only be used with the client they were created from')
