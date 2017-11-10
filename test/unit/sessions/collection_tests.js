a9097f748d021b0420463aff9bd103ec581939be
Matt Broadstone <mbroadst@gmail.com>
2017-11-10 14:51:25 -0500

    feat(causal-consistency): support `afterClusterTime` in readConcern
    NODE-1106

---
+++ test/unit/sessions/collection_tests.js
@@ -0,0 +1,55 @@
+'use strict';
+const MongoClient = require('../../..').MongoClient,
+  Timestamp = require('bson').Timestamp,
+  expect = require('chai').expect,
+  mock = require('../../mock');
+
+const test = {};
+describe('Sessions', function() {
+  describe('Collection', function() {
+    afterEach(() => mock.cleanup());
+    beforeEach(() => {
+      return mock.createServer().then(server => {
+        test.server = server;
+      });
+    });
+
+    it('should include `afterClusterTime` in read command with causal consistency', {
+      metadata: { requires: { topology: 'single' } },
+
+      test: function() {
+        let findCommand;
+        let insertOperationTime = Timestamp.fromNumber(Date.now());
+        test.server.setMessageHandler(request => {
+          const doc = request.document;
+          if (doc.ismaster) {
+            request.reply(mock.DEFAULT_ISMASTER_36);
+          } else if (doc.insert) {
+            request.reply({ ok: 1, operationTime: insertOperationTime });
+          } else if (doc.find) {
+            findCommand = doc;
+            request.reply({ ok: 1 });
+          } else if (doc.endSessions) {
+            request.reply({ ok: 1 });
+          }
+        });
+
+        return MongoClient.connect(`mongodb://${test.server.uri()}/test`).then(client => {
+          const session = client.startSession({ causalConsistency: true });
+          const coll = client.db('foo').collection('bar');
+
+          return coll
+            .insert({ a: 42 }, { session: session })
+            .then(() =>
+              coll.findOne({}, null, { session: session, readConcern: { level: 'majoroy' } })
+            )
+            .then(() => {
+              expect(findCommand.readConcern).to.have.keys(['level', 'afterClusterTime']);
+              expect(findCommand.readConcern.afterClusterTime).to.eql(insertOperationTime);
+              return client.close();
+            });
+        });
+      }
+    });
+  });
+});
