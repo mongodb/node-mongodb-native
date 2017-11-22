4204fc630d3b162e99712b74ae8c73127611045d
Jessica Lord <jlord@github.com>
2017-11-22 16:20:37 -0500

    test(dns txt records): start a test runner

---
+++ test/functional/dns_txt_records_tests.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var path = require('path');
+
+var parse = require('../../lib/url_parser');
+var expect = require('chai').expect;
+
+// ./node_modules/.bin/mongodb-test-runner -l test/functional/dns_txt_records_tests.js
+
+function getTests() {
+  return fs
+    .readdirSync(path.join(__dirname, 'specs/dns-txt-records'))
+    .filter(x => x.indexOf('json') !== -1)
+    .map(x => [x, fs.readFileSync(path.join(__dirname, 'specs/dns-txt-records', x), 'utf8')])
+    .map(x => [path.basename(x[0], '.json'), JSON.parse(x[1])]);
+}
+
+describe('DNS and TXT record tests', function() {
+    getTests().forEach(function (test) {
+      if (!test[1].comment) test[1].comment = test[0];
+
+      it(test[1].comment, {
+        metadata: {
+          requires: { topology: ['single'] }
+        },
+        test: function(done) {
+          parse(test[1].uri, function(err, object) {
+            if (test[1].error) {
+              expect(err).to.exist;
+              expect(object).to.not.exist;
+            } else {
+              expect(err).to.be.null;
+              expect(object).to.exist;
+            }
+            done();
+          });
+        }
+      });
+  });
+});
