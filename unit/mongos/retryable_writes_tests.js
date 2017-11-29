d71422e05a5d2a2c0fc6f7f34855a5f7c450bd19
Matt Broadstone <mbroadst@gmail.com>
2017-11-29 13:59:09 -0500

    test(retryable-writes): add basic tests verifying retry behavior
    NODE-1105

--- unit/mongos/retryable_writes_tests.js
+++ unit/mongos/retryable_writes_tests.js
@@ -58,4 +58,59 @@ describe('Retryable Writes (Mongos)', function() {
       topology.connect();
     }
   });
+
+  it('should retry write commands where `retryWrites` is true, and not increment `txnNumber`', {
+    metadata: { requires: { topology: ['single'] } },
+    test: function(done) {
+      const mongos = new Mongos(test.servers.map(server => server.address()), {
+        connectionTimeout: 3000,
+        socketTimeout: 0,
+        haInterval: 10000,
+        localThresholdMS: 500,
+        size: 1
+      });
+
+      const sessionPool = new ServerSessionPool(mongos);
+      const session = new ClientSession(mongos, sessionPool);
+
+      let command = null,
+        insertCount = 0;
+
+      const messageHandler = () => {
+        return request => {
+          const doc = request.document;
+          if (doc.ismaster) {
+            request.reply(test.defaultFields);
+          } else if (doc.insert) {
+            insertCount++;
+            if (insertCount === 1) {
+              request.connection.destroy();
+            } else {
+              command = doc;
+              request.reply({ ok: 1 });
+            }
+          }
+        };
+      };
+
+      test.servers[0].setMessageHandler(messageHandler('MONGOS1'));
+      test.servers[1].setMessageHandler(messageHandler('MONGOS2'));
+      mongos.once('fullsetup', function() {
+        mongos.insert('test.test', [{ a: 1 }], { retryWrites: true, session: session }, function(
+          err
+        ) {
+          if (err) console.dir(err);
+          expect(err).to.not.exist;
+          expect(command).to.have.property('txnNumber');
+          expect(command.txnNumber).to.eql(1);
+
+          mongos.destroy();
+          done();
+        });
+      });
+
+      mongos.on('error', done);
+      mongos.connect();
+    }
+  });
 });
