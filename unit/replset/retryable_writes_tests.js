e41627d87810b0a8d2e138e4f6ce2ddf2a2e77fc
Matt Broadstone <mbroadst@gmail.com>
2017-12-01 15:31:30 -0500

    feat(retryable-writes): retry on "not master" stepdown errors
    NODE-1105

--- unit/replset/retryable_writes_tests.js
+++ unit/replset/retryable_writes_tests.js
@@ -111,4 +111,58 @@ describe('Retryable Writes (ReplSet)', function() {
       replset.connect();
     }
   });
+
+  it('should retry write commands where `retryWrites` is true, and there is a "not master" error', {
+    metadata: { requires: { topology: ['single'] } },
+    test: function(done) {
+      var replset = new ReplSet(
+        [test.primaryServer.address(), test.firstSecondaryServer.address()],
+        {
+          setName: 'rs',
+          connectionTimeout: 100,
+          socketTimeout: 0,
+          haInterval: 100,
+          size: 5,
+          minSize: 1
+        }
+      );
+
+      const sessionPool = new ServerSessionPool(replset);
+      const session = new ClientSession(replset, sessionPool);
+
+      let command = null,
+        insertCount = 0;
+
+      test.primaryServer.setMessageHandler(request => {
+        const doc = request.document;
+        if (doc.ismaster) {
+          request.reply(test.primaryStates[0]);
+        } else if (doc.insert) {
+          insertCount++;
+          if (insertCount === 1) {
+            request.reply({ ok: 0, errmsg: 'not master' }); // simulate a stepdown
+          } else {
+            command = doc;
+            request.reply({ ok: 1 });
+          }
+        }
+      });
+
+      replset.on('all', () => {
+        replset.insert('test.test', [{ a: 1 }], { retryWrites: true, session: session }, function(
+          err
+        ) {
+          expect(err).to.not.exist;
+          expect(command).to.have.property('txnNumber');
+          expect(command.txnNumber).to.eql(1);
+
+          replset.destroy();
+          done();
+        });
+      });
+
+      replset.on('error', done);
+      replset.connect();
+    }
+  });
 });
