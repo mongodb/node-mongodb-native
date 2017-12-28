44dc59580f8b806ddc99e76b0c6a0c8509140cbc
Matt Broadstone <mbroadst@gmail.com>
2017-12-28 18:37:45 -0500

    test(config): modernize local test configuration

--- test/config.js
+++ test/config.js
@@ -1,110 +1,91 @@
 'use strict';
-var ConfigurationBase = require('mongodb-test-runner').ConfigurationBase;
-var inherits = require('util').inherits;
-var f = require('util').format;
+const ConfigurationBase = require('mongodb-test-runner').ConfigurationBase;
+const f = require('util').format;
 
-var clone = function(obj) {
-  var copy = {};
-  for (var name in obj) copy[name] = obj[name];
-  return copy;
-};
+class NativeConfiguration extends ConfigurationBase {
+  constructor(options) {
+    super(options);
 
-var NativeConfiguration = function(options) {
-  ConfigurationBase.call(this, options);
-
-  this.type = 'native';
-  this.topology = options.topology || this.defaultTopology;
-  this.replicasetName = options.replicasetName || 'rs';
-};
-inherits(NativeConfiguration, ConfigurationBase);
-
-NativeConfiguration.prototype.defaultTopology = function(
-  serverHost,
-  serverPort,
-  serverOpts,
-  _mongo
-) {
-  return new _mongo.Server(serverHost, serverPort, serverOpts || {});
-};
-
-NativeConfiguration.prototype.start = function(callback) {
-  var self = this;
-  if (this.skipStart) return callback();
-
-  var client = this.newClient({}, { host: self.host, port: self.port });
-  this.manager
-    .purge()
-    .then(function() {
-      console.log('[purge the directories]');
-      return self.manager.start();
-    })
-    .then(function() {
-      console.log('[started the topology]');
-      return client.connect();
-    })
-    .then(function() {
-      console.log('[get connection to topology]');
-      var db = client.db(self.db);
-      return db.dropDatabase();
-    })
-    .then(function() {
-      console.log('[dropped database]');
-      return client.close();
-    })
-    .then(function() {
-      callback(null);
-    })
-    .catch(function(err) {
-      callback(err, null);
-    });
-};
-
-NativeConfiguration.prototype.newClient = function(dbOptions, serverOptions) {
-  serverOptions = serverOptions || {};
-  // Override implementation
-  if (this.options.newDbInstance) {
-    return this.options.newDbInstance(dbOptions, serverOptions);
+    this.type = 'native';
+    this.topology = options.topology || this.defaultTopology;
+    this.replicasetName = options.replicasetName || 'rs';
   }
 
-  // Set up the options
-  var keys = Object.keys(this.options);
-  if (keys.indexOf('sslOnNormalPorts') !== -1) serverOptions.ssl = true;
-
-  // Fall back
-  var dbHost = (serverOptions && serverOptions.host) || 'localhost';
-  var dbPort = (serverOptions && serverOptions.port) || this.options.port || 27017;
-
-  // Default topology
-  var DbTopology = this.mongo.Server;
-  // If we have a specific topology
-  if (this.options.topology) {
-    DbTopology = this.options.topology;
+  defaultTopology(serverHost, serverPort, serverOpts, _mongo) {
+    return new _mongo.Server(serverHost, serverPort, serverOpts || {});
   }
 
-  // Return a new MongoClient instance
-  return new this.mongo.MongoClient(
-    new DbTopology(dbHost, dbPort, serverOptions, this.mongo),
-    dbOptions
-  );
-};
+  start(callback) {
+    const self = this;
+    if (this.skipStart) return callback();
 
-NativeConfiguration.prototype.url = function(username, password) {
-  var url = this.options.url || 'mongodb://%slocalhost:27017/' + this.db;
-  // Fall back
-  var auth = '';
-  if (username && password) {
-    auth = f('%s:%s@', username, password);
+    const client = this.newClient({}, { host: self.host, port: self.port });
+    this.manager
+      .purge()
+      .then(function() {
+        console.log('[purge the directories]');
+        return self.manager.start();
+      })
+      .then(function() {
+        console.log('[started the topology]');
+        return client.connect();
+      })
+      .then(function() {
+        console.log('[get connection to topology]');
+        return client.db(self.db).dropDatabase();
+      })
+      .then(function() {
+        console.log('[dropped database]');
+        return client.close();
+      })
+      .then(function() {
+        callback(null);
+      })
+      .catch(function(err) {
+        callback(err, null);
+      });
   }
 
-  return f(url, auth);
-};
+  newClient(dbOptions, serverOptions) {
+    serverOptions = serverOptions || {};
+    // Override implementation
+    if (this.options.newDbInstance) {
+      return this.options.newDbInstance(dbOptions, serverOptions);
+    }
 
-NativeConfiguration.prototype.writeConcern = function() {
-  return clone(this.options.writeConcern || { w: 1 });
-};
+    // Set up the options
+    const keys = Object.keys(this.options);
+    if (keys.indexOf('sslOnNormalPorts') !== -1) serverOptions.ssl = true;
 
-NativeConfiguration.prototype.writeConcernMax = function() {
-  return clone(this.options.writeConcernMax || { w: 1 });
-};
+    // Fall back
+    const dbHost = (serverOptions && serverOptions.host) || 'localhost';
+    const dbPort = (serverOptions && serverOptions.port) || this.options.port || 27017;
+
+    // Default topology
+    const DbTopology = this.options.topology ? this.options.topology : this.mongo.Server;
+
+    // Return a new MongoClient instance
+    return new this.mongo.MongoClient(
+      new DbTopology(dbHost, dbPort, serverOptions, this.mongo),
+      dbOptions
+    );
+  }
+
+  url(username, password) {
+    const url = this.options.url || 'mongodb://%slocalhost:27017/' + this.db;
+
+    // Fall back
+    const auth = username && password ? f('%s:%s@', username, password) : '';
+    return f(url, auth);
+  }
+
+  writeConcern() {
+    return Object.assign({}, this.options.writeConcern || { w: 1 });
+  }
+
+  writeConcernMax() {
+    return Object.assign({}, this.options.writeConcernMax || { w: 1 });
+  }
+}
 
 module.exports = NativeConfiguration;
