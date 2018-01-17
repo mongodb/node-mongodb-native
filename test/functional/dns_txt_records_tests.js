16cf900ad9e922e9b71b03a863fb171d0f579afb
Jessica Lord <jlord@github.com>
2018-01-17 12:13:49 -0500

    test(dns txt records): run dns txt records tests

--- test/functional/dns_txt_records_tests.js
+++ test/functional/dns_txt_records_tests.js
@@ -2,39 +2,49 @@
 
 var fs = require('fs');
 var path = require('path');
-
 var parse = require('../../lib/url_parser');
-
-// node test/runner.js -t functional -f dns_txt_records_tets.js
 
 function getTests() {
   return fs
     .readdirSync(path.join(__dirname, 'specs/dns-txt-records'))
-    .filter(function(x) {x.indexOf('json') !== -1})
-    .map(function(x) {[x, fs.readFileSync(path.join(__dirname, 'specs/dns-txt-records', x), 'utf8')]})
-    .map(function(x) {[path.basename(x[0], '.json'), JSON.parse(x[1])]});
+    .filter(function(x) { return x.indexOf('json') !== -1; })
+    .map(function(x) { return [x, fs.readFileSync(path.join(__dirname, 'specs/dns-txt-records', x), 'utf8')]; })
+    .map(function(x) { return [path.basename(x[0], '.json'), JSON.parse(x[1])]; });
 }
 
-getTests().forEach(function (t) {
-  if (!test[1].comment) test[1].comment = test[0];
+exports['mongodb+srv tests'] = {
+  metadata: { requires: { topology: ['single'] } },
+  test: function(configure, test) {
+    console.log('Running DNS Initial Seedlist Discovery spec tests');
 
-  exports[test[1].comment] = {
-    test: function(configure, test) {
-      parse(test[1].url, function(err, object) {
-        if (test[1].error) {
-          test.equal(err, new Error);
+    var specs = getTests();
+    specs.forEach(function (spec) {
+      var comment = spec[1].comment ? spec[1].comment : spec[0];
+      var uri = spec[1].uri;
+      var options = spec[1].options;
+      var error = spec[1].error;
+
+      console.log('  ', comment);
+      parse(uri, function(err, object) {
+        if (error) {
+          test.ok(err instanceof Error);
           test.equal(object, null);
-        } else {
-          test.equal(err, null);
-          if (test[1].options && test[1].options.replicaSet) {
-            test.equal(object.rs_options.rs_name, test[1].options.replicaSet);
+          return;
+        }
+
+        test.equal(err, null);
+        if (options) {
+          if (options.replicaSet) {
+            test.equal(object.rs_options.rs_name, options.replicaSet);
           }
-          if (test[1].options && test[1].options.ssl) {
-            test.equal(object.server_options.ssl, test[1].options.ssl);
+
+          if (options.ssl) {
+            test.equal(object.server_options.ssl, options.ssl);
           }
         }
-        t.done();
-      })
-    }
+      });
+    });
+
+    test.done();
   }
-});
+};
