389a646f260b67b0b202abb486c73e99eb0b0cb5
Matt Broadstone <mbroadst@gmail.com>
2018-04-17 13:59:01 -0400

    refactor(txns): better define what a retryable error is

--- sessions.js
+++ sessions.js
@@ -6,6 +6,7 @@ const BSON = retrieveBSON();
 const Binary = BSON.Binary;
 const uuidV4 = require('./utils').uuidV4;
 const MongoError = require('./error').MongoError;
+const MongoNetworkError = require('./error').MongoNetworkError;
 
 function assertAlive(session, callback) {
   if (session.serverSession == null) {
@@ -206,9 +207,33 @@ class ClientSession extends EventEmitter {
   }
 }
 
+// see: https://github.com/mongodb/specifications/blob/master/source/retryable-writes/retryable-writes.rst#terms
+const RETRYABLE_ERROR_CODES = new Set([
+  6, // HostUnreachable
+  7, // HostNotFound
+  64, // WriteConcernFailed
+  89, // NetworkTimeout
+  91, // ShutdownInProgress
+  189, // PrimarySteppedDown
+  9001, // SocketException
+  11600, // InterruptedAtShutdown
+  11602, // InterruptedDueToReplStateChange
+  10107, // NotMaster
+  13435, // NotMasterNoSlaveOk
+  13436 // NotMasterOrSecondary
+]);
+
 function isRetryableError(error) {
-  if (error.codeName === 'NoSuchTransaction') return false;
-  return true;
+  if (
+    RETRYABLE_ERROR_CODES.has(error.code) ||
+    error instanceof MongoNetworkError ||
+    error.message.match(/not master/) ||
+    error.message.match(/node is recovering/)
+  ) {
+    return true;
+  }
+
+  return false;
 }
 
 function resetTransactionState(clientSession) {
