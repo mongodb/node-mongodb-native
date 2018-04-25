f6ca3fb878e8330728cd0c83eae8f138b055664f
Dan Aprahamian <dan.aprahamian@gmail.com>
2018-04-25 18:18:22 -0400

    chore(tests): removing outdated tests
    These tests are definitely not doing what we want them to do.

--- test/functional/uri_tests.js
+++ test/functional/uri_tests.js
@@ -106,55 +106,6 @@ describe('URI', function() {
     }
   });
 
-  it('should correctly connect via normal url using connect', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: { requires: { topology: 'single' } },
-
-    // The actual test we wish to run
-    test: function(done) {
-      var mongodb = this.configuration.require;
-
-      mongodb.connect('mongodb://localhost/?safe=false', function(err, client) {
-        client.close();
-        done();
-      });
-    }
-  });
-
-  it('should correctly connect via normal url using require', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: { requires: { topology: 'single' } },
-
-    // The actual test we wish to run
-    test: function(done) {
-      require('../..')('mongodb://localhost/', function(err, client) {
-        client.close();
-        done();
-      });
-    }
-  });
-
-  it('should correctly connect via normal url journal option', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: { requires: { topology: 'single' } },
-
-    // The actual test we wish to run
-    test: function(done) {
-      var self = this;
-      var MongoClient = self.configuration.require.MongoClient;
-
-      MongoClient.connect('mongodb://localhost/?journal=true', function(err, client) {
-        var db = client.db(self.configuration.db);
-        expect(db.writeConcern.j).to.be.true;
-        client.close();
-        done();
-      });
-    }
-  });
-
   it('should correctly connect via normal url using ip', {
     // Add a tag that our runner can trigger on
     // in this case we are setting that node needs to be higher than 0.10.X to run
