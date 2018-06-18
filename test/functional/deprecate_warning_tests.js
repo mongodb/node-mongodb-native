b6ca66a37331b4e47132d5511ad572251197b2f8
Sophie Saskin <sophiesaskin@Sophies-MacBook-Pro.local>
2018-06-18 10:51:16 -0400

    feat(deprecation): create deprecation function
    Uniform deprecation functions for parameters and functions
    
    NODE-1430

---
+++ test/functional/deprecate_warning_tests.js
@@ -0,0 +1,125 @@
+'use strict';
+const setupDatabase = require('./shared').setupDatabase;
+const expect = require('chai').expect;
+require('mocha-sinon');
+const deprecateParams = require('../../lib/utils').deprecateParams;
+
+describe('Deprecation Warnings', function() {
+  before(function() {
+    return setupDatabase(this.configuration, []);
+  });
+
+  beforeEach(function() {
+    this.sinon.stub(console, 'warn');
+  });
+
+  it('collection find deprecate warning test', function(done) {
+    const configuration = this.configuration;
+    const client = configuration.newClient({ w: 1 }, { poolSize: 1, auto_reconnect: false });
+
+    client.connect(function(err, client) {
+      const db = client.db(configuration.db);
+      let collection, cursor;
+      const close = e => cursor.close(() => client.close(() => done(e)));
+
+      Promise.resolve()
+        .then(() => db.createCollection('deprecation_test'))
+        .then(() => (collection = db.collection('deprecation_test')))
+        .then(() => collection.find({}, { maxScan: 5, fields: 'hi', snapshot: true }))
+        .then(_cursor => (cursor = _cursor))
+        .then(() => expect(console.warn.calledThrice).to.be.true)
+        .then(() => close())
+        .catch(e => close(e));
+    });
+  });
+
+  const tester = deprecateParams(
+    function(options) {
+      if (options) {
+        options = null;
+      }
+    },
+    'Tester',
+    new Set(['maxScan', 'snapshot', 'fields']),
+    0
+  );
+
+  const tester2 = deprecateParams(
+    function(options) {
+      if (options) {
+        options = null;
+      }
+    },
+    'Tester2',
+    new Set(['maxScan', 'snapshot', 'fields']),
+    0
+  );
+
+  it('multiple functions with the same deprecated params deprecate warning test', function(done) {
+    Promise.resolve()
+      .then(() => tester({ maxScan: 5 }))
+      .then(() => tester2({ maxScan: 5 }))
+      .then(() => expect(console.warn.calledTwice).to.be.true)
+      .then(() => done());
+  });
+
+  const tester3 = deprecateParams(
+    function(options) {
+      if (options) {
+        options = null;
+      }
+    },
+    'Tester3',
+    new Set(['maxScan', 'snapshot', 'fields']),
+    0
+  );
+
+  it('no deprecated params passed in deprecate warning test', function(done) {
+    Promise.resolve()
+      .then(() => tester3({}, {}))
+      .then(() => expect(console.warn.called).to.be.false)
+      .then(() => done());
+  });
+
+  const tester4 = deprecateParams(
+    function(options) {
+      if (options) {
+        options = null;
+      }
+    },
+    'Tester4',
+    new Set(['maxScan', 'snapshot', 'fields']),
+    0,
+    'manual message'
+  );
+
+  it('manually inputted message test', function(done) {
+    Promise.resolve()
+      .then(() => tester4({ maxScan: 5, fields: 'hi', snapshot: true }))
+      .then(() => expect(console.warn.calledWith('manual message')).to.be.true)
+      .then(() => done());
+  });
+
+  const tester5 = deprecateParams(
+    function(options) {
+      if (options) {
+        options = null;
+      }
+    },
+    'Tester5',
+    new Set(['maxScan', 'snapshot', 'fields']),
+    0
+  );
+
+  it('same function only warns once per deprecated parameter', function(done) {
+    Promise.resolve()
+      .then(() => tester5({ maxScan: 5 }))
+      .then(() => tester5({ maxScan: 5 }))
+      .then(() => expect(console.warn.calledOnce).to.be.true)
+      .then(() => done());
+  });
+
+  //   it('logging deprecated warnings test', function(done) {
+  //     done();
+  //   });
+});
