e5026a7657355eeb373c011d44ee6a1cfd0c147b
Dan Aprahamian <dan.aprahamian@gmail.com>
2018-07-09 16:38:07 -0400

    Updated documentation

--- core/scripts/main.js
+++ core/scripts/main.js
@@ -52,18 +52,4 @@ $(function () {
 
     $(window).on('resize', _onResize);
     _onResize();
-
-    // disqus code
-    if (config.disqus) {
-        $(window).on('load', function () {
-            var disqus_shortname = config.disqus; // required: replace example with your forum shortname
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-            var s = document.createElement('script'); s.async = true;
-            s.type = 'text/javascript';
-            s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-            document.getElementsByTagName('BODY')[0].appendChild(s);
-        });
-    }
 });
