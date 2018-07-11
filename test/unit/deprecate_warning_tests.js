62f160d1581e79fb4c7a2b9dfd27b9d163a83089
Sophie Saskin <sophie_saskin@brown.edu>
2018-07-11 17:25:17 -0400

    update test suit with nested describes, other small updates in response to changes requested

--- test/unit/deprecate_warning_tests.js
+++ test/unit/deprecate_warning_tests.js
@@ -31,211 +31,211 @@ describe('Deprecation Warnings', function() {
     messages.length = 0;
   });
 
-  function setupMultFunctionswithSameOptions() {
-    const f1 = makeTestFunction({
-      name: 'f1',
-      deprecatedOptions: deprecatedOptions,
-      optionsIndex: 0
+  describe('Mult functions with same options', function() {
+    beforeEach(function() {
+      const f1 = makeTestFunction({
+        name: 'f1',
+        deprecatedOptions: deprecatedOptions,
+        optionsIndex: 0
+      });
+      const f2 = makeTestFunction({
+        name: 'f2',
+        deprecatedOptions: deprecatedOptions,
+        optionsIndex: 0
+      });
+      f1({ maxScan: 5 });
+      f2({ maxScan: 5 });
     });
-    const f2 = makeTestFunction({
-      name: 'f2',
-      deprecatedOptions: deprecatedOptions,
-      optionsIndex: 0
-    });
-    f1({ maxScan: 5 });
-    f2({ maxScan: 5 });
-  }
 
-  it('multiple functions with the same deprecated options should both warn', {
-    metadata: { requires: { node: '>=6.0.0' } },
-    test: function(done) {
-      setupMultFunctionswithSameOptions();
-      process.nextTick(() => {
-        expect(messages).to.deep.equal([
+    it('multiple functions with the same deprecated options should both warn', {
+      metadata: { requires: { node: '>=6.0.0' } },
+      test: function(done) {
+        process.nextTick(() => {
+          expect(messages).to.deep.equal([
+            'f1 option [maxScan]' + defaultMessage,
+            'f2 option [maxScan]' + defaultMessage
+          ]);
+          expect(messages).to.have.a.lengthOf(2);
+          done();
+        });
+      }
+    });
+
+    it('multiple functions with the same deprecated options should both warn', {
+      metadata: { requires: { node: '<6.0.0' } },
+      test: function(done) {
+        ensureCalledWith(console.error, [
           'f1 option [maxScan]' + defaultMessage,
           'f2 option [maxScan]' + defaultMessage
         ]);
-        expect(messages).to.have.a.lengthOf(2);
+        expect(console.error).to.have.been.calledTwice;
         done();
-      });
-    }
-  });
-
-  it('multiple functions with the same deprecated options should both warn', {
-    metadata: { requires: { node: '<6.0.0' } },
-    test: function(done) {
-      setupMultFunctionswithSameOptions();
-      ensureCalledWith(console.error, [
-        'f1 option [maxScan]' + defaultMessage,
-        'f2 option [maxScan]' + defaultMessage
-      ]);
-      expect(console.error).to.have.been.calledTwice;
-      done();
-    }
-  });
-
-  function setupNoOptions() {
-    const f = makeTestFunction({
-      name: 'f',
-      deprecatedOptions: deprecatedOptions,
-      optionsIndex: 0
+      }
     });
-    f({});
-  }
+  });
 
-  it('should not warn if no deprecated options passed in', {
-    metadata: { requires: { node: '>=6.0.0' } },
-    test: function(done) {
-      setupNoOptions();
-      process.nextTick(() => {
-        expect(messages).to.have.a.lengthOf(0);
-        done();
+  describe('No options', function() {
+    beforeEach(function() {
+      const f = makeTestFunction({
+        name: 'f',
+        deprecatedOptions: deprecatedOptions,
+        optionsIndex: 0
       });
-    }
-  });
-
-  it('should not warn if no deprecated options passed in', {
-    metadata: { requires: { node: '<6.0.0' } },
-    test: function(done) {
-      setupNoOptions();
-      expect(console.error).to.have.not.been.called;
-      done();
-    }
-  });
-
-  function setupUserMsgHandler() {
-    const customMsgHandler = (name, option) => {
-      return 'custom msg for function ' + name + ' and option ' + option;
-    };
-
-    const f = makeTestFunction({
-      name: 'f',
-      deprecatedOptions: deprecatedOptions,
-      optionsIndex: 0,
-      msgHandler: customMsgHandler
+      f({});
     });
 
-    f({ maxScan: 5, snapshot: true, fields: 'hi' });
-  }
+    it('should not warn if no deprecated options passed in', {
+      metadata: { requires: { node: '>=6.0.0' } },
+      test: function(done) {
+        process.nextTick(() => {
+          expect(messages).to.have.a.lengthOf(0);
+          done();
+        });
+      }
+    });
 
-  it('should use user-specified message handler', {
-    metadata: { requires: { node: '>=6.0.0' } },
-    test: function(done) {
-      setupUserMsgHandler();
-      process.nextTick(() => {
-        expect(messages).to.deep.equal([
+    it('should not warn if no deprecated options passed in', {
+      metadata: { requires: { node: '<6.0.0' } },
+      test: function(done) {
+        expect(console.error).to.have.not.been.called;
+        done();
+      }
+    });
+  });
+
+  describe('Custom Message Handler', function() {
+    beforeEach(function() {
+      const customMsgHandler = (name, option) => {
+        return 'custom msg for function ' + name + ' and option ' + option;
+      };
+
+      const f = makeTestFunction({
+        name: 'f',
+        deprecatedOptions: deprecatedOptions,
+        optionsIndex: 0,
+        msgHandler: customMsgHandler
+      });
+
+      f({ maxScan: 5, snapshot: true, fields: 'hi' });
+    });
+
+    it('should use user-specified message handler', {
+      metadata: { requires: { node: '>=6.0.0' } },
+      test: function(done) {
+        process.nextTick(() => {
+          expect(messages).to.deep.equal([
+            'custom msg for function f and option maxScan',
+            'custom msg for function f and option snapshot',
+            'custom msg for function f and option fields'
+          ]);
+          expect(messages).to.have.a.lengthOf(3);
+          done();
+        });
+      }
+    });
+
+    it('should use user-specified message handler', {
+      metadata: { requires: { node: '<6.0.0' } },
+      test: function(done) {
+        ensureCalledWith(console.error, [
           'custom msg for function f and option maxScan',
           'custom msg for function f and option snapshot',
           'custom msg for function f and option fields'
         ]);
-        expect(messages).to.have.a.lengthOf(3);
+        expect(console.error).to.have.been.calledThrice;
         done();
-      });
-    }
-  });
-
-  it('should use user-specified message handler', {
-    metadata: { requires: { node: '<6.0.0' } },
-    test: function(done) {
-      setupUserMsgHandler();
-      ensureCalledWith(console.error, [
-        'custom msg for function f and option maxScan',
-        'custom msg for function f and option snapshot',
-        'custom msg for function f and option fields'
-      ]);
-      expect(console.error).to.have.been.calledThrice;
-      done();
-    }
-  });
-
-  function setupOncePerOption() {
-    const f = makeTestFunction({
-      name: 'f',
-      deprecatedOptions: deprecatedOptions,
-      optionsIndex: 0
+      }
     });
-    f({ maxScan: 5, fields: 'hi' });
-    f({ maxScan: 5, fields: 'hi' });
-  }
+  });
 
-  it('each function should only warn once per deprecated option', {
-    metadata: { requires: { node: '>=6.0.0' } },
-    test: function(done) {
-      setupOncePerOption();
-      process.nextTick(() => {
-        expect(messages).to.deep.equal([
+  describe('Warn once', function() {
+    beforeEach(function() {
+      const f = makeTestFunction({
+        name: 'f',
+        deprecatedOptions: deprecatedOptions,
+        optionsIndex: 0
+      });
+      f({ maxScan: 5, fields: 'hi' });
+      f({ maxScan: 5, fields: 'hi' });
+    });
+
+    it('each function should only warn once per deprecated option', {
+      metadata: { requires: { node: '>=6.0.0' } },
+      test: function(done) {
+        process.nextTick(() => {
+          expect(messages).to.deep.equal([
+            'f option [maxScan]' + defaultMessage,
+            'f option [fields]' + defaultMessage
+          ]);
+          expect(messages).to.have.a.lengthOf(2);
+          done();
+        });
+      }
+    });
+
+    it('each function should only warn once per deprecated option', {
+      metadata: { requires: { node: '<6.0.0' } },
+      test: function(done) {
+        ensureCalledWith(console.error, [
           'f option [maxScan]' + defaultMessage,
           'f option [fields]' + defaultMessage
         ]);
-        expect(messages).to.have.a.lengthOf(2);
+        expect(console.error).to.have.been.calledTwice;
         done();
-      });
-    }
+      }
+    });
   });
 
-  it('each function should only warn once per deprecated option', {
-    metadata: { requires: { node: '<6.0.0' } },
-    test: function(done) {
-      setupOncePerOption();
-      ensureCalledWith(console.error, [
-        'f option [maxScan]' + defaultMessage,
-        'f option [fields]' + defaultMessage
-      ]);
-      expect(console.error).to.have.been.calledTwice;
-      done();
-    }
-  });
+  describe('Maintain functionality', function() {
+    beforeEach(function() {
+      const config = {
+        name: 'f',
+        deprecatedOptions: ['multiply', 'add'],
+        optionsIndex: 0
+      };
 
-  function setupMaintainFunctionality() {
-    const config = {
-      name: 'f',
-      deprecatedOptions: ['multiply', 'add'],
-      optionsIndex: 0
-    };
+      const operateBy2 = (options, num) => {
+        if (options.multiply === true) {
+          return num * 2;
+        }
+        if (options.add === true) {
+          return num + 2;
+        }
+      };
 
-    const operateBy2 = (options, num) => {
-      if (options.multiply === true) {
-        return num * 2;
+      const f = deprecateOptions(config, operateBy2);
+
+      const mult = f({ multiply: true }, 5);
+      const add = f({ add: true }, 5);
+
+      expect(mult).to.equal(10);
+      expect(add).to.equal(7);
+    });
+
+    it('should maintain functions original functionality', {
+      metadata: { requires: { node: '>=6.0.0' } },
+      test: function(done) {
+        process.nextTick(() => {
+          expect(messages).to.deep.equal([
+            'f option [multiply]' + defaultMessage,
+            'f option [add]' + defaultMessage
+          ]);
+          expect(messages).to.have.a.lengthOf(2);
+          done();
+        });
       }
-      if (options.add === true) {
-        return num + 2;
-      }
-    };
+    });
 
-    const f = deprecateOptions(config, operateBy2);
-
-    const mult = f({ multiply: true }, 5);
-    const add = f({ add: true }, 5);
-
-    expect(mult).to.equal(10);
-    expect(add).to.equal(7);
-  }
-
-  it('should maintain functions original functionality', {
-    metadata: { requires: { node: '>=6.0.0' } },
-    test: function(done) {
-      setupMaintainFunctionality();
-      process.nextTick(() => {
-        expect(messages).to.deep.equal([
+    it('should maintain functions original functionality', {
+      metadata: { requires: { node: '<6.0.0' } },
+      test: function(done) {
+        ensureCalledWith(console.error, [
           'f option [multiply]' + defaultMessage,
           'f option [add]' + defaultMessage
         ]);
-        expect(messages).to.have.a.lengthOf(2);
+        expect(console.error).to.have.been.calledTwice;
         done();
-      });
-    }
-  });
-
-  it('should maintain functions original functionality', {
-    metadata: { requires: { node: '<6.0.0' } },
-    test: function(done) {
-      setupMaintainFunctionality();
-      ensureCalledWith(console.error, [
-        'f option [multiply]' + defaultMessage,
-        'f option [add]' + defaultMessage
-      ]);
-      expect(console.error).to.have.been.calledTwice;
-      done();
-    }
+      }
+    });
   });
 });
