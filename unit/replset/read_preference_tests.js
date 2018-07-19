fe9a9adf305b918df2e59d61a9bbb7c7907d8487
Matt Broadstone <mbroadst@gmail.com>
2018-07-19 16:00:41 -0400

    fix(read-preference): correct server sort for `nearest` selection
    The server sorting during `nearest` server selection was incorrectly
    using a greater-than sign, instead of following the rules of array
    sort comparitors. This fixes the issue for `pickNearest` as well
    as `pickNearestMaxStalenessSeconds`, and contributes tests for
    both paths.
    
    NODE-1577

--- unit/replset/read_preference_tests.js
+++ unit/replset/read_preference_tests.js
@@ -5,8 +5,10 @@ const ReplSet = require('../../../../lib/topologies/replset');
 const ReadPreference = require('../../../../lib/topologies/read_preference');
 const mock = require('mongodb-mock-server');
 const ReplSetFixture = require('../common').ReplSetFixture;
+const ReplSetState = require('../../../../lib/topologies/replset_state');
+const MongoError = require('../../../..').MongoError;
 
-describe('Secondaries (ReplSet)', function() {
+describe('ReadPreference (ReplSet)', function() {
   let test;
   before(() => (test = new ReplSetFixture()));
   afterEach(() => mock.cleanup());
@@ -47,5 +49,48 @@ describe('Secondaries (ReplSet)', function() {
 
       replSet.connect({ readPreference: new ReadPreference('secondary') });
     }
+  });
+
+  it('should correctly sort servers by `lastIsMasterMS` during nearest selection', function() {
+    const state = new ReplSetState();
+    const sampleData = [
+      { type: 'RSPrimary', lastIsMasterMS: 5 },
+      { type: 'RSSecondary', lastIsMasterMS: 4 },
+      { type: 'RSSecondary', lastIsMasterMS: 4 },
+      { type: 'RSSecondary', lastIsMasterMS: 109 },
+      { type: 'RSSecondary', lastIsMasterMS: 110 },
+      { type: 'RSSecondary', lastIsMasterMS: 110 },
+      { type: 'RSSecondary', lastIsMasterMS: 245 },
+      { type: 'RSSecondary', lastIsMasterMS: 221 },
+      { type: 'RSSecondary', lastIsMasterMS: 199 },
+      { type: 'RSSecondary', lastIsMasterMS: 129 },
+      { type: 'RSSecondary', lastIsMasterMS: 131 },
+      { type: 'RSSecondary', lastIsMasterMS: 284 },
+      { type: 'RSSecondary', lastIsMasterMS: 298 },
+      { type: 'RSSecondary', lastIsMasterMS: 289 },
+      { type: 'RSSecondary', lastIsMasterMS: 312 }
+    ];
+
+    // load mock data into replset state
+    sampleData.forEach(desc => {
+      desc.ismaster = { maxWireVersion: 6 }; // for maxStalenessSeconds test
+      desc.staleness = Math.floor(Math.random() * 100);
+
+      if (desc.type === 'RSPrimary') {
+        state.primary = desc;
+      } else {
+        state.secondaries.push(desc);
+      }
+    });
+
+    // select the nearest server without max staleness seconds
+    let server = state.pickServer(ReadPreference.nearest);
+    expect(server).to.not.be.an.instanceOf(MongoError);
+    expect(server.lastIsMasterMS).to.equal(4);
+
+    // select the nearest server with max staleness seconds
+    server = state.pickServer(new ReadPreference('nearest', { maxStalenessSeconds: 100 }));
+    expect(server).to.not.be.an.instanceOf(MongoError);
+    expect(server.lastIsMasterMS).to.equal(4);
   });
 });
