33b2999e52a9991ce15abdaeb5496d870184dc55
Rebecca Weinberger <rew@mit.edu>
2018-08-10 12:23:03 -0400

    Test for throwing error

--- test/functional/apm_logging_tests.js
+++ test/functional/apm_logging_tests.js
@@ -42,176 +42,204 @@ function getLogData(logArray) {
   );
 }
 
-function runTestClient(context, urlOptions, closeCb) {
-  const configuration = context.configuration;
-  const url = configuration.url() + urlOptions;
-  const client = new MongoClient(url, { w: 1, useNewUrlParser: true });
-  const events = [];
-
-  loggingEvents.forEach(event => client.on(event, e => events.push(stringify(e))));
-
-  client.connect(function(err) {
-    expect(err).to.be.null;
-    client.close(err => {
-      // set timeout to make sure all file writes are completed
-      setTimeout(() => {
-        const data = getLogData(fakeLogFile);
-        closeCb(err, data, events);
-      }, 10);
-    });
-  });
-}
-
 const testLogFileName = `${process.cwd()}/apm_test_log`;
 const defaultFileName = `${process.cwd()}/monitor.log`;
-const fakeLogFile = [];
 
 describe('APM Logging', function() {
-  let createStreamStub;
+  describe('Stubbed streams', function() {
+    const fakeLogFile = [];
+    let createStreamStub;
 
-  before(function() {
-    const fakeStream = new stream.Writable();
-    // fake stream will 'write' data to our internal variable, fakeLogFile
-    sinon.stub(fakeStream, 'write').callsFake(data => {
-      fakeLogFile.push(EJSON.parse(data));
+    before(function() {
+      const fakeStream = new stream.Writable();
+      // fake stream will 'write' data to our internal variable, fakeLogFile
+      const writeStreamStub = sinon.stub(fakeStream, 'write');
+      writeStreamStub.callsFake(data => {
+        fakeLogFile.push(EJSON.parse(data));
+      });
+      // avoid writing to file system & enable checking which file write streams are created on
+      createStreamStub = sinon.stub(fs, 'createWriteStream');
+      createStreamStub.callsFake(() => {
+        return fakeStream;
+      });
+      return setupDatabase(this.configuration);
     });
-    // avoid writing to file system & enable checking which file write streams are created on
-    createStreamStub = sinon.stub(fs, 'createWriteStream').callsFake(() => {
-      return fakeStream;
+
+    beforeEach(function() {
+      fakeLogFile.length = 0;
     });
-    return setupDatabase(this.configuration);
-  });
 
-  beforeEach(function() {
-    fakeLogFile.length = 0;
-  });
-
-  after(function() {
-    createStreamStub.restore();
-  });
-
-  it('should append timestamp to a pre-existing log filename to avoid overwriting', function(done) {
-    // suppress and spy on the renaming function
-    const renameStub = sinon.stub(fs, 'renameSync');
-    // trick the code into thinking log files already exist
-    const existsStub = sinon.stub(fs, 'existsSync').callsFake(() => {
-      return true;
+    after(function() {
+      createStreamStub.restore();
     });
-    const testStartStamp = new Date();
-    const cb = err => {
-      const renameArgs = renameStub.lastCall.args;
-      const fileTimestamp = new Date(renameArgs[1].substring(renameArgs[1].length - 24));
-      expect(renameArgs[0]).to.equal(testLogFileName);
-      // ensure the date was appended to the file after this test started
-      expect(fileTimestamp).to.be.above(testStartStamp);
-      existsStub.restore();
-      renameStub.restore();
-      done(err);
-    };
-    runTestClient(this, '?monitor=command&monitorOut=apm_test_log', cb);
-  });
 
-  it('should create stream to default location if monitorOut not specified', function(done) {
-    const cb = err => {
-      expect(createStreamStub.lastCall).to.have.been.calledWith(defaultFileName);
-      done(err);
-    };
-    runTestClient(this, '?monitor=command', cb);
-  });
+    function runTestClient(context, urlOptions, closeCb) {
+      const configuration = context.configuration;
+      const url = configuration.url() + urlOptions;
+      const client = new MongoClient(url, { w: 1, useNewUrlParser: true });
+      const events = [];
 
-  it('should create stream to custom location if monitorOut specified', function(done) {
-    const cb = err => {
-      expect(createStreamStub.lastCall).to.have.been.calledWith(testLogFileName);
-      done(err);
-    };
-    runTestClient(this, '?monitor=command&monitorOut=apm_test_log', cb);
-  });
+      loggingEvents.forEach(event => client.on(event, e => events.push(stringify(e))));
 
-  it('should log to stdout if specified', function(done) {
-    const spy = sinon.stub(process.stdout, 'write');
-    const cb = err => {
-      expect(spy).to.have.been.calledTwice;
-      spy.restore();
-      done(err);
-    };
-    runTestClient(this, '?monitor=command&monitorOut=stdout', cb);
-  });
+      client.connect(err => {
+        expect(err).to.be.null;
+        client.close(err => {
+          const data = getLogData(fakeLogFile);
+          closeCb(err, data, events);
+        });
+      });
+    }
 
-  it('should log to stderr if specified', function(done) {
-    const spy = sinon.stub(process.stderr, 'write');
-    const cb = err => {
-      expect(spy).to.have.been.calledTwice;
-      spy.restore();
-      done(err);
-    };
-    runTestClient(this, '?monitor=command&monitorOut=stderr', cb);
-  });
+    it('should create stream to default location if monitorOut not specified', function(done) {
+      const cb = err => {
+        expect(createStreamStub.lastCall).to.have.been.calledWith(defaultFileName);
+        done(err);
+      };
+      runTestClient(this, '?monitor=command', cb);
+    });
 
-  it('should log correct event objects in the same order they were emitted', function(done) {
-    const cb = (err, data, events) => {
-      expect(events).to.deep.equal(data.eventStrings);
-      done(err);
-    };
-    runTestClient(this, '?monitor=all&monitorOut=apm_test_log', cb);
-  });
+    it('should create stream to custom file if specified', function(done) {
+      const cb = err => {
+        expect(createStreamStub.lastCall).to.have.been.calledWith(testLogFileName);
+        done(err);
+      };
+      runTestClient(this, '?monitor=command&monitorOut=apm_test_log', cb);
+    });
 
-  it('log only sdam events', function(done) {
-    const cb = (err, data) => {
-      expect(data.numLines).to.equal(6);
-      expect(data.command).to.be.empty;
-      done(err);
-    };
-    runTestClient(this, '?monitor=sdam&monitorOut=apm_test_log', cb);
-  });
+    it('should log to stdout if specified', function(done) {
+      const spy = sinon.stub(process.stdout, 'write');
+      const cb = err => {
+        spy.restore();
+        expect(spy).to.have.been.calledTwice;
+        done(err);
+      };
+      runTestClient(this, '?monitor=command&monitorOut=stdout', cb);
+    });
 
-  it('log only command events', function(done) {
-    const cb = (err, data) => {
-      expect(data.numLines).to.equal(2);
-      expect(data.sdam).to.be.empty;
-      done(err);
-    };
-    runTestClient(this, '?monitor=command&monitorOut=apm_test_log', cb);
-  });
+    it('should log to stderr if specified', function(done) {
+      const spy = sinon.stub(process.stderr, 'write');
+      const cb = err => {
+        spy.restore();
+        expect(spy).to.have.been.calledTwice;
+        done(err);
+      };
+      runTestClient(this, '?monitor=command&monitorOut=stderr', cb);
+    });
 
-  // skip for now: this test depends on the new fixes in the core uri parser
-  it.skip('log both command and sdam events using `monitor=command,sdam`', function(done) {
-    const cb = (err, data) => {
-      expect(data.numLines).to.equal(8);
-      expect(data.sdam).to.not.be.empty;
-      expect(data.command).to.not.be.empty;
-      done(err);
-    };
-    runTestClient(this, '?monitor=command,sdam&monitorOut=apm_test_log', cb);
-  });
+    it('should log correct event objects in the same order they were emitted', function(done) {
+      const cb = (err, data, events) => {
+        expect(events).to.deep.equal(data.eventStrings);
+        done(err);
+      };
+      runTestClient(this, '?monitor=all&monitorOut=apm_test_log', cb);
+    });
 
-  it('log both command and sdam events using `monitor=all`', function(done) {
-    const cb = (err, data) => {
-      expect(data.numLines).to.equal(8);
-      expect(data.sdam).to.not.be.empty;
-      expect(data.command).to.not.be.empty;
-      done(err);
-    };
-    runTestClient(this, '?monitor=all&monitorOut=apm_test_log', cb);
-  });
+    it('log only sdam events', function(done) {
+      const cb = (err, data) => {
+        expect(data.numLines).to.equal(6);
+        expect(data.command).to.be.empty;
+        done(err);
+      };
+      runTestClient(this, '?monitor=sdam&monitorOut=apm_test_log', cb);
+    });
 
-  it('should call callback with error when log filename cannot be resolved', function(done) {
-    const configuration = this.configuration;
-    const url = configuration.url() + '?monitor=all&monitorOut=invalid:name';
-    const client = new MongoClient(url, { w: 1, useNewUrlParser: true });
+    it('log only command events', function(done) {
+      const cb = (err, data) => {
+        expect(data.numLines).to.equal(2);
+        expect(data.sdam).to.be.empty;
+        done(err);
+      };
+      runTestClient(this, '?monitor=command&monitorOut=apm_test_log', cb);
+    });
 
-    client.connect(function(err) {
-      expect(err).to.exist;
-      expect(err).to.be.an.instanceOf(TypeError);
-      done();
+    // skip for now: this test depends on the new fixes in the core uri parser
+    it.skip('log both command and sdam events using `monitor=command,sdam`', function(done) {
+      const cb = (err, data) => {
+        expect(data.numLines).to.equal(8);
+        expect(data.sdam).to.not.be.empty;
+        expect(data.command).to.not.be.empty;
+        done(err);
+      };
+      runTestClient(this, '?monitor=command,sdam&monitorOut=apm_test_log', cb);
+    });
+
+    it('log both command and sdam events using `monitor=all`', function(done) {
+      const cb = (err, data) => {
+        expect(data.numLines).to.equal(8);
+        expect(data.sdam).to.not.be.empty;
+        expect(data.command).to.not.be.empty;
+        done(err);
+      };
+      runTestClient(this, '?monitor=all&monitorOut=apm_test_log', cb);
+    });
+
+    it('should append timestamp to a pre-existing log filename to avoid overwriting', function(done) {
+      // suppress and spy on the renaming function
+      const renameStub = sinon.stub(fs, 'renameSync');
+      // trick the code into thinking log files already exist
+      const existsStub = sinon.stub(fs, 'existsSync').callsFake(() => {
+        return true;
+      });
+      const testStartStamp = new Date();
+      const cb = err => {
+        const renameArgs = renameStub.lastCall.args;
+        const fileTimestamp = new Date(renameArgs[1].substring(renameArgs[1].length - 24));
+        expect(renameArgs[0]).to.equal(testLogFileName);
+        // ensure the date was appended to the file at the same time or after this test started
+        expect(fileTimestamp).to.be.at.least(testStartStamp);
+        existsStub.restore();
+        renameStub.restore();
+        done(err);
+      };
+      runTestClient(this, '?monitor=command&monitorOut=apm_test_log', cb);
     });
   });
 
-  // TODO: how to test throwing errors if we don't know exactly when the error will be thrown?
-  it.skip('should throw error when file stream emits error after client has connected', function(done) {
-    const configuration = this.configuration;
-    const url = configuration.url() + '?monitor=all&monitorOut=nonexistent/file/path';
-    const client = new MongoClient(url, { w: 1, useNewUrlParser: true });
+  describe('Error handling', function() {
+    before(function() {
+      return setupDatabase(this.configuration);
+    });
 
-    client.connect(() => done());
+    it('should call callback with error when log filename cannot be resolved', function(done) {
+      const configuration = this.configuration;
+      const url = configuration.url() + '?monitor=all&monitorOut=invalid:name';
+      const client = new MongoClient(url, { w: 1, useNewUrlParser: true });
+
+      client.connect(function(err) {
+        expect(err).to.exist;
+        expect(err).to.be.an.instanceOf(TypeError);
+        done();
+      });
+    });
+
+    it('should throw error and close client when file stream emits error after client has connected', function(done) {
+      const configuration = this.configuration;
+      const url = configuration.url() + '?monitor=all';
+      const client = new MongoClient(url, { w: 1, useNewUrlParser: true });
+
+      const writeStream = new stream.Writable();
+      sinon.stub(writeStream, 'write');
+      const createStreamStub = sinon.stub(fs, 'createWriteStream');
+
+      // give us access to the write stream in order to manipulate it
+      createStreamStub.callsFake(() => {
+        return writeStream;
+      });
+
+      client.connect((err, client) => {
+        expect(err).to.be.null;
+
+        // inspect parameters when client closes
+        sinon.stub(client, 'close').callsFake((force, fn) => {
+          expect(force).to.equal(true);
+          expect(fn).to.throw();
+          createStreamStub.restore();
+          done();
+        });
+
+        // force stream to error
+        writeStream.destroy(new Error('error after connecting'));
+      });
+    });
   });
 });
