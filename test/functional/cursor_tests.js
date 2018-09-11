d7779902883b65e8b3028d01c77d4df5cd2573dc
Matt Broadstone <mbroadst@gmail.com>
2018-09-11 14:20:11 -0400

    test(cursor): support both cursor types when spying in cursor test

--- test/functional/cursor_tests.js
+++ test/functional/cursor_tests.js
@@ -297,9 +297,14 @@ describe('Cursor', function() {
       client.connect((err, client) => {
         const db = client.db(configuration.db);
 
-        const internalClientCursor = sinon.spy(client.topology.s.coreTopology, 'cursor');
-        const expectedReadPreference = new ReadPreference(ReadPreference.SECONDARY);
+        let internalClientCursor;
+        if (configuration.usingUnifiedTopology()) {
+          internalClientCursor = sinon.spy(client.topology, 'cursor');
+        } else {
+          internalClientCursor = sinon.spy(client.topology.s.coreTopology, 'cursor');
+        }
 
+        const expectedReadPreference = new ReadPreference(ReadPreference.SECONDARY);
         const cursor = db.collection('countTEST').find({ qty: { $gt: 4 } });
         cursor.count(true, { readPreference: ReadPreference.SECONDARY }, err => {
           expect(err).to.be.null;
