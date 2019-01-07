aafe71ba7530312afdccfe37fe01ab2f29c34679
Dan Aprahamian <dan.aprahamian@gmail.com>
2019-01-07 11:49:40 -0500

    fix(bulk): properly calculate batch size for bulk writes
    Bulk Writes were failing to account for the size of the array
    index key when calculating the batch size in a bulk write.
    We now calculate the size of the key based off of the max batch
    size, and incorporate that number in to the byte size calculations.
    
    Fixes NODE-1778

--- test/functional/bulk_tests.js
+++ test/functional/bulk_tests.js
@@ -1513,4 +1513,62 @@ describe('Bulk', function() {
         client.close();
       });
   });
+
+  it('should properly account for array key size in bulk unordered inserts', function(done) {
+    const client = this.configuration.newClient({ w: 1 }, { monitorCommands: true });
+    const documents = new Array(20000).fill('').map(() => ({
+      arr: new Array(19).fill('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    }));
+
+    let db;
+
+    client
+      .connect()
+      // NOTE: Hack to get around unrelated strange error in bulkWrites for right now.
+      .then(() => {
+        db = client.db(this.configuration.db);
+        return db.dropCollection('doesnt_matter').catch(() => {});
+      })
+      .then(() => {
+        return db.createCollection('doesnt_matter');
+      })
+      .then(() => {
+        const coll = db.collection('doesnt_matter');
+
+        coll.insert(documents, { ordered: false }, err => {
+          client.close(() => {
+            done(err);
+          });
+        });
+      });
+  });
+
+  it('should properly account for array key size in bulk ordered inserts', function(done) {
+    const client = this.configuration.newClient();
+    const documents = new Array(20000).fill('').map(() => ({
+      arr: new Array(19).fill('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    }));
+
+    let db;
+
+    client
+      .connect()
+      // NOTE: Hack to get around unrelated strange error in bulkWrites for right now.
+      .then(() => {
+        db = client.db(this.configuration.db);
+        return db.dropCollection('doesnt_matter').catch(() => {});
+      })
+      .then(() => {
+        return db.createCollection('doesnt_matter');
+      })
+      .then(() => {
+        const coll = db.collection('doesnt_matter');
+
+        coll.insert(documents, { ordered: false }, err => {
+          client.close(() => {
+            done(err);
+          });
+        });
+      });
+  });
 });
