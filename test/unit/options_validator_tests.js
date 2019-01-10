c00cf94e7e016070a4720dc8b63877607b6fd03f
Katherine Walker <katherine.walker@mongodb.com>
2019-01-10 17:48:42 -0500

    update test

--- test/unit/options_validator_tests.js
+++ test/unit/options_validator_tests.js
@@ -519,7 +519,7 @@ describe('Options Validation', function() {
     };
 
     const testObject = { a: true, b: 45 };
-    validate(validationSchema, testObject, { optionsValidationLevel: 'warn' });
+    validate(validationSchema, testObject, { optionsValidationLevel: 'warn' }, true);
 
     expect(stub).to.have.been.calledOnce;
     expect(stub).to.have.been.calledWith('provided option [b] is an unknown option');
