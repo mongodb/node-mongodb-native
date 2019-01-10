81ab078cd1324ce61baf24984ca5e6946c6307b7
Katherine Walker <katherine.walker@mongodb.com>
2019-01-10 17:17:48 -0500

    fix(options_validator): warn on unknown options
    Fixes NODE-1381

--- test/unit/options_validator_tests.js
+++ test/unit/options_validator_tests.js
@@ -511,4 +511,19 @@ describe('Options Validation', function() {
     const testResult = testClass.testOperation();
     expect(testResult).to.deep.equal({ a: false });
   });
+
+  it('Should warn on unknown option', function() {
+    const stub = sinon.stub(console, 'warn');
+    const validationSchema = {
+      a: { type: 'boolean' }
+    };
+
+    const testObject = { a: true, b: 45 };
+    validate(validationSchema, testObject, { optionsValidationLevel: 'warn' });
+
+    expect(stub).to.have.been.calledOnce;
+    expect(stub).to.have.been.calledWith('provided option [b] is an unknown option');
+
+    console.warn.restore();
+  });
 });
