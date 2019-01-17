8a7d5fa5eadc320377c98b9fe4abe505c2f4a3bc
Katherine Walker <katherine.walker@mongodb.com>
2019-01-17 10:50:14 -0500

    fix(options_validator): use validationOptions instead of magic boolean

--- test/unit/options_validator_tests.js
+++ test/unit/options_validator_tests.js
@@ -519,7 +519,10 @@ describe('Options Validation', function() {
     };
 
     const testObject = { a: true, b: 45 };
-    validate(validationSchema, testObject, { optionsValidationLevel: 'warn' }, true);
+    validate(validationSchema, testObject, {
+      optionsValidationLevel: 'warn',
+      warnOnUnknownOptions: true
+    });
 
     expect(stub).to.have.been.calledOnce;
     expect(stub).to.have.been.calledWith('provided option [b] is an unknown option');
