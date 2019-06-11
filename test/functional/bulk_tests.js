6bcf1e4e433653ec784861052e982ae831fdc561
Sam Pal <sam.pal@10gen.com>
2019-06-11 10:11:15 -0400

    fix(Bulk): change BulkWriteError message to first item from writeErrors (#2013)
    * fix(Bulk): change BulkWriteError message to first item from writeErrors
    
    Fixes NODE-1965

--- test/functional/bulk_tests.js
+++ test/functional/bulk_tests.js
@@ -937,6 +937,59 @@ describe('Bulk', function() {
     }
   });
 
+  it('should provide descriptive error message for unordered batch with duplicate key errors on inserts', function(done) {
+    const configuration = this.configuration;
+    const client = configuration.newClient(configuration.writeConcernMax(), {
+      poolSize: 1
+    });
+
+    client.connect((err, client) => {
+      const db = client.db(configuration.db);
+      const col = db.collection('err_batch_write_unordered_ops_legacy_6');
+
+      // Add unique index on a field causing all inserts to fail
+      col.createIndexes(
+        [
+          {
+            name: 'err_batch_write_unordered_ops_legacy_6',
+            key: { a: 1 },
+            unique: true
+          }
+        ],
+        err => {
+          expect(err).to.not.exist;
+
+          // Initialize the unordered Batch
+          const batch = col.initializeUnorderedBulkOp();
+
+          // Add some operations to be executed in order
+          batch.insert({ a: 1 });
+          batch.insert({ a: 1 });
+
+          // Execute the operations
+          batch.execute(configuration.writeConcernMax(), (err, result) => {
+            expect(err).to.exist;
+            expect(result).to.not.exist;
+
+            // Test basic settings
+            result = err.result;
+            expect(result.nInserted).to.equal(1);
+            expect(result.hasWriteErrors()).to.equal(true);
+            expect(result.getWriteErrorCount() === 1).to.equal(true);
+
+            // Individual error checking
+            const error = result.getWriteErrorAt(0);
+            expect(error.code === 11000).to.equal(true);
+            expect(error.errmsg).to.exist;
+            expect(err.message).to.equal(error.errmsg);
+
+            client.close(done);
+          });
+        }
+      );
+    });
+  });
+
   it(
     'should Correctly Execute Unordered Batch of with upserts causing duplicate key errors on updates',
     {
