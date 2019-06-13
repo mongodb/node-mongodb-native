09294f78b01dc7590c277d0adbe69f4f32f58985
Sam Pal <sam.pal@10gen.com>
2019-06-13 16:57:31 -0400

    add beforeEach and afterEach to reduce repetition

--- test/functional/error_tests.js
+++ test/functional/error_tests.js
@@ -10,68 +10,69 @@ describe('Errors', function() {
     return setupDatabase(this.configuration);
   });
 
+  let client;
+  beforeEach(function() {
+    client = this.configuration.newClient(this.configuration.writeConcernMax(), { poolSize: 1 });
+    return client.connect();
+  });
+  afterEach(function() {
+    return client.close();
+  });
+
   it('should fail insert due to unique index', function(done) {
-    const configuration = this.configuration;
-    const client = configuration.newClient(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect((err, client) => {
-      const db = client.db(configuration.db);
-      const collection = db.collection('test_failing_insert_due_to_unique_index');
-      collection.createIndexes(
-        [
-          {
-            name: 'test_failing_insert_due_to_unique_index',
-            key: { a: 1 },
-            unique: true
-          }
-        ],
-        { w: 1 },
-        err => {
+    const db = client.db(this.configuration.db);
+    const collection = db.collection('test_failing_insert_due_to_unique_index');
+    collection.createIndexes(
+      [
+        {
+          name: 'test_failing_insert_due_to_unique_index',
+          key: { a: 1 },
+          unique: true
+        }
+      ],
+      { w: 1 },
+      err => {
+        expect(err).to.not.exist;
+
+        collection.insertOne({ a: 2 }, { w: 1 }, err => {
           expect(err).to.not.exist;
 
           collection.insertOne({ a: 2 }, { w: 1 }, err => {
-            expect(err).to.not.exist;
-
-            collection.insertOne({ a: 2 }, { w: 1 }, err => {
-              expect(err.code).to.equal(11000);
-              client.close(done);
-            });
+            expect(err.code).to.equal(11000);
+            done();
           });
-        }
-      );
-    });
+        });
+      }
+    );
   });
 
   it('should fail insert due to unique index strict', function(done) {
-    const configuration = this.configuration;
-    const client = configuration.newClient(configuration.writeConcernMax(), { poolSize: 1 });
-    client.connect((err, client) => {
-      const db = client.db(configuration.db);
-      db.dropCollection('test_failing_insert_due_to_unique_index_strict', () => {
-        db.createCollection('test_failing_insert_due_to_unique_index_strict', err => {
-          expect(err).to.not.exist;
-          db.collection('test_failing_insert_due_to_unique_index_strict', (err, collection) => {
-            collection.createIndexes(
-              [
-                {
-                  name: 'test_failing_insert_due_to_unique_index_strict',
-                  key: { a: 1 },
-                  unique: true
-                }
-              ],
-              { w: 1 },
-              err => {
-                expect(err).to.not.exist;
-                collection.insertOne({ a: 2 }, { w: 1 }, err => {
-                  expect(err).to.not.exist;
-
-                  collection.insertOne({ a: 2 }, { w: 1 }, err => {
-                    expect(err.code).to.equal(11000);
-                    client.close(done);
-                  });
-                });
+    const db = client.db(this.configuration.db);
+    db.dropCollection('test_failing_insert_due_to_unique_index_strict', () => {
+      db.createCollection('test_failing_insert_due_to_unique_index_strict', err => {
+        expect(err).to.not.exist;
+        db.collection('test_failing_insert_due_to_unique_index_strict', (err, collection) => {
+          collection.createIndexes(
+            [
+              {
+                name: 'test_failing_insert_due_to_unique_index_strict',
+                key: { a: 1 },
+                unique: true
               }
-            );
-          });
+            ],
+            { w: 1 },
+            err => {
+              expect(err).to.not.exist;
+              collection.insertOne({ a: 2 }, { w: 1 }, err => {
+                expect(err).to.not.exist;
+
+                collection.insertOne({ a: 2 }, { w: 1 }, err => {
+                  expect(err.code).to.equal(11000);
+                  done();
+                });
+              });
+            }
+          );
         });
       });
     });
@@ -80,17 +81,13 @@ describe('Errors', function() {
   it('should return an error object with message when mixing included and excluded fields', {
     metadata: { requires: { mongodb: '>3.0' } },
     test: function(done) {
-      const configuration = this.configuration;
-      const client = configuration.newClient(configuration.writeConcernMax(), { poolSize: 1 });
-      client.connect((err, client) => {
-        const db = client.db(configuration.db);
-        const c = db.collection('test_error_object_should_include_message');
-        c.insertOne({ a: 2, b: 5 }, { w: 1 }, err => {
-          expect(err).to.not.exist;
-          c.findOne({ a: 2 }, { fields: { a: 1, b: 0 } }, err => {
-            expect(err.errmsg).to.equal('Projection cannot have a mix of inclusion and exclusion.');
-            client.close(done);
-          });
+      const db = client.db(this.configuration.db);
+      const c = db.collection('test_error_object_should_include_message');
+      c.insertOne({ a: 2, b: 5 }, { w: 1 }, err => {
+        expect(err).to.not.exist;
+        c.findOne({ a: 2 }, { fields: { a: 1, b: 0 } }, err => {
+          expect(err.errmsg).to.equal('Projection cannot have a mix of inclusion and exclusion.');
+          done();
         });
       });
     }
@@ -99,16 +96,11 @@ describe('Errors', function() {
   it('should handle error throw in user callback', {
     metadata: { requires: { mongodb: '>3.0' } },
     test: function(done) {
-      const configuration = this.configuration;
-      const client = configuration.newClient({ w: 1 }, { poolSize: 1 });
-
-      client.connect((err, client) => {
-        const db = client.db(configuration.db);
-        const c = db.collection('test_error_object_should_include_message');
-        c.findOne({}, { fields: { a: 1, b: 0 } }, err => {
-          expect(err.errmsg).to.equal('Projection cannot have a mix of inclusion and exclusion.');
-          client.close(done);
-        });
+      const db = client.db(this.configuration.db);
+      const c = db.collection('test_error_object_should_include_message');
+      c.findOne({}, { fields: { a: 1, b: 0 } }, err => {
+        expect(err.errmsg).to.equal('Projection cannot have a mix of inclusion and exclusion.');
+        done();
       });
     }
   });
