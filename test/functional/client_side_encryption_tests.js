f7f4097c498e76a280b7b86cb00a85a605d4060d
Matt Broadstone <mbroadst@gmail.com>
2019-06-15 09:21:02 -0400

    test: skip client side encryption tests if lacking AWS credentials

--- test/functional/client_side_encryption_tests.js
+++ test/functional/client_side_encryption_tests.js
@@ -5,6 +5,9 @@ const TestRunnerContext = require('./runner').TestRunnerContext;
 const gatherTestSuites = require('./runner').gatherTestSuites;
 const generateTopologyTests = require('./runner').generateTopologyTests;
 
+const missingAwsConfiguration =
+  process.env.AWS_ACCESS_KEY_ID == null || process.env.AWS_SECRET_ACCESS_KEY == null;
+
 describe('Client Side Encryption', function() {
   const testContext = new TestRunnerContext();
   const testSuites = gatherTestSuites(path.join(__dirname, 'spec', 'client-side-encryption'));
@@ -12,6 +15,11 @@ describe('Client Side Encryption', function() {
   before(function() {
     return testContext.setup(this.configuration);
   });
+
+  if (missingAwsConfiguration) {
+    console.log('skipping Client Side Encryption tests due to lack of AWS credentials');
+    return;
+  }
 
   generateTopologyTests(testSuites, testContext);
 });
