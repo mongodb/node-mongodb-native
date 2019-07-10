4c9f77c57022298f9d3000204e914ed7fd39aa0d
Katherine Walker <katherine.walker@mongodb.com>
2019-07-10 15:05:07 -0400

    use URI from spec tests

--- test/unit/uri_parser_tests.js
+++ test/unit/uri_parser_tests.js
@@ -8,17 +8,14 @@ describe('New URL Parser', function() {
    * @ignore
    */
   it('should return the correct hostname when given a URL with srv', function(done) {
-    parseConnectionString(
-      'mongodb+srv://admin:password@compass-data-sets-e06dc.mongodb.net/admin',
-      (err, result) => {
-        expect(err).to.not.exist;
-        expect(result.hosts.length).to.equal(1);
-        expect(result.hosts[0]).to.deep.equal({
-          host: 'compass-data-sets-e06dc.mongodb.net',
-          port: 27017
-        });
-        done();
-      }
-    );
+    parseConnectionString('mongodb+srv://auser:apass@test1.test.build.10gen.cc', (err, result) => {
+      expect(err).to.not.exist;
+      expect(result.hosts.length).to.equal(1);
+      expect(result.hosts[0]).to.deep.equal({
+        host: 'test1.test.build.10gen.cc',
+        port: 27017
+      });
+      done();
+    });
   });
 });
