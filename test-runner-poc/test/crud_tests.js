1a7d1a0236097c7ed5f1cba7f9d353f7c7d7f4d4
Sam Pal <sam.pal@10gen.com>
2019-07-12 14:09:35 -0400

    isolating test runner poc and adding basic tests

---
+++ test-runner-poc/test/crud_tests.js
@@ -0,0 +1,15 @@
+'use strict';
+/* include stuff */
+
+describe('CRUD', function() {
+
+  it('should insert a document', {
+    metadata: { requires: { mongodb: '>=3.6.0', topology: 'single' } },
+    test: function() {
+      // const client = new MongoClient('mongodb://localhost:27017');
+      // return client.connect()
+      //   .then(() => insert)
+      //   .then(() => find);
+    }
+  });
+});
