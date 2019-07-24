075596895f346e8a5f8ff55d061ad254e45d42d3
Dan Aprahamian <dan.aprahamian@gmail.com>
2019-07-24 14:08:10 -0400

    test(ClientSideEncryption): fully skip when no credentials

--- test/functional/client_side_encryption_tests.js
+++ test/functional/client_side_encryption_tests.js
@@ -9,17 +9,17 @@ const missingAwsConfiguration =
   process.env.AWS_ACCESS_KEY_ID == null || process.env.AWS_SECRET_ACCESS_KEY == null;
 
 describe('Client Side Encryption', function() {
+  if (missingAwsConfiguration) {
+    console.log('skipping Client Side Encryption tests due to lack of AWS credentials');
+    return;
+  }
+
   const testContext = new TestRunnerContext();
   const testSuites = gatherTestSuites(path.join(__dirname, 'spec', 'client-side-encryption'));
   after(() => testContext.teardown());
   before(function() {
     return testContext.setup(this.configuration);
   });
-
-  if (missingAwsConfiguration) {
-    console.log('skipping Client Side Encryption tests due to lack of AWS credentials');
-    return;
-  }
 
   generateTopologyTests(testSuites, testContext);
 });
