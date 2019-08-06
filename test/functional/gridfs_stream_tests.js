d38c0213ef938ccb1cb45262ba19d09f9c7029d5
Rosemary Yin <rosemary.yin@Rosemarys-MacBook-Pro.local>
2019-08-06 10:58:23 -0400

    removed if statement from gridfs_stream_tests

--- test/functional/gridfs_stream_tests.js
+++ test/functional/gridfs_stream_tests.js
@@ -1189,25 +1189,14 @@ describe('GridFS Stream', function() {
 
                 download.on('end', function() {
                   var result = testSpec.assert.result;
-                  if (!result) {
-                    test.ok(false);
-
-                    // We need to abort in order to close the underlying cursor,
-                    // and by extension the implicit session used for the cursor.
-                    // This is only necessary if the cursor is not exhausted
-                    download.abort();
-                    client.close();
-                    done();
-                  }
-
+                  expect(result).to.exist;
                   test.equal(res.toString('hex'), result.$hex);
 
                   // We need to abort in order to close the underlying cursor,
                   // and by extension the implicit session used for the cursor.
                   // This is only necessary if the cursor is not exhausted
                   download.abort();
-                  client.close();
-                  done();
+                  client.close(done);
                 });
               };
 
