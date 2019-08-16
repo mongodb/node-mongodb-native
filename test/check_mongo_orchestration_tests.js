ed3e75b6b6a8d9b7bfa8be784920d978c929a5b0
Rosemary Yin <rosemary.yin@Rosemarys-MacBook-Pro.local>
2019-08-16 10:18:40 -0400

    install mongo-orchestration

--- test/check_mongo_orchestration_tests.js
+++ test/check_mongo_orchestration_tests.js
@@ -1,7 +1,9 @@
 'use strict';
 const shell = require('shelljs');
 if (!shell.which('mongo-orchestration')) {
-  shell.echo('Warning: please install mongo-orchestration.');
+  //shell.echo('Warning: please install mongo-orchestration.');
+  shell.echo('Mongo-orchestration not found. Installing mongo-orchestration.');
+  shell.exec('pip install mongo-orchestration');
 } else {
   shell.echo('Path to Mongo-Orchestration:');
   shell.echo(shell.which('mongo-orchestration'));
