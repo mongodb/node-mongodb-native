756f53a9b10b6b5e8a69ff0dc88d9fdcbee71d63
Rosemary Yin <rosemary.yin@Rosemarys-MacBook-Pro.local>
2019-08-22 14:04:37 -0400

    start up a single node

--- test/check_mongo_orchestration_tests.js
+++ test/check_mongo_orchestration_tests.js
@@ -1,19 +1,13 @@
 'use strict';
 const shell = require('shelljs');
-if (!shell.which('mongo-orchestration')) {
-  //shell.echo('Warning: please install mongo-orchestration.');
-  // shell.echo('Mongo-orchestration not found. Installing mongo-orchestration.');
-  // shell.exec('pip install mongo-orchestration');
+if (!shell.test('-e', 'mongo-orchestration')) {
+  shell.echo('Mongo-orchestration not found. Installing mongo-orchestration.');
   shell.exec('git clone https://github.com/10gen/mongo-orchestration.git');
   shell.cd('mongo-orchestration');
   shell.exec('pip install . --user');
-  shell.exec('ls');
-  shell.cd('mongo_orchestration');
-  shell.exec('ls')
-  shell.cd('configurations')
-  shell.exec('mo servers/clean.json start');
-  //shell.exec('pip install git+https://github.com/10gen/mongo-orchestration.git --user')
+} else {
+  shell.cd('mongo-orchestration');
 }
-shell.echo('Path to Mongo-Orchestration:');
-shell.echo(shell.which('mongo-orchestration'));
-shell.cd(shell.which('mongo-orchestration'));
+shell.cd('mongo_orchestration');
+shell.cd('configurations');
+shell.exec('../../scripts/mo servers/clean.json start');
