cb7a5e7f2c91677579a444c5917a2c32a4663d94
Dan Aprahamian <dan.aprahamian@gmail.com>
2019-09-17 15:20:33 -0400

    incorporating feedback

--- test/examples/transactions.js
+++ test/examples/transactions.js
@@ -251,7 +251,6 @@ describe('examples(transactions):', function() {
         .insertOne({ xyz: 0 }, { w: 'majority' });
 
       // Step 1: Start a Client Session
-
       const session = client.startSession();
 
       // Step 2: Optional. Define options to use for the transaction
@@ -263,7 +262,6 @@ describe('examples(transactions):', function() {
 
       // Step 3: Use withTransaction to start a transaction, execute the callback, and commit (or abort on error)
       // Note: The callback for withTransaction MUST be async and/or return a Promise.
-      let error;
       try {
         await session.withTransaction(async () => {
           const coll1 = client.db('mydb1').collection('foo');
@@ -274,15 +272,9 @@ describe('examples(transactions):', function() {
           await coll1.insertOne({ abc: 1 }, { session });
           await coll2.insertOne({ xyz: 999 }, { session });
         }, transactionOptions);
-      } catch (e) {
-        error = e;
-      }
-
-      session.endSession();
-      await client.close();
-
-      if (error) {
-        throw error;
+      } finally {
+        session.endSession();
+        await client.close();
       }
       // End Transactions withTxn API Example 1
     }
