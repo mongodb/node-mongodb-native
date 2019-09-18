96c84b6e6a851b6b287d086795157a4bd6fdb6ef
Matt Broadstone <mbroadst@mongodb.com>
2019-09-18 17:12:16 -0400

    test: refactor core logic of the new test runner, reused eachAsync

--- test/runner/index.js
+++ test/runner/index.js
@@ -1,123 +1,113 @@
-"use strict";
+'use strict';
 
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
 const environments = require('../environments');
 const TestConfiguration = require('../config');
 const parseConnectionString = require('../../lib/core/uri_parser');
 const mock = require('mongodb-mock-server');
+const eachAsync = require('../../lib/utils').eachAsync;
 
-let mongoClient;
-let filters = [];
-let files = [];
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const filters = [];
 
-function addFilter(filter) {
-  if (typeof filter !== 'object') {
-    throw new Error('Type of filter must be an object');
-  }
-  if (!filter.filter || typeof filter.filter !== 'function') {
-    throw new Error('Object filters must have a function named filter');
-  }
-  filters.push(filter);
+function initializeEnvironment(context, callback) {
+  const environmentName = context.environmentName;
+  const version = context.version;
 
-}
-function environmentSetup(environmentCallback) {
-  const mongodb_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-  mongoClient = new MongoClient(mongodb_uri);
+  console.log(`[environment: ${environmentName}]`);
+  const Environment = environments[environmentName];
 
-  mongoClient.connect((err, client) => {
+  parseConnectionString(MONGODB_URI, (err, parsedURI) => {
     if (err) {
-      return environmentCallback(err);
+      callback(err);
+      return;
     }
-    createFilters(environmentParser);
 
-    function environmentParser(environmentName, version) {
-      console.log('environmentName ', environmentName);
-      const Environment = environments[environmentName];
-      parseConnectionString(mongodb_uri, (err, parsedURI) => {
-        if (err) {
-          return environmentCallback(err);
-        }
-        const environment = new Environment(parsedURI, version);
-        environment.mongo = require('../../index');
-        client.close(() => environmentCallback(err, environment));
-      });
-    }
+    const environment = new Environment(parsedURI, version);
+    environment.mongo = require('../../index');
+    callback(null, environment);
   });
 }
 
-function createFilters(callback) {
-  let filtersInitialized = 0;
-  const filterFiles = fs.readdirSync(path.join(__dirname, 'filters'));
+function initializeFilters(client, callback) {
+  const filterFiles = fs
+    .readdirSync(path.join(__dirname, 'filters'))
+    .filter(x => x.indexOf('js') !== -1);
 
-  let topology, version;
+  // context object that can be appended to as part of filter initialization
+  const context = {};
 
-  filterFiles.filter(x => x.indexOf('js') !== -1).forEach(x => {
-    const FilterModule = require(path.join(__dirname, 'filters', x));
-    const filter = new FilterModule();
+  eachAsync(
+    filterFiles,
+    (filterName, cb) => {
+      const FilterModule = require(path.join(__dirname, 'filters', filterFiles[i]));
+      const filter = new FilterModule();
 
-    if (typeof filter.initializeFilter === 'function') {
-      filter.initializeFilter(_increment);
-    } else {
-      _increment();
-    }
-
-    //Makes sure to wait for all the filters to be initialized and added before calling the callback
-    function _increment() {
-      topology = topology || filter.runtimeTopology;
-      version = version || filter.mongoVersion;
-      filtersInitialized += 1;
-      addFilter(filter);
-
-      if (filtersInitialized === filterFiles.length) {
-        callback(topology, version);
+      if (typeof filter !== 'object') {
+        throw new TypeError('Type of filter must be an object');
       }
-    }
-  });
+
+      if (!filter.filter || typeof filter.filter !== 'function') {
+        throw new TypeError('Object filters must have a function named filter');
+      }
+
+      filters.push(filter);
+      if (typeof filter.initializeFilter === 'function') {
+        filter.initializeFilter(client, context, cb);
+      } else {
+        cb();
+      }
+    },
+    () => callback(null, context)
+  );
 }
 
 before(function(done) {
-  environmentSetup((err, environment) => {
+  console.log(`connection string: ${MONGODB_URI}`);
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  done = client.close(done);
+
+  client.connect(err => {
     if (err) {
       done(err);
-    } else {
-      this.configuration = new TestConfiguration(environment);
-      done();
+      return;
     }
+
+    initializeFilters(client, (err, context) => {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      initializeEnvironment(context, (err, environment) => {
+        if (err) {
+          done(err);
+          return;
+        }
+
+        this.configuration = new TestConfiguration(environment);
+        done();
+      });
+    });
   });
 });
 
-beforeEach(function(done) {
-  // Assigned this to a new variable called self in order to preserve context and access tests within the _run function.
-  const self = this;
-  let filtersExecuted = 0;
-  if (filters.length) {
-    filters.forEach(function(filter) {
-      _run(filter);
-    });
-  }
-
-  function _run(filter) {
-    filtersExecuted += 1;
-
-    if (!filter.filter(self.currentTest)) {
-      //If we filter out a test, we'd like to ensure that any before hooks are skipped, as long as that hook is not the root hook.
-      if (!self.currentTest.parent.parent.root) {
-        self.currentTest.parent._beforeEach = [];
+beforeEach(function() {
+  for (let i = 0; i < filters.length; ++i) {
+    const filter = filter[i];
+    if (!filter.filter(this.currentTest)) {
+      // if we filter out a test, we'd like to ensure that any before hooks are skipped, as
+      // long as that hook is not the root hook.
+      if (!this.currentTest.parent.parent.root) {
+        this.currentTest.parent._beforeEach = [];
       }
-      self.skip();
-    }
-    if (filtersExecuted === filters.length) {
-      done();
+
+      this.skip();
+      return;
     }
   }
 });
 
 afterEach(() => mock.cleanup());
-
-after(function(done) {
-  mongoClient.close(() => {
-    done();
-  });
-});
