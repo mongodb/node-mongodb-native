2c6549843669f6b850e181e75b1a77a7cc20f015
Matt Broadstone <mbroadst@mongodb.com>
2019-09-26 09:34:57 -0400

    test: move test runner plugins into their own folder, load them

--- test/runner/plugins/session_leak_checker.js
+++ test/runner/plugins/session_leak_checker.js
@@ -2,8 +2,8 @@
 
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const core = require('../../lib/core');
-const MongoClient = require('../../lib/mongo_client');
+const core = require('../../../lib/core');
+const MongoClient = require('../../../lib/mongo_client');
 const ServerSessionPool = core.Sessions.ServerSessionPool;
 
 const sandbox = sinon.createSandbox();
