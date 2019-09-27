7fd6071d817eb28fa39a30647cbbca5fb03d8653
Matt Broadstone <mbroadst@mongodb.com>
2019-09-27 14:53:00 -0400

    fix(replset): destroy primary before removing from replsetstate

--- test/runner/plugins/client_leak_checker.js
+++ test/runner/plugins/client_leak_checker.js
@@ -26,3 +26,9 @@ afterEach(() => {
 });
 
 after(() => wtfnode.dump());
+
+require('leaked-handles').set({
+  fullStack: true, // use full stack traces
+  timeout: 30000, // run every 30 seconds instead of 5.
+  debugSockets: true // pretty print tcp thrown exceptions.
+});
