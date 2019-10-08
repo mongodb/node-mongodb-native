2026a829b408717b85a3425fd5b783550fb99868
Matt Broadstone <mbroadst@mongodb.com>
2019-10-08 10:38:31 -0400

    test(transactions): improve mongos discovery in testing
    The way that we were determining whether we ran against a sharded
    environment was based on the environment previously, this needed
    to change to use the `topologyType` set on the test context. Also,
    the way we build urls has changed until we come up with a more
    dynamic way to resolve sharded deployments from a single host
    passed in to the connection string

--- test/functional/spec-runner/utils.js
+++ test/functional/spec-runner/utils.js
@@ -1,8 +1,7 @@
 'use strict';
-const environments = require('../../../test-runner/environments');
 
 function resolveConnectionString(configuration, spec) {
-  const isShardedEnvironment = configuration.environment instanceof environments.sharded;
+  const isShardedEnvironment = configuration.topologyType === 'Sharded';
   const useMultipleMongoses = spec && !!spec.useMultipleMongoses;
 
   return isShardedEnvironment && !useMultipleMongoses
