0463f7838dbfc0740f6fa52174b96241eb0c3980
Matt Broadstone <mbroadst@mongodb.com>
2019-10-09 17:46:38 -0400

    test: force close clients in some tests where cursors are left open
    This is a stopgap for getting these tests to pass, since closing
    the cursor was still flakey

--- test/functional/operation_promises_example_tests.js
+++ test/functional/operation_promises_example_tests.js
@@ -6982,7 +6982,8 @@ describe('Operation (Promises)', function() {
             cursor.on('end', function() {
               test.ok(new Date().getTime() - s.getTime() > 1000);
 
-              client.close(done);
+              // TODO: forced because the cursor is still open/active
+              client.close(true, done);
             });
           });
       });
