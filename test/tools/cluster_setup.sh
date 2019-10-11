f9cef27a3b8713bed983ed98f90ea281fa8b7c93
Matt Broadstone <mbroadst@mongodb.com>
2019-10-11 10:40:10 -0400

    chore(travis): fix travis.yml by escaping complex regexs

--- test/tools/cluster_setup.sh
+++ test/tools/cluster_setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "usage: cluster_setup <single|replicaset|sharded>"
+    echo "usage: cluster_setup <server|replica_set|sharded_cluster>"
     exit
 fi
 
-
-if [[ $1 == "replicaset" ]]; then
-    mlaunch init --replicaset --nodes 3 --arbiter --name rs --port 31000 --enableMajorityReadConcern --setParameter enableTestCommands=1  
-elif [[ $1 == "sharded" ]]; then
+if [[ $1 == "replica_set" ]]; then
+    mlaunch init --replicaset --nodes 3 --arbiter --name rs --port 31000 --enableMajorityReadConcern --setParameter enableTestCommands=1
+    echo "mongodb://localhost:31000/?replicaSet=rs"
+elif [[ $1 == "sharded_cluster" ]]; then
     mlaunch init --replicaset --nodes 3 --arbiter --name rs --port 51000 --enableMajorityReadConcern --setParameter enableTestCommands=1 --sharded 1 --mongos 2
-elif [[ $1 == "single" ]]; then
+    echo "mongodb://localhost:51000,localhost:51001/"
+elif [[ $1 == "server" ]]; then
     mlaunch init --single --setParameter enableTestCommands=1
-    echo "unimplemented"
+    echo "mongodb://localhost:27017"
 else
     echo "unsupported topology: $1"
 fi
-
