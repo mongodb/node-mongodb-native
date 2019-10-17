41415e95966e894250ff930ec1ce5e60b569549b
Dan Aprahamian <dan.aprahamian@gmail.com>
2019-10-17 15:13:36 -0400

    docs(guide): change basic connect boilerplate for guide examples
    Fixes NODE-2243

--- docs/guide/includes/connect-boilerplate.js
+++ docs/guide/includes/connect-boilerplate.js
@@ -1,10 +1,14 @@
 const { MongoClient } = require('mongodb');
 
 // Connection URL
-const url = 'mongodb://localhost:27017,localhost:27018/?replicaSet=foo';
+const url = 'mongodb://localhost:27017';
 
 // Create a new MongoClient
 const client = new MongoClient(url);
+
+async function main() {
+  // Place your code in this function
+}
 
 // Function to connect to the server and run your code
 async function run() {
@@ -12,6 +16,8 @@ async function run() {
     // Connect the client to the server
     await client.connect();
     console.log('Connected successfully to server');
+
+    await main(client);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
