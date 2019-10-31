b67e3666a0f887c3838e2b53b291ffb16feaabee
Matt Broadstone <mbroadst@mongodb.com>
2019-10-31 08:44:20 -0400

    test: add script for running each test, useful for finding leaks
    This script runs each test by itself through mocha, making it much
    easier to spot when a test leaks connections.

---
+++ test/tools/run_each_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "usage: run_each_test <test path>"
+    exit
+fi
+
+TEST_PATH=$1
+find $TEST_PATH -type f \( -iname "*_tests.js" ! -iname "*atlas*" ! -path "*node-next*" \) -exec npx mocha {} \;
