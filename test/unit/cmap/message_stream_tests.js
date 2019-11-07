4eb0f4660f96378c0b66c85559680e529b23fe81
Matt Broadstone <mbroadst@mongodb.com>
2019-11-07 09:16:42 -0500

    feat: add `MessageStream` for streamed wire protocol messaging
    This is a duplex stream that can read and write MongoDB wire
    protocol messages, with optional compression. This bring in a
    dependency on the `bl` module.

---
+++ test/unit/cmap/message_stream_tests.js
@@ -0,0 +1,120 @@
+'use strict';
+const BSON = require('bson');
+const Readable = require('stream').Readable;
+const Writable = require('stream').Writable;
+const MessageStream = require('../../../lib/core/cmap/message_stream');
+const Msg = require('../../../lib/core/connection/msg').Msg;
+const expect = require('chai').expect;
+
+function bufferToStream(buffer) {
+  const stream = new Readable();
+  stream.push(buffer);
+  stream.push(null);
+  return stream;
+}
+
+function streamToBuffer(stream) {
+  return new Promise((resolve, reject) => {
+    let buffers = [];
+    stream.on('error', reject);
+    stream.on('data', data => buffers.push(data));
+    stream.on('end', () => resolve(Buffer.concat(buffers)));
+  });
+}
+
+describe('Message Stream', function() {
+  describe('reading', function() {
+    [
+      {
+        description: 'valid OP_REPLY',
+        data: Buffer.from(
+          '370000000100000001000000010000000000000000000000000000000000000001000000130000001069736d6173746572000100000000',
+          'hex'
+        ),
+        documents: [{ ismaster: 1 }]
+      },
+      {
+        description: 'valid OP_MSG',
+        data: Buffer.from(
+          '370000000100000000000000dd0700000000000000220000001069736d6173746572000100000002246462000600000061646d696e0000',
+          'hex'
+        ),
+        documents: [{ $db: 'admin', ismaster: 1 }]
+      },
+      {
+        description: 'Invalid message size (negative)',
+        data: Buffer.from('ffffffff', 'hex'),
+        error: 'Invalid message size: -1'
+      },
+      {
+        description: 'Invalid message size (exceeds maximum)',
+        data: Buffer.from('01000004', 'hex'),
+        error: 'Invalid message size: 67108865, max allowed: 67108864'
+      }
+    ].forEach(test => {
+      it(test.description, function(done) {
+        const bson = new BSON();
+        const error = test.error;
+        const inputStream = bufferToStream(test.data);
+        const messageStream = new MessageStream({ bson });
+
+        messageStream.on('message', msg => {
+          if (error) {
+            done(new Error(`expected error: ${error}`));
+            return;
+          }
+
+          msg.parse();
+
+          if (test.documents) {
+            expect(msg).to.have.property('documents');
+            expect(msg.documents).to.eql(test.documents);
+          }
+
+          done();
+        });
+
+        messageStream.on('error', err => {
+          if (error == null) {
+            done(err);
+            return;
+          }
+
+          expect(err.message).to.equal(error);
+          done();
+        });
+
+        inputStream.pipe(messageStream);
+      });
+    });
+  });
+
+  describe('writing', function() {
+    it('should write a message to the stream', function(done) {
+      const readableStream = new Readable({ read() {} });
+      const writeableStream = new Writable({
+        write: (chunk, _, callback) => {
+          readableStream.push(chunk);
+          callback();
+        }
+      });
+
+      readableStream.on('data', data => {
+        expect(data.toString('hex')).to.eql(
+          '370000000300000000000000dd0700000000000000220000001069736d6173746572000100000002246462000600000061646d696e0000'
+        );
+
+        done();
+      });
+
+      const bson = new BSON();
+      const messageStream = new MessageStream({ bson });
+      messageStream.pipe(writeableStream);
+
+      const command = new Msg(bson, 'admin.$cmd', { ismaster: 1 }, {});
+      messageStream.writeMessage(command, null, err => {
+        done(err);
+      });
+    });
+  });
+});
