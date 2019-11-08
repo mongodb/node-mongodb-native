68ab92542a0e848c73570fa367f011eba850be5f
Matt Broadstone <mbroadst@mongodb.com>
2019-11-08 11:07:33 -0500

    test: add tests for command monitoring with new connection type

--- test/functional/cmap/connection_tests.js
+++ test/functional/cmap/connection_tests.js
@@ -24,4 +24,31 @@ describe('Connection', function() {
       });
     });
   });
+
+  it('should emit command monitoring events', function(done) {
+    const connectOptions = Object.assign(
+      { connectionType: Connection, bson: new BSON(), monitorCommands: true },
+      this.configuration.options
+    );
+
+    connect(connectOptions, (err, conn) => {
+      expect(err).to.not.exist;
+
+      const events = [];
+      conn.on('commandStarted', event => events.push(event));
+      conn.on('commandSucceeded', event => events.push(event));
+      conn.on('commandFailed', event => events.push(event));
+
+      conn.command('admin.$cmd', { ismaster: 1 }, (err, ismaster) => {
+        expect(err).to.not.exist;
+        expect(ismaster).to.exist;
+        expect(ismaster.ok).to.equal(1);
+
+        expect(events).to.have.length(2);
+        console.dir({ events }, { depth: 3 });
+
+        conn.destroy(done);
+      });
+    });
+  });
 });
