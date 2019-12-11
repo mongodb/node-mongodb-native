d0fbe81f65eeceffec54b5fc83456d4db91c6418
Matt Broadstone <mbroadst@mongodb.com>
2019-12-11 12:16:56 -0500

    refactor: simplify required api of `Connection` type

--- test/unit/cmap/connection_pool.test.js
+++ test/unit/cmap/connection_pool.test.js
@@ -12,31 +12,15 @@ const expect = chai.expect;
 class Connection {
   constructor(options) {
     options = options || {};
-    this.generation = options.generation;
+
     this.id = options.id;
-    this.maxIdleTimeMS = options.maxIdleTimeMS;
-    this.poolId = options.poolId;
-    this.address = options.address;
+    this.generation = options.generation;
     this.readyToUse = false;
     this.lastMadeAvailable = undefined;
-    this.callbacks = [];
-  }
-
-  get metadata() {
-    return {
-      id: this.id,
-      generation: this.generation,
-      poolId: this.poolId,
-      address: this.adress
-    };
   }
 
   timeIdle() {
     return this.readyToUse ? Date.now() - this.lastMadeAvailable : 0;
-  }
-
-  write(callback) {
-    setTimeout(() => callback());
   }
 
   makeReadyToUse() {
@@ -44,26 +28,8 @@ class Connection {
     this.lastMadeAvailable = Date.now();
   }
 
-  makeInUse() {
-    this.readyToUse = false;
-    this.lastMadeAvailable = undefined;
-  }
-
-  waitUntilConnect(callback) {
-    if (this.readyToUse) {
-      return callback(null, this);
-    }
-
-    this.callbacks.push(callback);
-  }
-
   connect(callback) {
-    this.callbacks.push(callback);
-    setTimeout(() => {
-      this.makeReadyToUse();
-      this.callbacks.forEach(c => c(null, this));
-      this.callbacks = [];
-    });
+    setTimeout(() => callback());
   }
 
   destroy(options, callback) {
