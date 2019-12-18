abe0ff974857263a429398b033a671514fad5c35
Matt Broadstone <mbroadst@mongodb.com>
2019-12-18 10:35:04 -0500

    refactor: do stricter equality checks for server errors
    The old ServerDescription comparison operator was checking whether
    incoming errors were the same object as the existing one, a
    condition that would always be false. This change introduces a
    method for more strictly comparing two errors.

---
+++ test/unit/sdam/server_description.test.js
@@ -0,0 +1,44 @@
+'use strict';
+const ServerDescription = require('../../../lib/core/sdam/server_description').ServerDescription;
+const expect = require('chai').expect;
+
+describe('ServerDescription', function() {
+  describe('error equality', function() {
+    [
+      {
+        description: 'equal error types and messages',
+        lhs: new ServerDescription('127.0.0.1:27017', null, { error: new Error('test') }),
+        rhs: new ServerDescription('127.0.0.1:27017', null, { error: new Error('test') }),
+        equal: true
+      },
+      {
+        description: 'equal error types and unequal messages',
+        lhs: new ServerDescription('127.0.0.1:27017', null, { error: new Error('test') }),
+        rhs: new ServerDescription('127.0.0.1:27017', null, { error: new Error('blah') }),
+        equal: false
+      },
+      {
+        description: 'unequal error types and equal messages',
+        lhs: new ServerDescription('127.0.0.1:27017', null, { error: new TypeError('test') }),
+        rhs: new ServerDescription('127.0.0.1:27017', null, { error: new Error('test') }),
+        equal: false
+      },
+      {
+        description: 'null lhs',
+        lhs: new ServerDescription('127.0.0.1:27017', null, { error: null }),
+        rhs: new ServerDescription('127.0.0.1:27017', null, { error: new Error('test') }),
+        equal: false
+      },
+      {
+        description: 'null rhs',
+        lhs: new ServerDescription('127.0.0.1:27017', null, { error: new TypeError('test') }),
+        rhs: new ServerDescription('127.0.0.1:27017', null, { error: undefined }),
+        equal: false
+      }
+    ].forEach(test => {
+      it(test.description, function() {
+        expect(test.lhs.equals(test.rhs)).to.equal(test.equal);
+      });
+    });
+  });
+});
