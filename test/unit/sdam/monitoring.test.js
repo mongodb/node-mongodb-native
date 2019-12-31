a13dc6803209494c41745ab7f9890dac26711849
Matt Broadstone <mbroadst@mongodb.com>
2019-12-31 13:03:23 -0500

    fix: recover on network error during initial connect

--- test/unit/sdam/monitoring.test.js
+++ test/unit/sdam/monitoring.test.js
@@ -58,6 +58,47 @@ describe('monitoring', function() {
     });
   });
 
+  it('should recover on error during initial connect', function(done) {
+    let acceptConnections = false;
+    mockServer.setMessageHandler(request => {
+      if (!acceptConnections) {
+        request.connection.destroy();
+        return;
+      }
+
+      const doc = request.document;
+      if (doc.ismaster) {
+        request.reply(Object.assign({}, mock.DEFAULT_ISMASTER));
+      } else if (doc.endSessions) {
+        request.reply({ ok: 1 });
+      }
+    });
+
+    setTimeout(() => {
+      acceptConnections = true;
+    }, 250);
+
+    // set `heartbeatFrequencyMS` to 250ms to force a quick monitoring check, and wait 500ms to validate below
+    const topology = new Topology(mockServer.uri(), { heartbeatFrequencyMS: 250 });
+    topology.connect(err => {
+      expect(err).to.not.exist;
+
+      setTimeout(() => {
+        expect(topology)
+          .property('description')
+          .property('servers')
+          .to.have.length(1);
+
+        const serverDescription = Array.from(topology.description.servers.values())[0];
+        expect(serverDescription)
+          .property('roundTripTime')
+          .to.be.greaterThan(0);
+
+        topology.close(done);
+      }, 500);
+    });
+  });
+
   describe('Monitor', function() {
     it('should connect and issue an initial server check', function(done) {
       mockServer.setMessageHandler(request => {
