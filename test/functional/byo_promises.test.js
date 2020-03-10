e8d344fac7e5a7005edabeca434b6653ffde0649
Thomas Reggi <thomas.reggi@mongodb.com>
2020-03-10 13:32:40 -0400

    fix: pass optional promise lib to maybePromise

--- test/functional/byo_promises.test.js
+++ test/functional/byo_promises.test.js
@@ -1,4 +1,5 @@
 'use strict';
+const maybePromise = require('./../../lib/utils').maybePromise;
 var expect = require('chai').expect;
 
 describe('BYO Promises', function() {
@@ -31,6 +32,74 @@ describe('BYO Promises', function() {
         promise.then(function() {
           client.close(done);
         });
+      });
+    }
+  });
+});
+
+class CustomPromise extends Promise {}
+CustomPromise.prototype.isCustomMongo = true;
+
+describe('Pptional PromiseLibrary / maybePromise', function() {
+  it('should correctly implement custom dependency-less promise', function(done) {
+    const getCustomPromise = v => new CustomPromise(resolve => resolve(v));
+    const getNativePromise = v => new Promise(resolve => resolve(v));
+    expect(getNativePromise()).to.not.have.property('isCustomMongo');
+    expect(getCustomPromise()).to.have.property('isCustomMongo');
+    expect(getNativePromise()).to.have.property('then');
+    expect(getCustomPromise()).to.have.property('then');
+    done();
+  });
+
+  it('should return a promise with extra property CustomMongo', function(done) {
+    const prom = maybePromise(CustomPromise, undefined, () => 'example');
+    expect(prom).to.have.property('isCustomMongo');
+    expect(prom).to.have.property('then');
+    done();
+  });
+
+  it('should return a native promise', function(done) {
+    const prom = maybePromise(undefined, undefined, () => 'example');
+    expect(prom).to.not.have.property('isCustomMongo');
+    expect(prom).to.have.property('then');
+    done();
+  });
+
+  it('should have cursor return native promise', {
+    metadata: { requires: { topology: 'single' } },
+    test: function(done) {
+      const configuration = this.configuration;
+      const client = this.configuration.newClient({ w: 1 }, { poolSize: 1 });
+      client.connect((err, client) => {
+        const db = client.db(configuration.db);
+        expect(err).to.be.null;
+        const collection = db.collection('test');
+        const cursor = collection.find({});
+        const isPromise = cursor.toArray();
+        expect(isPromise).to.not.have.property('isCustomMongo');
+        expect(isPromise).to.have.property('then');
+        isPromise.then(() => client.close(done));
+      });
+    }
+  });
+
+  it('should have cursor return custom promise from new client options', {
+    metadata: { requires: { topology: 'single' } },
+    test: function(done) {
+      const configuration = this.configuration;
+      const client = this.configuration.newClient(
+        { w: 1 },
+        { poolSize: 1, promiseLibrary: CustomPromise }
+      );
+      client.connect((err, client) => {
+        const db = client.db(configuration.db);
+        expect(err).to.be.null;
+        const collection = db.collection('test');
+        const cursor = collection.find({});
+        const isPromise = cursor.toArray();
+        expect(isPromise).to.have.property('isCustomMongo');
+        expect(isPromise).to.have.property('then');
+        isPromise.then(() => client.close(done));
       });
     }
   });
