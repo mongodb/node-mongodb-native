b1c61a9d33e211451b8e0471679fb5defacb5e08
Neal Beeken <neal.beeken@mongodb.com>
2020-03-19 11:22:17 -0400

    docs(examples): correct replaceOne usage
    Use the correct parameters to the replaceOne example usage
    
    NODE-2502

--- test/functional/document_validation.test.js
+++ test/functional/document_validation.test.js
@@ -124,7 +124,7 @@ describe('Document Validation', function() {
                             // updateMany
                             col.replaceOne(
                               { e: 1 },
-                              { $set: { e: 1 } },
+                              { e: 1 },
                               { upsert: true, bypassDocumentValidation: true },
                               function(err) {
                                 test.equal(null, err);
