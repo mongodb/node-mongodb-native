fba81e389d61d9f68d7046b3425cd2cb7feab11e
Thomas Reggi <thomas.reggi@mongodb.com>
2020-04-06 12:08:12 -0400

    NODE-2528/3.6/tls-spec-skip

--- test/functional/uri_options_spec.test.js
+++ test/functional/uri_options_spec.test.js
@@ -10,31 +10,36 @@ const MongoParseError = core.MongoParseError;
 const loadSpecTests = require('../spec').loadSpecTests;
 
 describe('URI Options (spec)', function() {
-  loadSpecTests('uri-options').forEach(suite => {
-    describe(suite.name, () => {
-      suite.tests.forEach(test => {
-        const itFn = test.warning ? it.skip : it;
+  loadSpecTests('uri-options')
+    .filter(suite => {
+      if (suite.name === 'tls-options') return false;
+      return true;
+    })
+    .forEach(suite => {
+      describe(suite.name, () => {
+        suite.tests.forEach(test => {
+          const itFn = test.warning ? it.skip : it;
 
-        itFn(test.description, {
-          metadata: { requires: { topology: 'single' } },
-          test: function(done) {
-            parse(test.uri, {}, (err, result) => {
-              if (test.valid === true) {
-                expect(err).to.not.exist;
-                if (test.options.compressors != null) {
-                  result.options.compressors = result.options.compression.compressors;
-                  result.options.zlibCompressionLevel =
-                    result.options.compression.zlibCompressionLevel;
+          itFn(test.description, {
+            metadata: { requires: { topology: 'single' } },
+            test: function(done) {
+              parse(test.uri, {}, (err, result) => {
+                if (test.valid === true) {
+                  expect(err).to.not.exist;
+                  if (test.options.compressors != null) {
+                    result.options.compressors = result.options.compression.compressors;
+                    result.options.zlibCompressionLevel =
+                      result.options.compression.zlibCompressionLevel;
+                  }
+                  expect(result.options).to.containSubset(test.options);
+                } else {
+                  expect(err).to.be.an.instanceof(MongoParseError);
                 }
-                expect(result.options).to.containSubset(test.options);
-              } else {
-                expect(err).to.be.an.instanceof(MongoParseError);
-              }
-              done();
-            });
-          }
+                done();
+              });
+            }
+          });
         });
       });
     });
-  });
 });
