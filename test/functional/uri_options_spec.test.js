fb0243081c08318ce25c0f4d81a1bb4b7000f63e
Thomas Reggi <thomas.reggi@mongodb.com>
2020-04-06 12:54:22 -0400

    propper test skipping

--- test/functional/uri_options_spec.test.js
+++ test/functional/uri_options_spec.test.js
@@ -16,37 +16,33 @@ const loadSpecTests = require('../spec').loadSpecTests;
 describe('URI Options (spec)', function() {
   loadSpecTests('uri-options').forEach(suite => {
     describe(suite.name, () => {
-      suite.tests
-        .filter(test => {
-          const query = qs.parse(url.parse(test.uri).query);
-          if (query.tlsAllowInvalidCertificates) return false;
-          if (query.tlsDisableOCSPEndpointCheck) return false;
-          if (query.tlsDisableCertificateRevocationCheck) return false;
-          return true;
-        })
-        .forEach(test => {
-          const itFn = test.warning ? it.skip : it;
+      suite.tests.forEach(test => {
+        const itFn = test.warning ? it.skip : it;
 
-          itFn(test.description, {
-            metadata: { requires: { topology: 'single' } },
-            test: function(done) {
-              parse(test.uri, {}, (err, result) => {
-                if (test.valid === true) {
-                  expect(err).to.not.exist;
-                  if (test.options.compressors != null) {
-                    result.options.compressors = result.options.compression.compressors;
-                    result.options.zlibCompressionLevel =
-                      result.options.compression.zlibCompressionLevel;
-                  }
-                  expect(result.options).to.containSubset(test.options);
-                } else {
-                  expect(err).to.be.an.instanceof(MongoParseError);
+        itFn(test.description, {
+          metadata: { requires: { topology: 'single' } },
+          test: function(done) {
+            const query = qs.parse(url.parse(test.uri).query);
+            if (query.hasOwnProperty('tlsDisableOCSPEndpointCheck')) return this.skip();
+            if (query.hasOwnProperty('tlsDisableCertificateRevocationCheck')) return this.skip();
+
+            parse(test.uri, {}, (err, result) => {
+              if (test.valid === true) {
+                expect(err).to.not.exist;
+                if (test.options.compressors != null) {
+                  result.options.compressors = result.options.compression.compressors;
+                  result.options.zlibCompressionLevel =
+                    result.options.compression.zlibCompressionLevel;
                 }
-                done();
-              });
-            }
-          });
+                expect(result.options).to.containSubset(test.options);
+              } else {
+                expect(err).to.be.an.instanceof(MongoParseError);
+              }
+              done();
+            });
+          }
         });
+      });
     });
   });
 });
