835ea4d59d72812f30bfe7c718073c4aadbe0c05
Thomas Reggi <thomas.reggi@mongodb.com>
2020-04-10 16:10:33 -0400

    tests

--- test/functional/hedge.test.js
+++ test/functional/hedge.test.js
@@ -42,7 +42,7 @@ describe('Hedge', function() {
     });
   });
 
-  it('should correctly set hedge on Mongos query on connect', {
+  it('should correctly set hedge using find option', {
     metadata: {
       requires: {
         generators: true,
@@ -77,7 +77,7 @@ describe('Hedge', function() {
     }
   });
 
-  it('should correctly set hedge on Mongos query on connect', {
+  it('should correctly set hedge using setReadPreference', {
     metadata: {
       requires: {
         generators: true,
@@ -107,6 +107,114 @@ describe('Hedge', function() {
               $readPreference: { mode: ReadPreference.SECONDARY, hedge: {} }
             });
 
+            client.close(done);
+          });
+      });
+    }
+  });
+
+  it('should correctly set hedge.enabled true', {
+    metadata: {
+      requires: {
+        generators: true,
+        topology: 'single'
+      }
+    },
+
+    test: function(done) {
+      var self = this;
+      const configuration = this.configuration;
+      const ReadPreference = self.configuration.require.ReadPreference;
+
+      const client = configuration.newClient(`mongodb://${test.server.uri()}/test`);
+      client.connect(function(err, client) {
+        expect(err).to.not.exist;
+        var db = client.db(self.configuration.db);
+        var readPreference = new ReadPreference(ReadPreference.SECONDARY, null, {
+          hedge: { enabled: true }
+        });
+
+        // Get a db with a new readPreference
+        db.collection('test')
+          .find({})
+          .setReadPreference(readPreference)
+          .toArray(function(err) {
+            expect(err).to.not.exist;
+            expect(test.checkCommand).to.eql({
+              $query: { find: 'test', filter: {}, returnKey: false, showRecordId: false },
+              $readPreference: { mode: ReadPreference.SECONDARY, hedge: { enabled: true } }
+            });
+
+            client.close(done);
+          });
+      });
+    }
+  });
+
+  it('should correctly set hedge.enabled false', {
+    metadata: {
+      requires: {
+        generators: true,
+        topology: 'single'
+      }
+    },
+
+    test: function(done) {
+      var self = this;
+      const configuration = this.configuration;
+      const ReadPreference = self.configuration.require.ReadPreference;
+
+      const client = configuration.newClient(`mongodb://${test.server.uri()}/test`);
+      client.connect(function(err, client) {
+        expect(err).to.not.exist;
+        var db = client.db(self.configuration.db);
+        var readPreference = new ReadPreference(ReadPreference.SECONDARY, null, {
+          hedge: { enabled: false }
+        });
+
+        // Get a db with a new readPreference
+        db.collection('test')
+          .find({})
+          .setReadPreference(readPreference)
+          .toArray(function(err) {
+            expect(err).to.not.exist;
+            expect(test.checkCommand).to.eql({
+              $query: { find: 'test', filter: {}, returnKey: false, showRecordId: false },
+              $readPreference: { mode: ReadPreference.SECONDARY, hedge: { enabled: false } }
+            });
+
+            client.close(done);
+          });
+      });
+    }
+  });
+
+  it('should correctly not set hedge', {
+    metadata: {
+      requires: {
+        generators: true,
+        topology: 'single'
+      }
+    },
+
+    test: function(done) {
+      var self = this;
+      const configuration = this.configuration;
+      const ReadPreference = self.configuration.require.ReadPreference;
+
+      const client = configuration.newClient(`mongodb://${test.server.uri()}/test`);
+      client.connect(function(err, client) {
+        expect(err).to.not.exist;
+        var db = client.db(self.configuration.db);
+        var readPreference = new ReadPreference(ReadPreference.SECONDARY);
+
+        // Get a db with a new readPreference
+        db.collection('test')
+          .find({})
+          .setReadPreference(readPreference)
+          .toArray(function(err) {
+            expect(err).to.not.exist;
+            expect(test.checkCommand.$readPreference).to.not.have.property('hedge');
             client.close(done);
           });
       });
