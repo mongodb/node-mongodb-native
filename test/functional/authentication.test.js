0210c97122bbda11c856dc3c1baabbb9847287a5
Matt Broadstone <mbroadst@mongodb.com>
2020-04-18 10:55:10 -0400

    refactor(auth): remove api specific to multiple authentication
    We have not supported multiple authentication credentials on a
    connection nor logging out for some time. This refactor removes
    dead code specific to those outdated behaviors.

--- test/functional/authentication.test.js
+++ test/functional/authentication.test.js
@@ -6,7 +6,7 @@ var setupDatabase = require('./shared').setupDatabase;
 
 describe('Authentication', function() {
   before(function() {
-    return setupDatabase(this.configuration);
+    return setupDatabase(this.configuration, ['admin']);
   });
 
   it('should still work for auth when using new url parser and no database is in url', {
