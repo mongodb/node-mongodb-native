8d612d537acd1b43017e0637d730451401b9e055
Thomas Reggi <thomas.reggi@mongodb.com>
2020-04-21 16:39:16 -0400

    cherry-picked"

--- test/functional/spec-runner/index.js
+++ test/functional/spec-runner/index.js
@@ -1,5 +1,5 @@
 'use strict';
-const Promise = require('bluebird');
+
 const path = require('path');
 const fs = require('fs');
 const chai = require('chai');
@@ -13,6 +13,17 @@ chai.use(require('./matcher').default);
 chai.config.includeStack = true;
 chai.config.showDiff = true;
 chai.config.truncateThreshold = 0;
+
+// Promise.try alternative https://stackoverflow.com/questions/60624081/promise-try-without-bluebird/60624164?noredirect=1#comment107255389_60624164
+function promiseTry(callback) {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(callback());
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
 
 function escape(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -633,14 +644,13 @@ function testOperation(operation, obj, context, options) {
       opOptions.nameOnly = true;
     }
 
-    if (CURSOR_COMMANDS.has(operationName)) {
-      // `find` creates a cursor, so we need to call `toArray` on it
-      const cursor = obj[operationName].apply(obj, args);
-      opPromise = cursor.toArray();
-    } else {
-      // wrap this in a `Promise.try` because some operations might throw
-      opPromise = Promise.try(() => obj[operationName].apply(obj, args));
-    }
+  if (CURSOR_COMMANDS.has(operationName)) {
+    // `find` creates a cursor, so we need to call `toArray` on it
+    const cursor = obj[operationName].apply(obj, args);
+    opPromise = cursor.toArray();
+  } else {
+    // wrap this in a `Promise.try` because some operations might throw
+    opPromise = promiseTry(() => obj[operationName].apply(obj, args));
   }
 
   if (operation.error) {
--- test/unit/cmap/connection_pool.test.js
+++ test/unit/cmap/connection_pool.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const Promise = require('bluebird');
+require('util.promisify/shim')();
+const util = require('util');
 const loadSpecTests = require('../../spec').loadSpecTests;
 const ConnectionPool = require('../../../lib/cmap/connection_pool').ConnectionPool;
 const EventEmitter = require('events').EventEmitter;
@@ -25,8 +26,8 @@ const ALL_POOL_EVENTS = new Set([
 ]);
 
 const PROMISIFIED_POOL_FUNCTIONS = {
-  checkOut: Promise.promisify(ConnectionPool.prototype.checkOut),
-  close: Promise.promisify(ConnectionPool.prototype.close)
+  checkOut: util.promisify(ConnectionPool.prototype.checkOut),
+  close: util.promisify(ConnectionPool.prototype.close)
 };
 
 function closePool(pool) {
@@ -370,7 +371,7 @@ describe('Connection Pool', function() {
       return p
         .then(() => {
           const connectionsToDestroy = Array.from(orphans).concat(Array.from(connections.values()));
-          return Promise.each(connectionsToDestroy, conn => {
+          const promises = connectionsToDestroy.map(conn => {
             return new Promise((resolve, reject) =>
               conn.destroy({ force: true }, err => {
                 if (err) return reject(err);
@@ -378,6 +379,7 @@ describe('Connection Pool', function() {
               })
             );
           });
+          return Promise.all(promises);
         })
         .then(() => {
           pool = undefined;
