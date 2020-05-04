75278d0b68f2476aef7204ba32bb1c0eaa649b59
Thomas Reggi <thomas.reggi@mongodb.com>
2020-05-04 14:06:29 -0400

    use class, symbol, validator, fix test

--- test/unit/promise_provider.test.js
+++ test/unit/promise_provider.test.js
@@ -3,35 +3,42 @@ const expect = require('chai').expect;
 
 const PromiseProvider = require('../../lib/promise_provider');
 
+const symbols = {
+  withOptions: Symbol('withOptions'),
+  withClientOptions: Symbol('withClientOptions'),
+  withS: Symbol('withS')
+};
+
 const candidates = {
-  withOptions: { options: { promiseLibrary: Symbol('withOptions') } },
-  withClientOptions: { clientOptions: { promiseLibrary: Symbol('withClientOptions') } },
-  withS: { s: { promiseLibrary: Symbol('withS') } }
+  withOptions: { options: { promiseLibrary: () => symbols.withOptions } },
+  withClientOptions: { clientOptions: { promiseLibrary: () => symbols.withClientOptions } },
+  withS: { s: { promiseLibrary: () => symbols.withS } }
 };
 
 describe('PromiseProvider', () => {
   it('should pass correct option along', () => {
-    // test single option
     const withOptions = PromiseProvider.get(candidates.withOptions);
-    expect(withOptions).to.equal(candidates.withOptions.options.promiseLibrary);
+    expect(withOptions()).to.equal(symbols.withOptions);
+
     const withClientOptions = PromiseProvider.get(candidates.withClientOptions);
-    expect(withClientOptions).to.equal(candidates.withClientOptions.clientOptions.promiseLibrary);
+    expect(withClientOptions()).to.equal(symbols.withClientOptions);
+
     const withS = PromiseProvider.get(candidates.withS);
-    expect(withS).to.equal(candidates.withS.s.promiseLibrary);
+    expect(withS()).to.equal(symbols.withS);
 
     // test non-viable option first, still retrieves second option
     const preWithOptions = PromiseProvider.get({}, candidates.withOptions);
-    expect(preWithOptions).to.equal(candidates.withOptions.options.promiseLibrary);
+    expect(preWithOptions()).to.equal(symbols.withOptions);
+
     const preWithClientOptions = PromiseProvider.get({}, candidates.withClientOptions);
-    expect(preWithClientOptions).to.equal(
-      candidates.withClientOptions.clientOptions.promiseLibrary
-    );
+    expect(preWithClientOptions()).to.equal(symbols.withClientOptions);
+
     const preWithS = PromiseProvider.get({}, candidates.withS);
-    expect(preWithS).to.equal(candidates.withS.s.promiseLibrary);
+    expect(preWithS()).to.equal(symbols.withS);
 
     // non-viable, with two viable, shows gets first viable
     const getsPremier = PromiseProvider.get({}, candidates.withOptions, candidates.withS);
-    expect(getsPremier).to.equal(candidates.withOptions.options.promiseLibrary);
+    expect(getsPremier()).to.equal(symbols.withOptions);
 
     // demonstrates default is retrieved
     const state = PromiseProvider.get();
