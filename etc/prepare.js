a5372fd237539c7d5bd7d116e4f2534e3f3a833d
Neal Beeken <neal.beeken@mongodb.com>
2020-09-09 15:12:33 -0400

    chore: configure scripts to build driver
    Using the postinstall hook, the prepare script will install
    the necessary dependencies to compile the driver if using
    git versioning of the module.
    
    NODE-2791

--- etc/prepare.js
+++ etc/prepare.js
@@ -3,7 +3,12 @@ var cp = require('child_process');
 var fs = require('fs');
 
 if (fs.existsSync('src')) {
-  cp.spawn('npm', ['run', 'build:dts'], { stdio: 'inherit' });
+  const result = cp.spawnSync('npm', ['run', 'build:dts'], { stdio: 'inherit', shell: true });
+  if (result.status !== 0) {
+    // We need devDependencies to build the driver
+    cp.spawnSync('npm', ['install', '--ignore-scripts'], { stdio: 'inherit', shell: true });
+    cp.spawnSync('npm', ['run', 'build:dts'], { stdio: 'inherit', shell: true });
+  }
 } else {
   if (!fs.existsSync('lib')) {
     console.warn('MongoDB: No compiled javascript present, the driver is not installed correctly.');
