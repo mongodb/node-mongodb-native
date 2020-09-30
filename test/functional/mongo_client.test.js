ecf8df6b6b3faa08bf5a78b81b3e005b2041f880
Thomas Reggi <thomas.reggi@mongodb.com>
2020-09-30 16:11:24 -0400

    cache read preference

--- test/functional/mongo_client.test.js
+++ test/functional/mongo_client.test.js
@@ -3,6 +3,7 @@
 var f = require('util').format;
 var test = require('./shared').assert;
 var setupDatabase = require('./shared').setupDatabase;
+const { ReadPreference } = require('../..');
 const Db = require('../../lib/db');
 const expect = require('chai').expect;
 
@@ -830,5 +831,11 @@ describe('MongoClient', function() {
           throw err;
         }
       });
+  });
+
+  it('should have readPreference accessor from options', function() {
+    const client = this.configuration.newClient({}, { readPreference: ReadPreference.SECONDARY });
+    expect(client.readPreference).to.be.instanceOf(ReadPreference);
+    expect(client.readPreference).to.have.property('mode', ReadPreference.SECONDARY);
   });
 });
