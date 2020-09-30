9114a8c3bac1d5e2ec740c4d82ecd43cdd3268af
Neal Beeken <neal.beeken@mongodb.com>
2020-09-30 13:30:02 -0400

    add git hash specifier option to spec fetch

--- etc/update-spec-tests.sh
+++ etc/update-spec-tests.sh
@@ -11,16 +11,26 @@ if [ ! -d ".git" ]; then
     exit 1
 fi
 
-if [ $# -ne 1 ]; then
-    echo "$0: This script must be passed exactly one argument for which tests to sync" >&2
+if [ $1 == "-h" ] || [ $1 == "--help" ]; then
+    echo "USAGE: $0 SPEC_NAME [GIT_REF]" >&2
+    echo -e "SPEC_NAME\tThe spec name to fetch matching the folder name in 'specifications/source'" >&2
+    echo -e "[GIT_REF]\tAn optional git reference to fetch specs from, can be commit short code or branch name" >&2
+    exit 0
+fi
+
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "$0: This script must be passed at least one argument for which tests to sync" >&2
+    echo "And optionally can be passed a git ref to fetch specs from" >&2
     exit 1
 fi
 
+git_ref=${2:-master}
 spec_root="test/spec"
 
 tmpdir=$(mktemp -d -t spec_testsXXXX)
-curl -sL https://github.com/mongodb/specifications/archive/master.zip -o "$tmpdir/specs.zip"
+curl -sL "https://github.com/mongodb/specifications/archive/${git_ref}.zip" -o "$tmpdir/specs.zip"
 unzip -d "$tmpdir" "$tmpdir/specs.zip" > /dev/null
 mkdir -p "$spec_root/$1"
-rsync -ah "$tmpdir/specifications-master/source/$1/tests/" "$spec_root/$1" --delete
+spec_folder_name=$(ls $tmpdir | grep -v 'specs.zip')
+rsync -ah "$tmpdir/${spec_folder_name}/source/$1/tests/" "$spec_root/$1" --delete
 rm -rf "$tmpdir"
