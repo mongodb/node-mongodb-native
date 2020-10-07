b1eb0f0b1b058167c7663ffb0a73a81898fb24e1
Thomas Reggi <thomas.reggi@mongodb.com>
2020-10-07 14:55:49 -0400

    fix lint

--- src/cursor/aggregation_cursor.ts
+++ src/cursor/aggregation_cursor.ts
@@ -1,7 +1,6 @@
 import { MongoError } from '../error';
 import { Cursor, CursorOptions } from './cursor';
 import { CursorState } from './core_cursor';
-import { deprecate } from 'util';
 import type { AggregateOperation, AggregateOptions } from '../operations/aggregate';
 import type { Document } from '../bson';
 import type { Sort } from '../operations/find';
