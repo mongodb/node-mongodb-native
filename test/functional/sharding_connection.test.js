f8fd310a11a91db82f1c0ddc57482b8edabc231b
Thomas Reggi <thomas.reggi@mongodb.com>
2020-10-13 17:14:28 -0400

    fix: adds topology discovery for sharded cluster
    NODE-2827

--- test/functional/sharding_connection.test.js
+++ test/functional/sharding_connection.test.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const withClient = require('./shared').withClient;
 const setupDatabase = require('./shared').setupDatabase;
 const expect = require('chai').expect;
+const TopologyType = require('../../lib/core/sdam/common').TopologyType;
 
 describe('Sharding (Connection)', function() {
   before(function() {
     return setupDatabase(this.configuration);
+  });
+
+  it('Should use sharded topology', {
+    metadata: { requires: { topology: 'sharded' } },
+    test: function() {
+      const client = this.configuration.newClient({}, { useUnifiedTopology: true });
+      return withClient(client, (client, done) => {
+        expect(client).to.exist;
+        expect(client)
+          .nested.property('topology.description.type')
+          .to.equal(TopologyType.Sharded);
+        return done();
+      })();
+    }
   });
 
   /**
