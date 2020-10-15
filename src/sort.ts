b832325d0bafe6744233de3c7f9c8a466c68d0cc
Thomas Reggi <thomas.reggi@mongodb.com>
2020-10-15 13:53:21 -0400

    get rid of class / static

--- src/sort.ts
+++ src/sort.ts
@@ -25,70 +25,84 @@ type SortDirectionForCmd = 1 | -1 | { $meta: string };
 type SortForCmd = { [key: string]: SortDirectionForCmd };
 
 /** @internal */
-class SortDigest {
-  static prepareDirection(direction: any = 1): SortDirectionForCmd {
-    const value = ('' + direction).toLowerCase();
-    if (SortDigest.isMeta(direction)) return direction;
-    switch (value) {
-      case 'ascending':
-      case 'asc':
-      case '1':
-        return 1;
-      case 'descending':
-      case 'desc':
-      case '-1':
-        return -1;
-      default:
-        throw new Error(`Invalid sort direction: ${JSON.stringify(direction)}`);
-    }
-  }
-  static isMeta(t: SortDirection): t is { $meta: string } {
-    return typeof t === 'object' && t !== null && '$meta' in t && typeof t.$meta === 'string';
-  }
-  static isPair(t: Sort): t is [string, SortDirection] {
-    if (Array.isArray(t) && t.length === 2) {
-      try {
-        SortDigest.prepareDirection(t[1]);
-        return true;
-      } catch (e) {
-        return false;
-      }
-    }
-    return false;
-  }
-  static pairToObject(v: [string, SortDirection]): SortForCmd {
-    return { [v[0]]: SortDigest.prepareDirection(v[1]) };
-  }
-  static isDeep(t: Sort): t is [string, SortDirection][] {
-    return Array.isArray(t) && Array.isArray(t[0]);
-  }
-  static deepToObject(t: [string, SortDirection][]): SortForCmd {
-    return t.reduce((acq, i) => {
-      return { ...acq, ...SortDigest.pairToObject(i) };
-    }, {});
-  }
-  static stringsToObject(t: string[]): SortForCmd {
-    return t.reduce((acq, key) => {
-      return { ...acq, [key]: 1 };
-    }, {});
-  }
-  static validate(t: { [key: string]: SortDirection }): SortForCmd {
-    return Object.keys(t).reduce((acq, key) => {
-      return { ...acq, [key]: SortDigest.prepareDirection(t[key]) };
-    }, {});
-  }
-  static prepare(sort: Sort | undefined, direction?: SortDirection): SortForCmd | undefined {
-    if (sort == null) return undefined;
-    if (Array.isArray(sort) && !sort.length) return undefined;
-    if (typeof sort === 'object' && !Object.keys(sort).length) return undefined;
-    if (typeof sort === 'string') return { [sort]: SortDigest.prepareDirection(direction) };
-    if (SortDigest.isPair(sort)) return SortDigest.pairToObject(sort);
-    if (SortDigest.isDeep(sort)) return SortDigest.deepToObject(sort);
-    if (Array.isArray(sort)) return SortDigest.stringsToObject(sort);
-    if (typeof sort === 'object') return SortDigest.validate(sort);
-    throw new Error(`Invalid sort format: ${JSON.stringify(sort)}`);
+function prepareDirection(direction: any = 1): SortDirectionForCmd {
+  const value = ('' + direction).toLowerCase();
+  if (isMeta(direction)) return direction;
+  switch (value) {
+    case 'ascending':
+    case 'asc':
+    case '1':
+      return 1;
+    case 'descending':
+    case 'desc':
+    case '-1':
+      return -1;
+    default:
+      throw new Error(`Invalid sort direction: ${JSON.stringify(direction)}`);
   }
 }
 
+/** @internal */
+function isMeta(t: SortDirection): t is { $meta: string } {
+  return typeof t === 'object' && t !== null && '$meta' in t && typeof t.$meta === 'string';
+}
+
+/** @internal */
+function isPair(t: Sort): t is [string, SortDirection] {
+  if (Array.isArray(t) && t.length === 2) {
+    try {
+      prepareDirection(t[1]);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+  return false;
+}
+
+/** @internal */
+function pairToObject(v: [string, SortDirection]): SortForCmd {
+  return { [v[0]]: prepareDirection(v[1]) };
+}
+
+/** @internal */
+function isDeep(t: Sort): t is [string, SortDirection][] {
+  return Array.isArray(t) && Array.isArray(t[0]);
+}
+
+/** @internal */
+function deepToObject(t: [string, SortDirection][]): SortForCmd {
+  return t.reduce((acq, i) => {
+    return { ...acq, ...pairToObject(i) };
+  }, {});
+}
+
+/** @internal */
+function stringsToObject(t: string[]): SortForCmd {
+  return t.reduce((acq, key) => {
+    return { ...acq, [key]: 1 };
+  }, {});
+}
+
+/** @internal */
+function validate(t: { [key: string]: SortDirection }): SortForCmd {
+  return Object.keys(t).reduce((acq, key) => {
+    return { ...acq, [key]: prepareDirection(t[key]) };
+  }, {});
+}
+
 /** converts a Sort type into a type that is valid for the server (SortForCmd) */
-export const formatSort = SortDigest.prepare;
+export function formatSort(
+  sort: Sort | undefined,
+  direction?: SortDirection
+): SortForCmd | undefined {
+  if (sort == null) return undefined;
+  if (Array.isArray(sort) && !sort.length) return undefined;
+  if (typeof sort === 'object' && !Object.keys(sort).length) return undefined;
+  if (typeof sort === 'string') return { [sort]: prepareDirection(direction) };
+  if (isPair(sort)) return pairToObject(sort);
+  if (isDeep(sort)) return deepToObject(sort);
+  if (Array.isArray(sort)) return stringsToObject(sort);
+  if (typeof sort === 'object') return validate(sort);
+  throw new Error(`Invalid sort format: ${JSON.stringify(sort)}`);
+}
