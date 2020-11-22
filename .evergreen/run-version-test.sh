e8602c49d72d866dbd2d57c3bf4cecc7b2ee2f55
emadum <eric.adum@mongodb.com>
2020-11-22 17:06:36 -0500

    experimental support for 3.1 and 3.6 tests

--- .evergreen/run-version-test.sh
+++ .evergreen/run-version-test.sh
@@ -3,27 +3,38 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 
 DRIVER_VERSION=$1
-echo "MONGODB_URI=$MONGODB_URI PLATFORM=$PLATFORM DRIVER_VERSION=$DRIVER_VERSION"
+echo "MONGODB_URI=$MONGODB_URI TOPOLOGY=$TOPOLOGY PLATFORM=$PLATFORM DRIVER_VERSION=$DRIVER_VERSION"
+if [[ $TOPOLOGY == "server" ]]; then
+  LEGACY_ENVIRONMENT='single'
+else
+  LEGACY_ENVIRONMENT=$TOPOLOGY
+fi
+
 case $DRIVER_VERSION in
   '3.6')
-    echo "Running latest tests"
+    VERSION_DESC="v3.6"
+    TEST_COMMAND='npm run test-nolint'
     ;;
   'fix-srv-test-timeout')
-    echo "Running latest tests (debug branch)"
+    VERSION_DESC="v3.6 (debug branch)"
+    TEST_COMMAND='npm run test-nolint'
     ;;
   '3.3')
-    echo "Testing NodeJS driver v3.3 not yet implemented"
-    exit 0
+    VERSION_DESC="v3.3"
+    TEST_COMMAND="./node_modules/.bin/mongodb-test-runner -s -l -e $LEGACY_ENVIRONMENT test/core test/unit test/functional"
     ;;
   '3.1')
-    echo "Testing NodeJS driver v3.1 not yet implemented"
-    exit 0
+    VERSION_DESC="v3.1"
+    TEST_COMMAND="./node_modules/.bin/mongodb-test-runner -s -l -e $LEGACY_ENVIRONMENT test/unit test/functional"
     ;;
   *)
     echo "Unsupported driver version: $DRIVER_VERSION"
     exit 1
     ;;
 esac
+
+echo "Testing NodeJS driver $VERSION_DESC"
+echo "TEST_COMMAND=$TEST_COMMAND"
 
 export PROJECT_DIRECTORY=$(cd $(dirname ${BASH_SOURCE[0]}) && cd .. && pwd)
 export NODE_LTS_NAME=dubnium
@@ -46,4 +57,4 @@ echo "Checked out version branch, running dependency installation"
 
 npm install --unsafe-perm
 echo "Library dependencies installed, running test suite"
-npm run test-nolint && echo "Tests complete"
+$TEST_COMMAND
