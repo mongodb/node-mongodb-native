6a1f7d763d4eb0947de0a26c087eba4dbb5dfaac
emadum <eric.adum@mongodb.com>
2020-11-24 15:09:19 -0500

    skip again

--- test/functional/change_stream.test.js
+++ test/functional/change_stream.test.js
@@ -1475,7 +1475,7 @@ describe('Change Streams', function() {
     }
   });
 
-  it.skip('should resume piping of Change Streams when a resumable error is encountered', {
+  it('should resume piping of Change Streams when a resumable error is encountered', {
     metadata: {
       requires: {
         generators: true,
