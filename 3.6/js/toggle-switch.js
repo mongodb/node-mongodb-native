5f69fa7b152e1b35d665edbf7ccaa07100409059
Neal Beeken <neal.beeken@mongodb.com>
2021-01-19 17:15:47 -0500

    Updated documentation

--- 3.6/js/toggle-switch.js
+++ 3.6/js/toggle-switch.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   $('.distroPicker').bootstrapToggle();
   $('.distroPicker').change(function () {
     if ($('.distroPicker').prop('checked')) {
-      $('.javascript6').addClass('hidden');
-      $('.javascript5').removeClass('hidden');
-    } else {
       $('.javascript5').addClass('hidden');
       $('.javascript6').removeClass('hidden');
+    } else {
+      $('.javascript6').addClass('hidden');
+      $('.javascript5').removeClass('hidden');
     }
   });
 });
