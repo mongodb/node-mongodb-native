ea70d8fd266e7ae1950a2929b2778f7e1254aa6e
Neal Beeken <neal.beeken@mongodb.com>
2021-03-19 12:24:16 -0400

    fix(NODE-2995): Add shared metadata MongoClient
    Automatic client side encryption needs to perform metadata look ups
    like listCollections. In situations where the connection pool size
    is constrained or in full use it can be impossible for an operation
    to proceed. Adding a separate client in these situations permits the
    metadata look ups to proceed unblocking operations.

---
+++ test/functional/client_side_encryption/deadlock_tests.js
@@ -0,0 +1,337 @@
+'use strict';
+
+const expect = require('chai').expect;
+const dropCollection = require('../shared').dropCollection;
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
+const EJSON = require('mongodb-extjson');
+
+/* REFERENCE: (note commit hash) */
+/* https://github.com/mongodb/specifications/blob/b3beada72ae1c992294ae6a8eea572003a274c35/source/client-side-encryption/tests/README.rst#deadlock-tests */
+
+const LOCAL_KEY = Buffer.from(
+  'Mng0NCt4ZHVUYUJCa1kxNkVyNUR1QURhZ2h2UzR2d2RrZzh0cFBwM3R6NmdWMDFBMUN3YkQ5aXRRMkhGRGdQV09wOGVNYUMxT2k3NjZKelhaQmRCZGJkTXVyZG9uSjFk',
+  'base64'
+);
+
+const externalKey = EJSON.parse(
+  fs.readFileSync(
+    path.resolve(__dirname, '../../spec/client-side-encryption/external/external-key.json')
+  )
+);
+const $jsonSchema = EJSON.parse(
+  fs.readFileSync(
+    path.resolve(__dirname, '../../spec/client-side-encryption/external/external-schema.json')
+  )
+);
+
+const kEvents = Symbol('events');
+const kClientsCreated = Symbol('clientsCreated');
+const CapturingMongoClient = class extends require('../../../index.js').MongoClient {
+  constructor(url, options) {
+    options = options || {};
+    options.useUnifiedTopology = true;
+    options.useNewUrlParser = true;
+    options.monitorCommands = true;
+    super(url, options);
+
+    this[kEvents] = [];
+    this.on('commandStarted', ev => this[kEvents].push(ev));
+
+    this[kClientsCreated] = 0;
+    this.on('topologyOpening', () => this[kClientsCreated]++);
+  }
+};
+
+function deadlockTest(options, assertions) {
+  return function() {
+    const url = this.configuration.url();
+    const clientTest = this.clientTest;
+    const ciphertext = this.ciphertext;
+
+    const clientEncryptedOpts = {
+      autoEncryption: {
+        keyVaultNamespace: 'keyvault.datakeys',
+        kmsProviders: { local: { key: LOCAL_KEY } },
+        bypassAutoEncryption: options.bypassAutoEncryption,
+        keyVaultClient: options.useKeyVaultClient ? this.clientKeyVault : undefined
+      },
+      maxPoolSize: options.maxPoolSize
+    };
+    const clientEncrypted = new CapturingMongoClient(url, clientEncryptedOpts);
+
+    return clientEncrypted
+      .connect()
+      .then(() => {
+        if (clientEncryptedOpts.autoEncryption.bypassAutoEncryption === true) {
+          return clientTest
+            .db('db')
+            .collection('coll')
+            .insertOne({ _id: 0, encrypted: ciphertext });
+        }
+        return clientEncrypted
+          .db('db')
+          .collection('coll')
+          .insertOne({ _id: 0, encrypted: 'string0' });
+      })
+      .then(() =>
+        clientEncrypted
+          .db('db')
+          .collection('coll')
+          .findOne({ _id: 0 })
+      )
+      .then(res => {
+        expect(res).to.have.property('_id', 0);
+        expect(res).to.have.property('encrypted', 'string0');
+        assertions(clientEncrypted, this.clientKeyVault);
+        return clientEncrypted.close();
+      });
+  };
+}
+
+function deadlockTests(metadata) {
+  describe('Connection Pool Deadlock Prevention', function() {
+    beforeEach(function() {
+      const mongodbClientEncryption = this.configuration.mongodbClientEncryption;
+      const url = this.configuration.url();
+
+      this.clientTest = new CapturingMongoClient(url);
+      this.clientKeyVault = new CapturingMongoClient(url, {
+        monitorCommands: true,
+        maxPoolSize: 1
+      });
+
+      this.clientEncryption = undefined;
+      this.ciphertext = undefined;
+
+      return this.clientTest
+        .connect()
+        .then(() => this.clientKeyVault.connect())
+        .then(() => dropCollection(this.clientTest.db('keyvault'), 'datakeys'))
+        .then(() => dropCollection(this.clientTest.db('db'), 'coll'))
+        .then(
+          () =>
+            this.clientTest
+              .db('keyvault')
+              .collection('datakeys')
+              .insertOne(externalKey),
+          { writeConcern: { w: 'majority' } }
+        )
+        .then(() =>
+          this.clientTest.db('db').createCollection('coll', { validator: { $jsonSchema } })
+        )
+        .then(() => {
+          this.clientEncryption = new mongodbClientEncryption.ClientEncryption(this.clientTest, {
+            kmsProviders: { local: { key: LOCAL_KEY } },
+            keyVaultNamespace: 'keyvault.datakeys',
+            keyVaultClient: this.keyVaultClient
+          });
+          this.clientEncryption.encryptPromisified = util.promisify(
+            this.clientEncryption.encrypt.bind(this.clientEncryption)
+          );
+
+          return this.clientEncryption.encryptPromisified('string0', {
+            algorithm: 'AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic',
+            keyAltName: 'local'
+          });
+        })
+        .then(ciphertext => {
+          this.ciphertext = ciphertext;
+        });
+    });
+
+    afterEach(function() {
+      return Promise.all([this.clientKeyVault.close(), this.clientTest.close()]).then(() => {
+        this.clientKeyVault = undefined;
+        this.clientTest = undefined;
+        this.clientEncryption = undefined;
+      });
+    });
+
+    const CASE1 = { maxPoolSize: 1, bypassAutoEncryption: false, useKeyVaultClient: false };
+    it(
+      'Case 1',
+      metadata,
+      deadlockTest(CASE1, clientEncrypted => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(2);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(4);
+
+        expect(events[0].command).to.have.property('listCollections');
+        expect(events[0].command.$db).to.equal('db');
+
+        expect(events[1].command).to.have.property('find');
+        expect(events[1].command.$db).to.equal('keyvault');
+
+        expect(events[2].command).to.have.property('insert');
+        expect(events[2].command.$db).to.equal('db');
+
+        expect(events[3].command).to.have.property('find');
+        expect(events[3].command.$db).to.equal('db');
+      })
+    );
+
+    const CASE2 = { maxPoolSize: 1, bypassAutoEncryption: false, useKeyVaultClient: true };
+    it(
+      'Case 2',
+      metadata,
+      deadlockTest(CASE2, (clientEncrypted, clientKeyVault) => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(2);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(3);
+
+        expect(events[0].command).to.have.property('listCollections');
+        expect(events[0].command.$db).to.equal('db');
+
+        expect(events[1].command).to.have.property('insert');
+        expect(events[1].command.$db).to.equal('db');
+
+        expect(events[2].command).to.have.property('find');
+        expect(events[2].command.$db).to.equal('db');
+
+        const keyVaultEvents = clientKeyVault[kEvents];
+        expect(keyVaultEvents).to.have.lengthOf(1);
+
+        expect(keyVaultEvents[0].command).to.have.property('find');
+        expect(keyVaultEvents[0].command.$db).to.equal('keyvault');
+      })
+    );
+
+    const CASE3 = { maxPoolSize: 1, bypassAutoEncryption: true, useKeyVaultClient: false };
+    it(
+      'Case 3',
+      metadata,
+      deadlockTest(CASE3, clientEncrypted => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(2);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(2);
+
+        expect(events[0].command).to.have.property('find');
+        expect(events[0].command.$db).to.equal('db');
+
+        expect(events[1].command).to.have.property('find');
+        expect(events[1].command.$db).to.equal('keyvault');
+      })
+    );
+
+    const CASE4 = { maxPoolSize: 1, bypassAutoEncryption: true, useKeyVaultClient: true };
+    it(
+      'Case 4',
+      metadata,
+      deadlockTest(CASE4, (clientEncrypted, clientKeyVault) => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(1);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(1);
+
+        expect(events[0].command).to.have.property('find');
+        expect(events[0].command.$db).to.equal('db');
+
+        const keyVaultEvents = clientKeyVault[kEvents];
+        expect(keyVaultEvents).to.have.lengthOf(1);
+
+        expect(keyVaultEvents[0].command).to.have.property('find');
+        expect(keyVaultEvents[0].command.$db).to.equal('keyvault');
+      })
+    );
+
+    const CASE5 = { maxPoolSize: 0, bypassAutoEncryption: false, useKeyVaultClient: false };
+    it(
+      'Case 5',
+      metadata,
+      deadlockTest(CASE5, clientEncrypted => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(1);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(5);
+
+        expect(events[0].command).to.have.property('listCollections');
+        expect(events[0].command.$db).to.equal('db');
+
+        expect(events[1].command).to.have.property('listCollections');
+        expect(events[1].command.$db).to.equal('keyvault');
+
+        expect(events[2].command).to.have.property('find');
+        expect(events[2].command.$db).to.equal('keyvault');
+
+        expect(events[3].command).to.have.property('insert');
+        expect(events[3].command.$db).to.equal('db');
+
+        expect(events[4].command).to.have.property('find');
+        expect(events[4].command.$db).to.equal('db');
+      })
+    );
+
+    const CASE6 = { maxPoolSize: 0, bypassAutoEncryption: false, useKeyVaultClient: true };
+    it(
+      'Case 6',
+      metadata,
+      deadlockTest(CASE6, (clientEncrypted, clientKeyVault) => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(1);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(3);
+
+        expect(events[0].command).to.have.property('listCollections');
+        expect(events[0].command.$db).to.equal('db');
+
+        expect(events[1].command).to.have.property('insert');
+        expect(events[1].command.$db).to.equal('db');
+
+        expect(events[2].command).to.have.property('find');
+        expect(events[2].command.$db).to.equal('db');
+
+        const keyVaultEvents = clientKeyVault[kEvents];
+        expect(keyVaultEvents).to.have.lengthOf(1);
+
+        expect(keyVaultEvents[0].command).to.have.property('find');
+        expect(keyVaultEvents[0].command.$db).to.equal('keyvault');
+      })
+    );
+
+    const CASE7 = { maxPoolSize: 0, bypassAutoEncryption: true, useKeyVaultClient: false };
+    it(
+      'Case 7',
+      metadata,
+      deadlockTest(CASE7, clientEncrypted => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(1);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(2);
+
+        expect(events[0].command).to.have.property('find');
+        expect(events[0].command.$db).to.equal('db');
+
+        expect(events[1].command).to.have.property('find');
+        expect(events[1].command.$db).to.equal('keyvault');
+      })
+    );
+
+    const CASE8 = { maxPoolSize: 0, bypassAutoEncryption: true, useKeyVaultClient: true };
+    it(
+      'Case 8',
+      metadata,
+      deadlockTest(CASE8, (clientEncrypted, clientKeyVault) => {
+        expect(clientEncrypted[kClientsCreated], 'Incorrect number of clients created').to.equal(1);
+
+        const events = clientEncrypted[kEvents];
+        expect(events).to.have.lengthOf(1);
+
+        expect(events[0].command).to.have.property('find');
+        expect(events[0].command.$db).to.equal('db');
+
+        const keyVaultEvents = clientKeyVault[kEvents];
+        expect(keyVaultEvents).to.have.lengthOf(1);
+
+        expect(keyVaultEvents[0].command).to.have.property('find');
+        expect(keyVaultEvents[0].command.$db).to.equal('keyvault');
+      })
+    );
+  });
+}
+
+module.exports = { deadlockTests };
--- test/spec/client-side-encryption/tests/README.rst
+++ test/spec/client-side-encryption/tests/README.rst
@@ -770,3 +770,177 @@ The following tests that setting ``bypassAutoEncryption=true`` really does bypas
 #. Use ``client_encrypted`` to insert the document ``{"unencrypted": "test"}`` into ``db.coll``. Expect this to succeed. 
 
 #. Validate that mongocryptd was not spawned. Create a MongoClient to localhost:27021 (or whatever was passed via ``--port``) with serverSelectionTimeoutMS=1000. Run an ``isMaster`` command and ensure it fails with a server selection timeout.
+
+Deadlock tests
+~~~~~~~~~~~~~~
+
+.. _Connection Monitoring and Pooling: /source/connection-monitoring-and-pooling/connection-monitoring-and-pooling.rst
+
+The following tests only apply to drivers that have implemented a connection pool (see the `Connection Monitoring and Pooling`_ specification).
+
+There are multiple parameterized test cases. Before each test case, perform the setup.
+
+Setup
+`````
+
+Create a ``MongoClient`` for setup operations named ``client_test``.
+
+Create a ``MongoClient`` for key vault operations with ``maxPoolSize=1`` named ``client_keyvault``. Capture command started events.
+
+Using ``client_test``, drop the collections ``keyvault.datakeys`` and ``db.coll``.
+
+Insert the document `external/external-key.json <../external/external-key.json>`_ into ``keyvault.datakeys`` with majority write concern.
+
+Create a collection ``db.coll`` configured with a JSON schema `external/external-schema.json <../external/external-schema.json>`_ as the validator, like so:
+
+.. code:: typescript
+
+   {"create": "coll", "validator": {"$jsonSchema": <json_schema>}}
+
+Create a ``ClientEncryption`` object, named ``client_encryption`` configured with:
+- ``keyVaultClient``=``client_test``
+- ``keyVaultNamespace``="keyvault.datakeys"
+- ``kmsProviders``=``{ "local": { "key": <base64 decoding of LOCAL_MASTERKEY> } }``
+
+Use ``client_encryption`` to encrypt the value "string0" with ``algorithm``="AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic" and ``keyAltName``="local". Store the result in a variable named ``ciphertext``.
+
+Proceed to run the test case.
+
+Each test case configures a ``MongoClient`` with automatic encryption (named ``client_encrypted``).
+
+Each test must assert the number of unique ``MongoClient``s created. This can be accomplished by capturing ``TopologyOpeningEvent``, or by checking command started events for a client identifier (not possible in all drivers).
+
+Running a test case
+```````````````````
+- Create a ``MongoClient`` named ``client_encrypted`` configured as follows:
+   - Set ``AutoEncryptionOpts``:
+      - ``keyVaultNamespace="keyvault.datakeys"``
+      - ``kmsProviders``=``{ "local": { "key": <base64 decoding of LOCAL_MASTERKEY> } }``
+      - Append ``TestCase.AutoEncryptionOpts`` (defined below)
+   - Capture command started events.
+   - Set ``maxPoolSize=TestCase.MaxPoolSize``
+- If the testcase sets ``AutoEncryptionOpts.bypassAutoEncryption=true``:
+   - Use ``client_test`` to insert ``{ "_id": 0, "encrypted": <ciphertext> }`` into ``db.coll``.
+- Otherwise:
+   - Use ``client_encrypted`` to insert ``{ "_id": 0, "encrypted": "string0" }``.
+- Use ``client_encrypted`` to run a ``findOne`` operation on ``db.coll``, with the filter ``{ "_id": 0 }``.
+- Expect the result to be ``{ "_id": 0, "encrypted": "string0" }``.
+- Check captured events against ``TestCase.Expectations``.
+- Check the number of unique ``MongoClient``s created is equal to ``TestCase.ExpectedNumberOfClients``.
+
+Case 1
+``````
+- MaxPoolSize: 1
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=false
+   - keyVaultClient=unset
+- Expectations:
+   - Expect ``client_encrypted`` to have captured four ``CommandStartedEvent``:
+      - a listCollections to "db".
+      - a find on "keyvault".
+      - an insert on "db".
+      - a find on "db"
+- ExpectedNumberOfClients: 2
+
+Case 2
+``````
+- MaxPoolSize: 1
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=false
+   - keyVaultClient=client_keyvault
+- Expectations:
+   - Expect ``client_encrypted`` to have captured three ``CommandStartedEvent``:
+      - a listCollections to "db".
+      - an insert on "db".
+      - a find on "db"
+   - Expect ``client_keyvault`` to have captured one ``CommandStartedEvent``:
+      - a find on "keyvault".
+- ExpectedNumberOfClients: 2
+
+Case 3
+``````
+- MaxPoolSize: 1
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=true
+   - keyVaultClient=unset
+- Expectations:
+   - Expect ``client_encrypted`` to have captured three ``CommandStartedEvent``:
+      - a find on "db"
+      - a find on "keyvault".
+- ExpectedNumberOfClients: 2
+
+Case 4
+``````
+- MaxPoolSize: 1
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=true
+   - keyVaultClient=client_keyvault
+- Expectations:
+   - Expect ``client_encrypted`` to have captured two ``CommandStartedEvent``:
+      - a find on "db"
+   - Expect ``client_keyvault`` to have captured one ``CommandStartedEvent``:
+      - a find on "keyvault".
+- ExpectedNumberOfClients: 1
+
+Case 5
+``````
+Drivers that do not support an unlimited maximum pool size MUST skip this test.
+
+- MaxPoolSize: 0
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=false
+   - keyVaultClient=unset
+- Expectations:
+   - Expect ``client_encrypted`` to have captured five ``CommandStartedEvent``:
+      - a listCollections to "db".
+      - a listCollections to "keyvault".
+      - a find on "keyvault".
+      - an insert on "db".
+      - a find on "db"
+- ExpectedNumberOfClients: 1
+
+Case 6
+``````
+Drivers that do not support an unlimited maximum pool size MUST skip this test.
+
+- MaxPoolSize: 0
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=false
+   - keyVaultClient=client_keyvault
+- Expectations:
+   - Expect ``client_encrypted`` to have captured three ``CommandStartedEvent``:
+      - a listCollections to "db".
+      - an insert on "db".
+      - a find on "db"
+   - Expect ``client_keyvault`` to have captured one ``CommandStartedEvent``:
+      - a find on "keyvault".
+- ExpectedNumberOfClients: 1
+
+Case 7
+``````
+Drivers that do not support an unlimited maximum pool size MUST skip this test.
+
+- MaxPoolSize: 0
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=true
+   - keyVaultClient=unset
+- Expectations:
+   - Expect ``client_encrypted`` to have captured three ``CommandStartedEvent``:
+      - a find on "db"
+      - a find on "keyvault".
+- ExpectedNumberOfClients: 1
+
+Case 8
+``````
+Drivers that do not support an unlimited maximum pool size MUST skip this test.
+
+- MaxPoolSize: 0
+- AutoEncryptionOpts:
+   - bypassAutoEncryption=true
+   - keyVaultClient=client_keyvault
+- Expectations:
+   - Expect ``client_encrypted`` to have captured two ``CommandStartedEvent``:
+      - a find on "db"
+   - Expect ``client_keyvault`` to have captured one ``CommandStartedEvent``:
+      - a find on "keyvault".
+- ExpectedNumberOfClients: 1
