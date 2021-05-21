6e3bab32204ea905ab9b949edccb68556b50d382
Valeri Karpov <val@karpov.io>
2021-05-21 13:10:13 -0400

    fix(cmap): undo flipping of `beforeHandshake` flag for timeout errors (#2813)

--- test/unit/cmap/connection.test.js
+++ test/unit/cmap/connection.test.js
@@ -66,4 +66,62 @@ describe('Connection', function() {
       }
     );
   });
+
+  it('should throw a network error with kBeforeHandshake set to false on timeout after hand shake', function(done) {
+    server.setMessageHandler(request => {
+      const doc = request.document;
+      if (doc.ismaster) {
+        request.reply(mock.DEFAULT_ISMASTER_36);
+      }
+      // respond to no other requests to trigger timeout event
+    });
+
+    const address = server.address();
+    const options = {
+      bson: new BSON(),
+      connectionType: Connection,
+      host: address.host,
+      port: address.port
+    };
+
+    connect(options, (err, conn) => {
+      expect(err).to.be.a('undefined');
+      expect(conn).to.be.instanceOf(Connection);
+      expect(conn)
+        .to.have.property('ismaster')
+        .that.is.a('object');
+
+      conn.command('$admin.cmd', { ping: 1 }, { socketTimeout: 50 }, err => {
+        const beforeHandshakeSymbol = Object.getOwnPropertySymbols(err)[0];
+        expect(beforeHandshakeSymbol).to.be.a('symbol');
+        expect(err).to.have.property(beforeHandshakeSymbol, false);
+
+        done();
+      });
+    });
+  });
+
+  it('should throw a network error with kBeforeHandshake set to true on timeout before hand shake', function(done) {
+    // respond to no requests to trigger timeout event
+    server.setMessageHandler(() => {});
+
+    const address = server.address();
+    const options = {
+      bson: new BSON(),
+      connectionType: Connection,
+      host: address.host,
+      port: address.port,
+      socketTimeout: 50
+    };
+
+    connect(options, (err, conn) => {
+      expect(conn).to.be.a('undefined');
+
+      const beforeHandshakeSymbol = Object.getOwnPropertySymbols(err)[0];
+      expect(beforeHandshakeSymbol).to.be.a('symbol');
+      expect(err).to.have.property(beforeHandshakeSymbol, true);
+
+      done();
+    });
+  });
 });
