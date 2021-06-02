22c3522e745d84ce35408a9fb7cab0fe75aa088b
Neal Beeken <neal.beeken@mongodb.com>
2021-06-02 17:24:00 -0400

    test: organize and test all types for roundtrip

--- test/unit/bson_import.test.js
+++ test/unit/bson_import.test.js
@@ -3,47 +3,83 @@
 const { expect } = require('chai');
 const BSON = require('../../src/bson');
 
-describe('BSON Library Import', function () {
-  it('should import bson-ext if it exists', function () {
-    try {
-      require('bson-ext');
-    } catch (_) {
-      this.skip();
+function isBSONExtInstalled() {
+  try {
+    require.resolve('bson-ext');
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+describe('When importing BSON', function () {
+  const types = [
+    ['Long', 23],
+    ['ObjectId', '123456789123456789123456'],
+    ['Binary', Buffer.from('abc', 'ascii')],
+    ['Timestamp', 23],
+    ['Code', 'function(){}'],
+    ['MinKey', undefined],
+    ['MaxKey', undefined],
+    ['Decimal128', '2.34'],
+    ['Int32', 23],
+    ['Double', 2.3],
+    ['BSONRegExp', 'abc']
+  ];
+  // Omitted types since they're deprecated:
+  // BSONSymbol
+  // DBRef
+
+  const options = {
+    promoteValues: false,
+    bsonRegExp: true
+  };
+
+  function testTypes() {
+    for (const [type, ctorArg] of types) {
+      it(`should correctly round trip ${type}`, function () {
+        const typeCtor = BSON[type];
+        expect(typeCtor).to.be.a('function');
+        const doc = { key: new typeCtor(ctorArg) };
+        const outputDoc = BSON.deserialize(BSON.serialize(doc), options);
+        expect(outputDoc).to.have.property('key').that.is.instanceOf(typeCtor);
+        expect(outputDoc).to.deep.equal(doc);
+      });
     }
-    expect(BSON.deserialize).to.be.a('function');
-    expect(BSON.serialize).to.be.a('function');
-    expect(BSON.calculateObjectSize).to.be.a('function');
-    // Confirms we are using the bson-ext library
-    expect(BSON.deserialize.toString()).to.include('[native code]');
-    expect(BSON.serialize.toString()).to.include('[native code]');
-    expect(BSON.calculateObjectSize.toString()).to.include('[native code]');
+
+    it('should correctly round trip Map', function () {
+      expect(BSON.Map).to.be.a('function');
+      const doc = { key: new BSON.Map([['2', 2]]) };
+      const outputDoc = BSON.deserialize(BSON.serialize(doc));
+      expect(outputDoc).to.have.nested.property('key.2', 2);
+    });
+  }
+
+  describe('bson-ext should be imported if it exists', function () {
+    before(function () {
+      if (!isBSONExtInstalled()) {
+        this.skip();
+      }
+
+      expect(BSON.deserialize.toString()).to.include('[native code]');
+      expect(BSON.serialize.toString()).to.include('[native code]');
+      expect(BSON.calculateObjectSize.toString()).to.include('[native code]');
+    });
+
+    testTypes();
   });
 
-  it('bson-ext should correctly round trip a Long', function () {
-    try {
-      require('bson-ext');
-    } catch (_) {
-      this.skip();
-    }
+  describe('js-bson should be imported by default', function () {
+    before(function () {
+      if (isBSONExtInstalled()) {
+        this.skip();
+      }
 
-    const longValue = BSON.Long.fromNumber(2);
+      expect(BSON.deserialize.toString()).to.not.include('[native code]');
+      expect(BSON.serialize.toString()).to.not.include('[native code]');
+      expect(BSON.calculateObjectSize.toString()).to.not.include('[native code]');
+    });
 
-    const roundTrip = BSON.deserialize(BSON.serialize({ longValue }));
-
-    expect(roundTrip).has.property('longValue');
-  });
-  it('should import js-bson if bson-ext does not exist', function () {
-    try {
-      require('bson-ext');
-      this.skip();
-      // eslint-disable-next-line no-empty
-    } catch (_) {}
-    expect(BSON.deserialize).to.be.a('function');
-    expect(BSON.serialize).to.be.a('function');
-    expect(BSON.calculateObjectSize).to.be.a('function');
-
-    expect(BSON.deserialize.toString()).to.not.include('[native code]');
-    expect(BSON.serialize.toString()).to.not.include('[native code]');
-    expect(BSON.calculateObjectSize.toString()).to.not.include('[native code]');
+    testTypes();
   });
 });
