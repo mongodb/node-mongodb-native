a2887dbcc191a5779c5f5858a907ccae9095c901
Warren James <28974128+W-A-James@users.noreply.github.com>
2021-06-14 14:12:15 -0400

    fix(NODE-1502): command monitoring objects hold internal state references (#2832)

--- src/cmap/command_monitoring_events.ts
+++ src/cmap/command_monitoring_events.ts
@@ -181,7 +181,7 @@ const OP_QUERY_KEYS = [
 function extractCommand(command: WriteProtocolMessageType): Document {
   if (command instanceof GetMore) {
     return {
-      getMore: command.cursorId,
+      getMore: deepCopy(command.cursorId),
       collection: collectionName(command),
       batchSize: command.numberToReturn
     };
@@ -190,15 +190,15 @@ function extractCommand(command: WriteProtocolMessageType): Document {
   if (command instanceof KillCursor) {
     return {
       killCursors: collectionName(command),
-      cursors: command.cursorIds
+      cursors: deepCopy(command.cursorIds)
     };
   }
 
   if (command instanceof Msg) {
-    return command.command;
+    return deepCopy(command.command);
   }
 
-  if (command.query && command.query.$query) {
+  if (command.query?.$query) {
     let result: Document;
     if (command.ns === 'admin.$cmd') {
       // up-convert legacy command
@@ -208,7 +208,7 @@ function extractCommand(command: WriteProtocolMessageType): Document {
       result = { find: collectionName(command) };
       Object.keys(LEGACY_FIND_QUERY_MAP).forEach(key => {
         if (typeof command.query[key] !== 'undefined') {
-          result[LEGACY_FIND_QUERY_MAP[key]] = command.query[key];
+          result[LEGACY_FIND_QUERY_MAP[key]] = deepCopy(command.query[key]);
         }
       });
     }
@@ -216,7 +216,7 @@ function extractCommand(command: WriteProtocolMessageType): Document {
     Object.keys(LEGACY_FIND_OPTIONS_MAP).forEach(key => {
       const legacyKey = key as keyof typeof LEGACY_FIND_OPTIONS_MAP;
       if (typeof command[legacyKey] !== 'undefined') {
-        result[LEGACY_FIND_OPTIONS_MAP[legacyKey]] = command[legacyKey];
+        result[LEGACY_FIND_OPTIONS_MAP[legacyKey]] = deepCopy(command[legacyKey]);
       }
     });
 
@@ -234,11 +234,23 @@ function extractCommand(command: WriteProtocolMessageType): Document {
     if (command.query.$explain) {
       return { explain: result };
     }
-
     return result;
   }
 
-  return command.query ? command.query : command;
+  const clonedQuery: Record<string, unknown> = {};
+  const clonedCommand: Record<string, unknown> = {};
+  if (command.query) {
+    for (const k in command.query) {
+      clonedQuery[k] = deepCopy(command.query[k]);
+    }
+    clonedCommand.query = clonedQuery;
+  }
+
+  for (const k in command) {
+    if (k === 'query') continue;
+    clonedCommand[k] = deepCopy(((command as unknown) as Record<string, unknown>)[k]);
+  }
+  return command.query ? clonedQuery : clonedCommand;
 }
 
 function extractReply(command: WriteProtocolMessageType, reply?: Document) {
