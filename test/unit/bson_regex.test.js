0826fc5c3a273921588366255d445519ae638a3a
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-06-25 11:05:05 -0400

    test(NODE-3150): consolidated code and reformatted `.then()`s

--- test/unit/bson_regex.test.js
+++ test/unit/bson_regex.test.js
@@ -8,21 +8,20 @@ describe('BSONRegExp', () => {
     // define option for tests to use
     const option = { bsonRegExp: true };
     for (const passOptionTo of ['client', 'db', 'collection', 'operation']) {
-      it(`should respond with BSONRegExp class with option passed to ${passOptionTo}`, function(done) {
+      it(`should respond with BSONRegExp class with option passed to ${passOptionTo}`, function() {
         const client = this.configuration.newClient(passOptionTo === 'client' ? option : undefined);
-        const close = e => client.close().then(() => done(e));
         let collection;
 
-        client
+        return client
           .connect()
-          .then(() => client.db('bson_regex_db', passOptionTo === 'db' ? option : undefined))
-          .then(db =>
-            db.collection('bson_regex_coll', passOptionTo === 'collection' ? option : undefined)
-          )
-          .then(coll => {
-            collection = coll;
-            collection.insertOne({ regex: new BSONRegExp('abc', 'imx') });
+          .then(() => {
+            const db = client.db('bson_regex_db', passOptionTo === 'db' ? option : undefined);
+            collection = db.collection(
+              'bson_regex_coll',
+              passOptionTo === 'collection' ? option : undefined
+            );
           })
+          .then(() => collection.insertOne({ regex: new BSONRegExp('abc', 'imx') }))
           .then(() =>
             collection.findOne(
               { regex: new BSONRegExp('abc', 'imx') },
@@ -34,34 +33,8 @@ describe('BSONRegExp', () => {
               .has.property('regex')
               .that.is.instanceOf(BSONRegExp)
           )
-          .finally(() => close());
-        // .catch(e => done(e));
+          .then(() => client.close());
       });
-
-      // it(`should respond with BSONRegExp class with option passed to ${passOptionTo}`, async function() {
-      //   try {
-      //     client = this.configuration.newClient(passOptionTo === 'client' ? option : undefined);
-      //     await client.connect();
-
-      //     const db = client.db('bson_regex_db', passOptionTo === 'db' ? option : undefined);
-      //     const collection = db.collection(
-      //       'bson_regex_coll',
-      //       passOptionTo === 'collection' ? option : undefined
-      //     );
-
-      //     await collection.insertOne({ regex: new BSONRegExp('abc', 'imx') });
-      //     const res = await collection.findOne(
-      //       { regex: new BSONRegExp('abc', 'imx') },
-      //       passOptionTo === 'operation' ? option : undefined
-      //     );
-
-      //     expect(res)
-      //       .has.property('regex')
-      //       .that.is.instanceOf(BSONRegExp);
-      //   } finally {
-      //     await client.close();
-      //   }
-      // });
     }
   });
 });
