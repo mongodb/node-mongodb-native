4867d92ddb8a90c900d1f1db2bf6c1bb1bd39769
emadum <eric.adum@mongodb.com>
2021-06-30 15:14:41 -0400

    fix(NODE-3290): backport versioned api validation and tests

--- test/functional/versioned-api.test.js
+++ test/functional/versioned-api.test.js
@@ -3,17 +3,80 @@
 const expect = require('chai').expect;
 const loadSpecTests = require('../spec/index').loadSpecTests;
 const runUnifiedTest = require('./unified-spec-runner/runner').runUnifiedTest;
+const ServerApiVersion = require('../../lib/core').ServerApiVersion;
 
 describe('Versioned API', function() {
-  it('should throw an error if serverApi version is provided via the uri with new parser', {
-    metadata: { topology: 'single' },
-    test: function(done) {
+  describe('client option validation', function() {
+    it('is supported as a client option when it is a valid ServerApiVersion string', function() {
+      const validVersions = Object.values(ServerApiVersion);
+      expect(validVersions.length).to.be.at.least(1);
+      for (const version of validVersions) {
+        const client = this.configuration.newClient('mongodb://localhost/', {
+          serverApi: version
+        });
+        expect(client.s.options)
+          .to.have.property('serverApi')
+          .deep.equal({ version });
+      }
+    });
+
+    it('is supported as a client option when it is an object with a valid version property', function() {
+      const validVersions = Object.values(ServerApiVersion);
+      expect(validVersions.length).to.be.at.least(1);
+      for (const version of validVersions) {
+        const client = this.configuration.newClient('mongodb://localhost/', {
+          serverApi: { version }
+        });
+        expect(client.s.options)
+          .to.have.property('serverApi')
+          .deep.equal({ version });
+      }
+    });
+
+    it('is not supported as a client option when it is an invalid string', function() {
+      expect(() =>
+        this.configuration.newClient('mongodb://localhost/', {
+          serverApi: 'bad'
+        })
+      ).to.throw(/^Invalid server API version=bad;/);
+    });
+
+    it('is not supported as a client option when it is a number', function() {
+      expect(() =>
+        this.configuration.newClient('mongodb://localhost/', {
+          serverApi: 1
+        })
+      ).to.throw(/^Invalid `serverApi` property;/);
+    });
+
+    it('is not supported as a client option when it is an object without a specified version', function() {
+      expect(() =>
+        this.configuration.newClient('mongodb://localhost/', {
+          serverApi: {}
+        })
+      ).to.throw(/^Invalid `serverApi` property;/);
+    });
+
+    it('is not supported as a client option when it is an object with an invalid specified version', function() {
+      expect(() =>
+        this.configuration.newClient('mongodb://localhost/', {
+          serverApi: { version: 1 }
+        })
+      ).to.throw(/^Invalid server API version=1;/);
+      expect(() =>
+        this.configuration.newClient('mongodb://localhost/', {
+          serverApi: { version: 'bad' }
+        })
+      ).to.throw(/^Invalid server API version=bad;/);
+    });
+
+    it('is not supported as a URI option even when it is a valid ServerApiVersion string', function(done) {
       const client = this.configuration.newClient({ serverApi: '1' }, { useNewUrlParser: true });
       client.connect(err => {
         expect(err).to.match(/URI cannot contain `serverApi`, it can only be passed to the client/);
         client.close(done);
       });
-    }
+    });
   });
 
   for (const versionedApiTest of loadSpecTests('versioned-api')) {
