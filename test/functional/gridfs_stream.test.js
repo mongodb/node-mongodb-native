e1357dee0502f1da15ada20b67d0d6db63c96d6a
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-07-19 23:21:05 -0400

    test(NODE-3405): adjust tests to expect MongoStreamClosedError

--- test/functional/gridfs_stream.test.js
+++ test/functional/gridfs_stream.test.js
@@ -517,11 +517,11 @@ describe('GridFS Stream', function () {
                 expect(c).to.equal(0);
                 uploadStream.write('b', 'utf8', function (error) {
                   expect(error.toString()).to.equal(
-                    'MongoDriverError: this stream has been aborted'
+                    'MongoStreamClosedError: Stream has been aborted'
                   );
                   uploadStream.end('c', 'utf8', function (error) {
                     expect(error.toString()).to.equal(
-                      'MongoDriverError: this stream has been aborted'
+                      'MongoStreamClosedError: Stream has been aborted'
                     );
                     // Fail if user tries to abort an aborted stream
                     uploadStream.abort().then(null, function (error) {
