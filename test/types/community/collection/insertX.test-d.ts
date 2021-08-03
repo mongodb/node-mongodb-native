dddc76944088bdf45895840182b7a5e96fa56455
Neal Beeken <neal.beeken@mongodb.com>
2021-08-03 16:55:37 -0400

    fix: lint

--- test/types/community/collection/insertX.test-d.ts
+++ test/types/community/collection/insertX.test-d.ts
@@ -1,4 +1,4 @@
-import { expectAssignable, expectError, expectNotAssignable, expectNotType, expectType } from 'tsd';
+import { expectError, expectNotAssignable, expectNotType, expectType } from 'tsd';
 import { MongoClient, ObjectId, OptionalId } from '../../../../src';
 import type { PropExists } from '../../utility_types';
 
