4dd841fe2aa7c3ccaf4faeb1e94c24571ae6ab64
Durran Jordan <durran@gmail.com>
2021-08-07 01:43:08 +0200

    test(NODE-3461): refactor lb manual to full test run

--- test/tools/runner/index.js
+++ test/tools/runner/index.js
@@ -66,7 +66,8 @@ before(function (_done) {
   // );
 
   const options = MONGODB_API_VERSION ? { serverApi: MONGODB_API_VERSION } : {};
-  const client = new MongoClient(MONGODB_URI, options);
+  const loadBalanced = SINGLE_MONGOS_LB_URI && MULTI_MONGOS_LB_URI;
+  const client = new MongoClient(loadBalanced ? SINGLE_MONGOS_LB_URI : MONGODB_URI, options);
   const done = err => client.close(err2 => _done(err || err2));
 
   client.connect(err => {
