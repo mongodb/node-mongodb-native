b5f7acb4474627d4d1834076222a6bc1c214a5cc
Daria Pardue <81593090+dariakp@users.noreply.github.com>
2021-09-13 13:03:29 -0400

    fix(NODE-3599): incorrect indexes return type

---
+++ test/types/indexes_test-d.ts
@@ -0,0 +1,26 @@
+import { expectType } from 'tsd';
+import { MongoClient, Document, AnyError } from '../../src';
+
+const client = new MongoClient('');
+const db = client.db('test');
+const collection = db.collection('test.find');
+
+// Promise variant testing
+expectType<Promise<Document[]>>(collection.indexes());
+expectType<Promise<Document[]>>(collection.indexes({}));
+
+// Explicit check for iterable result
+for (const index of await collection.indexes()) {
+  expectType<Document>(index);
+}
+
+// Callback variant testing
+collection.indexes((err, indexes) => {
+  expectType<AnyError | undefined>(err);
+  expectType<Document[] | undefined>(indexes);
+});
+
+collection.indexes({}, (err, indexes) => {
+  expectType<AnyError | undefined>(err);
+  expectType<Document[] | undefined>(indexes);
+});
