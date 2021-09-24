a8e9938abcb00b67816438fb7c9db890e35d63c9
Neal Beeken <neal.beeken@mongodb.com>
2021-09-24 17:46:54 -0400

    fix(NODE-3609): correct listDatabases return type (#2986)

---
+++ test/types/admin.test-d.ts
@@ -0,0 +1,12 @@
+import { expectType } from 'tsd';
+import { Document, MongoClient } from '../../src/index';
+
+const client = new MongoClient('');
+const admin = client.db().admin();
+
+expectType<{
+  databases: ({ name: string; sizeOnDisk?: number; empty?: boolean } & Document)[];
+  totalSize?: number;
+  totalSizeMb?: number;
+  ok: 1 | 0;
+}>(await admin.listDatabases());
