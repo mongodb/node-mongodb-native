c681e15297a95563c2d1baf0945b24c208fedc12
Durran Jordan <durran@gmail.com>
2021-10-26 18:07:03 +0200

    test(NODE-3596): update max staleness spec tests

--- test/functional/unit-sdam/monitoring.test.js
+++ test/functional/unit-sdam/monitoring.test.js
@@ -26,7 +26,7 @@ describe('monitoring', function () {
     mockServer.setMessageHandler(request => {
       const doc = request.document;
       if (doc.ismaster || doc.hello) {
-        request.reply(Object.assign({}, mock.DEFAULT_ISMASTER));
+        request.reply(Object.assign({}, mock.DEFAULT_ISMASTER_36));
       } else if (doc.endSessions) {
         request.reply({ ok: 1 });
       }
