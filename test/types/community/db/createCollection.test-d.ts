1492d4f76b78338fb8a1a73dad069282b2fc444d
skrtheboss <denisfrenademetz97@gmail.com>
2021-10-28 10:27:39 +0200

    fix: simplify overloads of Db's createCollection function
    This adds support for calling createCollection with optional options.
    This is useful if for example a wrapper function is used.

---
+++ test/types/community/db/createCollection.test-d.ts
@@ -0,0 +1,82 @@
+import { expectType } from 'tsd';
+
+import {
+  MongoClient,
+  ObjectId,
+  Collection,
+  CreateCollectionOptions,
+  AnyError,
+  Callback
+} from '../../../../src/index';
+
+const client = new MongoClient('');
+const db = client.db('test');
+
+interface SubTestSchema {
+  field1: string;
+  field2: string;
+}
+
+type FruitTypes = 'apple' | 'pear';
+
+// test with collection type
+interface TestSchema {
+  _id: ObjectId;
+  stringField: string;
+  numberField: number;
+  optionalNumberField?: number;
+  dateField: Date;
+  fruitTags: string[];
+  maybeFruitTags?: FruitTypes[];
+  readonlyFruitTags: ReadonlyArray<string>;
+  subInterfaceField: SubTestSchema;
+  subInterfaceArray: SubTestSchema[];
+}
+
+const options: CreateCollectionOptions = {};
+
+// createCollection
+
+expectType<Promise<Collection<TestSchema>>>(db.createCollection<TestSchema>('test'));
+
+expectType<Promise<Collection<TestSchema>>>(db.createCollection<TestSchema>('test', options));
+
+// ensure we can use the create collection in a promise based wrapper function
+function extendedPromiseBasedCreateCollection(
+  name: string,
+  optionalOptions?: CreateCollectionOptions
+): Promise<Collection<TestSchema>> {
+  return db.createCollection<TestSchema>(name, optionalOptions);
+}
+
+expectType<Promise<Collection<TestSchema>>>(extendedPromiseBasedCreateCollection('test'));
+
+expectType<void>(
+  db.createCollection<TestSchema>('test', (err, collection) => {
+    expectType<AnyError | undefined>(err);
+    expectType<Collection<TestSchema> | undefined>(collection);
+  })
+);
+
+expectType<void>(
+  db.createCollection<TestSchema>('test', options, (err, collection) => {
+    expectType<AnyError | undefined>(err);
+    expectType<Collection<TestSchema> | undefined>(collection);
+  })
+);
+
+// ensure we can use the create collection in a callback based wrapper function
+function extendedCallbackBasedCreateCollection(
+  name: string,
+  callback: Callback<Collection<TestSchema>>,
+  optionalOptions?: CreateCollectionOptions
+): void {
+  db.createCollection<TestSchema>(name, optionalOptions, callback);
+}
+
+expectType<void>(
+  extendedCallbackBasedCreateCollection('test', (err, collection) => {
+    expectType<AnyError | undefined>(err);
+    expectType<Collection<TestSchema> | undefined>(collection);
+  })
+);
