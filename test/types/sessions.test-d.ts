53b3164b8ceb19c8c8be8b5084fd03476f5635b9
Durran Jordan <durran@gmail.com>
2021-11-10 16:28:50 +0100

    fix(NODE-3344): allow setting `defaultTransactionOptions` with POJO rather than ReadConcern instance (#3032)
    Co-authored-by: Valeri Karpov <val@karpov.io>

---
+++ test/types/sessions.test-d.ts
@@ -0,0 +1,17 @@
+import { expectType, expectError } from 'tsd';
+import { MongoClient } from '../../src/mongo_client';
+import { ReadConcern, ReadConcernLevel } from '../../src/read_concern';
+import type { ClientSession } from '../../src/sessions';
+
+// test mapped cursor types
+const client = new MongoClient('');
+// should allow ReadConcern or ReadConcernLike as readConcern in defaultTransactionOptions
+expectType<ClientSession>(
+  client.startSession({ defaultTransactionOptions: { readConcern: { level: 'snapshot' } } })
+);
+expectType<ClientSession>(
+  client.startSession({
+    defaultTransactionOptions: { readConcern: new ReadConcern(ReadConcernLevel.local) }
+  })
+);
+expectError(client.startSession({ defaultTransactionOptions: { readConcern: 1 } }));
