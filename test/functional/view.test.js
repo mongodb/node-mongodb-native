db26916083ad08cb32e7c2ed1fd8e39a96b8be0c
Bailey Pearson <bailey.pearson@mongodb.com>
2022-01-07 10:31:24 -0500

    Make view test a real integration test

--- test/functional/view.test.js
+++ test/functional/view.test.js
@@ -1,62 +1,47 @@
 'use strict';
 const { expect } = require('chai');
-const mock = require('../tools/mongodb-mock/index');
-const { Long } = require('../../src');
-const { isHello } = require('../../src/utils');
 
 describe('Views', function () {
-  it('should successfully pass through collation to findAndModify command', {
+  let client, db;
+  beforeEach(async function () {
+    const configuration = this.configuration;
+    client = this.configuration.newClient();
+    await client.connect();
+    db = client.db(configuration.db);
+  });
+
+  afterEach(async function () {
+    await db.dropCollection('test');
+    await client.close();
+  });
+
+  it('should successfully create a view on a collection', {
     metadata: { requires: { topology: 'single' } },
 
     async test() {
-      const configuration = this.configuration;
-
-      // Default message fields
-      const defaultFields = Object.assign({}, mock.HELLO);
-
-      // Primary server states
-      const primary = [Object.assign({}, defaultFields)];
-
-      let commandResult = null;
-
-      // Boot the mock
-      const singleServer = await mock.createServer();
-      singleServer.setMessageHandler(request => {
-        var doc = request.document;
-        if (isHello(doc)) {
-          request.reply(primary[0]);
-        } else if (doc.listCollections) {
-          request.reply({
-            ok: 1,
-            cursor: {
-              id: Long.fromNumber(0),
-              ns: 'test.cmd$.listCollections',
-              firstBatch: []
-            }
-          });
-        } else if (doc.create) {
-          commandResult = doc;
-          request.reply({ ok: 1 });
-        } else if (doc.endSessions) {
-          request.reply({ ok: 1 });
-        }
-      });
-
-      // Connect to the mocks
-      const client = configuration.newClient(`mongodb://${singleServer.uri()}/test`);
-      await client.connect();
-      const db = client.db(this.configuration.db);
-
-      // Simple findAndModify command returning the new document
-      const r = await db.createCollection('test', { viewOn: 'users', pipeline: [{ $match: {} }] });
-      expect(r).to.exist;
-      expect(commandResult).to.containSubset({
-        create: 'test',
+      const result = await db.createCollection('test', {
         viewOn: 'users',
         pipeline: [{ $match: {} }]
       });
 
-      await client.close();
+      expect(result).to.exist;
+
+      const newView = await db
+        .listCollections({
+          type: 'view',
+          name: 'test'
+        })
+        .next();
+
+      console.error(newView);
+
+      expect(newView).to.exist;
+
+      const options = newView.options;
+      expect(options).to.haveOwnProperty('viewOn', 'users');
+      expect(options)
+        .to.haveOwnProperty('pipeline')
+        .to.deep.equal([{ $match: {} }]);
     }
   });
 });
