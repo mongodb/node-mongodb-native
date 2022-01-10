50b2e425d44768ed899f9b1d52c186537bb7141c
Bailey Pearson <bailey.pearson@mongodb.com>
2022-01-10 09:32:42 -0500

    Remove topology restrictions on view test

--- test/integration/collection-management/view.test.ts
+++ test/integration/collection-management/view.test.ts
@@ -19,31 +19,27 @@ describe('Views', function () {
     await client.close();
   });
 
-  it('should successfully create a view on a collection', {
-    metadata: { requires: { topology: 'single' } },
+  it('should successfully create a view on a collection', async function () {
+    const result = await db.createCollection('test', {
+      viewOn: 'users',
+      pipeline: [{ $match: {} }]
+    });
 
-    async test() {
-      const result = await db.createCollection('test', {
-        viewOn: 'users',
-        pipeline: [{ $match: {} }]
-      });
+    expect(result).to.exist;
 
-      expect(result).to.exist;
+    const newView = await db
+      .listCollections({
+        type: 'view',
+        name: 'test'
+      })
+      .next();
 
-      const newView = await db
-        .listCollections({
-          type: 'view',
-          name: 'test'
-        })
-        .next();
+    expect(newView).to.exist;
 
-      expect(newView).to.exist;
-
-      const options = (newView as CollectionInfo).options ?? null;
-      expect(options).to.haveOwnProperty('viewOn', 'users');
-      expect(options)
-        .to.haveOwnProperty('pipeline')
-        .to.deep.equal([{ $match: {} }]);
-    }
-  } as any);
+    const options = (newView as CollectionInfo).options ?? null;
+    expect(options).to.haveOwnProperty('viewOn', 'users');
+    expect(options)
+      .to.haveOwnProperty('pipeline')
+      .to.deep.equal([{ $match: {} }]);
+  });
 });
