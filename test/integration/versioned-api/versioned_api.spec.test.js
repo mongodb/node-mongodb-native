c65e7707daee234874d9a903eeac73344e7496a9
Daria Pardue <daria.pardue@mongodb.com>
2022-01-21 19:19:21 -0500

    test: remove duplicate versioned api test

--- test/integration/versioned-api/versioned_api.spec.test.js
+++ test/integration/versioned-api/versioned_api.spec.test.js
@@ -5,15 +5,6 @@ const { loadSpecTests } = require('../../spec/');
 const { runUnifiedTest } = require('../../tools/unified-spec-runner/runner');
 
 describe('Versioned API', function () {
-  it('should throw an error if serverApi version is provided via the uri', {
-    metadata: { topology: 'single' },
-    test: function () {
-      expect(() => this.configuration.newClient({ serverApi: '1' })).to.throw(
-        /URI cannot contain `serverApi`, it can only be passed to the client/
-      );
-    }
-  });
-
   for (const versionedApiTest of loadSpecTests('versioned-api')) {
     expect(versionedApiTest).to.exist;
     context(String(versionedApiTest.description), function () {
