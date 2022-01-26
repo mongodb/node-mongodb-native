f64bdeeca796e44a271a37b39034d0a33b6c623c
Bailey Pearson <bailey.pearson@mongodb.com>
2022-01-26 09:10:51 -0500

    use runUnifiedSuite in collection_management spec tests

--- test/integration/collection-management/collection_management.spec.test.js
+++ test/integration/collection-management/collection_management.spec.test.js
@@ -1,21 +1,8 @@
 'use strict';
 
-const { expect } = require('chai');
 const { loadSpecTests } = require('../../spec/index');
-const { runUnifiedTest } = require('../../tools/unified-spec-runner/runner');
+const { runUnifiedSuite } = require('../../tools/unified-spec-runner/runner');
 
 describe('Collection management unified spec tests', function () {
-  for (const collectionManagementTest of loadSpecTests('collection-management')) {
-    expect(collectionManagementTest).to.exist;
-    context(String(collectionManagementTest.description), function () {
-      for (const test of collectionManagementTest.tests) {
-        it(String(test.description), {
-          metadata: { sessions: { skipLeakTests: true } },
-          test: async function () {
-            await runUnifiedTest(this, collectionManagementTest, test);
-          }
-        });
-      }
-    });
-  }
+  runUnifiedSuite(loadSpecTests('collection-management'));
 });
