58d481a1078a7f078d64818d25b7ee6c1ae4b22e
Bailey Pearson <bailey.pearson@mongodb.com>
2022-01-31 13:36:18 -0500

    run all read write spec tests with auth

--- test/integration/read-write-concern/read_write_concern.spec.test.js
+++ test/integration/read-write-concern/read_write_concern.spec.test.js
@@ -6,8 +6,7 @@ const { loadSpecTests } = require('../../spec');
 describe('Read Write Concern spec tests', function () {
   describe('operation spec tests', function () {
     const testContext = new TestRunnerContext();
-    const isAuthEnabled = process.env.AUTH === 'auth';
-    const testSuites = isAuthEnabled ? [] : loadSpecTests('read-write-concern/operation');
+    const testSuites = loadSpecTests('read-write-concern/operation');
 
     after(() => testContext.teardown());
     before(function () {
