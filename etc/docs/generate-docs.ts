1413992668cb78cfccd241aed80242205ee4e642
Bailey Pearson <bailey.pearson@mongodb.com>
2022-02-24 19:59:25 -0500

    clean up build script

--- etc/docs/generate-docs.ts
+++ etc/docs/generate-docs.ts
@@ -3,34 +3,55 @@
 import { parse, stringify } from '@iarna/toml';
 import * as child_process from 'child_process';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { json } from 'stream/consumers';
 import { promisify } from 'util';
 
 const exec = promisify(child_process.exec);
 
+const RELEASES_TOML_FILE = './template/data/releases.toml';
+const RELEASES_JSON_FILE = './template/static/versions.json';
+
+interface JsonVersionSchema {
+  version: string;
+}
+
+interface VersionSchema {
+  version: string;
+  status: 'current' | 'supported' | 'not-supported';
+  api: string;
+  usesMongoDBManual?: boolean;
+  docs?: string;
+  semvarVersion: string;
+}
+interface TomlVersionSchema {
+  current: string;
+  mongodDBManual: string;
+  versions: VersionSchema[]
+}
+
 /**
  * ATTENTION - EDIT THIS BEFORE RELEASING
  */
-const NEW_VERSION = {
+const NEW_VERSION: VersionSchema = {
   // version - the display name for the version of the driver on the docs website
   //  ex: Driver 4.5
-  version: '',
-
-  // version_id - the actual version number of the docs
-  //  ex: 4.5
-  version_id: '',
+  version: 'Driver 4.5',
 
   // status - a status for the version.  typically would probably just be 'latest'
-  status: '',
+  status: 'current',
 
   // api - the name of the folder for the generated documentation to live.  typically should be version_id prefixed with `./`
   //  ex: ./4.5
-  api: '',
+  api: './4.5',
 
   // usesMongoDBManual - if true, includes a link to the mongodb documentation for the new version
-  usesMongoDBManual: true
+  usesMongoDBManual: true,
+
+  // the 
+  semvarVersion: '4.5'
 };
 
-function validateVersionInformation() {
+function validateVersionInformation(jsonVersions: JsonVersionSchema[]) {
   const isVersionInfoValid = ['version', 'version_id', 'status', 'api'].every(
     key => NEW_VERSION[key] !== ''
   );
@@ -41,32 +62,36 @@ function validateVersionInformation() {
     );
     process.exit(1);
   }
+
+  if (jsonVersions.some(({ version }) => version === NEW_VERSION.semvarVersion)) {
+    console.error(
+      'Error - attempting to publish docs for a release that already exists.'
+    );
+    process.exit(1);
+  }
+
 }
 
-async function copyNewDocsToGeneratedSite({ version_id }) {
-  const versionName = version_id;
-  const outputDirectory = `./temp/${versionName}`;
+async function copyNewDocsToGeneratedSite({ semvarVersion }: VersionSchema ) {
+  const outputDirectory = `./temp/${semvarVersion}`;
   const pathToBuiltDocs = './build';
   const command = `cp -R ${pathToBuiltDocs} ${outputDirectory}`;
   return await exec(command);
 }
 
-function updateSiteTemplateForNewVersion(newVersion: any) {
-  const RELEASES_TOML_FILE = './template/data/releases.toml';
+async function updateSiteTemplateForNewVersion(tomlData: TomlVersionSchema, jsonData: JsonVersionSchema[]) {
+  tomlData.versions.unshift(NEW_VERSION);
+  tomlData.current = NEW_VERSION.version;
+  writeFileSync(RELEASES_TOML_FILE, stringify(tomlData as any));
 
-  const contents = parse(readFileSync(RELEASES_TOML_FILE, { encoding: 'utf8' })) as any;
-  contents.versions.unshift(newVersion);
-  writeFileSync(RELEASES_TOML_FILE, stringify(contents));
-
-  const RELEASES_JSON_FILE = './template/static/versions.json';
-
-  const versions = JSON.parse(readFileSync(RELEASES_JSON_FILE, { encoding: 'utf8' }));
-  versions.unshift({ version: newVersion.version_id });
-  writeFileSync(RELEASES_JSON_FILE, JSON.stringify(versions, null, 4));
+  jsonData.unshift({ version: NEW_VERSION.semvarVersion});
+  writeFileSync(RELEASES_JSON_FILE, JSON.stringify(jsonData, null, 4));
+  
+  // generate the site from the template
+  await exec(`hugo -s template -d ../temp -b "/node-mongodb-native"`);
 }
 
 async function main() {
-  validateVersionInformation();
 
   const pathToBuiltDocs = './build';
   const docsExist = existsSync(pathToBuiltDocs);
@@ -77,14 +102,17 @@ async function main() {
     process.exit(1);
   }
 
-  updateSiteTemplateForNewVersion(NEW_VERSION);
+  const tomlVersions = parse(readFileSync(RELEASES_TOML_FILE, { encoding: 'utf8' })) as unknown as TomlVersionSchema;
+  const jsonVersions = JSON.parse(readFileSync(RELEASES_JSON_FILE, { encoding: 'utf8' })) as unknown as JsonVersionSchema[];
 
-  // generate the site from the template
-  await exec(`hugo -s template -d ../temp -b "/node-mongodb-native"`);
+  validateVersionInformation(jsonVersions);
+
+  await updateSiteTemplateForNewVersion(tomlVersions, jsonVersions);
+
   await copyNewDocsToGeneratedSite(NEW_VERSION);
 
   // copy the generated site to the docs folder
-  await exec(`cp -R temp/. ../docs/.`);
+  await exec(`cp -R temp/. ../../docs/.`);
 
   // cleanup
   await exec('rm -rf temp');
