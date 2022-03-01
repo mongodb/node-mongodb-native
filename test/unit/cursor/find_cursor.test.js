ddc4d2485470e9428649066b49333738bdf0569b
Durran Jordan <durran@gmail.com>
2022-03-01 13:31:12 +0100

    test(NODE-3521): add next tests for session

--- test/unit/cursor/find_cursor.test.js
+++ test/unit/cursor/find_cursor.test.js
@@ -10,6 +10,111 @@ const { FindCursor } = require('../../../src/cursor/find_cursor');
 
 const test = {};
 describe('Find Cursor', function () {
+  describe('#next', function () {
+    afterEach(function () {
+      mock.cleanup()
+    });
+    beforeEach(function () {
+      return mock.createServer().then(mockServer => {
+        test.server = mockServer;
+      });
+    });
+
+    context('when there is a data bearing server', function () {
+      beforeEach(function () {
+        test.server.setMessageHandler(request => {
+          const doc = request.document;
+          if (isHello(doc)) {
+            request.reply(mock.HELLO);
+          } else if (doc.find) {
+            request.reply({
+              cursor: {
+                id: Long.fromNumber(1),
+                ns: 'test.test',
+                firstBatch: [{ _id: 1, name: 'test' }]
+              },
+              ok: 1
+            });
+          }
+        });
+      });
+
+      it('sets the session on the cursor', function (done) {
+        const topology = new Topology(test.server.hostAddress());
+        const cursor = new FindCursor(topology, MongoDBNamespace.fromString('test.test'), {}, {});
+        topology.connect(function () {
+          cursor.next(function () {
+            expect(cursor.session).to.exist;
+            topology.close(done);
+          });
+        });
+      });
+    });
+
+    context('when there is no data bearing server', function () {
+      beforeEach(function () {
+        test.server.setMessageHandler(request => {
+          const doc = request.document;
+          if (isHello(doc)) {
+            request.reply({ errmsg: 'network error' });
+          } else if (doc.find) {
+            request.reply({
+              cursor: {
+                id: Long.fromNumber(1),
+                ns: 'test.test',
+                firstBatch: [{ _id: 1, name: 'test' }]
+              },
+              ok: 1
+            });
+          }
+        });
+      });
+
+      it('does not set the session on the cursor', function (done) {
+        const topology = new Topology(test.server.hostAddress(), { serverSelectionTimeoutMS: 2 });
+        const cursor = new FindCursor(topology, MongoDBNamespace.fromString('test.test'), {}, {});
+        topology.connect(function () {
+          cursor.next(function () {
+            expect(cursor.session).to.not.exist;
+            topology.close(done);
+          });
+        });
+      });
+    });
+
+    context('when a data bearing server becomes available', function () {
+      beforeEach(function () {
+        let helloCalls = 0;
+        test.server.setMessageHandler(request => {
+          const doc = request.document;
+          if (isHello(doc)) {
+            request.reply(helloCalls > 0 ? { errmsg: 'network error' } : mock.HELLO);
+          } else if (doc.find) {
+            request.reply({
+              cursor: {
+                id: Long.fromNumber(1),
+                ns: 'test.test',
+                firstBatch: [{ _id: 1, name: 'test' }]
+              },
+              ok: 1
+            });
+          }
+        });
+      });
+
+      it('sets the session on the cursor', function (done) {
+        const topology = new Topology(test.server.hostAddress(), { serverSelectionTimeoutMS: 2 });
+        const cursor = new FindCursor(topology, MongoDBNamespace.fromString('test.test'), {}, {});
+        topology.connect(function () {
+          cursor.next(function () {
+            expect(cursor.session).to.exist;
+            topology.close(done);
+          });
+        });
+      });
+    });
+  });
+
   describe('Response', function () {
     afterEach(() => mock.cleanup());
     beforeEach(() => {
