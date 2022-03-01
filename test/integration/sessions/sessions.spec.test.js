d44477e1082ea68c75be1839730d0c8d4bee6559
Bailey Pearson <bailey.pearson@mongodb.com>
2022-03-01 10:03:34 -0500

    Remove .only from sessions spec

--- test/integration/sessions/sessions.spec.test.js
+++ test/integration/sessions/sessions.spec.test.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 const { runUnifiedSuite } = require('../../tools/unified-spec-runner/runner');
 const { loadSpecTests } = require('../../spec');
 
-describe.only('Sessions spec tests', function () {
+describe('Sessions spec tests', function () {
   runUnifiedSuite(loadSpecTests(join('sessions', 'tests')), [
     'Dirty explicit session is discarded (insert)',
     'Dirty explicit session is discarded (findAndModify)'
