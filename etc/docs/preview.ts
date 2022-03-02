2c9ed6ec297b72d560bb7fa0693f5916029d1370
Bailey Pearson <bailey.pearson@mongodb.com>
2022-03-02 16:12:19 -0500

    Add express as dev dependency and use for simple file server

--- etc/docs/preview.ts
+++ etc/docs/preview.ts
@@ -1,36 +1,16 @@
-import { on } from 'events';
-import { PathLike } from 'fs';
-import { access, readFile } from 'fs/promises';
-import { createServer } from 'http';
-import { join } from 'path';
+import * as express from 'express';
 
-const fromDir = (...filePath) => join('docs', ...filePath);
-const server = createServer();
-server.listen(8080, 'localhost', 128, () => console.log('Listening on http://localhost:8080'));
-const canAccess = async (pathlike: PathLike) => await access(pathlike).then(() => true,() => false);
+const app = express();
 
-async function main() {
-  for await (const [request, response] of on(server, 'request')) {
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    console.log(request.method, url.pathname);
-    const pathname = url.pathname.replace('/node-mongodb-native', '');
-    console.log(pathname)
-    try {
-      if (pathname === '/') {
-        response.end(await readFile(fromDir('index.html')));
-        continue;
-      } else {
-        const filePath = fromDir(pathname.slice(1));
-        if (await canAccess(filePath)) {
-          response.end(await readFile(filePath))
-        } else {
-          response.end(await readFile(fromDir('404.html')))
-        };
-      }
-    } catch (error) {
-      response.statusCode = 500;
-      response.end(`Bad Request: ${error}`, 'utf8');
-    }
-  }
-}
-main().then(() => console.log('end')).catch(e => console.log(e)).finally(() => server.close());
+app.use((req, res, next) => {
+  next();
+
+  console.log(req.method, req.url, res.statusCode);
+})
+
+app.use(express.static('docs'));
+app.use('/node-mongodb-native', express.static('docs'));
+
+app.listen(8080, () => {
+  console.log('listening on port 8080')
+})
