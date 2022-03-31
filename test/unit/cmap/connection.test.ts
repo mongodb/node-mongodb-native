e0636cdfd710a8d985461947456b4e3ffad0723a
Neal Beeken <neal.beeken@mongodb.com>
2022-03-31 10:22:45 -0400

    fix: imports

--- test/unit/cmap/connection.test.ts
+++ test/unit/cmap/connection.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { EventEmitter } from 'events';
 import { Socket } from 'net';
-import sinon from 'sinon';
+import * as sinon from 'sinon';
 
 import { connect } from '../../../src/cmap/connect';
 import { Connection, hasSessionSupport } from '../../../src/cmap/connection';
