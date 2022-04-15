c439f972784cfe71b5e2da9e54c6963c65a8862b
Durran Jordan <durran@gmail.com>
2022-04-15 12:55:47 +0200

    test(NODE-3928): add additional tests

--- test/unit/cmap/commands.test.js
+++ test/unit/cmap/commands.test.js
@@ -3,6 +3,24 @@ const { Response } = require('../../../src/cmap/commands');
 
 describe('commands', function () {
   describe('Response', function () {
+    describe('#parse', function () {
+      context('when the message body is invalid', function () {
+        const message = Buffer.from([]);
+        const header = {
+          length: 0,
+          requestId: 0,
+          responseTo: 0,
+          opCode: 0
+        };
+        const body = Buffer.from([]);
+
+        it('throws an exception', function () {
+          const response = new Response(message, header, body);
+          expect(() => response.parse()).to.throw();
+        });
+      });
+    });
+
     describe('#constructor', function () {
       context('when the message body is invalid', function () {
         const message = Buffer.from([]);
@@ -15,8 +33,59 @@ describe('commands', function () {
         const body = Buffer.from([]);
 
         it('does not throw an exception', function () {
+          let error;
+          try {
+            const response = new Response(message, header, body);
+          }
+          catch(err) {
+            error = err;
+          }
+          expect(error).to.be.undefined;
+        });
+
+        it('initializes the documents to an empty array', function () {
+          const response = new Response(message, header, body);
+          expect(response.documents).to.be.empty;
+        });
+
+        it('does not set the responseFlags', function () {
+          const response = new Response(message, header, body);
+          expect(response.responseFlags).to.be.undefined;
+        });
+
+        it('does not set the cursorNotFound flag', function () {
+          const response = new Response(message, header, body);
+          expect(response.cursorNotFound).to.be.undefined;
+        });
+
+        it('does not set the cursorId', function () {
+          const response = new Response(message, header, body);
+          expect(response.cursorId).to.be.undefined;
+        });
+
+        it('does not set startingFrom', function () {
+          const response = new Response(message, header, body);
+          expect(response.startingFrom).to.be.undefined;
+        });
+
+        it('does not set numberReturned', function () {
           const response = new Response(message, header, body);
           expect(response.numberReturned).to.be.undefined;
+        });
+
+        it('does not set queryFailure', function () {
+          const response = new Response(message, header, body);
+          expect(response.queryFailure).to.be.undefined;
+        });
+
+        it('does not set shardConfigStale', function () {
+          const response = new Response(message, header, body);
+          expect(response.shardConfigStale).to.be.undefined;
+        });
+
+        it('does not set awaitCapable', function () {
+          const response = new Response(message, header, body);
+          expect(response.awaitCapable).to.be.undefined;
         });
       });
     });
