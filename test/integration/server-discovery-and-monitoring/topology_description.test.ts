b48076349476abc7479597a90a4420a08ae88493
Bailey Pearson <bailey.pearson@mongodb.com>
2022-04-19 13:16:32 -0400

    test: add integration test for localThresholdMS

--- test/integration/server-discovery-and-monitoring/topology_description.test.ts
+++ test/integration/server-discovery-and-monitoring/topology_description.test.ts
@@ -12,14 +12,14 @@ describe('TopologyDescription (integration tests)', function () {
 
   context('options', function () {
     context('localThresholdMS', function () {
-      it('should default to 15ms', async function () {
+      it('should be true', async function () {
         const options: MongoClientOptions = {};
         client = await this.configuration.newClient(options).connect();
         const topologyDescription = getTopology(client).description;
         expect(topologyDescription).to.have.ownProperty('localThresholdMS').to.equal(15);
       });
 
-      it('should be set to the localThresholdMS option when it is passed in', async function () {
+      it('should be false', async function () {
         const options: MongoClientOptions = {
           localThresholdMS: 30
         };
