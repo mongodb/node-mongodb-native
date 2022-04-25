0cf8f9658942f83dbfd284d3681bd2770e1f207b
Neal Beeken <neal.beeken@mongodb.com>
2022-04-25 15:36:49 -0400

    fixup fixture check

--- test/tools/runner/hooks/resource_check.fixture.ts
+++ test/tools/runner/hooks/resource_check.fixture.ts
@@ -1,11 +1,20 @@
-const { expect } = require('chai');
+import { expect } from 'chai';
 
 let startingMemoryUsage;
 
-const compareString = (a, b) => String.prototype.localeCompare.call(String(a), String(b));
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Process {
+      _getActiveHandles(): ReadonlyArray<{ fd?: number }>;
+      _getActiveRequests(): unknown[];
+    }
+  }
+}
+
 const sorted = array => {
   const copy = Array.from(array);
-  copy.sort((a, b) => (typeof a === 'number' ? a - b : compareString(a, b)));
+  copy.sort((a, b) => Number(a) - Number(b));
   return copy;
 };
 
@@ -13,7 +22,7 @@ async function mochaGlobalSetup() {
   const activeHandles = process._getActiveHandles();
   const activeRequests = process._getActiveRequests();
 
-  expect(sorted(activeHandles.map(({ fd }) => fd))).to.deep.equal([1, 2]);
+  expect(sorted(activeHandles.map(({ fd = null }) => fd))).to.deep.equal([1, 2]);
   expect(activeRequests).to.have.a.lengthOf(0);
 
   startingMemoryUsage = process.memoryUsage();
@@ -25,7 +34,7 @@ async function mochaGlobalTeardown() {
   const activeRequests = process._getActiveRequests();
 
   try {
-    expect(activeHandles).to.have.a.lengthOf(2); // should on
+    expect(sorted(activeHandles.map(({ fd }) => fd))).to.deep.equal([1, 2]);
     expect(activeRequests).to.have.a.lengthOf(0);
     // Very generous check to double memory usage by the end of testing
     // should catch wildly unbounded allocations only
