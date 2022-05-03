29b4478397ff8f1c4bb8375bfb0824ace09f1445
Le Roux Bodenstein <lerouxb@gmail.com>
2022-05-03 13:38:35 +0100

    Support clustered collections

--- src/operations/create_collection.ts
+++ src/operations/create_collection.ts
@@ -41,6 +41,17 @@ export interface TimeSeriesCollectionOptions extends Document {
   granularity?: 'seconds' | 'minutes' | 'hours' | string;
 }
 
+/** @public
+ * Configuration options for clustered collections
+ * @see https://www.mongodb.com/docs/v5.3/core/clustered-collections/
+ */
+export interface ClusteredCollectionOptions extends Document {
+  name?: string;
+  key?: Document;
+  unique?: boolean;
+  v?: string;
+}
+
 /** @public */
 export interface CreateCollectionOptions extends CommandOperationOptions {
   /** Returns an error if the collection does not exist */
@@ -73,7 +84,9 @@ export interface CreateCollectionOptions extends CommandOperationOptions {
   pkFactory?: PkFactory;
   /** A document specifying configuration options for timeseries collections. */
   timeseries?: TimeSeriesCollectionOptions;
-  /** The number of seconds after which a document in a timeseries collection expires. */
+  /** A document specifying configuration options for clustered collections. */
+  clusteredIndex?: ClusteredCollectionOptions;
+  /** The number of seconds after which a document in a timeseries or clustered collection expires. */
   expireAfterSeconds?: number;
 }
 
