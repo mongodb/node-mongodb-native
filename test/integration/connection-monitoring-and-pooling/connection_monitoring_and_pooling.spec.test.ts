150b09fa519eaeae5199d9680d9348dcb3d226fb
Daria Pardue <daria.pardue@mongodb.com>
2022-05-06 18:11:14 -0400

    test: too many beforeEach blocks

--- test/integration/connection-monitoring-and-pooling/connection_monitoring_and_pooling.spec.test.ts
+++ test/integration/connection-monitoring-and-pooling/connection_monitoring_and_pooling.spec.test.ts
@@ -7,74 +7,77 @@ import { isAnyRequirementSatisfied } from '../../tools/unified-spec-runner/unifi
 describe.only('Connection Monitoring and Pooling Spec Tests (Integration)', function () {
   const suites: CmapTest[] = loadSpecTests('connection-monitoring-and-pooling');
 
-  for (const test of suites /*.filter(test => test.style === 'integration')*/) {
-    let hostAddress: HostAddress, threadContext: ThreadContext, client: MongoClient;
+  for (const test of suites.filter(test => {
+    // return true;
+    // return test.description === 'must throw error if checkOut is called on a closed pool';
+    return test.style === 'unit';
+  })) {
+    describe(test.description, function () {
+      let hostAddress: HostAddress, threadContext: ThreadContext, client: MongoClient;
 
-    beforeEach(async function () {
-      let utilClient: MongoClient;
-      if (this.configuration.isLoadBalanced) {
-        // The util client can always point at the single mongos LB frontend.
-        console.log('TEST DEBUG: Load balanced', this.configuration.singleMongosLoadBalancerUri);
-        utilClient = this.configuration.newClient(this.configuration.singleMongosLoadBalancerUri);
-      } else {
-        console.log('TEST DEBUG: Not load balanced');
-        utilClient = this.configuration.newClient();
-      }
-
-      await utilClient.connect();
-
-      const allRequirements = test.runOn || [];
-
-      const someRequirementMet =
-        !allRequirements.length ||
-        (await isAnyRequirementSatisfied(this.currentTest.ctx, allRequirements, utilClient));
-
-      if (!someRequirementMet) {
-        await utilClient.close();
-        this.skip();
-        // NOTE: the rest of the code below won't execute after the skip is invoked
-      }
-
-      try {
-        const serverMap = utilClient.topology.s.description.servers;
-        const hosts = shuffle(serverMap.keys());
-        const selectedHostUri = hosts[0];
-        hostAddress = serverMap.get(selectedHostUri).hostAddress;
-        threadContext = new ThreadContext(
-          hostAddress,
-          this.configuration.isLoadBalanced ? { loadBalanced: true } : {}
-        );
-
-        if (test.failPoint) {
-          client = this.configuration.newClient(
-            `mongodb://${hostAddress}/${
-              this.configuration.isLoadBalanced ? '?loadBalanced=true' : '?directConnection=true'
-            }`
-          );
-          console.log('TEST DEBUG: failpoint', hostAddress);
-          await client.connect();
-          await client.db('admin').command(test.failPoint);
+      beforeEach(async function () {
+        let utilClient: MongoClient;
+        if (this.configuration.isLoadBalanced) {
+          // The util client can always point at the single mongos LB frontend.
+          utilClient = this.configuration.newClient(this.configuration.singleMongosLoadBalancerUri);
+        } else {
+          utilClient = this.configuration.newClient();
         }
-      } finally {
-        await utilClient.close();
-      }
-    });
 
-    afterEach(async function () {
-      await threadContext?.tearDown();
-      if (!client) {
-        return;
-      }
-      if (test.failPoint) {
-        await client
-          .db('admin')
-          .command({ configureFailPoint: test.failPoint.configureFailPoint, mode: 'off' });
-      }
-      await client.close();
-    });
+        await utilClient.connect();
 
-    it(test.description, async function () {
-      await runCmapTest(test, threadContext);
+        const allRequirements = test.runOn || [];
+
+        const someRequirementMet =
+          !allRequirements.length ||
+          (await isAnyRequirementSatisfied(this.currentTest.ctx, allRequirements, utilClient));
+
+        if (!someRequirementMet) {
+          await utilClient.close();
+          this.skip();
+          // NOTE: the rest of the code below won't execute after the skip is invoked
+        }
+
+        try {
+          const serverMap = utilClient.topology.s.description.servers;
+          const hosts = shuffle(serverMap.keys());
+          const selectedHostUri = hosts[0];
+          hostAddress = serverMap.get(selectedHostUri).hostAddress;
+          threadContext = new ThreadContext(
+            hostAddress,
+            this.configuration.isLoadBalanced ? { loadBalanced: true } : {}
+          );
+
+          if (test.failPoint) {
+            client = this.configuration.newClient(
+              `mongodb://${hostAddress}/${
+                this.configuration.isLoadBalanced ? '?loadBalanced=true' : '?directConnection=true'
+              }`
+            );
+            await client.connect();
+            await client.db('admin').command(test.failPoint);
+          }
+        } finally {
+          await utilClient.close();
+        }
+      });
+
+      afterEach(async function () {
+        await threadContext?.tearDown();
+        if (!client) {
+          return;
+        }
+        if (test.failPoint) {
+          await client
+            .db('admin')
+            .command({ configureFailPoint: test.failPoint.configureFailPoint, mode: 'off' });
+        }
+        await client.close();
+      });
+
+      it('should pass', async function () {
+        await runCmapTest(test, threadContext);
+      });
     });
   }
 });
