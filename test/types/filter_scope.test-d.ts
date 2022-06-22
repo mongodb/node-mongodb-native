ad9a77df7e1f2c384ada4e38030cd07b116c443f
Bailey Pearson <bailey.pearson@mongodb.com>
2022-06-22 15:58:43 -0400

    chore: reformatting

--- test/types/filter_scope.test-d.ts
+++ test/types/filter_scope.test-d.ts
@@ -17,11 +17,17 @@ interface User {
   optionalKey2?: string;
 }
 
-// Functionality 1
-// The Filter type should provide type strictness by default on keys explicitly included on the
-// collection schema.  Users who do not want type strictness should have multiple options to work
-// around this (casting as any, using Collection<Document>, using Filter<Document> or not
-// using Typescript).
+/*
+ * Functionality 1
+ *
+ * Breaking: Yes
+ * Currently Supported: No
+ *
+ * The Filter type should provide type strictness by default on keys explicitly included on the
+ * collection schema.  Users who do not want type strictness should have multiple options to work
+ * around this (casting as any, using Collection<Document>, using Filter<Document> or not
+ * using Typescript).
+ */
 expectAssignable<Filter<User>>({ name: 'bailey' }); // valid
 expectError<Filter<User>>({ unknownProperty: 3 });
 
@@ -29,7 +35,15 @@ expectError<Filter<User>>({ unknownProperty: 3 });
 expectAssignable<Filter<User>>({ unknownProperty: 3 } as any);
 expectAssignable<Filter<User>>({ unknownProperty: 3 } as Filter<Document>);
 
-// Functionality 2
+/*
+ * Functionality 2
+ *
+ * Breaking: No
+ * Currently Supported: Partially
+ *
+ * Type strictness is enforced for recursive and circular types to the best of Typescripts
+ * abilities.
+ */
 interface RecursiveType {
   name: string;
   recur?: RecursiveType;
@@ -55,32 +69,52 @@ expectAssignable<Filter<RecursiveType>>({ friend: { name: 'bailey' } });
 expectError<Filter<RecursiveType>>({ name: 42 });
 expectError<Filter<RecursiveType>>({ friend: { name: 42 } });
 
-// Functionality 3
-// The Filter type should provide type exactness for queries that use dot notation.
-// The Node driver currently supports type exactness for queries that use dot notation.
+/*
+ * Functionality 3
+ *
+ * Breaking: No
+ *
+ * The Filter type should provide type exactness for queries that use dot notation.
+ */
 expectAssignable<Filter<User>>({ 'metadata.id': 'unique id' });
 expectError<Filter<User>>({ 'metadata.unknownProperty': 'error' });
 expectError<Filter<User>>({ 'metadata.id': 3 });
 
-// Functionality 4
-// The Filter type should provide type checking on the keys for known $-prefixed query
-// operators and provides type checking on some associated properties.  This is a best effort
-// attempt - some query operators allow aggregation expressions or pipelines that would be too
-// complex to reasonably type.
+/*
+ * Functionality 4
+ *
+ * Breaking: No
+ * Currently Supported: Yes
+ *
+ * The Filter type should provide type checking on the keys for known $-prefixed query
+ * operators and provides type checking on some associated properties.
+ */
 expectAssignable<Filter<User>>({ $and: [{ name: 'bailey' }] });
 expectError<Filter<User>>({ $unknownOperator: 'bailey' });
 
-// Functionality 5
-// The Filter type should be strict by default.  This would prevent automatic forward compatibility
-// for any future root filter operators.  However, the Filter type should have a strict mode
-// toggle that allows users to disable the strictness in favor of forward compatibility.
+/*
+ * Functionality 5
+ *
+ * Breaking: Yes
+ * Currently Supported: No
+ *
+ * The Filter type should be strict by default.  This would prevent automatic forward compatibility
+ * for any future root filter operators.  However, the Filter type should have a strict mode
+ * toggle that allows users to disable the strictness in favor of forward compatibility.
+ */
 
 // NOTE: the syntax here is a potential solution to provide a toggle but need not be the final solution
 expectAssignable<Filter<User, 'not strict'>>({ $unknownOperator: 'bailey' });
 
-// Functionality 6
-// The Filter type should permit users to specify $-prefixed keys in their schema without
-// flagging them as invalid queries (see $-prefixed keys in schema below).
+/*
+ * Functionality 6
+ *
+ * Breaking: No
+ * Currently Supported: Yes
+ *
+ * The Filter type should permit users to specify $-prefixed keys in their schema without
+ * flagging them as invalid queries (see $-prefixed keys in schema below).
+ */
 interface SchemaWithDollar {
   $name: string;
 }
@@ -88,10 +122,16 @@ interface SchemaWithDollar {
 expectAssignable<Filter<SchemaWithDollar>>({ $name: 'bailey' });
 expectError<Filter<SchemaWithDollar>>({ $name: 23 });
 
-// Functionality 7
-// The Filter type should be ergonomic to use.  This means that Filters can be constructed
-// easily using two common object construction techniques in Javascript: programatically adding
-// fields and using an object literal
+/*
+ * Functionality 7
+ *
+ * Breaking: No
+ * Currently Supported: Partially
+ *
+ * The Filter type should be ergonomic to use.  This means that Filters can be constructed
+ * easily using two common object construction techniques in Javascript: programatically adding
+ * fields and using an object literal
+ */
 
 // object literal support
 const filter: Filter<User> = {
@@ -99,6 +139,7 @@ const filter: Filter<User> = {
   age: 24
 };
 
+// buildable support
 function build(user: User): Filter<User> {
   const filter: Filter<User> = {};
 
@@ -115,12 +156,18 @@ function build(user: User): Filter<User> {
   return filter;
 }
 
-// Functionality 8
-// Numeric types should enforce exact typing for equality queries, but allow any numeric type for
-// range based queries. the Javascript 'number' type should be assignable to all types, purely
-// for convenience while users are building filters and to prevent type errors in scenarios when
-// users are importing filters in from other places that don't have typescript support
-// (Compass for example)
+/*
+ * Functionality 8
+ *
+ * Breaking: Yes
+ * Currently Supported: No
+ *
+ * Numeric types should enforce exact typing for equality queries, but allow any numeric type for
+ * range based queries. the Javascript 'number' type should be assignable to all types, purely
+ * for convenience while users are building filters and to prevent type errors in scenarios when
+ * users are importing filters in from other places that don't have typescript support
+ * (Compass for example)
+ */
 interface NumericSchema {
   age: number;
   reallyOldAge: Decimal128;
