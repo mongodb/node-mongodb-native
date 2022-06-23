c6abd0f0f1598811fc9885718cef90828a75de8b
Durran Jordan <durran@gmail.com>
2022-06-23 16:27:46 +0200

    test(NODE-4322): unskip dirty sessions test

--- test/integration/sessions/sessions.spec.test.ts
+++ test/integration/sessions/sessions.spec.test.ts
@@ -4,7 +4,6 @@ import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 describe('Sessions spec tests', function () {
   runUnifiedSuite(loadSpecTests('sessions'), [
     // TODO(NODE-3951): fix broken dirty sessions spec tests
-    'Dirty explicit session is discarded (insert)',
     'Dirty explicit session is discarded (findAndModify)'
   ]);
 });
