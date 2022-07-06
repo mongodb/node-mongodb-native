a7e0bc19777bf56a6359cc6d6b24dd7c88a54277
Bailey Pearson <bailey.pearson@mongodb.com>
2022-07-06 15:28:29 -0400

    refactor: move out libmongocrypt import into a utility function

--- src/utils.ts
+++ src/utils.ts
@@ -1407,3 +1407,23 @@ export function commandSupportsReadConcern(command: Document, options?: Document
 
   return false;
 }
+
+/**
+ * A utility function to get the instance of mongodb-client-encryption, if it exists.
+ *
+ * @throws MongoMissingDependencyError if mongodb-client-encryption isn't installed.
+ * @returns
+ */
+export function getMongoDBClientEncryption() {
+  let mongodbClientEncryption;
+
+  // Note (NODE-4254): This is to get around the circular dependency between
+  // mongodb-client-encryption and the driver in the test scenarios.
+  if (process.env.MONGODB_CLIENT_ENCRYPTION_OVERRIDE) {
+    mongodbClientEncryption = require(process.env.MONGODB_CLIENT_ENCRYPTION_OVERRIDE);
+  } else {
+    mongodbClientEncryption = require('mongodb-client-encryption');
+  }
+
+  return mongodbClientEncryption;
+}
