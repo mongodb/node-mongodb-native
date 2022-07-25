35c2a68fb1a9602b3b57c4b9b645e5e5f8d1f847
Bailey Pearson <bailey.pearson@mongodb.com>
2022-07-25 13:08:53 -0400

    test: sync updated spec tests

--- test/integration/change-streams/change_streams.spec.test.ts
+++ test/integration/change-streams/change_streams.spec.test.ts
@@ -1,15 +1,8 @@
 import * as path from 'path';
-import { gte } from 'semver';
 
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
 describe('Change Streams Spec - Unified', function () {
-  runUnifiedSuite(
-    loadSpecTests(path.join('change-streams', 'unified')),
-    ({ description }, { version }) =>
-      description === 'change stream resumes after StaleShardVersion' && gte(version, '6.0.0')
-        ? 'TODO(NODE-4434): fix StaleShardVersion resumability tests'
-        : false
-  );
+  runUnifiedSuite(loadSpecTests(path.join('change-streams', 'unified')));
 });
