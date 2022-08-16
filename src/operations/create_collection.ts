5644d3c9962581cf1a925c2d085585fe20ba9eb2
Daria Pardue <daria.pardue@mongodb.com>
2022-08-16 18:30:33 -0400

    docs(NODE-4230): update clustered collection reference link (#3364)

--- src/operations/create_collection.ts
+++ src/operations/create_collection.ts
@@ -44,8 +44,7 @@ export interface TimeSeriesCollectionOptions extends Document {
 
 /** @public
  * Configuration options for clustered collections
- * TODO: NODE-4230 replace with normal manual link once it is on there.
- * @see https://www.mongodb.com/docs/v5.3/core/clustered-collections/
+ * @see https://www.mongodb.com/docs/manual/core/clustered-collections/
  */
 export interface ClusteredCollectionOptions extends Document {
   name?: string;
