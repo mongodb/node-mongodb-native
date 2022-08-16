6425c7ad3fb9b4244ad3cb3aff504df7ab78013a
Durran Jordan <durran@gmail.com>
2022-08-16 16:48:53 -0400

    test(NODE-4333): add unit test to check dependencies (#3361)

---
+++ test/unit/dependency.test.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import { dependencies } from '../../package.json';
+
+const EXPECTED_DEPENDENCIES = ['bson', 'denque', 'mongodb-connection-string-url', 'socks'];
+
+describe('package.json', function () {
+  describe('dependencies', function () {
+    it('only contains the expected dependencies', function () {
+      expect(Object.getOwnPropertyNames(dependencies)).to.deep.equal(EXPECTED_DEPENDENCIES);
+    });
+  });
+});
