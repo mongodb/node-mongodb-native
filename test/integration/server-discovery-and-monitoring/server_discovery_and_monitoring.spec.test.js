e0138ab045e477aff8176fcb48ad2c0c6da338a1
Daria Pardue <daria.pardue@mongodb.com>
2022-08-22 17:57:55 -0400

    test: run unified sdam tests

--- test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.js
+++ test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.js
@@ -1,80 +1,31 @@
 'use strict';
-const { TestRunnerContext, generateTopologyTests } = require('../../tools/spec-runner');
 const { loadSpecTests } = require('../../spec');
+const { runUnifiedSuite } = require('../../tools/unified-spec-runner/runner');
+const path = require('path');
 
-class SDAMRunnerContext extends TestRunnerContext {
-  constructor() {
-    super();
+const filter = ({ description }) => {
+  // return description !== 'Concurrent shutdown error on insert' ? 'skip' : false;
 
-    this.currentPrimary = null;
+  const isAuthEnabled = process.env.AUTH === 'auth';
+  switch (description) {
+    case 'Reset server and pool after AuthenticationFailure error':
+    case 'Reset server and pool after misc command error':
+    case 'Reset server and pool after network error during authentication':
+    case 'Reset server and pool after network timeout error during authentication':
+    case 'Reset server and pool after shutdown error during authentication':
+      // Some tests are failing when setting a failCommand when auth is enabled
+      // and time out waiting for the PoolCleared event
+      return isAuthEnabled ? 'TODO(NODE-3891): fix tests broken when AUTH enabled' : false;
+    case 'Network error on Monitor check':
+    case 'Network timeout on Monitor handshake':
+    case 'Network timeout on Monitor check':
+    case 'Driver extends timeout while streaming':
+      return 'TODO(NODE-4573): fix socket leaks';
+    default:
+      return false;
   }
+};
 
-  configureFailPoint(args) {
-    return this.enableFailPoint(args.failPoint);
-  }
-
-  recordPrimary(client) {
-    const servers = client.topology.description.servers;
-    const primary = Array.from(servers.values()).filter(sd => sd.type === 'RSPrimary')[0];
-    this.currentPrimary = primary.address;
-  }
-
-  waitForPrimaryChange(client) {
-    const currentPrimary = this.currentPrimary;
-
-    return new Promise(resolve => {
-      function eventHandler(event) {
-        if (
-          event.newDescription.type === 'RSPrimary' &&
-          event.newDescription.address !== currentPrimary
-        ) {
-          resolve();
-          client.removeListener('serverDescriptionChanged', eventHandler);
-        }
-      }
-
-      client.on('serverDescriptionChanged', eventHandler);
-    });
-  }
-}
-
-// 'TODO: NODE-3891 - fix tests broken when AUTH enabled'
-// Some tests are failing when setting a failCommand when auth is enabled.
-const isAuthEnabled = process.env.AUTH === 'auth';
-const failpointTests = [
-  'Reset server and pool after AuthenticationFailure error',
-  'Reset server and pool after misc command error',
-  'Reset server and pool after network error during authentication',
-  'Reset server and pool after network timeout error during authentication',
-  'Reset server and pool after shutdown error during authentication'
-];
-const skippedTests = [...(isAuthEnabled ? failpointTests : []), 'Network error on Monitor check'];
-
-function sdamDisabledTestFilter(test) {
-  const { description } = test;
-  return !skippedTests.includes(description);
-}
-
-describe('SDAM', function () {
-  describe('integration spec tests', function () {
-    const testContext = new SDAMRunnerContext();
-    const testSuites = loadSpecTests('server-discovery-and-monitoring/integration');
-
-    beforeEach(async function () {
-      if (this.configuration.isLoadBalanced) {
-        this.currentTest.skipReason = 'Cannot run in a loadBalanced environment';
-        this.skip();
-      }
-    });
-
-    beforeEach(async function () {
-      await testContext.setup(this.configuration);
-    });
-
-    afterEach(async () => {
-      await testContext.teardown();
-    });
-
-    generateTopologyTests(testSuites, testContext, sdamDisabledTestFilter);
-  });
+describe('SDAM Unified Tests', function () {
+  runUnifiedSuite(loadSpecTests(path.join('server-discovery-and-monitoring', 'unified')), filter);
 });
