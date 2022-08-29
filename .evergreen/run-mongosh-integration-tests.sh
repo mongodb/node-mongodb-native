3b6fc97cf624e836a0f166878509b6ae746cfa3f
Anna Henningsen <anna.henningsen@mongodb.com>
2022-08-29 17:21:26 +0200

    chore(NODE-4593): fix mongosh integration tests

--- .evergreen/run-mongosh-integration-tests.sh
+++ .evergreen/run-mongosh-integration-tests.sh
@@ -4,7 +4,14 @@ set -o xtrace   # Write all commands first to stderr
 
 source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
 
+npm cache clear --force || true
+npm i -g npm@8.x || true
+
 npm pack | tee npm-pack.log
+
+npm cache clear --force || true
+npm i -g npm@8.x || true
+
 export TARBALL_FILENAME="$(tail -n1 npm-pack.log)"
 cd /tmp
 git clone --depth=10 https://github.com/mongodb-js/mongosh.git
