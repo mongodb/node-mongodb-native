b5f5d2a122113c254159594cd32a9cd248b7461f
Durran Jordan <durran@gmail.com>
2022-08-31 17:17:32 +0200

    test(NODE-4160): pass options to client

--- test/integration/node-specific/examples/aws_handler.js
+++ test/integration/node-specific/examples/aws_handler.js
@@ -2,17 +2,17 @@
 const { MongoClient } = require('mongodb');
 
 // Get the URI for the cluster then set AWS_ACCESS_KEY_ID as the username in the
-// URI and AWS_SECRET_ACCESS_KEY as the password, then set the appropriate
-// options.
-const connectionString = new URL(process.env.MONGODB_URI);
-connectionString.username = process.env.AWS_ACCESS_KEY_ID;
-connectionString.password = encodeURIComponent(process.env.AWS_SECRET_ACCESS_KEY);
-connectionString.searchParams.set('authSource', '$external');
-connectionString.searchParams.set('authMechanism', 'MONGODB-AWS');
-
-// MongoClient now auto-connects so no need to store the connect()
+// URI and AWS_SECRET_ACCESS_KEY as the password, then set the appropriate auth
+// options. Note that MongoClient now auto-connects so no need to store the connect()
 // promise anywhere and reference it.
-const client = new MongoClient(connectionString.toString());
+const client = new MongoClient(process.env.MONGODB_URI, {
+  auth: {
+    username: process.env.AWS_ACCESS_KEY_ID,
+    password: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  authSource: '$external',
+  authMechanism: 'MONGODB-AWS'
+});
 
 module.exports.handler = async function () {
   const databases = await client.db('admin').command({ listDatabases: 1 });
