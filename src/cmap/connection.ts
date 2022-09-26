cd4acfe7fdeaabde8f4540957f7d937dc3f9f442
Bailey Pearson <bailey.pearson@mongodb.com>
2022-09-26 14:00:12 -0400

    chore: add back wire version check to connection

--- src/cmap/connection.ts
+++ src/cmap/connection.ts
@@ -645,7 +645,7 @@ function supportsOpMsg(conn: Connection) {
     return false;
   }
 
-  return !description.__nodejs_mock_server__;
+  return maxWireVersion(conn) >= 6 && !description.__nodejs_mock_server__;
 }
 
 function streamIdentifier(stream: Stream, options: ConnectionOptions): string {
