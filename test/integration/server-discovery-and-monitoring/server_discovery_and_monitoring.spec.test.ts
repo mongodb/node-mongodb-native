52f8e205a1319cad1bd7525730ef8278ba8d84ab
Bailey Pearson <bailey.pearson@mongodb.com>
2022-10-03 12:07:21 -0400

    unskip ALL tests

--- test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
+++ test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
@@ -1,37 +1,8 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Socket } from 'net';
 import * as path from 'path';
 
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
-import { sleep } from '../../tools/utils';
 
 describe('SDAM Unified Tests', function () {
-  afterEach(async function () {
-    if (this.currentTest!.pending) {
-      return;
-    }
-    // TODO(NODE-4573): fix socket leaks
-    const LEAKY_TESTS = [
-      'Command error on Monitor handshake',
-      'Network error on Monitor handshake',
-      'Network timeout on Monitor handshake'
-    ];
-
-    await sleep(250);
-    const sockArray = (process as any)._getActiveHandles().filter(handle => {
-      // Stdio are instanceof Socket so look for fd to be null
-      return handle.fd == null && handle instanceof Socket && handle.destroyed !== true;
-    });
-    if (!sockArray.length) {
-      return;
-    }
-    for (const sock of sockArray) {
-      sock.destroy();
-    }
-    if (!LEAKY_TESTS.some(test => test === this.currentTest!.title)) {
-      this.test!.error(new Error(`Test failed to clean up ${sockArray.length} socket(s)`));
-    }
-  });
   runUnifiedSuite(loadSpecTests(path.join('server-discovery-and-monitoring', 'unified')));
 });
