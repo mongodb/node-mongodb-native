977ecc2f32b2ea5678b68632e2c31735cf2ee1a5
Bailey Pearson <bailey.pearson@mongodb.com>
2022-11-21 11:46:31 -0500

    ci: skip ts-next tests

--- .evergreen/generate_evergreen_tasks.js
+++ .evergreen/generate_evergreen_tasks.js
@@ -446,19 +446,19 @@ SINGLETON_TASKS.push(
         { func: 'run lint checks' }
       ]
     },
-    {
-      name: 'run-typescript-next',
-      tags: ['run-typescript-next'],
-      commands: [
-        {
-          func: 'install dependencies',
-          vars: {
-            NODE_LTS_NAME: LOWEST_LTS
-          }
-        },
-        { func: 'run typescript next' }
-      ]
-    },
+    // {
+    //   name: 'run-typescript-next',
+    //   tags: ['run-typescript-next'],
+    //   commands: [
+    //     {
+    //       func: 'install dependencies',
+    //       vars: {
+    //         NODE_LTS_NAME: LOWEST_LTS
+    //       }
+    //     },
+    //     { func: 'run typescript next' }
+    //   ]
+    // },
     {
       name: 'run-typescript-current',
       tags: ['run-typescript-current'],
