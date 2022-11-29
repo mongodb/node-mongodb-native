c875ac8903334a3ac761b026fbd47358b543909a
Daria Pardue <daria.pardue@mongodb.com>
2022-11-29 16:58:01 -0500

    fix: disable causal consistency on implicit sessions

--- src/sessions.ts
+++ src/sessions.ts
@@ -169,8 +169,10 @@ export class ClientSession extends TypedEventEmitter<ClientSessionEvents> {
     this[kServerSession] = this.explicit ? this.sessionPool.acquire() : null;
     this[kTxnNumberIncrement] = 0;
 
+    const canEnableCausalConsistency = this.explicit && options.snapshot !== true;
     this.supports = {
-      causalConsistency: options.snapshot !== true && options.causalConsistency !== false
+      // if we can enable causal consistency, do so by default
+      causalConsistency: canEnableCausalConsistency && options.causalConsistency !== false
     };
 
     this.clusterTime = options.initialClusterTime;
