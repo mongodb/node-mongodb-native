cfc9347c6e0ac732993dd60b0051816255c58c62
Bailey Pearson <bailey.pearson@mongodb.com>
2022-11-30 12:21:29 -0500

    fix: allow clones that use https

--- etc/check-remote.sh
+++ etc/check-remote.sh
@@ -1,21 +1,6 @@
 #! /bin/bash
 
-echo "full list of remotes"
-git remote -v
-
-printf "\n\n"
-
-echo "push origin remote"
-git remote get-url --push origin
-
-printf "\n\n"
-
-echo "looking for 'github.com:mongodb' in uri"
-git remote get-url --push origin | grep -v "github.com:mongodb"
-
-printf "\n\n"
-
-if git remote get-url --push origin | grep -qv "github.com:mongodb"; then
+if git remote get-url --push origin | grep -qv "github.com:mongodb\|github.com/mongodb"; then
     echo "git remote does not match node-mongodb-native.  are you working off of a fork?"
     exit 1
 fi
