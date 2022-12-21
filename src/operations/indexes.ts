f84bd8279605fee5fc311e8f77207d83c95b55e8
Neal Beeken <neal.beeken@mongodb.com>
2022-12-21 12:16:04 -0500

    fix: lint

--- src/operations/indexes.ts
+++ src/operations/indexes.ts
@@ -1,12 +1,7 @@
 import type { Document } from '../bson';
 import type { Collection } from '../collection';
 import type { Db } from '../db';
-import {
-  MongoCompatibilityError,
-  MONGODB_ERROR_CODES,
-  MongoError,
-  MongoServerError
-} from '../error';
+import { MongoCompatibilityError, MONGODB_ERROR_CODES, MongoError } from '../error';
 import type { OneOrMore } from '../mongo_types';
 import { ReadPreference } from '../read_preference';
 import type { Server } from '../sdam/server';
