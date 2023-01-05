8fa7fa0c710ead299b666ca09c357ca3d864d8cd
Neal Beeken <neal.beeken@mongodb.com>
2023-01-05 13:45:14 -0500

    try bson v5 with mongosh

--- .evergreen/run-mongosh-integration-tests.sh
+++ .evergreen/run-mongosh-integration-tests.sh
@@ -12,9 +12,19 @@ npm pack | tee npm-pack.log
 npm cache clear --force || true
 npm i -g npm@8.x || true
 
+rm -rf "js-bson"
+git clone --depth=1 https://github.com/mongodb/js-bson.git -b "NODE-4892-version-tag"
+pushd "js-bson"
+npm install
+npm pack | tee npm-pack.log
+BSON_TARBALL_FILENAME="$(tail -n1 npm-pack.log)"
+mv "$BSON_TARBALL_FILENAME" ..
+popd
+
 export TARBALL_FILENAME="$(tail -n1 npm-pack.log)"
-cd /tmp
-git clone --depth=10 https://github.com/mongodb-js/mongosh.git
+# cd /tmp
+git clone --depth=10 https://github.com/nbbeeken/mongosh.git -b "test-bson-v5"
 cd mongosh
-export REPLACE_PACKAGE="mongodb:${PROJECT_DIRECTORY}/${TARBALL_FILENAME}"
+export REPLACE_PACKAGE="mongodb:${PROJECT_DIRECTORY}/${TARBALL_FILENAME},bson:${PROJECT_DIRECTORY}/${BSON_TARBALL_FILENAME}"
+
 npm run test-nodedriver
