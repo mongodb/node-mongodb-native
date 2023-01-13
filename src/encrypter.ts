e526327df080c07227e75a8c62dae9b55f89444d
Bailey Pearson <bailey.pearson@mongodb.com>
2023-01-13 09:48:23 -0500

    fix lint

--- src/encrypter.ts
+++ src/encrypter.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-import { deserialize, serialize } from './bson';
 import { MONGO_CLIENT_EVENTS } from './constants';
 import type { AutoEncrypter, AutoEncryptionOptions } from './deps';
 import { MongoInvalidArgumentError, MongoMissingDependencyError } from './error';
