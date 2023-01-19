627f867eaeaf9b7a4b8e417633f398b5cd2c96d6
Neal Beeken <neal.beeken@mongodb.com>
2023-01-19 10:21:56 -0500

    rm function

--- test/unit/bson.test.js
+++ test/unit/bson.test.js
@@ -26,27 +26,23 @@ describe('When importing BSON', function () {
     bsonRegExp: true
   };
 
-  function testTypes() {
-    for (const [type, ctorArg] of types) {
-      it(`should correctly round trip ${type}`, function () {
-        const typeCtor = BSON[type];
-        expect(typeCtor).to.be.a('function');
-        const doc = { key: new typeCtor(ctorArg) };
-        const outputDoc = BSON.deserialize(BSON.serialize(doc), options);
-        expect(outputDoc).to.have.property('key').that.is.instanceOf(typeCtor);
-        expect(outputDoc).to.deep.equal(doc);
-      });
-    }
-
-    it('should correctly round trip Map', function () {
-      expect(BSON.Map).to.be.a('function');
-      const doc = { key: new BSON.Map([['2', 2]]) };
-      const outputDoc = BSON.deserialize(BSON.serialize(doc));
-      expect(outputDoc).to.have.nested.property('key.2', 2);
+  for (const [type, ctorArg] of types) {
+    it(`should correctly round trip ${type}`, function () {
+      const typeCtor = BSON[type];
+      expect(typeCtor).to.be.a('function');
+      const doc = { key: new typeCtor(ctorArg) };
+      const outputDoc = BSON.deserialize(BSON.serialize(doc), options);
+      expect(outputDoc).to.have.property('key').that.is.instanceOf(typeCtor);
+      expect(outputDoc).to.deep.equal(doc);
     });
   }
 
-  testTypes();
+  it('should correctly round trip Map', function () {
+    expect(BSON.Map).to.be.a('function');
+    const doc = { key: new BSON.Map([['2', 2]]) };
+    const outputDoc = BSON.deserialize(BSON.serialize(doc));
+    expect(outputDoc).to.have.nested.property('key.2', 2);
+  });
 });
 
 describe('MongoDB export', () => {
