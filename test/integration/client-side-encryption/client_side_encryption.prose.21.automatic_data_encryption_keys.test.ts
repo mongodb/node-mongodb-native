dc3b1960315d329a0a947676ec9d7cbc75cc2425
Neal Beeken <neal.beeken@mongodb.com>
2023-02-01 16:08:41 -0500

    update tests

--- test/integration/client-side-encryption/client_side_encryption.prose.21.automatic_data_encryption_keys.test.ts
+++ test/integration/client-side-encryption/client_side_encryption.prose.21.automatic_data_encryption_keys.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { inspect } from 'util';
 
 import { Collection, Db, MongoServerError } from '../../mongodb';
 import { installNodeDNSWorkaroundHooks } from '../../tools/runner/hooks/configuration';
@@ -19,7 +20,7 @@ const LOCAL_KEY = Buffer.from(
   'base64'
 );
 
-describe('21. Automatic Data Encryption Keys', metadata, () => {
+describe.only('21. Automatic Data Encryption Keys', metadata, () => {
   installNodeDNSWorkaroundHooks();
 
   let db: Db;
@@ -27,117 +28,133 @@ describe('21. Automatic Data Encryption Keys', metadata, () => {
   let client;
   let MongoCryptCreateEncryptedCollectionError;
 
-  beforeEach(async function () {
-    client = this.configuration.newClient();
-    const {
-      ClientEncryption,
-      MongoCryptCreateEncryptedCollectionError: MongoCryptCreateEncryptedCollectionErrorCtor
-    } = this.configuration.mongodbClientEncryption;
-    MongoCryptCreateEncryptedCollectionError = MongoCryptCreateEncryptedCollectionErrorCtor;
+  const runProseTestsFor = ({ masterKey }) => {
+    beforeEach(async function () {
+      client = this.configuration.newClient();
+      const {
+        ClientEncryption,
+        MongoCryptCreateEncryptedCollectionError: MongoCryptCreateEncryptedCollectionErrorCtor
+      } = this.configuration.mongodbClientEncryption;
+      MongoCryptCreateEncryptedCollectionError = MongoCryptCreateEncryptedCollectionErrorCtor;
 
-    clientEncryption = new ClientEncryption(client, {
-      keyVaultClient: client,
-      keyVaultNamespace: 'keyvault.datakeys',
-      kmsProviders: { local: { key: LOCAL_KEY } }
-    });
+      clientEncryption = new ClientEncryption(client, {
+        keyVaultClient: client,
+        keyVaultNamespace: 'keyvault.datakeys',
+        kmsProviders: { local: { key: LOCAL_KEY } }
+      });
 
-    if (typeof clientEncryption.createEncryptedCollection !== 'function') {
-      if (this.currentTest)
-        this.currentTest.skipReason =
-          'TODO I SHOULD NOT BE SKIPPED, INSTALL FLE VERSION WITH THIS API ' + '-'.repeat(400);
-      this.test?.skip();
-      return;
-    }
-
-    db = client.db('automatic_data_encryption_keys');
-    await db.dropDatabase().catch(() => null);
-  });
-
-  afterEach(async function () {
-    await db?.dropDatabase().catch(() => null);
-    await client?.close();
-  });
-
-  it('Case 1: Simple Creation and Validation', async () => {
-    const createCollectionOptions = {
-      encryptedFields: { fields: [{ path: 'ssn', bsonType: 'string', keyId: null }] }
-    };
-
-    const { collection } = await clientEncryption.createEncryptedCollection(db, 'testing1', {
-      provider: 'local',
-      createCollectionOptions,
-      createDataKeyOptions: null
-    });
-
-    expect(collection).to.be.instanceOf(Collection);
-    expect(collection.namespace).to.equal('automatic_data_encryption_keys.testing1');
-
-    const result = await collection.insertOne({ ssn: '123-45-6789' }).catch(error => error);
-    expect(result).to.be.instanceOf(MongoServerError);
-    expect(result).to.have.property('code', documentValidationFailureCode);
-  });
-
-  it('Case 2: Missing encryptedFields', async () => {
-    const createCollectionOptions = {};
-
-    const result = await clientEncryption
-      .createEncryptedCollection(db, 'testing1', {
-        provider: 'local',
-        createCollectionOptions,
-        createDataKeyOptions: null
-      })
-      .catch(error => error);
-
-    expect(result).to.be.instanceOf(TypeError);
-  });
-
-  it('Case 3: Invalid keyId', async () => {
-    const createCollectionOptions = {
-      encryptedFields: { fields: [{ path: 'ssn', bsonType: 'string', keyId: false }] }
-    };
-
-    const result = await clientEncryption
-      .createEncryptedCollection(db, 'testing1', {
-        provider: 'local',
-        createCollectionOptions,
-        createDataKeyOptions: null
-      })
-      .catch(error => error);
-
-    expect(result).to.be.instanceOf(MongoCryptCreateEncryptedCollectionError);
-    expect(result).nested.property('cause.code', typeMismatchCode);
-    // BSON field 'create.encryptedFields.fields.keyId' is the wrong type 'bool', expected type 'binData'
-    expect(result.cause.message)
-      .to.match(/bool/i)
-      .and.match(/binData/i)
-      .and.match(/keyId/i);
-  });
-
-  it('Case 4: Insert encrypted value', async () => {
-    const createCollectionOptions = {
-      encryptedFields: { fields: [{ path: 'ssn', bsonType: 'string', keyId: null }] }
-    };
-
-    const { collection, encryptedFields } = await clientEncryption.createEncryptedCollection(
-      db,
-      'testing1',
-      {
-        provider: 'local',
-        createCollectionOptions,
-        createDataKeyOptions: null
+      if (typeof clientEncryption.createEncryptedCollection !== 'function') {
+        if (this.currentTest)
+          this.currentTest.skipReason =
+            'TODO I SHOULD NOT BE SKIPPED, INSTALL FLE VERSION WITH THIS API ' + '-'.repeat(400);
+        this.test?.skip();
+        return;
       }
-    );
 
-    expect(collection).to.be.instanceOf(Collection);
-    expect(collection.namespace).to.equal('automatic_data_encryption_keys.testing1');
-
-    const ssn = clientEncryption.encrypt('123-45-6789', {
-      algorithm: 'Unindexed',
-      keyId: encryptedFields.fields[0].keyId
+      db = client.db('automatic_data_encryption_keys');
+      await db.dropDatabase().catch(() => null);
     });
 
-    const result = await collection.insertOne({ ssn }).catch(error => error);
-    expect(result).to.be.instanceOf(MongoServerError);
-    expect(result).to.have.property('code', documentValidationFailureCode);
-  });
+    afterEach(async function () {
+      await db?.dropDatabase().catch(() => null);
+      await client?.close();
+    });
+
+    it('Case 1: Simple Creation and Validation', async () => {
+      const createCollectionOptions = {
+        encryptedFields: { fields: [{ path: 'ssn', bsonType: 'string', keyId: null }] }
+      };
+
+      const { collection } = await clientEncryption.createEncryptedCollection(db, 'testing1', {
+        provider: 'local',
+        createCollectionOptions,
+        masterKey
+      });
+
+      expect(collection).to.be.instanceOf(Collection);
+      expect(collection.namespace).to.equal('automatic_data_encryption_keys.testing1');
+
+      const result = await collection.insertOne({ ssn: '123-45-6789' }).catch(error => error);
+      expect(result).to.be.instanceOf(MongoServerError);
+      expect(result).to.have.property('code', documentValidationFailureCode);
+    });
+
+    it('Case 2: Missing encryptedFields', async () => {
+      const createCollectionOptions = {};
+
+      const result = await clientEncryption
+        .createEncryptedCollection(db, 'testing1', {
+          provider: 'local',
+          createCollectionOptions,
+          masterKey
+        })
+        .catch(error => error);
+
+      expect(result).to.be.instanceOf(TypeError);
+    });
+
+    it('Case 3: Invalid keyId', async () => {
+      const createCollectionOptions = {
+        encryptedFields: { fields: [{ path: 'ssn', bsonType: 'string', keyId: false }] }
+      };
+
+      const result = await clientEncryption
+        .createEncryptedCollection(db, 'testing1', {
+          provider: 'local',
+          createCollectionOptions,
+          masterKey
+        })
+        .catch(error => error);
+
+      expect(result).to.be.instanceOf(MongoCryptCreateEncryptedCollectionError);
+      expect(result).nested.property('cause.code', typeMismatchCode);
+      // BSON field 'create.encryptedFields.fields.keyId' is the wrong type 'bool', expected type 'binData'
+      expect(result.cause.message)
+        .to.match(/bool/i)
+        .and.match(/binData/i)
+        .and.match(/keyId/i);
+    });
+
+    it('Case 4: Insert encrypted value', async () => {
+      const createCollectionOptions = {
+        encryptedFields: { fields: [{ path: 'ssn', bsonType: 'string', keyId: null }] }
+      };
+
+      const { collection, encryptedFields } = await clientEncryption.createEncryptedCollection(
+        db,
+        'testing1',
+        {
+          provider: 'local',
+          createCollectionOptions,
+          masterKey
+        }
+      );
+
+      expect(collection).to.be.instanceOf(Collection);
+      expect(collection.namespace).to.equal('automatic_data_encryption_keys.testing1');
+
+      const ssn = clientEncryption.encrypt('123-45-6789', {
+        algorithm: 'Unindexed',
+        keyId: encryptedFields.fields[0].keyId
+      });
+
+      const result = await collection.insertOne({ ssn }).catch(error => error);
+      expect(result).to.be.instanceOf(MongoServerError);
+      expect(result).to.have.property('code', documentValidationFailureCode);
+    });
+  };
+
+  for (const kmsProviderOptions of [
+    { masterKey: null },
+    {
+      masterKey: {
+        region: 'us-east-1',
+        key: 'arn:aws:kms:us-east-1:579766882180:key/89fcc2c4-08b0-4bd9-9f25-e30687b580d0'
+      }
+    }
+  ]) {
+    context(`provider options ${inspect(kmsProviderOptions)}`, () => {
+      runProseTestsFor(kmsProviderOptions);
+    });
+  }
 });
