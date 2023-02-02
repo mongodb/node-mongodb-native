7ede59fab642e97a3e0849e863691a1d4a8f67f3
Neal Beeken <neal.beeken@mongodb.com>
2023-02-02 14:32:52 -0500

    test(NODE-5028): assert MongoClients are garbage collectable

---
+++ test/integration/node-specific/resource_clean_up.test.ts
@@ -0,0 +1,108 @@
+import { fork } from 'node:child_process';
+import { on, once } from 'node:events';
+import { readFile, unlink, writeFile } from 'node:fs/promises';
+import * as path from 'node:path';
+
+import { expect } from 'chai';
+import { parseSnapshot } from 'v8-heapsnapshot';
+
+const HEAPSNAPSHOT_BEFORE = './before.heapsnapshot.json';
+const HEAPSNAPSHOT_AFTER = './after.heapsnapshot.json';
+
+const REPRO_SCRIPT = uri => `
+const { MongoClient } = require('${path.resolve(__dirname, '../../../lib')}');
+const process = require('node:process');
+const v8 = require('node:v8');
+
+async function run(i) {
+  const mongoClient = new MongoClient(
+    '${uri}',
+    { maxPoolSize: 3 }
+  );
+  await mongoClient.connect();
+  const db = mongoClient.db();
+  await Promise.all([
+    db.collections(),
+    db.collections(),
+    db.collections(),
+    db.collections(),
+    db.collections(),
+    db.collections(),
+    db.collections(),
+    db.collections()
+  ]);
+  await mongoClient.close();
+}
+
+async function main() {
+  // v8.writeHeapSnapshot('${HEAPSNAPSHOT_BEFORE}');
+  const startingMemoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+  process.send({ startingMemoryUsed });
+  for (let i = 0; i < 100; i++) {
+    await run(i);
+    if (global.gc) {
+      global.gc();
+    }
+  }
+  const endingMemoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+  process.send({ endingMemoryUsed });
+  v8.writeHeapSnapshot('${HEAPSNAPSHOT_AFTER}');
+}
+
+main()
+  .then(result => {
+    process.exit(0);
+  })
+  .catch(error => {
+    process.exit(1);
+  });
+
+`;
+
+const SCRIPT_NAME = './memScript.js';
+
+describe('Driver Resources', () => {
+  let startingMemoryUsed;
+  let endingMemoryUsed;
+
+  let heapAfter;
+
+  before('create leak reproduction script', async function () {
+    await writeFile(SCRIPT_NAME, REPRO_SCRIPT(this.configuration.url()), { encoding: 'utf8' });
+
+    const script = fork(SCRIPT_NAME, { execArgv: ['--expose-gc'] });
+    const messages = on(script, 'message');
+    const willClose = once(script, 'close');
+
+    const starting = await messages.next();
+    const ending = await messages.next();
+
+    startingMemoryUsed = starting.value[0].startingMemoryUsed;
+    endingMemoryUsed = ending.value[0].endingMemoryUsed;
+
+    // process exit
+    const [exitCode] = await willClose;
+    expect(exitCode).to.equal(0);
+
+    heapAfter = await readFile(HEAPSNAPSHOT_AFTER, { encoding: 'utf8' }).then(c => JSON.parse(c));
+  });
+
+  after('cleanup leak reproduction script', async function () {
+    await unlink(SCRIPT_NAME);
+    // await unlink(HEAPSNAPSHOT_BEFORE);
+    await unlink(HEAPSNAPSHOT_AFTER);
+  });
+
+  it('should all be cleaned up after close and garbage collectable', async () => {
+    // plus/minus 3 MB
+    expect(endingMemoryUsed).to.be.within(startingMemoryUsed - 3, startingMemoryUsed + 3);
+  });
+
+  it('should not have MongoClients residing in memory', async () => {
+    const heap = await parseSnapshot(heapAfter);
+    const clients = heap.nodes.filter(n => n.name === 'MongoClient' && n.type === 'object');
+    // lengthOf crashes chai b/c it tries to print out a gig
+    // Allow GC to miss a few
+    expect(clients.length).to.be.within(0, 2);
+  });
+});
