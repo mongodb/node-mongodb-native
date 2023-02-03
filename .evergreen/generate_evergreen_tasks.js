6a6dde1d97784416556070325238873b1e4cc038
Durran Jordan <durran@gmail.com>
2023-02-03 16:06:28 +0100

    test(NODE-4894): re-enable kerberos tests on 18 (#3527)

--- .evergreen/generate_evergreen_tasks.js
+++ .evergreen/generate_evergreen_tasks.js
@@ -688,9 +688,9 @@ for (const variant of BUILD_VARIANTS.filter(
   );
 }
 
-// TODO(NODE-4894): fix kerberos tests on Node18
+// TODO(NODE-5021): Drop support for Kerberos 1.x on in 6.0.0
 for (const variant of BUILD_VARIANTS.filter(
-  variant => variant.expansions && ['hydrogen', 'latest'].includes(variant.expansions.NODE_LTS_NAME)
+  variant => variant.expansions && ['latest'].includes(variant.expansions.NODE_LTS_NAME)
 )) {
   variant.tasks = variant.tasks.filter(
     name => !['test-auth-kerberos'].includes(name)
