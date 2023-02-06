793240a286cd53991c51a86fd77c2c12b911685e
Neal Beeken <neal.beeken@mongodb.com>
2023-02-06 12:20:19 -0500

    fix: bump to latest fle

--- .evergreen/generate_evergreen_tasks.js
+++ .evergreen/generate_evergreen_tasks.js
@@ -622,7 +622,7 @@ const oneOffFuncAsTasks = oneOffFuncs.map(oneOffFunc => ({
   ]
 }));
 
-const FLE_PINNED_COMMIT = 'f38c549ffd8a6c086e1705fac12943b3a7b1fac5'
+const FLE_PINNED_COMMIT = '77b51c00ab4ff58916dd39f55657e1ecc0af281c'
 
 for (const version of ['5.0', 'rapid', 'latest']) {
   for (const ref of [FLE_PINNED_COMMIT, 'master']) {
