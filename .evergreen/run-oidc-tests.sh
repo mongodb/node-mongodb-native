74a75f591591352f9d85f1ae454db453c2848c87
Durran Jordan <durran@gmail.com>
2023-02-08 00:21:46 +0100

    test(NODE-5038): change setup js

--- .evergreen/run-oidc-tests.sh
+++ .evergreen/run-oidc-tests.sh
@@ -1,3 +1,7 @@
-mongosh ${DRIVERS_TOOLS}/.evergreen/auth_oidc/setup_oidc.js
+#!/bin/bash
+
+set -o errexit  # Exit the script with error if any of the commands fail
+
+mongo $DRIVERS_TOOLS/.evergreen/auth_oidc/setup_oidc.js
 
 npm run check:oidc
