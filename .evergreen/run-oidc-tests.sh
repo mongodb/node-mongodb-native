045bbf2ce771af465fe97f297009ea9c7d311d6c
Durran Jordan <durran@gmail.com>
2023-02-08 18:47:57 +0100

    test(NODE-5038): install mongosh

--- .evergreen/run-oidc-tests.sh
+++ .evergreen/run-oidc-tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -o errexit  # Exit the script with error if any of the commands fail
+set -o xtrace   # Write all commands first to stderr
+
+source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
 
 npm run check:oidc
--- .evergreen/setup-oidc-roles.sh
+++ .evergreen/setup-oidc-roles.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 cd ${DRIVERS_TOOLS}/.evergreen/auth_oidc
 . ./activate_venv.sh
-mongo setup_oidc.js
+
+# Install mongosh
+curl https://github.com/mongodb-js/mongosh/releases/download/v1.6.2/mongodb-mongosh-shared-openssl3-1.6.2.x86_64.rpm --output mongosh.rpm
+yum -y install mongosh.rpm
+
+mongosh setup_oidc.js
