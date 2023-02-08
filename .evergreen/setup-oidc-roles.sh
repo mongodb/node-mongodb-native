b143222faf3de426e926b7ffb69f38c3f5c47a43
Durran Jordan <durran@gmail.com>
2023-02-08 19:57:40 +0100

    test(NODE-5038): update curl options

--- .evergreen/setup-oidc-roles.sh
+++ .evergreen/setup-oidc-roles.sh
@@ -3,7 +3,7 @@ cd ${DRIVERS_TOOLS}/.evergreen/auth_oidc
 . ./activate_venv.sh
 
 # Install mongosh
-curl https://github.com/mongodb-js/mongosh/releases/download/v1.6.2/mongodb-mongosh-shared-openssl3-1.6.2.x86_64.rpm --output mongosh.rpm
+curl -L https://github.com/mongodb-js/mongosh/releases/download/v1.6.2/mongodb-mongosh-shared-openssl3-1.6.2.x86_64.rpm -o mongosh.rpm
 
 ls -la
 
