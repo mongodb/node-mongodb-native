0a91153b537871b2c97f043a3897b0df7aeabe4e
Durran Jordan <durran@gmail.com>
2023-02-08 22:02:15 +0100

    test(NODE-5038): update scripts

--- .evergreen/bootstrap-oidc.sh
+++ .evergreen/bootstrap-oidc.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -o errexit  # Exit the script with error if any of the commands fail
+set -o xtrace   # Write all commands first to stderr
+
 cd ${DRIVERS_TOOLS}/.evergreen/auth_oidc
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
--- .evergreen/run-oidc-tests.sh
+++ .evergreen/run-oidc-tests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -o errexit  # Exit the script with error if any of the commands fail
-set -o xtrace   # Write all commands first to stderr
 
 source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
 
