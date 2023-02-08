d306bc8fc3902a4f6ba4c12886619efde86319fd
Durran Jordan <durran@gmail.com>
2023-02-08 21:09:18 +0100

    test(NODE-5038): export arn

--- .evergreen/bootstrap-oidc.sh
+++ .evergreen/bootstrap-oidc.sh
@@ -1,6 +1,7 @@
 cd ${DRIVERS_TOOLS}/.evergreen/auth_oidc
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_ROLE_ARN=${AWS_ROLE_ARN}
 export AWS_TOKEN_DIR=${AWS_TOKEN_DIR}
 . ./activate_venv.sh
 python oidc_write_orchestration.py
