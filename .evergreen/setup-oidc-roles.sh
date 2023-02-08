eb3122e1ebb4911a00ffaa3f8a1e9c5f389447ad
Durran Jordan <durran@gmail.com>
2023-02-08 19:40:23 +0100

    test(NODE-5038): show dir

--- .evergreen/setup-oidc-roles.sh
+++ .evergreen/setup-oidc-roles.sh
@@ -4,6 +4,9 @@ cd ${DRIVERS_TOOLS}/.evergreen/auth_oidc
 
 # Install mongosh
 curl https://github.com/mongodb-js/mongosh/releases/download/v1.6.2/mongodb-mongosh-shared-openssl3-1.6.2.x86_64.rpm --output mongosh.rpm
+
+ls -la
+
 sudo yum -y install mongosh.rpm
 
 mongosh setup_oidc.js
