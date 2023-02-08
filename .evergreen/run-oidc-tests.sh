bf43443ddd1e485116cb312fb53ed2618a0daa80
Durran Jordan <durran@gmail.com>
2023-02-08 22:13:08 +0100

    test(NODE-5038): setup uris

--- .evergreen/run-oidc-tests.sh
+++ .evergreen/run-oidc-tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+set -o errexit  # Exit the script with error if any of the commands fail
+set -o xtrace   # Write all commands first to stderr
 
 source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
+
+MONGODB_URI=${MONGODB_URI:-"mongodb://localhost"}
+MONGODB_URI_SINGLE="${MONGODB_URI}/?authMechanism=MONGODB-OIDC"
+export MONGODB_URI="$MONGODB_URI_SINGLE"
 
 npm run check:oidc
