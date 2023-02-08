cba78096f346aba5acdb11d51a7435cb1b669154
Bailey Pearson <bailey.pearson@mongodb.com>
2023-02-08 12:10:57 -0500

    add stuff

--- src/operations/collections.ts
+++ src/operations/collections.ts
@@ -25,24 +25,26 @@ export class CollectionsOperation extends AbstractOperation<Collection[]> {
     session: ClientSession | undefined,
     callback: Callback<Collection[]>
   ): void {
-    const db = this.db;
-
     // Let's get the collection names
-    db.listCollections(
-      {},
-      { ...this.options, nameOnly: true, readPreference: this.readPreference, session }
-    ).toArray((err, documents) => {
-      if (err || !documents) return callback(err);
-      // Filter collections removing any illegal ones
-      documents = documents.filter(doc => doc.name.indexOf('$') === -1);
-
-      // Return the collection objects
-      callback(
-        undefined,
-        documents.map(d => {
-          return new Collection(db, d.name, db.s.options);
-        })
+    this.db
+      .listCollections(
+        {},
+        { ...this.options, nameOnly: true, readPreference: this.readPreference, session }
+      )
+      .toArray()
+      .then(
+        documents => {
+          const collections = [];
+          for (const { name } of documents) {
+            if (!name.includes('$')) {
+              // Filter collections removing any illegal ones
+              collections.push(new Collection(this.db, name, this.db.s.options));
+            }
+          }
+          // Return the collection objects
+          callback(undefined, collections);
+        },
+        error => callback(error)
       );
-    });
   }
 }
