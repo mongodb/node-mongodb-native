e219e3c95dbdb58682445195d04378dae5ca16e6
Durran Jordan <durran@gmail.com>
2023-02-09 01:51:00 +0100

    test(NODE-5038): retry bootstrap

--- .evergreen/bootstrap-oidc.sh
+++ .evergreen/bootstrap-oidc.sh
@@ -3,17 +3,12 @@ set -o errexit  # Exit the script with error if any of the commands fail
 set -o xtrace   # Write all commands first to stderr
 
 cd ${DRIVERS_TOOLS}/.evergreen/auth_oidc
-. ./activate_venv.sh
 
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-export AWS_ROLE_ARN=${AWS_ROLE_ARN}
+export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
 export AWS_TOKEN_DIR=${AWS_TOKEN_DIR}
 
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_ROLE_ARN
-echo $AWS_TOKEN_DIR
-
+. ./activate_venv.sh
 python oidc_write_orchestration.py
 python oidc_get_tokens.py
