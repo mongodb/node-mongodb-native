0aec3091cad4e8b7520dcc3162604927d8845721
Durran Jordan <durran@gmail.com>
2023-02-09 02:19:30 +0100

    test(NODE-5038): try in non sub process

--- .evergreen/bootstrap-oidc.sh
+++ .evergreen/bootstrap-oidc.sh
@@ -9,6 +9,11 @@ export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
 export AWS_TOKEN_DIR=${AWS_TOKEN_DIR}
 
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_SESSION_TOKEN
+echo $AWS_TOKEN_DIR
+
 . ./activate_venv.sh
 python oidc_write_orchestration.py
 python oidc_get_tokens.py
