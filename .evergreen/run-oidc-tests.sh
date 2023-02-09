1376bc69da3634fe69d73e227891d011cf92b1ff
Durran Jordan <durran@gmail.com>
2023-02-09 03:07:58 +0100

    test(NODE-5038): add auth mech props

--- .evergreen/run-oidc-tests.sh
+++ .evergreen/run-oidc-tests.sh
@@ -5,7 +5,7 @@ set -o xtrace   # Write all commands first to stderr
 source "${PROJECT_DIRECTORY}/.evergreen/init-nvm.sh"
 
 MONGODB_URI=${MONGODB_URI:-"mongodb://127.0.0.1:27017"}
-MONGODB_URI_SINGLE="${MONGODB_URI}/?authMechanism=MONGODB-OIDC"
+MONGODB_URI_SINGLE="${MONGODB_URI}/?authMechanism=MONGODB-OIDC&authMechanismProperties=DEVICE_NAME:aws"
 
 echo $MONGODB_URI_SINGLE
 
