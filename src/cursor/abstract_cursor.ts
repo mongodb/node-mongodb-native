71d0d79e012e298be2307a73c097494072224bdb
Neal Beeken <neal.beeken@mongodb.com>
2023-02-14 09:59:26 -0500

    fix(NODE-5052): prevent cursor and changestream close logic from running more than once (#3562)

--- src/cursor/abstract_cursor.ts
+++ src/cursor/abstract_cursor.ts
@@ -828,13 +828,16 @@ function cleanupCursor(
 
   cursor[kKilled] = true;
 
+  if (session.hasEnded) {
+    return completeCleanup();
+  }
+
   executeOperation(
     cursor[kClient],
     new KillCursorsOperation(cursorId, cursorNs, server, { session })
-  ).finally(() => {
-    completeCleanup();
-  });
-  return;
+  )
+    .catch(() => null)
+    .finally(completeCleanup);
 }
 
 /** @internal */
