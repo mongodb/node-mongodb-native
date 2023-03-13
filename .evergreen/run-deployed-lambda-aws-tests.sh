8dfbd1c1cd71042fa1e17e9663711b1931b3fbe1
Durran Jordan <durran@gmail.com>
2023-03-13 22:45:07 +0100

    test: add sam to box

--- .evergreen/run-deployed-lambda-aws-tests.sh
+++ .evergreen/run-deployed-lambda-aws-tests.sh
@@ -9,4 +9,10 @@ set -o errexit  # Exit the script with error if any of the commands fail
 # export TEST_LAMBDA_DIRECTORY=${TEST_LAMBDA_DIRECTORY}
 # export LAMBDA_STACK_NAME=${LAMBDA_STACK_NAME}
 
+# TODO(BUILD-16797): Install sam on EVG
+curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+sam --version
+
 . ${DRIVERS_TOOLS}/.evergreen/run-deployed-lambda-aws-tests.sh
