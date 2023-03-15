ccd05a9f5412fe28c7c7790c513cc859529f0a57
Warren James <warren.james@mongodb.com>
2023-03-15 13:21:55 -0400

    test(NODE-4685): Update test name

--- test/integration/command-logging-and-monitoring/command_logging.spec.test.ts
+++ test/integration/command-logging-and-monitoring/command_logging.spec.test.ts
@@ -3,6 +3,6 @@ import * as path from 'path';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
-describe('Command Logging Spec (unified)', () => {
+describe('Command Logging Spec', () => {
   runUnifiedSuite(loadSpecTests(path.join('command-logging-and-monitoring', 'logging')));
 });
