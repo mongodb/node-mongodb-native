ca3fa4dae0e91055346fc3034771161b62f6e455
clemclx <clement.cloux@ynov.com>
2023-03-15 14:16:44 +0100

    fix(NODE-5106): Fix tests to pass leak_checker

--- test/integration/mongo_client.test.ts
+++ test/integration/mongo_client.test.ts
@@ -7,18 +7,36 @@ describe('MongoClient', () => {
   let client: MongoClient;
   let topologyOpenEvents;
 
+  /** Keep track number of call to client connect to close as many as connect (otherwise leak_checker hook will failed) */
+  let clientConnectCounter: number;
+
+  /**
+   * Wrap the connect method of the client to keep track
+   * of number of times connect is called
+   */
+  function clientConnect() {
+    if (!client) {
+      return;
+    }
+    clientConnectCounter++;
+    return client.connect();
+  }
+
   beforeEach(async function () {
     client = this.configuration.newClient();
     topologyOpenEvents = [];
+    clientConnectCounter = 0;
     client.on('open', event => topologyOpenEvents.push(event));
   });
 
   afterEach(async function () {
-    await client.close();
+    /** Close as many times as connect calls in the runned test (tracked by clientConnectCounter) */
+    const clientClosePromises = [...new Array(clientConnectCounter)].map(() => client.close());
+    await Promise.all(clientClosePromises);
   });
 
   it('Concurrents client connect correctly locked (only one topology created)', async function () {
-    await Promise.all([client.connect(), client.connect(), client.connect()]);
+    await Promise.all([clientConnect(), clientConnect(), clientConnect()]);
 
     expect(topologyOpenEvents).to.have.lengthOf(1);
     expect(client.topology?.isConnected()).to.be.true;
@@ -30,7 +48,7 @@ describe('MongoClient', () => {
 
     // first call rejected to simulate a connection failure
     try {
-      await client.connect();
+      await clientConnect();
     } catch (err) {
       expect(err).to.exist;
     }
@@ -39,7 +57,7 @@ describe('MongoClient', () => {
 
     // second call should connect
     try {
-      await client.connect();
+      await clientConnect();
     } catch (err) {
       expect.fail(`client connect throwed unexpected error`);
     }
