5253a66835120a7b306128387169320500e3c3e5
Warren James <warren.james@mongodb.com>
2023-03-16 16:39:22 -0400

    test(NODE-4685): skip command logging tests

--- test/integration/command-logging-and-monitoring/command_logging.spec.test.ts
+++ test/integration/command-logging-and-monitoring/command_logging.spec.test.ts
@@ -3,6 +3,6 @@ import * as path from 'path';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
-describe('Command Logging Spec', () => {
+describe.skip('Command Logging Spec', () => {
   runUnifiedSuite(loadSpecTests(path.join('command-logging-and-monitoring', 'logging')));
-});
+}).skipReason = 'TODO(NODE-4686): Unskip these tests';
