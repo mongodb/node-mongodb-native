1ce04b4ce23f970643f5f6ad7e96e46891d0b66c
Warren James <warren.james@mongodb.com>
2023-03-17 15:44:40 -0400

    test(NODE-4685): Move commmand monitoring and command logging spec tests to same file

--- test/integration/command-logging-and-monitoring/command_logging.spec.test.ts
+++
@@ -1,8 +0,0 @@
-import * as path from 'path';
-
-import { loadSpecTests } from '../../spec';
-import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
-
-describe.skip('Command Logging Spec', () => {
-  runUnifiedSuite(loadSpecTests(path.join('command-logging-and-monitoring', 'logging')));
-}).skipReason = 'TODO(NODE-4686): Unskip these tests';
