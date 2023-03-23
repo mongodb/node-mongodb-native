d87fc74568226bbbb790e5bf34e92e486eb0c0e5
Clément Cloux <clement.cloux@ynov.com>
2023-03-23 20:38:38 +0100

    fix(NODE-5106): fix test MongoClient import
    Co-authored-by: Neal Beeken <neal.beeken@mongodb.com>

--- test/integration/mongo_client.test.ts
+++ test/integration/mongo_client.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 
-import { MongoClient } from '../../src';
+import { MongoClient } from '../mongodb';
 
 describe('MongoClient', () => {
   let client: MongoClient;
