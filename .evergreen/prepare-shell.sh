67836d886d077959df8cdc691174406f8857aade
Durran Jordan <durran@gmail.com>
2023-04-06 19:38:19 +0200

    test: point to tools master

--- .evergreen/prepare-shell.sh
+++ .evergreen/prepare-shell.sh
@@ -31,7 +31,7 @@ export PATH="$MONGODB_BINARIES:$PATH"
 
 if [ ! -d "$DRIVERS_TOOLS" ]; then
   # Only clone driver tools if it does not exist
-  git clone --branch DRIVERS-2384 --depth=1 "https://github.com/mongodb-labs/drivers-evergreen-tools.git" "${DRIVERS_TOOLS}"
+  git clone --depth=1 "https://github.com/mongodb-labs/drivers-evergreen-tools.git" "${DRIVERS_TOOLS}"
 fi
 
 cat <<EOT > "$MONGO_ORCHESTRATION_HOME/orchestration.config"
