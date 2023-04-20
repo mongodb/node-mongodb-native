2e26e93e4422aa5bbf9411af73448c4a2d039537
Neal Beeken <neal.beeken@mongodb.com>
2023-04-20 14:18:33 -0400

    wip

--- test/integration/run-command/run_command.spec.test.ts
+++ test/integration/run-command/run_command.spec.test.ts
@@ -1,6 +1,6 @@
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
-describe('RunCommand spec', () => {
+describe.only('RunCommand spec', () => {
   runUnifiedSuite(loadSpecTests('run-command'));
 });
