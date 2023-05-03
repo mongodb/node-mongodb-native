b86180ad7456eee80d9063358fc93372aa2a4eea
Warren James <warren.james@mongodb.com>
2023-05-03 16:36:09 -0400

    test(NODE-4720): test update

--- test/integration/node-specific/topology.test.js
+++ test/integration/node-specific/topology.test.js
@@ -7,10 +7,14 @@ describe('Topology', function () {
   it('should correctly track states of a topology', {
     metadata: { requires: { apiVersion: false, topology: '!load-balanced' } }, // apiVersion not supported by newTopology()
     test: async function () {
-      const topology = new Topology({}, this.configuration.options.hosts, {
-        ...this.configuration.options,
-        metadata: makeClientMetadata({ driverInfo: {} })
-      });
+      const topology = new Topology(
+        this.configuration.newClient(),
+        this.configuration.options.hosts,
+        {
+          ...this.configuration.options,
+          metadata: makeClientMetadata({ driverInfo: {} })
+        }
+      );
 
       const states = [];
       topology.on('stateChanged', (_, newState) => {
