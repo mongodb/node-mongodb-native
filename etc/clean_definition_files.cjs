a8070563ad61dbd97bd12592fc34c5c6274105b7
Neal Beeken <neal.beeken@mongodb.com>
2023-05-03 16:00:20 -0400

    chore(NODE-5247): update dependencies

---
+++ etc/clean_definition_files.cjs
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require('path');
+
+function* walk(root) {
+  const directoryContents = fs.readdirSync(root);
+  for (const filepath of directoryContents) {
+    const fullPath = path.join(root, filepath);
+    const stat = fs.statSync(fullPath);
+    if (stat.isDirectory()) {
+      yield* walk(fullPath);
+    } else if (stat.isFile()) {
+      yield fullPath;
+    }
+  }
+}
+
+const libPath = path.resolve(__dirname, '..', 'lib');
+if (fs.existsSync(libPath)) {
+  const definitionFiles = Array.from(walk(libPath)).filter(filePath => {
+    return filePath.endsWith('.d.ts') || filePath.endsWith('.d.ts.map');
+  });
+  for (const definitionFile of definitionFiles) {
+    fs.unlinkSync(definitionFile);
+  }
+}
