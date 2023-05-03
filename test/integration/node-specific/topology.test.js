62e5ea2bb69d4a71e1c91aeb3e6283e6ef80df28
Warren James <warren.james@mongodb.com>
2023-05-03 15:14:48 -0400

    test(NODE-4720): fix test failure

--- test/integration/node-specific/topology.test.js
+++ test/integration/node-specific/topology.test.js
@@ -1,34 +1,37 @@
 'use strict';
 const { expect } = require('chai');
-const { makeClientMetadata } = require('../../mongodb');
+const { makeClientMetadata, Topology } = require('../../mongodb');
 
-describe('Topology', function () {
+describe('Topology', function() {
   it('should correctly track states of a topology', {
     metadata: { requires: { apiVersion: false, topology: '!load-balanced' } }, // apiVersion not supported by newTopology()
-    test: function (done) {
-      const topology = this.configuration.newTopology({
+    test: async function() {
+      const topology = new Topology({}, this.configuration.options.hosts, {
+        ...this.configuration.options,
         metadata: makeClientMetadata({ driverInfo: {} })
       });
 
       const states = [];
-      topology.on('stateChanged', (_, newState) => states.push(newState));
-      topology.connect(err => {
-        try {
-          expect(err).to.not.exist;
-        } catch (error) {
-          done(error);
-        }
-        topology.close({}, err => {
-          try {
-            expect(err).to.not.exist;
-            expect(topology.isDestroyed()).to.be.true;
-            expect(states).to.eql(['connecting', 'connected', 'closing', 'closed']);
-            done();
-          } catch (error) {
-            done(error);
-          }
+      topology.on('stateChanged', (_, newState) => {
+        states.push(newState);
+      });
+
+      await new Promise((resolve, reject) => {
+        topology.connect((err) => {
+          if (err) reject(err);
+          else resolve(null);
         });
       });
+
+      await new Promise((resolve, reject) => {
+        topology.close({}, (err) => {
+          if (err) reject(err);
+          else resolve(null);
+        });
+      })
+
+      expect(topology.isDestroyed()).to.be.true;
+      expect(states).to.eql(['connecting', 'connected', 'closing', 'closed']);
     }
   });
 });
