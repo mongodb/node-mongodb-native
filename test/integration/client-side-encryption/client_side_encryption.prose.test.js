ab3605fb3f9329699ab1d1f67fdaabe7380c6952
Neal Beeken <neal.beeken@mongodb.com>
2023-05-18 16:27:46 -0400

    docs: add todos

--- test/integration/client-side-encryption/client_side_encryption.prose.test.js
+++ test/integration/client-side-encryption/client_side_encryption.prose.test.js
@@ -1178,6 +1178,7 @@ describe('Client Side Encryption Prose Tests', metadata, function () {
           .to.be.instanceOf(Error)
           .to.have.property('name', 'MongoServerSelectionError');
 
+        // TODO(NODE-5296): check error.message once AggregateErrors are handled correctly
         expect(insertError, 'Error must contain ECONNREFUSED').to.satisfy(
           error =>
             /ECONNREFUSED/.test(error.message) ||
@@ -1274,6 +1275,7 @@ TODO(NODE-5283): The error thrown in this test fails an instanceof check with Mo
         client = new MongoClient('mongodb://localhost:27021/db?serverSelectionTimeoutMS=1000');
         const error = await client.connect().catch(e => e);
 
+        // TODO(NODE-5296): check error.message once AggregateErrors are handled correctly
         expect(
           error,
           'Error MUST be a MongoServerSelectionError error that contains ECONNREFUSED information'
