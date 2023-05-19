8cfa7562da2792abca81379a454cd5f6b06eb092
Durran Jordan <durran@gmail.com>
2023-05-19 14:11:06 +0200

    test: setup scripts

--- .evergreen/run-oidc-tests.sh
+++ .evergreen/run-oidc-tests.sh
@@ -2,14 +2,16 @@
 set -o errexit  # Exit the script with error if any of the commands fail
 set -o xtrace   # Write all commands first to stderr
 
+PROVIDER_NAME=${PROVIDER_NAME:-"aws"}
+PROJECT_DIRECTORY=${PROJECT_DIRECTORY:-"."}
 source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
 
 MONGODB_URI=${MONGODB_URI:-"mongodb://127.0.0.1:27017"}
-MONGODB_URI_SINGLE="${MONGODB_URI}/?authMechanism=MONGODB-OIDC&authMechanismProperties=DEVICE_NAME:aws"
 
-echo $MONGODB_URI_SINGLE
-
-export MONGODB_URI="$MONGODB_URI_SINGLE"
 export OIDC_TOKEN_DIR=${OIDC_TOKEN_DIR}
 
-npm run check:oidc
+if [ "$PROVIDER_NAME" = "azure" ]; then
+  npm run check:oidc-azure
+else
+  npm run check:oidc
+fi
---
+++ test/manual/mongodb_oidc_azure.prose.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('MONGODB-OIDC (Azure)', function () {
+  it('passes', function () {
+    expect(true).to.be.true;
+  });
+});
