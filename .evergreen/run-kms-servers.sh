e7729cf6b0ed28d6e1075976c27bcf741ca53eb7
Durran Jordan <durran@gmail.com>
2023-06-01 16:36:47 -0400

    fix: cast to number and check nan

--- .evergreen/run-kms-servers.sh
+++ .evergreen/run-kms-servers.sh
@@ -7,3 +7,6 @@ python -u kms_kmip_server.py &
 python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 8000  &
 python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 8001  &
 python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/server.pem --port 8002 --require_client_cert &
+
+echo "Finished starting KMS servers"
+exit 0;
