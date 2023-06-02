62adedc61030a7e8515fd15f5a6e284bdf16543e
Bailey Pearson <bailey.pearson@mongodb.com>
2023-06-02 07:46:17 -0600

    remove opttoolchain from init file

--- .evergreen/init-node-and-npm-env.sh
+++ .evergreen/init-node-and-npm-env.sh
@@ -7,8 +7,6 @@
 ## access to `npm`, `node`, or need to install something globally from
 ## npm.
 
-# export PATH="/opt/mongodbtoolchain/v2/bin:$PATH"
-
 NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
 if [[ "$OS" == "Windows_NT" ]]; then
   NODE_ARTIFACTS_PATH=$(cygpath --unix "$NODE_ARTIFACTS_PATH")
