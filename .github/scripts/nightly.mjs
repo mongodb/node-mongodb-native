6ad7d16b8d3df98146bafb0d3547d6deef6bac44
Neal Beeken <neal.beeken@mongodb.com>
2023-06-05 17:07:45 -0400

    ci: wrong test value

--- .github/scripts/nightly.mjs
+++ .github/scripts/nightly.mjs
@@ -48,7 +48,7 @@ class NightlyVersion {
     const showInfo = JSON.parse(stdout);
     const version = showInfo?.['dist-tags']?.nightly ?? '0.0.0-dev.YYYYMMDD.sha.##########';
     // TODO: return new NightlyVersion(version);
-    return new NightlyVersion('mongodb-5.6.0-dev.20230603.sha.008fd6f')
+    return new NightlyVersion('5.6.0-dev.20230605.sha.bf36a92')
   }
   static async currentCommit() {
     const { stdout } = await exec('git rev-parse --short HEAD', { encoding: 'utf8' });
