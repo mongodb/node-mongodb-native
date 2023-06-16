6b85764f9e30a003eb52fb13738f318eda908f2c
Bailey Pearson <bailey.pearson@mongodb.com>
2023-06-16 09:31:50 -0600

    comments

--- test/integration/auth/scram_sha_256.test.ts
+++ test/integration/auth/scram_sha_256.test.ts
@@ -6,18 +6,19 @@ import * as deps from '../../../src/deps';
 import { type MongoClient } from '../../mongodb';
 
 describe('SCRAM_SHA_256', function () {
+  beforeEach(function () {
+    if (!this.configuration.parameters.authenticationMechanisms.includes('SCRAM-SHA-256')) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      this.currentTest!.skipReason = 'Test requires that SCRAM-SHA-256 be enabled on the server.';
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      this.currentTest!.skip();
+    }
+  });
+
   context('when saslprep is not a function', () => {
     let client: MongoClient;
-    beforeEach(function () {
-      if (!this.configuration.parameters.authenticationMechanisms.includes('SCRAM-SHA-256')) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.currentTest!.skipReason = 'Test requires that SCRAM-SHA-256 be enabled on the server.';
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.currentTest!.skip();
-      }
-    });
 
-    beforeEach('setup mocks', function () {
+    beforeEach(function () {
       sinon.stub(deps, 'saslprep').value({});
       client = this.configuration.newClient({ authMechanism: 'SCRAM-SHA-256' });
     });
@@ -40,6 +41,24 @@ describe('SCRAM_SHA_256', function () {
         'message',
         'Warning: no saslprep library specified. Passwords will not be sanitized'
       );
+    });
+  });
+
+  context('when saslprep is a function', () => {
+    let client: MongoClient;
+
+    beforeEach(function () {
+      client = this.configuration.newClient({ authMechanism: 'SCRAM-SHA-256' });
+    });
+
+    afterEach(() => client.close());
+
+    it('calls saslprep', { requires: { auth: 'enabled' } }, async function () {
+      const spy = sinon.spy(deps, 'saslprep');
+
+      await client.connect();
+
+      expect(spy.called).to.be.true;
     });
   });
 });
