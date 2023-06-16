a500bd7292e70a058995ed31ced68c5789246759
Bailey Pearson <bailey.pearson@mongodb.com>
2023-06-16 12:52:04 -0600

    add comment and guard

--- etc/expand-tasks.mjs
+++ etc/expand-tasks.mjs
@@ -1,10 +1,18 @@
 // usage: node expand-task.js <task name>
 // must be run from root of the Node driver
+//
+// The output is pipeable: `node expand-task.js <task name> | jq '.'`
 
 import { readFileSync } from 'fs';
 import { load } from 'js-yaml';
+import { gte } from 'semver';
 import { Readable } from 'stream';
 import { inspect } from 'util';
+
+if (!gte(process.version, 16)) {
+  console.error('expand-tasks.mjs requires Node16+');
+  process.exit(1);
+}
 
 const config = load(readFileSync('.evergreen/config.yml'));
 const taskName = (process.argv[2] ?? '').trim();
