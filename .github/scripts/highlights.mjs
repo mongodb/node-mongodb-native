d8a527152266b9075ed4236404a3c65473f14b7b
Daria Pardue <daria.pardue@mongodb.com>
2023-06-22 18:47:02 -0400

    chore: fix release highlight formatting

--- .github/scripts/highlights.mjs
+++ .github/scripts/highlights.mjs
@@ -70,7 +70,7 @@ async function pullRequestHighlights(prs) {
   if (!highlights.length) return '';
 
   highlights.unshift('## Release Notes\n\n');
-  return highlights.join('');
+  return highlights.join('\n\n');
 }
 
 console.log('List of PRs to collect highlights from:', prs);
