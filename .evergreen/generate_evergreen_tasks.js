8bd2618cdad51b45757783ebb877de0d32dd1c1a
Durran Jordan <durran@gmail.com>
2023-06-22 18:13:35 +0200

    test(NODE-5361): use task groups for lambda

--- .evergreen/generate_evergreen_tasks.js
+++ .evergreen/generate_evergreen_tasks.js
@@ -302,16 +302,6 @@ AWS_LAMBDA_HANDLER_TASKS.push({
   ]
 });
 
-// Add the deployed lambda function tests.
-AWS_LAMBDA_HANDLER_TASKS.push({
-  name: 'test-deployed-lambda',
-  tags: ['latest', 'lambda'],
-  commands: [
-    { func: 'install dependencies' },
-    { func: 'run deployed aws lambda tests' }
-  ]
-});
-
 // Add task for testing lambda example with aws auth.
 AWS_LAMBDA_HANDLER_TASKS.push({
   name: 'test-lambda-aws-auth-example',
@@ -764,6 +754,14 @@ BUILD_VARIANTS.push({
 });
 
 BUILD_VARIANTS.push({
+  name: 'rhel8-test-aws-lambda',
+  display_name: 'AWS Lambda Deployed Tests',
+  run_on: DEFAULT_OS,
+  batchtime: 20160,
+  tasks: ['test_aws_lambda_task_group']
+});
+
+BUILD_VARIANTS.push({
   name: 'rhel8-no-auth-tests',
   display_name: 'No Auth Tests',
   run_on: DEFAULT_OS,
@@ -777,7 +775,7 @@ BUILD_VARIANTS.push({
   name: 'rhel8-test-lambda',
   display_name: 'AWS Lambda handler tests',
   run_on: DEFAULT_OS,
-  tasks: ['test-lambda-example', 'test-lambda-aws-auth-example', 'test-deployed-lambda']
+  tasks: ['test-lambda-example', 'test-lambda-aws-auth-example']
 });
 
 BUILD_VARIANTS.push({
