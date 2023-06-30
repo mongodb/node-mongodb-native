ab7675157528021022c106704c7e41052e7a0e71
Neal Beeken <neal.beeken@mongodb.com>
2023-06-30 10:09:08 -0400

    chore(NODE-5382): backport release automation scripts

---
+++ .github/scripts/pr_list.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import * as url from 'node:url';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import { getCurrentHistorySection, output } from './util.mjs';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const historyFilePath = path.join(__dirname, '..', '..', 'HISTORY.md');
+
+/**
+ * @param {string} history
+ * @returns {string[]}
+ */
+function parsePRList(history) {
+  const prRegexp = /node-mongodb-native\/issues\/(?<prNum>\d+)\)/iu;
+  return history
+    .split('\n')
+    .map(line => prRegexp.exec(line)?.groups?.prNum ?? '')
+    .filter(prNum => prNum !== '');
+}
+
+const historyContents = await fs.readFile(historyFilePath, { encoding: 'utf8' });
+
+const currentHistorySection = getCurrentHistorySection(historyContents);
+
+const prs = parsePRList(currentHistorySection);
+
+await output('pr_list', prs.join(','));
---
+++ .github/scripts/util.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import * as process from 'node:process';
+import * as fs from 'node:fs/promises';
+
+export async function output(key, value) {
+  const { GITHUB_OUTPUT = '' } = process.env;
+  const output = `${key}=${value}\n`;
+  console.log('outputting:', output);
+
+  if (GITHUB_OUTPUT.length === 0) {
+    // This is always defined in Github actions, and if it is not for some reason, tasks that follow will fail.
+    // For local testing it's convenient to see what scripts would output without requiring the variable to be defined.
+    console.log('GITHUB_OUTPUT not defined, printing only');
+    return;
+  }
+
+  const outputFile = await fs.open(GITHUB_OUTPUT, 'a');
+  await outputFile.appendFile(output, { encoding: 'utf8' });
+  await outputFile.close();
+}
+
+/**
+ * @param {string} historyContents
+ * @returns {string}
+ */
+export function getCurrentHistorySection(historyContents) {
+  /** Markdown version header */
+  const VERSION_HEADER = /^#.+\(\d{4}-\d{2}-\d{2}\)$/g;
+
+  const historyLines = historyContents.split('\n');
+
+  // Search for the line with the first version header, this will be the one we're releasing
+  const headerLineIndex = historyLines.findIndex(line => VERSION_HEADER.test(line));
+  if (headerLineIndex < 0) throw new Error('Could not find any version header');
+
+  console.log('Found markdown header current release', headerLineIndex, ':', historyLines[headerLineIndex]);
+
+  // Search lines starting after the first header, and add back the offset we sliced at
+  const nextHeaderLineIndex = historyLines
+    .slice(headerLineIndex + 1)
+    .findIndex(line => VERSION_HEADER.test(line)) + headerLineIndex + 1;
+  if (nextHeaderLineIndex < 0) throw new Error(`Could not find previous version header, searched ${headerLineIndex + 1}`);
+
+  console.log('Found markdown header previous release', nextHeaderLineIndex, ':', historyLines[nextHeaderLineIndex]);
+
+  return historyLines.slice(headerLineIndex, nextHeaderLineIndex).join('\n');
+}
