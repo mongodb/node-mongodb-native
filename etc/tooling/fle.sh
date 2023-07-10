63bf8971672a2d1640c78c4ed6cb53b09ad9ce5c
Bailey Pearson <bailey.pearson@mongodb.com>
2023-07-10 10:24:48 -0600

    address comments

--- etc/tooling/fle.sh
+++ etc/tooling/fle.sh
@@ -3,15 +3,15 @@
 USAGE="usage: fle.sh <command>
 
 commands:
-	link [--build-libmongocrypt]: builds the node bindings, optionally building libmongocrypt and links it to the driver
+	link [--build-libmongocrypt]: (optionally builds libmongocrypt), builds the node bindings and links the bindings to the driver
 	rebuild: rebuilds the node bindings
 	unlink: unlinks the node bindings from the driver
 
 required environment variables:
-	node_bindings - a path, relative or absolute, to the directory that contains the Node libmongocrypt bindings"
+	NODE_BINDINGS - a path, relative or absolute, to the directory that contains the Node libmongocrypt bindings"
 
 build_libmongocrypt() {
-	cd $node_bindings
+	cd $NODE_BINDINGS
 	# clean out any stale builds
 	git clean -fdx
 	bash etc/build-static.sh
@@ -19,21 +19,21 @@ build_libmongocrypt() {
 }
 
 build_node_bindings() {
-	cd $node_bindings
+	cd $NODE_BINDINGS
 	npm run rebuild
 	cd -
 }
 
 link_bindings() {
-	npm link $node_bindings
+	npm link $NODE_BINDINGS
 }
 
 unlink_bindings() {
-	npm unlink $node_bindings
+	npm unlink $NODE_BINDINGS
 }
 
-if [[ "$node_bindings" == "" ]]; then
-	echo "node_bindings path must be set."
+if [[ "$NODE_BINDINGS" == "" ]]; then
+	echo "NODE_BINDINGS path must be set."
 	exit 1
 fi
 
