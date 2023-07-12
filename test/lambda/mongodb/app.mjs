f313c49ff41a9dabb331002b3b3283e76f9d525e
Durran Jordan <durran@gmail.com>
2023-07-12 12:36:33 +0200

    test: console

--- test/lambda/mongodb/app.mjs
+++ test/lambda/mongodb/app.mjs
@@ -1,3 +1,4 @@
+import { version } from '@aws-sdk/credential-providers/package.json';
 import { MongoClient } from 'mongodb';
 
 // Creates the client that is cached for all requests, subscribes to
@@ -6,75 +7,8 @@ const mongoClient = new MongoClient(process.env.MONGODB_URI, {
   monitorCommands: true
 });
 
-let openConnections = 0;
-let heartbeatCount = 0;
-let totalHeartbeatDuration = 0;
-let totalCommands = 0;
-let totalCommandDuration = 0;
-
-mongoClient.on('commandStarted', (event) => {
-  console.log('commandStarted', event);
-});
-
-mongoClient.on('commandSucceeded', (event) => {
-  totalCommands++;
-  totalCommandDuration += event.duration;
-  console.log('commandSucceeded', event);
-});
-
-mongoClient.on('commandFailed', (event) => {
-  totalCommands++;
-  totalCommandDuration += event.duration;
-  console.log('commandFailed', event);
-});
-
-mongoClient.on('serverHeartbeatStarted', (event) => {
-  console.log('serverHeartbeatStarted', event);
-});
-
-mongoClient.on('serverHeartbeatSucceeded', (event) => {
-  heartbeatCount++;
-  totalHeartbeatDuration += event.duration;
-  console.log('serverHeartbeatSucceeded', event);
-});
-
-mongoClient.on('serverHeartbeatFailed', (event) => {
-  heartbeatCount++;
-  totalHeartbeatDuration += event.duration;
-  console.log('serverHeartbeatFailed', event);
-});
-
-mongoClient.on('connectionCreated', (event) => {
-  openConnections++;
-  console.log('connectionCreated', event);
-});
-
-mongoClient.on('connectionClosed', (event) => {
-  openConnections--;
-  console.log('connectionClosed', event);
-});
-
 // Populate the connection pool.
 await mongoClient.connect();
-
-// Create the response to send back.
-function createResponse() {
-  return {
-    averageCommandDuration: totalCommandDuration / totalCommands,
-    averageHeartbeatDuration: totalHeartbeatDuration / heartbeatCount,
-    openConnections: openConnections,
-    heartbeatCount: heartbeatCount
-  };
-}
-
-// Reset the numbers.
-function reset() {
-  openConnections = 0;
-  heartbeatCount = 0;
-  totalHeartbeatDuration = 0;
-  totalCommands = 0;
-  totalCommandDuration = 0;
-}
 
 /**
  * The handler function itself performs an insert/delete and returns the
@@ -83,17 +17,16 @@ function reset() {
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  */
-export const lambdaHandler = async (event) => {
+export const lambdaHandler = async () => {
+  console.log('SDK VERSION', version);
+  console.log('AWS_REGION', process.env.AWS_REGION);
   const db = mongoClient.db('lambdaTest');
   const collection = db.collection('test');
   const { insertedId } = await collection.insertOne({ n: 1 });
   await collection.deleteOne({ _id: insertedId });
-  // Create the response and then reset the numbers.
-  const response = JSON.stringify(createResponse());
-  reset();
 
   return {
     statusCode: 200,
-    body: response
+    body: ''
   };
 };
