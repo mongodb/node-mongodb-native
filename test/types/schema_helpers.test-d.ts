9b3d7df8cf75d4bf755bcf5d62ed05ea9c00eb21
Warren James <warren.james@mongodb.com>
2023-08-03 10:11:09 -0400

    Apply suggestions from code review
    Co-authored-by: Durran Jordan <durran@gmail.com>

--- test/types/schema_helpers.test-d.ts
+++ test/types/schema_helpers.test-d.ts
@@ -79,17 +79,10 @@ expectAssignable<SchemaWithIdType | null>(
   await typeTestCollection.findOneAndReplace({ a: 1 }, { a: 5 })
 );
 expectAssignable<SchemaWithIdInterface | null>(
-  (
-    await interfaceTestCollection.findOneAndReplace(
-      { a: 1 },
-      { a: 5 },
-      { includeResultMetadata: true }
-    )
-  ).value
+    await interfaceTestCollection.findOneAndReplace({ a: 1 }, { a: 5 })
 );
 expectAssignable<SchemaWithIdType | null>(
-  (await typeTestCollection.findOneAndUpdate({ a: 1 }, { a: 5 }, { includeResultMetadata: true }))
-    .value
+  await typeTestCollection.findOneAndUpdate({ a: 1 }, { a: 5 })
 );
 expectAssignable<SchemaWithIdInterface | null>(
   await interfaceTestCollection.findOneAndUpdate({ a: 1 }, { a: 5 })
