9268b35d5ba0736ff38e691024d89f1ac6c28515
Neal Beeken <neal.beeken@mongodb.com>
2023-08-04 11:51:14 -0400

    feat(NODE-5233)!: prevent session from one client from being used on another (#3790)
    Co-authored-by: Bailey Pearson <bailey.pearson@mongodb.com>

--- src/operations/execute_operation.ts
+++ src/operations/execute_operation.ts
@@ -7,6 +7,7 @@ import {
   MongoError,
   MongoErrorLabel,
   MongoExpiredSessionError,
+  MongoInvalidArgumentError,
   MongoNetworkError,
   MongoNotConnectedError,
   MongoRuntimeError,
@@ -118,6 +119,8 @@ async function executeOperationAsync<
     throw new MongoExpiredSessionError('Use of expired sessions is not permitted');
   } else if (session.snapshotEnabled && !topology.capabilities.supportsSnapshotReads) {
     throw new MongoCompatibilityError('Snapshot reads require MongoDB 5.0 or later');
+  } else if (session.client !== client) {
+    throw new MongoInvalidArgumentError('ClientSession must be from the same MongoClient');
   }
 
   const readPreference = operation.readPreference ?? ReadPreference.primary;
