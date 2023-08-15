4cf1e96c68410d743c592b27e8864917ca16e160
Neal Beeken <neal.beeken@mongodb.com>
2023-08-15 16:20:28 -0400

    fix(NODE-5537): remove credentials from ConnectionPoolCreatedEvent options (#3813)

--- test/integration/connection-monitoring-and-pooling/connection_monitoring_and_pooling.test.ts
+++ test/integration/connection-monitoring-and-pooling/connection_monitoring_and_pooling.test.ts
@@ -1,3 +1,7 @@
+import { expect } from 'chai';
+import { once } from 'events';
+
+import { type MongoClient } from '../../mongodb';
 import { loadSpecTests } from '../../spec';
 import { type CmapTest, runCmapTestSuite } from '../../tools/cmap_spec_runner';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
@@ -23,4 +27,49 @@ describe('Connection Monitoring and Pooling (Node Driver)', function () {
     '../integration/connection-monitoring-and-pooling/unified-cmap-node-specs'
   );
   runUnifiedSuite(unifiedTests);
+
+  describe('ConnectionPoolCreatedEvent', () => {
+    let client: MongoClient;
+    beforeEach(async function () {
+      client = this.configuration.newClient();
+    });
+
+    afterEach(async function () {
+      await client.close();
+    });
+
+    describe('constructor()', () => {
+      it('when auth is enabled redacts credentials from options', {
+        metadata: { requires: { auth: 'enabled' } },
+        async test() {
+          const poolCreated = once(client, 'connectionPoolCreated');
+          await client.connect();
+          const [event] = await poolCreated;
+          expect(event).to.have.deep.nested.property('options.credentials', {});
+
+          const poolOptions = Array.from(client.topology?.s.servers.values() ?? []).map(
+            s => s.pool.options
+          );
+          expect(poolOptions).to.have.length.of.at.least(1);
+
+          for (const { credentials = {} } of poolOptions) {
+            expect(
+              Object.keys(credentials),
+              'pool.options.credentials must exist and have keys'
+            ).to.not.equal(0);
+          }
+        }
+      });
+
+      it('when auth is disabled does not add a credentials property to options', {
+        metadata: { requires: { auth: 'disabled' } },
+        async test() {
+          const poolCreated = once(client, 'connectionPoolCreated');
+          await client.connect();
+          const [event] = await poolCreated;
+          expect(event).to.not.have.nested.property('options.credentials');
+        }
+      });
+    });
+  });
 });
