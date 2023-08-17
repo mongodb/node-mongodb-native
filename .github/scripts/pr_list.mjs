e1af3437c309c145e2af6ebceb005771b10e736a
Neal Beeken <neal.beeken@mongodb.com>
2023-08-17 17:41:02 -0400

    chore: update release automation scripts 5.x (#3823)

--- .github/scripts/pr_list.mjs
+++ .github/scripts/pr_list.mjs
@@ -13,10 +13,14 @@ const historyFilePath = path.join(__dirname, '..', '..', 'HISTORY.md');
  */
 function parsePRList(history) {
   const prRegexp = /node-mongodb-native\/issues\/(?<prNum>\d+)\)/iu;
-  return history
-    .split('\n')
-    .map(line => prRegexp.exec(line)?.groups?.prNum ?? '')
-    .filter(prNum => prNum !== '');
+  return Array.from(
+    new Set(
+      history
+        .split('\n')
+        .map(line => prRegexp.exec(line)?.groups?.prNum ?? '')
+        .filter(prNum => prNum !== '')
+    )
+  );
 }
 
 const historyContents = await fs.readFile(historyFilePath, { encoding: 'utf8' });
