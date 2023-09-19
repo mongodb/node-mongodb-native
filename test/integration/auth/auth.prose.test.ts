7f1626b752c057d4b5e4c67087fcee73bbf4e686
Durran Jordan <durran@gmail.com>
2023-09-19 18:32:11 +0200

    test: fix fail message

--- test/integration/auth/auth.prose.test.ts
+++ test/integration/auth/auth.prose.test.ts
@@ -260,7 +260,7 @@ describe('Authentication Spec Prose Tests', function () {
           try {
             this.configuration.newClient({}, noUsernameOptions);
             expect.fail(
-              'Creating a new client with a password and no username must fail validation.'
+              'Creating a new client with a password and no user must fail validation.'
             );
           } catch (noUserError) {
             // NOTE: This prose test fails Node's validation of the credentials object - a username
