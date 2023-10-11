bf4fa87849d4eb7d07e31fd85f658375b34f313c
Aditi Khare <aditi.khare@mongodb.com>
2023-10-11 18:02:38 -0400

    Fixed failing tests

--- test/integration/connection-monitoring-and-pooling/connection_monitoring_and_pooling.spec.test.ts
+++ test/integration/connection-monitoring-and-pooling/connection_monitoring_and_pooling.spec.test.ts
@@ -58,7 +58,7 @@ describe('Connection Monitoring and Pooling Spec Tests (Integration) - cmap-form
   });
 });
 
-describe.skip('Connection Monitoring and Pooling Spec Tests (Integration) - logging', function () {
+describe('Connection Monitoring and Pooling Spec Tests (Integration) - logging', function () {
   const tests = loadSpecTests('connection-monitoring-and-pooling', 'logging');
 
   runUnifiedSuite(tests, test => {
