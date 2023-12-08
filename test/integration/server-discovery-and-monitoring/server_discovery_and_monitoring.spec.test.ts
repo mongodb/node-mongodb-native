00ab2959e6569e5f155dab74a17645fc1754955c
Aditi Khare <aditi.khare@mongodb.com>
2023-12-08 16:54:11 -0500

    Lint fixes

--- test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
+++ test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
@@ -3,6 +3,6 @@ import * as path from 'path';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
-describe.only('SDAM Unified Tests', function () {
-  runUnifiedSuite(loadSpecTests(path.join('server-discovery-and-monitoring', 'unified-logging')));
+describe('SDAM Unified Tests', function () {
+  runUnifiedSuite(loadSpecTests(path.join('server-discovery-and-monitoring', 'unified')));
 });
