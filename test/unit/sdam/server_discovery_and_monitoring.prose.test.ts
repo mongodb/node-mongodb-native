aa2542d55b2e5cc9cf4919b5e3a81a873f276373
Warren James <warren.james@mongodb.com>
2024-01-08 17:25:55 -0500

    Add prose test

---
+++ test/unit/sdam/server_discovery_and_monitoring.prose.test.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import { createServer, type Server } from 'net';
+
+import { MongoClient, SERVER_HEARTBEAT_FAILED, SERVER_HEARTBEAT_STARTED } from '../../mongodb';
+
+describe('Heartbeat tests', function () {
+  let client: MongoClient;
+  // Mock server
+  let server: Server;
+  // Shared array
+  const events: string[] = [];
+
+  beforeEach(async function () {
+    // Create TCP server that responds to hellos by closing the connection
+    // and pushing "client connection created" to shared array
+    server = createServer(clientSocket => {
+      console.log(`connection from ${clientSocket.remoteAddress}`);
+      events.push('client connected');
+      clientSocket.end();
+    });
+    server.listen(9999);
+
+    // set up client to connect to mock server with the following configuration
+    // {
+    //    serverSelectionTimeoutMS: 500,
+    //    maxPoolSize: 1,
+    //    minPoolSize: 0
+    // }
+    client = new MongoClient('mongodb://localhost:9999', {
+      serverSelectionTimeoutMS: 500,
+      maxPoolSize: 1,
+      minPoolSize: 0
+    });
+
+    // Listen to `ServerHeartbeatStartedEvent` and `ServerHeartbeatSucceededEvent`, pushing the
+    // event name to the shared array when event is emitted
+    for (const e of [SERVER_HEARTBEAT_STARTED, SERVER_HEARTBEAT_FAILED]) {
+      client.on(e, () => {
+        events.push(e);
+      });
+    }
+
+    // Attempt to connect to mock server
+    const maybeError = await client.connect().catch(e => e);
+    // Catch error
+    expect(maybeError).to.be.instanceOf(Error);
+  });
+
+  afterEach(async function () {
+    if (server.listening) server.close();
+  });
+
+  it('emits the first HeartbeatStartedEvent after the monitoring socket was created', async function () {
+    expect(events).to.have.lengthOf(3);
+    expect(events[0]).to.equal('client connection created');
+    expect(events[1]).to.equal(SERVER_HEARTBEAT_STARTED);
+    expect(events[3]).to.equal(SERVER_HEARTBEAT_FAILED);
+  });
+});
