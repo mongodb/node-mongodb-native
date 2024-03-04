5f87446e0aa1f72771dd98b56e19c8e61c09a637
Aditi Khare <aditi.khare@mongodb.com>
2024-03-04 16:46:29 -0500

    Basic implementation done, need to UTR changes + sync spec tests still

--- src/error.ts
+++ src/error.ts
@@ -200,6 +200,7 @@ export class MongoError extends Error {
  * @category Error
  */
 export class MongoServerError extends MongoError {
+  errorResponse?: ErrorDescription;
   codeName?: string;
   writeConcernError?: Document;
   errInfo?: Document;
@@ -223,9 +224,17 @@ export class MongoServerError extends MongoError {
       this[kErrorLabels] = new Set(message.errorLabels);
     }
 
+    this.errorResponse = message;
+
     for (const name in message) {
-      if (name !== 'errorLabels' && name !== 'errmsg' && name !== 'message')
+      if (
+        name !== 'errorLabels' &&
+        name !== 'errmsg' &&
+        name !== 'message' &&
+        name !== 'errorResponse'
+      ) {
         this[name] = message[name];
+      }
     }
   }
 
