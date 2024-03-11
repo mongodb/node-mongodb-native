de6efdf75364a70a51324044734cdf2205723090
Aditi Khare <aditi.khare@mongodb.com>
2024-03-11 13:57:25 -0400

    added modifyCollection helper to utr/operations.ts

--- test/integration/crud/crud.spec.test.js
+++ test/integration/crud/crud.spec.test.js
@@ -425,10 +425,6 @@ describe('CRUD spec v1', function () {
   }
 });
 
-// TODO(NODE-5998) - The Node driver UTR does not have a Collection.modifyCollection helper.
-const SKIPPED_TESTS = ['findOneAndUpdate document validation errInfo is accessible'];
 describe('CRUD unified', function () {
-  runUnifiedSuite(loadSpecTests(path.join('crud', 'unified')), ({ description }) =>
-    SKIPPED_TESTS.includes(description) ? `the Node driver does not have a collMod helper.` : false
-  );
+  runUnifiedSuite(loadSpecTests(path.join('crud', 'unified')));
 });
