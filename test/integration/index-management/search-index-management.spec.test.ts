7d510a3d6827211ea481250f50f69b6e031bace2
Aditi Khare <aditi.khare@mongodb.com>
2024-03-15 15:09:52 -0400

    synced tests

--- test/integration/index-management/search-index-management.spec.test.ts
+++ test/integration/index-management/search-index-management.spec.test.ts
@@ -3,6 +3,6 @@ import { join } from 'path';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
-describe('Search Index Management Tests (Unified)', function () {
+describe.only('Search Index Management Tests (Unified)', function () {
   runUnifiedSuite(loadSpecTests(join('index-management')));
 });
