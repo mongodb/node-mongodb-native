894b4fa7d87f92b79fee6fbd058beb0c3d8ae9e9
Bailey Pearson <bailey.pearson@mongodb.com>
2024-03-19 12:01:21 -0600

    fix range v2 tests

--- test/integration/client-side-encryption/client_side_encryption.prose.22.range_explicit_encryption.test.ts
+++ test/integration/client-side-encryption/client_side_encryption.prose.22.range_explicit_encryption.test.ts
@@ -25,7 +25,9 @@ const metaData: MongoDBMetadataUI = {
 
     // The Range Explicit Encryption tests require MongoDB server 7.0+ for QE v2.
     // The tests must not run against a standalone.
-    mongodb: '>=7.0.0',
+    //
+    // `rangePreview` is not supported on 8.0+ servers.
+    mongodb: '>=7.0.0 <8.0.0',
     topology: '!single'
   }
 };
@@ -127,14 +129,6 @@ const readEncryptedFieldsFile = (dataType: string): Promise<string> =>
 describe('Range Explicit Encryption', function () {
   installNodeDNSWorkaroundHooks();
 
-  beforeEach(async function () {
-    if (semver.gte(this.configuration.version, '7.999.999')) {
-      if (this.currentTest)
-        this.currentTest.skipReason = 'TODO(NODE-5908): skip rangePreview tests on server 8.0+';
-      return this.currentTest?.skip();
-    }
-  });
-
   let clientEncryption;
   let keyId;
   let keyVaultClient;
@@ -145,7 +139,7 @@ describe('Range Explicit Encryption', function () {
   let encryptedTwoHundred;
   let compareNumericValues;
   for (const { type: dataType, rangeOptions, factory } of dataTypes) {
-    context(`datatype ${dataType}`, async function () {
+    context(`datatype ${dataType}`, function () {
       beforeEach(async function () {
         compareNumericValues = function (value: unknown, expected: number): void {
           if (dataType === 'DoubleNoPrecision' || dataType === 'DoublePrecision') {
