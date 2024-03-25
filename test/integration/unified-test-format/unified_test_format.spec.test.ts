1534985324190ec76a0e5d061d032cba23ca7f46
Bailey Pearson <bailey.pearson@mongodb.com>
2024-03-25 15:35:21 -0600

    asdf

--- test/integration/unified-test-format/unified_test_format.spec.test.ts
+++ test/integration/unified-test-format/unified_test_format.spec.test.ts
@@ -3,12 +3,6 @@ import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 import { type TestFilter } from '../../tools/unified-spec-runner/schema';
 
 const filter: TestFilter = ({ description }) => {
-  if (description === 'unpin after transient error within a transaction and commit') {
-    // OLD COMMENT: commitTransaction retry seems to be swallowed by mongos in this case
-    // TODO(NODE-3943):
-    return `TODO(NODE-3943): commitTransaction retry seems to be swallowed by mongos in this case`;
-  }
-
   if (description === 'Client side error in command starting transaction') {
     // TODO(NODE-2034): Will be implemented as part of NODE-2034
     return 'TODO(NODE-2034): Specify effect of client-side errors on in-progress transactions';
@@ -17,10 +11,6 @@ const filter: TestFilter = ({ description }) => {
   if (description === 'Dirty explicit session is discarded') {
     // TODO(NODE-3951): investigate why this is failing while the legacy version is passing
     return 'TODO(NODE-3951): investigate why this is failing while the legacy version is passing';
-  }
-
-  if (description === 'A successful find event with a getmore and the server kills the cursor') {
-    return 'TODO(NODE-3308): failures due unnecessary getMore and killCursors calls in 5.0';
   }
 
   if (
