d7340a30561f7e5635c03ca17080f753a6b4b9db
Aditi Khare <aditi.khare@mongodb.com>
2024-03-25 15:35:55 -0400

    added ttests to skip in NODE-6047

--- test/integration/index-management/search-index-management.test.ts
+++ test/integration/index-management/search-index-management.test.ts
@@ -1,50 +1,84 @@
 import { expect } from 'chai';
 
-import type { Collection, CommandStartedEvent, MongoClient } from '../../mongodb';
+import { type Collection, type CommandStartedEvent, type MongoClient } from '../../mongodb';
 
 describe('Search Index Management Integration Tests', function () {
   describe('read concern and write concern ', function () {
-    context('when operation is run withTransaction', function () {
-      let client: MongoClient;
-      let collection: Collection;
-      let commandStartedEvents: CommandStartedEvent[];
+    let client: MongoClient;
+    let collection: Collection;
+    let commandStartedEvents: CommandStartedEvent[];
 
-      beforeEach(async function () {
-        client = this.configuration.newClient({}, { monitorCommands: true });
-        await client.connect();
-        collection = client.db('client').collection('searchIndexManagement');
-        commandStartedEvents = [];
-        client.on('commandStarted', e => commandStartedEvents.push(e));
-      });
+    beforeEach(async function () {
+      client = this.configuration.newClient({}, { monitorCommands: true });
+      await client.connect();
+      collection = client.db('client').collection('searchIndexManagement');
+      commandStartedEvents = [];
+      client.on('commandStarted', e => commandStartedEvents.push(e));
+    });
 
-      afterEach(async function () {
-        await client.close();
-      });
+    afterEach(async function () {
+      await client.close();
+    });
 
-      context('when operation is listSearchIndexes', function () {
-        it('should not include write concern or read concern in command', async function () {
-          client.withSession(session => {
-            return session.withTransaction(
-              async () => {
-                expect(session.transaction.isStarting).to.equal(true);
-                expect(session.transaction.isActive).to.equal(true);
-                try {
-                  const res = collection.listSearchIndexes({ session });
-                  const arrayedRes = await res.toArray();
-                  console.log(arrayedRes);
-                } catch (e) {
-                  expect(e.errmsg).to.match(/^.*Atlas.*$/);
-                } finally {
-                  expect(commandStartedEvents[0]).to.exist;
-                  expect(commandStartedEvents[0].command.readConcern).to.not.exist;
-                  expect(commandStartedEvents[0].command.writeConcern).to.not.exist;
-                }
-              },
-              { readConcern: 'local', writeConcern: { w: 1 } }
-            );
-          });
+    context('when listSearchIndexes operation is run withTransaction', function () {
+      it.skip('should not include write concern or read concern in command', async function () {
+        await client.withSession(session => {
+          return session.withTransaction(
+            async () => {
+              expect(session.transaction.isStarting).to.equal(true);
+              expect(session.transaction.isActive).to.equal(true);
+              try {
+                const res = collection.listSearchIndexes({ session });
+                await res.toArray();
+              } catch (e) {
+                expect(e.errmsg).to.match(/^.*Atlas.*$/);
+              } finally {
+                expect(commandStartedEvents[0]).to.exist;
+                expect(commandStartedEvents[0].command.readConcern).to.not.exist;
+                expect(commandStartedEvents[0].command.writeConcern).to.not.exist;
+              }
+            },
+            { readConcern: 'local', writeConcern: { w: 1 } }
+          );
         });
-      });
+      }).skipReason =
+        'TODO(NODE-6047): Ignore read/write concern in applySession for Atlas Search Index Helpers';
+    });
+
+    context('when listSearchIndexes operation is run with causalConsistency', function () {
+      it.skip('should not include write concern or read concern in command', async function () {
+        await client.withSession({ causalConsistency: true }, async session => {
+          try {
+            const res = collection.listSearchIndexes({ session });
+            await res.toArray();
+          } catch (e) {
+            expect(e.errmsg).to.match(/^.*Atlas.*$/);
+          } finally {
+            expect(commandStartedEvents[0]).to.exist;
+            expect(commandStartedEvents[0].command.readConcern).to.not.exist;
+            expect(commandStartedEvents[0].command.writeConcern).to.not.exist;
+          }
+        });
+      }).skipReason =
+        'TODO(NODE-6047): Ignore read/write concern in applySession for Atlas Search Index Helpers';
+    });
+
+    context('when listSearchIndexes operation is run with snapshot on', function () {
+      it.skip('should not include write concern or read concern in command', async function () {
+        await client.withSession({ snapshot: true }, async session => {
+          try {
+            const res = collection.listSearchIndexes({ session });
+            await res.toArray();
+          } catch (e) {
+            expect(e.errmsg).to.match(/^.*Atlas.*$/);
+          } finally {
+            expect(commandStartedEvents[0]).to.exist;
+            expect(commandStartedEvents[0].command.readConcern).to.not.exist;
+            expect(commandStartedEvents[0].command.writeConcern).to.not.exist;
+          }
+        });
+      }).skipReason =
+        'TODO(NODE-6047): Ignore read/write concern in applySession for Atlas Search Index Helpers';
     });
   });
 });
