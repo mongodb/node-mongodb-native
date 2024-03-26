be2e640d1ffa7ef5d8492bc0c8df225b5fc1fe2e
Warren James <warren.james@mongodb.com>
2024-03-25 21:14:35 -0400

    lint fix

--- test/tools/unified-spec-runner/match.ts
+++ test/tools/unified-spec-runner/match.ts
@@ -38,7 +38,6 @@ import {
 } from '../../mongodb';
 import { ejson } from '../utils';
 import { type CmapEvent, type CommandEvent, type EntitiesMap, type SdamEvent } from './entities';
-import { trace } from './runner';
 import {
   type ExpectedCmapEvent,
   type ExpectedCommandEvent,
