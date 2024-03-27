da3fe1419fedee8347071159b0416966f3aee126
Bailey Pearson <bailey.pearson@mongodb.com>
2024-03-27 08:43:12 -0600

    remove skipped test

--- test/integration/command-logging-and-monitoring/command_logging_and_monitoring.spec.test.ts
+++ test/integration/command-logging-and-monitoring/command_logging_and_monitoring.spec.test.ts
@@ -5,22 +5,7 @@ import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
 describe('Command Logging and Monitoring Spec', function () {
   describe('Command Monitoring Spec (unified)', () => {
-    runUnifiedSuite(
-      loadSpecTests(path.join('command-logging-and-monitoring', 'monitoring')),
-      ({ description }) => {
-        // This is skipped because our command monitoring happens at the connection
-        // level and is using the server reply for the single insert that the bulk
-        // performed since it was only one document in the test. The test expectation
-        // is that we are using the bulk write result which was returned to the user
-        // as the reply in the command succeeded event instead of our raw reply from
-        // the server. There's nothing we can change here.
-        return description.includes(
-          'A successful unordered bulk write with an unacknowledged write concern'
-        )
-          ? `Test not applicable to Node.`
-          : false;
-      }
-    );
+    runUnifiedSuite(loadSpecTests(path.join('command-logging-and-monitoring', 'monitoring')));
   });
 
   describe('Command Logging Spec', () => {
