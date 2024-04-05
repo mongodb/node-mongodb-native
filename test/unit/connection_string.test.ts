11f26e18643b3c5f23ff0260a3c17a3cbbad00a6
Warren James <warren.james@mongodb.com>
2024-04-05 14:57:39 -0400

    revert duplicated tests

--- test/unit/connection_string.test.ts
+++ test/unit/connection_string.test.ts
@@ -14,7 +14,6 @@ import {
   DOCUMENT_DB_MSG,
   FEATURE_FLAGS,
   type Log,
-  Long,
   MongoAPIError,
   MongoClient,
   MongoCredentials,
@@ -921,56 +920,6 @@ describe('Connection String', function () {
         ).to.throw(MongoAPIError);
       });
     });
-  });
-
-  describe('when timeoutMS is provided as a client option', function () {
-    const connString = 'mongodb://host';
-    const types = ['number', 'bigint', 'Long'];
-    const values = [-100, 0, 100];
-
-    for (const type of types) {
-      describe(`when timeoutMS provided as a ${type}`, function () {
-        for (const v of values) {
-          const timeoutMS =
-            type === 'number' ? v : type === 'bigint' ? BigInt(v) : Long.fromNumber(v);
-          describe(`when timeoutMS is ${
-            v < 0 ? 'negative' : v > 0 ? 'positive' : 'zero'
-          }`, function () {
-            if (v >= 0) {
-              it('sets the value correctly as a number', function () {
-                const options = parseOptions(connString, { timeoutMS });
-                expect(options.timeoutMS).to.equal(v);
-              });
-            } else {
-              it('throws MongoParseError', function () {
-                expect(() => parseOptions(connString, { timeoutMS })).to.throw(MongoParseError);
-              });
-            }
-          });
-        }
-      });
-    }
-  });
-
-  describe('when timeoutMS is provided in the connection string', function () {
-    const values = [-100, 0, 100];
-    for (const v of values) {
-      const connString = `mongodb://host?timeoutMS=${v}`;
-      describe(`when timeoutMS is ${
-        v < 0 ? 'negative' : v > 0 ? 'positive' : 'zero'
-      }`, function () {
-        if (v >= 0) {
-          it('sets the value correctly as a number', function () {
-            const options = parseOptions(connString);
-            expect(options.timeoutMS).to.equal(v);
-          });
-        } else {
-          it('throws MongoParseError', function () {
-            expect(() => parseOptions(connString)).to.throw(MongoParseError);
-          });
-        }
-      });
-    }
   });
 
   describe('non-genuine hosts', () => {
