26d2130492a1b66f17f50a8a6909df37a7598e25
Bailey Pearson <bailey.pearson@mongodb.com>
2024-04-05 10:10:37 -0600

    restrict integration test to only run on replsets

--- test/integration/node-specific/errors.test.ts
+++ test/integration/node-specific/errors.test.ts
@@ -12,29 +12,35 @@ describe('Error (Integration)', function () {
   });
 
   context('when a server selection error is stringified', function () {
-    it('the error"s topology description correctly displays the `servers`', async function () {
-      const client: MongoClient = this.configuration.newClient({ serverSelectionTimeoutMS: 1000 });
-      try {
-        await client.connect();
+    it(
+      'the error"s topology description correctly displays the `servers`',
+      { requires: { topology: 'replicaset' } },
+      async function () {
+        const client: MongoClient = this.configuration.newClient({
+          serverSelectionTimeoutMS: 1000
+        });
+        try {
+          await client.connect();
 
-        const error = await client
-          .db('foo')
-          .collection('bar')
-          .find(
-            {},
-            {
-              // Use meaningless read preference tags to ensure that the server selection fails
-              readPreference: new ReadPreference('secondary', [{ ny: 'ny' }])
-            }
-          )
-          .toArray()
-          .catch(e => JSON.parse(JSON.stringify(e)));
+          const error = await client
+            .db('foo')
+            .collection('bar')
+            .find(
+              {},
+              {
+                // Use meaningless read preference tags to ensure that the server selection fails
+                readPreference: new ReadPreference('secondary', [{ ny: 'ny' }])
+              }
+            )
+            .toArray()
+            .catch(e => JSON.parse(JSON.stringify(e)));
 
-        const servers = error.reason.servers;
-        expect(Object.keys(servers).length > 0).to.be.true;
-      } finally {
-        await client.close();
+          const servers = error.reason.servers;
+          expect(Object.keys(servers).length > 0).to.be.true;
+        } finally {
+          await client.close();
+        }
       }
-    });
+    );
   });
 });
