acb2c67b60aad8354c34d18cf369bab25cd7ba88
Bailey Pearson <bailey.pearson@mongodb.com>
2024-04-10 22:49:41 -0600

    fix(NODE-5530): make topology descriptions JSON stringifiable  (#4070)
    Co-authored-by: Aditi Khare <106987683+aditi-khare-mongoDB@users.noreply.github.com>

---
+++ test/integration/node-specific/errors.test.ts
@@ -0,0 +1,46 @@
+import { expect } from 'chai';
+
+import { MongoClient, MongoServerSelectionError, ReadPreference } from '../../mongodb';
+
+describe('Error (Integration)', function () {
+  it('NODE-5296: handles aggregate errors from dns lookup', async function () {
+    const error = await MongoClient.connect('mongodb://localhost:27222', {
+      serverSelectionTimeoutMS: 1000
+    }).catch(e => e);
+    expect(error).to.be.instanceOf(MongoServerSelectionError);
+    expect(error.message).not.to.be.empty;
+  });
+
+  context('when a server selection error is stringified', function () {
+    it(
+      'the error"s topology description correctly displays the `servers`',
+      { requires: { topology: 'replicaset' } },
+      async function () {
+        const client: MongoClient = this.configuration.newClient({
+          serverSelectionTimeoutMS: 1000
+        });
+        try {
+          await client.connect();
+
+          const error = await client
+            .db('foo')
+            .collection('bar')
+            .find(
+              {},
+              {
+                // Use meaningless read preference tags to ensure that the server selection fails
+                readPreference: new ReadPreference('secondary', [{ ny: 'ny' }])
+              }
+            )
+            .toArray()
+            .catch(e => JSON.parse(JSON.stringify(e)));
+
+          const servers = error.reason.servers;
+          expect(Object.keys(servers).length > 0).to.be.true;
+        } finally {
+          await client.close();
+        }
+      }
+    );
+  });
+});
