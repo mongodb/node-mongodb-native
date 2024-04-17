2b474947bc01a248252bf4cd72d0f1b888990bc0
Durran Jordan <durran@gmail.com>
2024-04-17 19:34:47 +0200

    test(NODE-6097): fix azure kms tests

--- .evergreen/copy-driver-to-azure-and-run.sh
+++ .evergreen/copy-driver-to-azure-and-run.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/env bash
 
 set -o errexit
+source "${DRIVERS_TOOLS}/.evergreen/csfle/azurekms/secrets-export.sh"
 
 if [ -z ${AZUREKMS_RESOURCEGROUP+omitted} ]; then echo "AZUREKMS_RESOURCEGROUP is unset" && exit 1; fi
 if [ -z ${AZUREKMS_VMNAME+omitted} ]; then echo "AZUREKMS_VMNAME is unset" && exit 1; fi
-if [ -z ${AZUREKMS_PRIVATEKEYPATH+omitted} ]; then echo "AZUREKMS_PRIVATEKEYPATH is unset" && exit 1; fi
 
 source "${PROJECT_DIRECTORY}/.evergreen/init-node-and-npm-env.sh"
+
+export AZUREKMS_PUBLICKEYPATH=/tmp/testazurekms_publickey
+export AZUREKMS_PRIVATEKEYPATH=/tmp/testazurekms_privatekey
 
 echo "compressing node driver source ... begin"
 tar -czf node-driver-source.tgz src
@@ -22,3 +25,8 @@ echo "decompressing node driver tar on azure ... begin"
 export AZUREKMS_CMD="tar xf node-driver-source.tgz"
 "${DRIVERS_TOOLS}/.evergreen/csfle/azurekms/run-command.sh"
 echo "decompressing node driver tar on azure ... end"
+
+echo "Running test ... begin"
+export AZUREKMS_CMD="env EXPECTED_AZUREKMS_OUTCOME=success bash src/.evergreen/run-azure-kms-tests.sh"
+${DRIVERS_TOOLS}/.evergreen/csfle/azurekms/run-command.sh
+echo "Running test ... end"
