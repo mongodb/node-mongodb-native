195bd62321fb39d61e0bc369cf07529fe2343b8b
Neal Beeken <neal.beeken@mongodb.com>
2024-04-23 16:51:07 -0400

    fix: stream cannot use cursor's next because it reaches exhaustion error in changestream prose test

--- src/cursor/abstract_cursor.ts
+++ src/cursor/abstract_cursor.ts
@@ -961,7 +961,7 @@ class ReadableCursorStream extends Readable {
 
   private _readNext() {
     // eslint-disable-next-line github/no-then
-    this._cursor.next().then(
+    next(this._cursor, { blocking: true, transform: true, shift: true }).then(
       result => {
         if (result == null) {
           this.push(null);
