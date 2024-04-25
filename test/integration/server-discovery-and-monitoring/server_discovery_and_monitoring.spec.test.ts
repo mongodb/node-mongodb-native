ca2bfb0c536b665862a944c214e7cc51b49dbf1e
Warren James <warren.james@mongodb.com>
2024-04-25 11:16:00 -0400

    test(NODE-6052): support assertions on topologyDescriptionChanged in expectEvents (#4089)
    Co-authored-by: Aditi Khare <106987683+aditi-khare-mongoDB@users.noreply.github.com>

--- test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
+++ test/integration/server-discovery-and-monitoring/server_discovery_and_monitoring.spec.test.ts
@@ -3,11 +3,26 @@ import * as path from 'path';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
+const skipTable: { pattern: string; reason: string }[] = [
+  {
+    pattern: 'Topology lifecycle',
+    reason: 'TODO(NODE-5723): Need to implement DRIVERS-2711 spec change'
+  },
+  {
+    pattern: 'connect with serverMonitoringMode=stream >=4.4',
+    reason: 'TODO(NODE-6045): Ensure that first server hearbeat does not report that it is awaited'
+  },
+  {
+    pattern: 'connect with serverMonitoringMode=auto >=4.4',
+    reason: 'TODO(NODE-6045): Ensure that first server hearbeat does not report that it is awaited'
+  }
+];
+
 describe('SDAM Unified Tests (Spec)', function () {
   const specTests = loadSpecTests(path.join('server-discovery-and-monitoring', 'unified'));
   runUnifiedSuite(specTests, test => {
-    if (['Topology lifecycle'].includes(test.description)) {
-      return 'see NODE-5723';
+    for (const { pattern, reason } of skipTable) {
+      if (test.description.includes(pattern)) return reason;
     }
     return false;
   });
