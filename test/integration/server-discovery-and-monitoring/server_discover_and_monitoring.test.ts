f7b5d0c39bdb986a786a8d68f84c89c7ec64babd
Warren James <warren.james@mongodb.com>
2024-05-13 23:37:00 -0400

    Add test to verify that rtt is never reported as zero for a successfull heartbeat

--- test/integration/server-discovery-and-monitoring/server_discover_and_monitoring.test.ts
+++ test/integration/server-discovery-and-monitoring/server_discover_and_monitoring.test.ts
@@ -1,3 +1,8 @@
+import { EventEmitter, once } from 'node:events';
+
+import { expect } from 'chai';
+
+import { type MongoClient, type ServerHeartbeatSucceededEvent } from '../../mongodb';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
@@ -7,4 +12,45 @@ describe('SDAM Unified Tests (Node Driver)', function () {
     '../integration/server-discovery-and-monitoring/unified-sdam-node-specs'
   );
   runUnifiedSuite(clonedAndAlteredSpecTests);
+});
+
+describe('Monitoring rtt tests', function () {
+  let client: MongoClient;
+  let durations: number[];
+  const thresh = 40;
+  const ee = new EventEmitter();
+  const listener = (ev: ServerHeartbeatSucceededEvent) => {
+    durations.push(ev.duration);
+    if (durations.length >= thresh) {
+      client.off('serverHeartbeatSucceeded', listener);
+      ee.emit('done');
+    }
+  };
+
+  beforeEach(function () {
+    durations = [];
+  });
+
+  afterEach(async function () {
+    await client?.close();
+  });
+
+  for (const serverMonitoringMode of ['poll', 'stream']) {
+    context(`when serverMonitoringMode is set to '${serverMonitoringMode}'`, function () {
+      beforeEach(async function () {
+        client = this.configuration.newClient({
+          heartbeatFrequencyMS: 100,
+          serverMonitoringMode
+        });
+        client.on('serverHeartbeatSucceeded', listener);
+
+        await client.connect();
+      });
+
+      it('duration of a successful heartbeat is never reported as 0ms', async function () {
+        await once(ee, 'done');
+        expect(durations.every(x => x !== 0)).to.be.true;
+      });
+    });
+  }
 });
