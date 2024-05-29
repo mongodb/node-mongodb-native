8cb6c064f20520a7c61a49313dec920b74ab4695
Durran Jordan <durran@gmail.com>
2024-05-29 09:51:56 -0400

    test: update for DRIVERS-2915

--- test/unit/assorted/auth.spec.test.ts
+++ test/unit/assorted/auth.spec.test.ts
@@ -1,9 +1,6 @@
 import { loadSpecTests } from '../../spec';
 import { executeUriValidationTest } from '../../tools/uri_spec_runner';
 
-// TODO(NODE-6172): Handle commas in TOKEN_RESOURCE.
-const SKIP = 'should handle a complicated url-encoded TOKEN_RESOURCE (MONGODB-OIDC)';
-
 describe('Auth option spec tests (legacy)', function () {
   const suites = loadSpecTests('auth', 'legacy');
 
@@ -11,9 +8,6 @@ describe('Auth option spec tests (legacy)', function () {
     describe(suite.name, function () {
       for (const test of suite.tests) {
         it(`${test.description}`, function () {
-          if (test.description === SKIP) {
-            this.test.skip();
-          }
           executeUriValidationTest(test);
         });
       }
