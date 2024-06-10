00a098a340e6eda52b6b4452875d32ce6664388e
Warren James <warren.james@mongodb.com>
2024-06-10 10:46:02 -0400

    test(NODE-6120): Implement Unified test runner changes for CSOT (#4121)

--- test/tools/unified-spec-runner/match.ts
+++ test/tools/unified-spec-runner/match.ts
@@ -25,6 +25,7 @@ import {
   MongoBulkWriteError,
   MongoClientBulkWriteError,
   MongoError,
+  MongoOperationTimeoutError,
   MongoServerError,
   ObjectId,
   type OneOrMore,
@@ -98,6 +99,19 @@ export function isMatchAsRootOperator(value: unknown): value is MatchAsRootOpera
   return typeof value === 'object' && value != null && '$$matchAsRoot' in value;
 }
 
+export interface LteOperator {
+  $$lte: number;
+}
+
+export function isLteOperator(value: unknown): value is LteOperator {
+  return (
+    typeof value === 'object' &&
+    value != null &&
+    '$$lte' in value &&
+    typeof value['$$lte'] === 'number'
+  );
+}
+
 export const SpecialOperatorKeys = [
   '$$exists',
   '$$type',
@@ -106,7 +120,8 @@ export const SpecialOperatorKeys = [
   '$$matchAsRoot',
   '$$matchAsDocument',
   '$$unsetOrMatches',
-  '$$sessionLsid'
+  '$$sessionLsid',
+  '$$lte'
 ];
 
 export type SpecialOperator =
@@ -117,7 +132,8 @@ export type SpecialOperator =
   | UnsetOrMatchesOperator
   | SessionLsidOperator
   | MatchAsDocumentOperator
-  | MatchAsRootOperator;
+  | MatchAsRootOperator
+  | LteOperator;
 
 type KeysOfUnion<T> = T extends object ? keyof T : never;
 export type SpecialOperatorKey = KeysOfUnion<SpecialOperator>;
@@ -130,7 +146,8 @@ export function isSpecialOperator(value: unknown): value is SpecialOperator {
     isUnsetOrMatchesOperator(value) ||
     isSessionLsidOperator(value) ||
     isMatchAsRootOperator(value) ||
-    isMatchAsDocumentOperator(value)
+    isMatchAsDocumentOperator(value) ||
+    isLteOperator(value)
   );
 }
 
@@ -390,6 +407,9 @@ export function specialCheck(
     );
 
     resultCheck(actual, expected.$$matchAsRoot as any, entities, path, false);
+  } else if (isLteOperator(expected)) {
+    expect(typeof actual).to.equal('number');
+    expect(actual).to.be.lte(expected.$$lte);
   } else {
     expect.fail(`Unknown special operator: ${JSON.stringify(expected)}`);
   }
@@ -757,6 +777,12 @@ export function expectErrorCheck(
     } else {
       expect(error).not.to.be.instanceOf(MongoServerError);
     }
+  }
+
+  if (expected.isTimeoutError === false) {
+    expect(error).to.not.be.instanceof(MongoOperationTimeoutError);
+  } else if (expected.isTimeoutError === true) {
+    expect(error).to.be.instanceof(MongoOperationTimeoutError);
   }
 
   if (expected.errorContains != null) {
