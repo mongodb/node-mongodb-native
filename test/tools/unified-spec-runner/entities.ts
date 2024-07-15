6f9925c928e5d38ef2865c3fe3def70265d3115f
Warren James <warren.james@mongodb.com>
2024-07-15 13:50:27 -0400

    don't ejsonify logs

--- test/tools/unified-spec-runner/entities.ts
+++ test/tools/unified-spec-runner/entities.ts
@@ -44,7 +44,7 @@ import {
   type TopologyOpeningEvent,
   WriteConcern
 } from '../../mongodb';
-import { ejson, getEnvironmentalOptions } from '../../tools/utils';
+import { getEnvironmentalOptions } from '../../tools/utils';
 import type { TestConfiguration } from '../runner/config';
 import { EntityEventRegistry } from './entity_event_registry';
 import { trace } from './runner';
