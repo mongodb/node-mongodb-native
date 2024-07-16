edf63e4acaa21e3912612632d48a1c31254cc146
Warren James <warren.james@mongodb.com>
2024-07-16 15:37:27 -0400

    unskip tests

--- test/integration/client-side-operations-timeout/client_side_operations_timeout.spec.test.ts
+++ test/integration/client-side-operations-timeout/client_side_operations_timeout.spec.test.ts
@@ -23,19 +23,13 @@ describe('CSOT spec tests', function () {
   for (const spec of specs) {
     for (const test of spec.tests) {
       // not one of the test suites listed in kickoff
-      if (!enabled.includes(spec.name)) test.skipReason = 'Not working yet';
+      if (!enabled.includes(spec.name)) {
+        test.skipReason = 'TODO(NODE-5684): Not working yet';
+      }
 
       // Cursor operation
       if (test.operations.find(operation => cursorOperations.includes(operation.name)))
-        test.skipReason = 'Not working yet';
-
-      // runCommand only uses options directly passed to it
-      if (
-        test.operations.find(
-          operation => operation.name === 'runCommand' && operation.arguments.timeoutMS == null
-        )
-      )
-        test.skipReason = 'Not working yet';
+        test.skipReason = 'TODO(NODE-5684): Not working yet';
     }
   }
   runUnifiedSuite(specs);
