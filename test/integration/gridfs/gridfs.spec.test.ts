caaa86c28bab571346d8a9617669b1ebaab49c25
Bailey Pearson <bailey.pearson@mongodb.com>
2024-07-17 10:10:27 -0600

    Update test/integration/gridfs/gridfs.spec.test.ts

--- test/integration/gridfs/gridfs.spec.test.ts
+++ test/integration/gridfs/gridfs.spec.test.ts
@@ -3,6 +3,6 @@ import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
 describe('GridFS Unified Tests', function () {
   runUnifiedSuite(loadSpecTests('gridfs'), ({ description }) => {
-    return description === 'download when final chunk is missing' ? `TODO(NODE-xxxx)` : false;
+    return description === 'download when final chunk is missing' ? `TODO(NODE-6279): throw a missing chunk error when last chunk is missing` : false;
   });
 });
