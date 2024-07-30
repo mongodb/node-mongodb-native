8e20e0c60138c20b73b16370e35d9a2662da2473
Bailey Pearson <bailey.pearson@mongodb.com>
2024-07-30 11:55:54 -0600

    tabs to spaces

--- test/explicit-resource-management/main.test.ts
+++ test/explicit-resource-management/main.test.ts
@@ -6,79 +6,79 @@ import { pipeline } from 'stream/promises';
 import { setTimeout } from 'timers/promises';
 
 async function setUpCollection(client: MongoClient) {
-	const collection = client.db('foo').collection<{ name: string }>('bar');
-	const documents: Array<{ name: string }> = Array.from({ length: 5 }).map(i => ({
-		name: String(i)
-	}));
-	await collection.insertMany(documents)
-	return collection;
+  const collection = client.db('foo').collection<{ name: string }>('bar');
+  const documents: Array<{ name: string }> = Array.from({ length: 5 }).map(i => ({
+    name: String(i)
+  }));
+  await collection.insertMany(documents)
+  return collection;
 }
 
 describe('explicit resource management smoke tests', function () {
-	describe('MongoClient', function () {
-		it('does not crash or error when used with await-using syntax', async function () {
+  describe('MongoClient', function () {
+    it('does not crash or error when used with await-using syntax', async function () {
             await using client = new MongoClient(process.env.MONGODB_URI!);
             await client.connect();
-		})
-	})
+    })
+  })
 
-	describe('Cursors', function () {
-		it('does not crash or error when used with await-using syntax', async function () {
-			await using client = new MongoClient(process.env.MONGODB_URI!);
-            await client.connect();
+  describe('Cursors', function () {
+    it('does not crash or error when used with await-using syntax', async function () {
+      await using client = new MongoClient(process.env.MONGODB_URI!);
+      await client.connect();
 
-            const collection = await setUpCollection(client);
+      const collection = await setUpCollection(client);
 
-            await using cursor = collection.find();
-            await cursor.next();
-            await cursor.next();
-            await cursor.next();
-		})
+      await using cursor = collection.find();
+      await cursor.next();
+      await cursor.next();
+      await cursor.next();
+    })
 
-		describe('cursor streams', function() {
-			it('does not crash or error when used with await-using syntax', async function() {
-                await using client = new MongoClient(process.env.MONGODB_URI!);
-                await client.connect();
+    describe('cursor streams', function() {
+      it('does not crash or error when used with await-using syntax', async function() {
+        await using client = new MongoClient(process.env.MONGODB_URI!);
+        await client.connect();
 
-                const collection = await setUpCollection(client);
+        const collection = await setUpCollection(client);
 
-                await using readable = collection.find().stream();
-			})
-		})
-	})
+        await using readable = collection.find().stream();
+      })
+    })
+  })
 
-	describe('Sessions', function () {
-		it('does not crash or error when used with await-using syntax', async function () {
-            await using client = new MongoClient(process.env.MONGODB_URI!);
-            await client.connect();
+  describe('Sessions', function () {
+    it('does not crash or error when used with await-using syntax', async function () {
+      await using client = new MongoClient(process.env.MONGODB_URI!);
+      await client.connect();
 
-            await using session = client.startSession();
-		})
-	})
+      await using session = client.startSession();
+    })
+  })
 
-	describe('ChangeStreams', function () {
-		it('does not crash or error when used with await-using syntax', async function () {
-			await using client = new MongoClient(process.env.MONGODB_URI!);
-            await client.connect();
+  describe('ChangeStreams', function () {
+    it('does not crash or error when used with await-using syntax', async function () {
+      await using client = new MongoClient(process.env.MONGODB_URI!);
+      await client.connect();
 
-            const collection = await setUpCollection(client);
-            await using cs = collection.watch();
+      const collection = await setUpCollection(client);
+      await using cs = collection.watch();
 
-            setTimeout(1000).then(() => collection.insertOne({ name: 'bailey' }));
-            await cs.next();
-		})
-	});
+      setTimeout(1000).then(() => collection.insertOne({ name: 'bailey' }));
+      await cs.next();
+    })
+  });
 
-	describe('GridFSDownloadStream', function () {
-		it('does not crash or error when used with await-using syntax', async function () {
-            await using client = new MongoClient(process.env.MONGODB_URI!);
-            await client.connect();
+  describe('GridFSDownloadStream', function () {
+    it('does not crash or error when used with await-using syntax', async function () {
+      await using client = new MongoClient(process.env.MONGODB_URI!);
+      await client.connect();
 
-            const bucket = new GridFSBucket(client.db('foo'));
-            const uploadStream = bucket.openUploadStream('foo.txt')
-            await pipeline(Readable.from("AAAAAAA".split('')), uploadStream);
+      const bucket = new GridFSBucket(client.db('foo'));
+      const uploadStream = bucket.openUploadStream('foo.txt')
+      await pipeline(Readable.from("AAAAAAA".split('')), uploadStream);
 
-            await using downloadStream = bucket.openDownloadStreamByName('foo.txt');
-		})
-	});
+      await using downloadStream = bucket.openDownloadStreamByName('foo.txt');
+    })
+  });
 })
