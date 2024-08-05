cc424d5c2df5d68579440ed6b520df663a217a9d
Bailey Pearson <bailey.pearson@mongodb.com>
2024-08-05 14:26:06 -0600

    remove void0 assignment

--- src/beta.ts
+++ src/beta.ts
@@ -7,12 +7,11 @@ export * from './index';
  *
  * Since we don't bundle tslib helpers, we need to polyfill this method.
  *
- * This is used in the generated JS.
+ * This is used in the generated JS.  Adapted from https://github.com/microsoft/TypeScript/blob/aafdfe5b3f76f5c41abeec412ce73c86da94c75f/src/compiler/factory/emitHelpers.ts#L1202.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function __exportStar(mod: Document) {
   for (const key of Object.keys(mod)) {
-    exports[key] = void 0;
     Object.defineProperty(exports, key, {
       enumerable: true,
       get: function () {
