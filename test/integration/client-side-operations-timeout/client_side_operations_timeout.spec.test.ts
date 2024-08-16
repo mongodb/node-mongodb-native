0bc7793c700429ebab72e56f905e8d32e2546d80
Warren James <warren.james@mongodb.com>
2024-08-16 17:40:03 -0400

    rework test skip logic

--- test/integration/client-side-operations-timeout/client_side_operations_timeout.spec.test.ts
+++ test/integration/client-side-operations-timeout/client_side_operations_timeout.spec.test.ts
@@ -10,27 +10,48 @@ const enabled = [
   'retryability-legacy-timeouts',
   'retryability-timeoutMS',
   'runCursorCommand',
-  'close-cursors'
+  'close-cursors',
+  'non-tailable-cursors',
+  'global-timeoutMS',
+  'legacy-timeouts',
+  'command-execution',
+  'cursors',
+  'error-transformations'
 ];
 
-const bulkWriteOperations = [
-  'timeoutMS applies to whole operation, not individual attempts - bulkWrite on collection',
-  'timeoutMS applies to whole operation, not individual attempts - insertMany on collection'
-];
+const skipped = {
+  bulkWrite: 'TODO(NODE-6274)',
+  'change-streams': 'TODO(NODE-6035)',
+  'convenient-transactions': 'TODO(NODE-5687)',
+  //'deprecated-options': 'TODO(NODE-5689)',
+  'gridfs-advanced': 'TODO(NODE-6275)',
+  'gridfs-delete': 'TODO(NODE-6275)',
+  'gridfs-download': 'TODO(NODE-6275)',
+  'gridfs-find': 'TODO(NODE-6275)',
+  'gridfs-upload': 'TODO(NODE-6275)',
+  'sessions-inherit-timeoutMS': 'TODO(NODE-5687)',
+  'sessions-override-operation-timeoutMS': 'TODO(NODE-5687)',
+  'sessions-override-timeoutMS': 'TODO(NODE-5687)',
+  'tailable-awaitData': 'TODO(NODE-6035)',
+  'tailable-non-awaitData': 'TODO(NODE-6035)'
+};
+
+const bulkWriteOperations =
+  /timeoutMS applies to whole operation, not individual attempts - (bulkWrite|insertMany) on .*/;
 
 describe('CSOT spec tests', function () {
   const specs = loadSpecTests(join('client-side-operations-timeout'));
   for (const spec of specs) {
     for (const test of spec.tests) {
-      // not one of the test suites listed in kickoff
-      if (!enabled.includes(spec.name)) {
-        test.skipReason = 'TODO(NODE-5684): Not working yet';
+      if (skipped[spec.name] != null) {
+        test.skipReason = skipped[spec.name];
       }
 
-      if (bulkWriteOperations.includes(test.description))
+      if (bulkWriteOperations.test(test.description))
         test.skipReason =
           'TODO(NODE-6274): update test runner to check errorResponse field of MongoBulkWriteError in isTimeoutError assertion';
     }
   }
+
   runUnifiedSuite(specs);
 });
