abc1cfdf6662b3e311168c4b84c0b3595144ae79
Warren James <warren.james@mongodb.com>
2024-08-21 14:44:34 -0400

    skipping spec tests

--- test/integration/client-side-operations-timeout/client_side_operations_timeout.spec.test.ts
+++ test/integration/client-side-operations-timeout/client_side_operations_timeout.spec.test.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
-const skipped = {
+const skippedSpecs = {
   bulkWrite: 'TODO(NODE-6274)',
   'change-streams': 'TODO(NODE-6035)',
   'convenient-transactions': 'TODO(NODE-5687)',
@@ -20,20 +20,31 @@ const skipped = {
   'tailable-non-awaitData': 'TODO(NODE-6035)'
 };
 
-const bulkWriteOperations =
-  /timeoutMS applies to whole operation, not individual attempts - (bulkWrite|insertMany) on .*/;
+const skippedTests = {
+  'timeoutMS can be configured on a MongoClient - insertMany on collection': 'TODO(NODE-6274)',
+  'timeoutMS can be configured on a MongoClient - bulkWrite on collection': 'TODO(NODE-6274)',
+  'timeoutMS can be configured on a MongoClient - createChangeStream on client': 'TODO(NODE-6305)',
+  'timeoutMS applies to whole operation, not individual attempts - createChangeStream on client':
+    'TODO(NODE-6305)',
+  'Tailable cursor iteration timeoutMS is refreshed for getMore - failure': 'TODO(NODE-6305)',
+  'Tailable cursor awaitData iteration timeoutMS is refreshed for getMore - failure':
+    'TODO(NODE-6305)',
+  'timeoutMS applies to whole operation, not individual attempts - insertMany on collection':
+    'TODO(NODE-6274)',
+  'timeoutMS applies to whole operation, not individual attempts - bulkWrite on collection':
+    'TODO(NODE-6274)'
+};
 
 describe('CSOT spec tests', function () {
   const specs = loadSpecTests(join('client-side-operations-timeout'));
   for (const spec of specs) {
     for (const test of spec.tests) {
-      if (skipped[spec.name] != null) {
-        test.skipReason = skipped[spec.name];
+      if (skippedSpecs[spec.name] != null) {
+        test.skipReason = skippedSpecs[spec.name];
       }
-
-      if (bulkWriteOperations.test(test.description))
-        test.skipReason =
-          'TODO(NODE-6274): update test runner to check errorResponse field of MongoBulkWriteError in isTimeoutError assertion';
+      if (skippedTests[test.description] != null) {
+        test.skipReason = skippedTests[test.description];
+      }
     }
   }
 
