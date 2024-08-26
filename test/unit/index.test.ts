2ac4b0fcc244550209da5f59d7c20f94cf6ac3cf
Warren James <warren.james@mongodb.com>
2024-08-26 14:56:18 -0400

    update export test

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -48,6 +48,8 @@ const EXPECTED_EXPORTS = [
   'ConnectionPoolMonitoringEvent',
   'ConnectionPoolReadyEvent',
   'ConnectionReadyEvent',
+  'CursorInitializeOptions',
+  'CursorTimeoutMode',
   'CURSOR_FLAGS',
   'Db',
   'DBRef',
