9f63397409ba5e8a4879bc51c7890babcab5b2e0
Durran Jordan <durran@gmail.com>
2024-09-19 16:00:55 +0200

    feat(NODE-6337): implement client bulk write batching

--- src/sdam/topology_description.ts
+++ src/sdam/topology_description.ts
@@ -43,6 +43,8 @@ export class TopologyDescription {
   heartbeatFrequencyMS: number;
   localThresholdMS: number;
   commonWireVersion: number;
+  maxMessageSizeBytes?: number;
+  maxWriteBatchSize?: number;
 
   /**
    * Create a TopologyDescription
@@ -71,6 +73,25 @@ export class TopologyDescription {
 
     // determine server compatibility
     for (const serverDescription of this.servers.values()) {
+      // Find the lowest maxMessageSizeBytes from all the servers.
+      if (this.maxMessageSizeBytes == null) {
+        this.maxMessageSizeBytes = serverDescription.maxMessageSizeBytes;
+      } else {
+        this.maxMessageSizeBytes = Math.min(
+          this.maxMessageSizeBytes,
+          serverDescription.maxMessageSizeBytes
+        );
+      }
+
+      // Find the lowest maxWriteBatchSize from all the servers.
+      if (this.maxWriteBatchSize == null) {
+        this.maxWriteBatchSize = serverDescription.maxWriteBatchSize;
+      } else {
+        this.maxWriteBatchSize = Math.min(
+          this.maxWriteBatchSize,
+          serverDescription.maxWriteBatchSize
+        );
+      }
       // Load balancer mode is always compatible.
       if (
         serverDescription.type === ServerType.Unknown ||
--- test/integration/crud/crud.prose.test.ts
+++ test/integration/crud/crud.prose.test.ts
@@ -3,6 +3,7 @@ import { once } from 'events';
 
 import { type CommandStartedEvent } from '../../../mongodb';
 import {
+  type AnyClientBulkWriteModel,
   type Collection,
   MongoBulkWriteError,
   type MongoClient,
@@ -147,6 +148,136 @@ describe('CRUD Prose Spec Tests', () => {
 
         const errInfoFromEvent = ev.reply.writeErrors[0].errInfo;
         expect(errInfoFromError).to.deep.equal(errInfoFromEvent);
+      }
+    });
+  });
+
+  describe('3. MongoClient.bulkWrite batch splits a writeModels input with greater than maxWriteBatchSize operations', function () {
+    // Test that MongoClient.bulkWrite properly handles writeModels inputs containing a number of writes greater than
+    // maxWriteBatchSize.
+    // This test must only be run on 8.0+ servers. This test must be skipped on Atlas Serverless.
+    // Construct a MongoClient (referred to as client) with command monitoring enabled to observe CommandStartedEvents.
+    // Perform a hello command using client and record the maxWriteBatchSize value contained in the response. Then,
+    // construct the following write model (referred to as model):
+    // InsertOne: {
+    //   "namespace": "db.coll",
+    //   "document": { "a": "b" }
+    // }
+    // Construct a list of write models (referred to as models) with model repeated maxWriteBatchSize + 1 times. Execute
+    // bulkWrite on client with models. Assert that the bulk write succeeds and returns a BulkWriteResult with an
+    // insertedCount value of maxWriteBatchSize + 1.
+    // Assert that two CommandStartedEvents (referred to as firstEvent and secondEvent) were observed for the bulkWrite
+    // command. Assert that the length of firstEvent.command.ops is maxWriteBatchSize. Assert that the length of
+    // secondEvent.command.ops is 1. If the driver exposes operationIds in its CommandStartedEvents, assert that
+    // firstEvent.operationId is equal to secondEvent.operationId.
+    let client: MongoClient;
+    let maxWriteBatchSize;
+    const models: AnyClientBulkWriteModel[] = [];
+    const commands: CommandStartedEvent[] = [];
+
+    beforeEach(async function () {
+      client = this.configuration.newClient({}, { monitorCommands: true });
+      await client.connect();
+      await client.db('db').collection('coll').drop();
+      const hello = await client.db('admin').command({ hello: 1 });
+      maxWriteBatchSize = hello.maxWriteBatchSize;
+
+      client.on('commandStarted', filterForCommands('bulkWrite', commands));
+      commands.length = 0;
+
+      Array.from({ length: maxWriteBatchSize + 1 }, () => {
+        models.push({
+          namespace: 'db.coll',
+          name: 'insertOne',
+          document: { a: 'b' }
+        });
+      });
+    });
+
+    afterEach(async function () {
+      await client.close();
+    });
+
+    it('splits the commands into 2 operations', {
+      metadata: { requires: { mongodb: '>=8.0.0', serverless: 'forbid' } },
+      async test() {
+        const result = await client.bulkWrite(models);
+        expect(result.insertedCount).to.equal(maxWriteBatchSize + 1);
+        expect(commands.length).to.equal(2);
+        expect(commands[0].command.ops.length).to.equal(maxWriteBatchSize);
+        expect(commands[1].command.ops.length).to.equal(1);
+      }
+    });
+  });
+
+  describe('4. MongoClient.bulkWrite batch splits when an ops payload exceeds maxMessageSizeBytes', function () {
+    // Test that MongoClient.bulkWrite properly handles a writeModels input which constructs an ops array larger
+    // than maxMessageSizeBytes.
+    // This test must only be run on 8.0+ servers. This test must be skipped on Atlas Serverless.
+    // Construct a MongoClient (referred to as client) with command monitoring enabled to observe CommandStartedEvents.
+    // Perform a hello command using client and record the following values from the response: maxBsonObjectSize
+    // and maxMessageSizeBytes. Then, construct the following document (referred to as document):
+    // {
+    //   "a": "b".repeat(maxBsonObjectSize - 500)
+    // }
+    // Construct the following write model (referred to as model):
+    // InsertOne: {
+    //   "namespace": "db.coll",
+    //   "document": document
+    // }
+    // Use the following calculation to determine the number of inserts that should be provided to
+    // MongoClient.bulkWrite: maxMessageSizeBytes / maxBsonObjectSize + 1 (referred to as numModels). This number
+    // ensures that the inserts provided to MongoClient.bulkWrite will require multiple bulkWrite commands to be
+    // sent to the server.
+    // Construct as list of write models (referred to as models) with model repeated numModels times. Then execute
+    // bulkWrite on client with models. Assert that the bulk write succeeds and returns a BulkWriteResult with
+    // an insertedCount value of numModels.
+    // Assert that two CommandStartedEvents (referred to as firstEvent and secondEvent) were observed. Assert
+    // that the length of firstEvent.command.ops is numModels - 1. Assert that the length of secondEvent.command.ops
+    // is 1. If the driver exposes operationIds in its CommandStartedEvents, assert that firstEvent.operationId is
+    // equal to secondEvent.operationId.
+    let client: MongoClient;
+    let maxBsonObjectSize;
+    let maxMessageSizeBytes;
+    let numModels;
+    const models: AnyClientBulkWriteModel[] = [];
+    const commands: CommandStartedEvent[] = [];
+
+    beforeEach(async function () {
+      client = this.configuration.newClient({}, { monitorCommands: true });
+      await client.connect();
+      await client.db('db').collection('coll').drop();
+      const hello = await client.db('admin').command({ hello: 1 });
+      maxBsonObjectSize = hello.maxBsonObjectSize;
+      maxMessageSizeBytes = hello.maxMessageSizeBytes;
+      numModels = Math.floor(maxMessageSizeBytes / maxBsonObjectSize + 1);
+
+      client.on('commandStarted', filterForCommands('bulkWrite', commands));
+      commands.length = 0;
+
+      Array.from({ length: numModels }, () => {
+        models.push({
+          name: 'insertOne',
+          namespace: 'db.coll',
+          document: {
+            a: 'b'.repeat(maxBsonObjectSize - 500)
+          }
+        });
+      });
+    });
+
+    afterEach(async function () {
+      await client.close();
+    });
+
+    it('splits the commands into 2 operations', {
+      metadata: { requires: { mongodb: '>=8.0.0', serverless: 'forbid' } },
+      async test() {
+        const result = await client.bulkWrite(models);
+        expect(result.insertedCount).to.equal(numModels);
+        expect(commands.length).to.equal(2);
+        expect(commands[0].command.ops.length).to.equal(numModels - 1);
+        expect(commands[1].command.ops.length).to.equal(1);
       }
     });
   });
