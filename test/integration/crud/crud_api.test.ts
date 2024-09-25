595bf9fca4c6fa2f563a45a0b99d5e2c4e14e06b
Neal Beeken <neal.beeken@mongodb.com>
2024-09-25 16:56:06 -0400

    refactor(NODE-6398): bulkWrite internals to use async/await

--- test/integration/crud/crud_api.test.ts
+++ test/integration/crud/crud_api.test.ts
@@ -1093,32 +1093,24 @@ describe('CRUD API', function () {
     }
   });
 
-  it('should correctly throw error on illegal callback when unordered bulkWrite encounters error', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: {
-      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-    },
-
-    test: async function () {
-      const ops = [];
-      // Create a set of operations that go over the 1000 limit causing two messages
-      let i = 0;
-      for (; i < 1005; i++) {
-        ops.push({ insertOne: { _id: i, a: i } });
-      }
-
-      ops.push({ insertOne: { _id: 0, a: i } });
-
-      const db = client.db();
-
-      const error = await db
-        .collection('t20_1')
-        .bulkWrite(ops, { ordered: false, writeConcern: { w: 1 } })
-        .catch(error => error);
-
-      expect(error).to.be.instanceOf(MongoError);
+  it('should correctly throw error on illegal callback when unordered bulkWrite encounters error', async function () {
+    const ops = [];
+    // Create a set of operations that go over the 1000 limit causing two messages
+    let i = 0;
+    for (; i < 1005; i++) {
+      ops.push({ insertOne: { _id: i, a: i } });
     }
+
+    ops.push({ insertOne: { _id: 0, a: i } });
+
+    const db = client.db();
+
+    const error = await db
+      .collection('t20_1')
+      .bulkWrite(ops, { ordered: false, writeConcern: { w: 1 } })
+      .catch(error => error);
+
+    expect(error).to.be.instanceOf(MongoError);
   });
 
   it('should correctly throw error on illegal callback when ordered bulkWrite encounters error', {
