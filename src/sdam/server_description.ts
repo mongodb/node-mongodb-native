b76b143bf6e7eebc916069e024da2e6a3d82f81f
Durran Jordan <durran@gmail.com>
2024-10-01 16:39:38 +0200

    chore: remove defaults

--- src/sdam/server_description.ts
+++ src/sdam/server_description.ts
@@ -18,12 +18,6 @@ const DATA_BEARING_SERVER_TYPES = new Set<ServerType>([
   ServerType.LoadBalancer
 ]);
 
-/** Default in case we are in load balanced mode. */
-const MAX_MESSAGE_SIZE_BYTES = 48000000;
-
-/** Default in case we are in load balanced mode. */
-const MAX_WRITE_BATCH_SIZE = 100000;
-
 /** @public */
 export interface TopologyVersion {
   processId: ObjectId;
@@ -79,6 +73,8 @@ export class ServerDescription {
   maxMessageSizeBytes: number;
   /** The max number of writes in a bulk write command. */
   maxWriteBatchSize: number;
+  /** The max bson object size. */
+  maxBsonObjectSize: number;
 
   // NOTE: does this belong here? It seems we should gossip the cluster time at the CMAP level
   $clusterTime?: ClusterTime;
@@ -121,8 +117,9 @@ export class ServerDescription {
     this.setVersion = hello?.setVersion ?? null;
     this.electionId = hello?.electionId ?? null;
     this.logicalSessionTimeoutMinutes = hello?.logicalSessionTimeoutMinutes ?? null;
-    this.maxMessageSizeBytes = hello?.maxMessageSizeBytes ?? MAX_MESSAGE_SIZE_BYTES;
-    this.maxWriteBatchSize = hello?.maxWriteBatchSize ?? MAX_WRITE_BATCH_SIZE;
+    this.maxMessageSizeBytes = hello?.maxMessageSizeBytes ?? 0;
+    this.maxWriteBatchSize = hello?.maxWriteBatchSize ?? 0;
+    this.maxBsonObjectSize = hello?.maxBsonObjectSize ?? 0;
     this.primary = hello?.primary ?? null;
     this.me = hello?.me?.toLowerCase() ?? null;
     this.$clusterTime = hello?.$clusterTime ?? null;
