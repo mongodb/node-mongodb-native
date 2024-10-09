df7d4aacbb2a098593feea4b49fddec867065026
Durran Jordan <durran@gmail.com>
2024-10-09 16:47:47 +0200

    test: run all tests

--- test/integration/retryable-writes/retryable_writes.spec.test.ts
+++ test/integration/retryable-writes/retryable_writes.spec.test.ts
@@ -3,6 +3,6 @@ import * as path from 'path';
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
 
-describe.only('Retryable Writes (unified)', function () {
+describe('Retryable Writes (unified)', function () {
   runUnifiedSuite(loadSpecTests(path.join('retryable-writes', 'unified')));
 });
