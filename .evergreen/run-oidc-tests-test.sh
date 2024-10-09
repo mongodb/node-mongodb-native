552d84863063af566fe04651665f029915a0000e
Neal Beeken <neal.beeken@mongodb.com>
2024-10-09 14:56:16 -0400

    chore: typo

--- .evergreen/run-oidc-tests-test.sh
+++ .evergreen/run-oidc-tests-test.sh
@@ -2,13 +2,10 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-cd src
-
 source $DRIVERS_TOOLS/.evergreen/auth_oidc/secrets-export.sh
 export PROJECT_DIRECTORY=$PROJECT_DIRECTORY
 export ENVIRONMENT=$ENVIRONMENT
 printenv
 export AWS_WEB_IDENTITY_TOKEN_FILE=$OIDC_TOKEN_FILE
 ls -la $OIDC_TOKEN_DIR
-ls .
 bash ./.evergreen/${SCRIPT}
