5f4a859a067d0332b4eb90dec180d10658574101
Durran Jordan <durran@gmail.com>
2024-10-24 08:58:35 +0200

    test: send drivers tools to script

--- .evergreen/run-oidc-tests-k8s.sh
+++ .evergreen/run-oidc-tests-k8s.sh
@@ -6,7 +6,7 @@ export K8S_DRIVERS_TAR_FILE=/tmp/node-mongodb-native.tgz
 tar czf $K8S_DRIVERS_TAR_FILE .
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/setup-pod.sh
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/run-self-test.sh
-export K8S_TEST_CMD="ENVIRONMENT=k8s ./.evergreen/${SCRIPT}"
+export K8S_TEST_CMD="DRIVERS_TOOLS=${DRIVERS_TOOLS} ENVIRONMENT=k8s ./.evergreen/${SCRIPT}"
 source $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/secrets-export.sh
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/run-driver-test.sh
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/teardown-pod.sh
