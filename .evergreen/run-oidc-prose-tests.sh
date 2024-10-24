294bf0eb747533c645cc93152563bf016ec7f0c9
Durran Jordan <durran@gmail.com>
2024-10-24 15:47:11 +0200

    test: tar correct directories

--- .evergreen/run-oidc-prose-tests.sh
+++ .evergreen/run-oidc-prose-tests.sh
@@ -8,10 +8,7 @@ source ./.evergreen/prepare-shell.sh
 
 ENVIRONMENT=${ENVIRONMENT:-"test"}
 PROJECT_DIRECTORY=${PROJECT_DIRECTORY:-"."}
-
-if [ -n "${K8S_VARIANT}" ]; then
-  source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
-fi
+source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
 
 if [ -z "${MONGODB_URI_SINGLE}" ]; then
   echo "Must specify MONGODB_URI_SINGLE"
@@ -33,11 +30,5 @@ else
     echo "Must specify K8S_VARIANT"
     exit 1
   fi
-
-  # Since this is running in a pod, we need to ensure Node is installed properly.
-  source $DRIVERS_TOOLS/.evergreen/install-node.sh
-  npm install "${NPM_OPTIONS}"
-  source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
-
   npm run check:oidc-k8s
 fi
