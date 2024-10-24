660486e3999b52c5d7f7151e2339987424252b9c
Durran Jordan <durran@gmail.com>
2024-10-24 16:44:37 +0200

    test: dont source env

--- .evergreen/run-oidc-tests-k8s.sh
+++ .evergreen/run-oidc-tests-k8s.sh
@@ -8,7 +8,7 @@ tar -czf $K8S_DRIVERS_TAR_FILE src drivers-tools
 cd -
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/setup-pod.sh
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/run-self-test.sh
-export K8S_TEST_CMD="source ./env.sh && cd src && ENVIRONMENT=k8s ./.evergreen/${SCRIPT}"
+export K8S_TEST_CMD="cd src && DRIVERS_TOOLS=${DRIVERS_TOOLS} ENVIRONMENT=k8s ./.evergreen/${SCRIPT}"
 source $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/secrets-export.sh
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/run-driver-test.sh
 bash $DRIVERS_TOOLS/.evergreen/auth_oidc/k8s/teardown-pod.sh
