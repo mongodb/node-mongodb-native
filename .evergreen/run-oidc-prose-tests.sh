1ab41614a2623d51e10bfda899d7d9a6b238c30a
Durran Jordan <durran@gmail.com>
2024-10-24 11:50:54 +0200

    test: check if in pod to reinit

--- .evergreen/run-oidc-prose-tests.sh
+++ .evergreen/run-oidc-prose-tests.sh
@@ -8,7 +8,10 @@ source ./.evergreen/prepare-shell.sh
 
 ENVIRONMENT=${ENVIRONMENT:-"test"}
 PROJECT_DIRECTORY=${PROJECT_DIRECTORY:-"."}
-source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
+
+if [ -n "${K8S_VARIANT}" ]; then
+  source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
+fi
 
 if [ -z "${MONGODB_URI_SINGLE}" ]; then
   echo "Must specify MONGODB_URI_SINGLE"
@@ -30,5 +33,11 @@ else
     echo "Must specify K8S_VARIANT"
     exit 1
   fi
+
+  # Since this is running in a pod, we need to ensure Node is installed properly.
+  source $DRIVERS_TOOLS/.evergreen/install-node.sh
+  npm install "${NPM_OPTIONS}"
+  source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
+
   npm run check:oidc-k8s
 fi
