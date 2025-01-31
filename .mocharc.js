74e6bc6ad94068c1fc82bc40985dd6ca5edd0948
Warren James <warren.james@mongodb.com>
2025-01-31 16:30:23 -0500

    format .mocharc.js

--- .mocharc.js
+++ .mocharc.js
@@ -4,21 +4,18 @@ const [major] = process.versions.node.split('.');
 
 /** @type {import("mocha").MochaOptions} */
 module.exports = {
-  "require": [
+  require: [
     "source-map-support/register",
     "ts-node/register",
     "test/tools/runner/chai_addons.ts",
     "test/tools/runner/ee_checker.ts"
   ],
-  "extension": [
-    "js",
-    "ts"
-  ],
-  "recursive": true,
-  "timeout": 60000,
-  "failZero": true,
-  "reporter": "test/tools/reporter/mongodb_reporter.js",
-  "sort": true,
-  "color": true,
-  "node-option": Number(major) >= 23 ? ['no-experimental-strip-types'] : undefined
-}
+  extension: ['js', 'ts'],
+  recursive: true,
+  timeout: 60000,
+  failZero: true,
+  reporter: 'test/tools/reporter/mongodb_reporter.js',
+  sort: true,
+  color: true,
+  'node-option': Number(major) >= 23 ? ['no-experimental-strip-types'] : undefined
+};
