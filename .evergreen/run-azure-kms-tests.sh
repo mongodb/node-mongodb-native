549c64d208448fd4a92b6a6b244fa26d615dd04c
Warren James <warren.james@mongodb.com>
2025-02-04 16:14:16 -0500

    update path

--- .evergreen/run-azure-kms-tests.sh
+++ .evergreen/run-azure-kms-tests.sh
@@ -18,4 +18,4 @@ export MONGODB_URI="mongodb://localhost:27017"
 export EXPECTED_AZUREKMS_OUTCOME=${EXPECTED_AZUREKMS_OUTCOME:-omitted}
 export TEST_CSFLE=true
 
-npx mocha --config test/mocha_mongodb.json test/integration/client-side-encryption/client_side_encryption.prose.19.on_demand_azure.test.ts
+npx mocha --config test/mocha_mongodb.js test/integration/client-side-encryption/client_side_encryption.prose.19.on_demand_azure.test.ts
--- .evergreen/run-serverless-tests.sh
+++ .evergreen/run-serverless-tests.sh
@@ -17,7 +17,7 @@ if [ -z ${SERVERLESS_ATLAS_USER+omitted} ]; then echo "SERVERLESS_ATLAS_USER is 
 if [ -z ${SERVERLESS_ATLAS_PASSWORD+omitted} ]; then echo "SERVERLESS_ATLAS_PASSWORD is unset" && exit 1; fi
 
 npx mocha \
-  --config test/mocha_mongodb.json \
+  --config test/mocha_mongodb.js \
   test/integration/crud/crud.spec.test.ts \
   test/integration/crud/crud.prose.test.ts \
   test/integration/retryable-reads/retryable_reads.spec.test.ts \
