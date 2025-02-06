95718d34cdeb84751956dc120788092e7fb0f1f2
bailey <bailey.pearson@mongodb.com>
2025-02-06 13:58:03 -0700

    Revert "don't even have copy of file for confusion"
    This reverts commit 80b2faff0d92e03b801c54f36ea6558fd363f09b.

--- .evergreen/prepare-mongodb-aws-ecs-auth.sh
+++ .evergreen/prepare-mongodb-aws-ecs-auth.sh
@@ -10,30 +10,9 @@ mkdir -p $ECS_SRC_DIR/.evergreen
 set -ex
 
 # write test file
-cat <<EOF >$PROJECT_DIRECTORY/.evergreen/run-mongodb-aws-ecs-test.sh
-#!/bin/bash
-
-set -o xtrace  # Write all commands first to stderr
-set -o errexit # Exit the script with error if any of the commands fail
-
-export MONGODB_URI="$1"
-
-tar -xzf src/src.tgz
-# produces src/ and drivers-tools/
-
-cd src
-
-source ./.evergreen/prepare-shell.sh # should not run git clone
-
-# load node.js
-source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
-
-# run the tests
-npm install aws4
-export MONGODB_AWS_SDK=$MONGODB_AWS_SDK
-if [ $MONGODB_AWS_SDK = 'false' ]; then rm -rf ./node_modules/@aws-sdk/credential-providers; fi
-npm run check:aws
-EOF
+echo "export MONGODB_AWS_SDK=$MONGODB_AWS_SDK" >>$PROJECT_DIRECTORY/.evergreen/run-mongodb-aws-ecs-test.sh
+echo "if [ $MONGODB_AWS_SDK = 'false' ]; then rm -rf ./node_modules/@aws-sdk/credential-providers; fi" >>$PROJECT_DIRECTORY/.evergreen/run-mongodb-aws-ecs-test.sh
+echo "npm run check:aws" >>$PROJECT_DIRECTORY/.evergreen/run-mongodb-aws-ecs-test.sh
 
 # copy test file to AWS ecs test directory
 cp $PROJECT_DIRECTORY/.evergreen/run-mongodb-aws-ecs-test.sh $ECS_SRC_DIR/.evergreen/
---
+++ .evergreen/run-mongodb-aws-ecs-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+export MONGODB_URI="$1"
+
+tar -xzf src/src.tgz
+# produces src/ and drivers-tools/
+
+cd src
+
+source ./.evergreen/prepare-shell.sh # should not run git clone
+
+# load node.js
+source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
+
+# run the tests
+npm install aws4
