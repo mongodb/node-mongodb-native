3d2b72303e489580b61e50c6b2ab3959c76f137b
Neal Beeken <neal.beeken@mongodb.com>
2025-02-11 09:32:30 -0500

    ci: skip serverless tests (#4409)

--- .evergreen/generate_evergreen_tasks.js
+++ .evergreen/generate_evergreen_tasks.js
@@ -780,17 +780,18 @@ BUILD_VARIANTS.push({
   tasks: customDependencyTests.map(({ name }) => name)
 });
 
+// TODO(NODE-6748): unskip serverless tests when getParameter and failPoints are possible
 // special case for serverless testing
-BUILD_VARIANTS.push({
-  name: 'rhel8-test-serverless',
-  display_name: 'Serverless Test',
-  run_on: DEFAULT_OS,
-  expansions: {
-    NODE_LTS_VERSION: LOWEST_LTS,
-    NPM_VERSION: 9
-  },
-  tasks: ['serverless_task_group']
-});
+// BUILD_VARIANTS.push({
+//   name: 'rhel8-test-serverless',
+//   display_name: 'Serverless Test',
+//   run_on: DEFAULT_OS,
+//   expansions: {
+//     NODE_LTS_VERSION: LOWEST_LTS,
+//     NPM_VERSION: 9
+//   },
+//   tasks: ['serverless_task_group']
+// });
 
 BUILD_VARIANTS.push({
   name: 'rhel8-test-gcp-kms',
