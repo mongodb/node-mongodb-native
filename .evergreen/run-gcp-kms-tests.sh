ba422064f9fb396bb63e69c167335611d368c5a8
Warren James <warren.james@mongodb.com>
2025-02-12 17:05:20 -0500

    ci(NODE-6678): Re-enable Node latest testing (#4393)
    Co-authored-by: Neal Beeken <neal.beeken@mongodb.com>

--- .evergreen/run-gcp-kms-tests.sh
+++ .evergreen/run-gcp-kms-tests.sh
@@ -20,4 +20,4 @@ export MONGODB_URI="mongodb://localhost:27017"
 export EXPECTED_GCPKMS_OUTCOME=${EXPECTED_GCPKMS_OUTCOME:-omitted}
 export TEST_CSFLE=true
 
-npx mocha --config test/mocha_mongodb.json test/integration/client-side-encryption/client_side_encryption.prose.17.on_demand_gcp.test.ts
+npx mocha --config test/mocha_mongodb.js test/integration/client-side-encryption/client_side_encryption.prose.17.on_demand_gcp.test.ts
---
+++ test/manual/mocharc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const [major] = process.versions.node.split('.');
+
+/** @type {import("mocha").MochaOptions} */
+module.exports = {
+  require: ['ts-node/register', 'test/tools/runner/chai_addons.ts'],
+  reporter: 'test/tools/reporter/mongodb_reporter.js',
+  failZero: true,
+  color: true,
+  timeout: 10000,
+  'node-option': Number(major) >= 23 ? ['no-experimental-strip-types'] : undefined
+};
