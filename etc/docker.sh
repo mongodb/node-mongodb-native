57efd6256e83d31d4a9680609ac6b89f8b6dcdd2
bailey <bailey.pearson@mongodb.com>
2025-02-13 11:24:58 -0700

    wahhooo

--- etc/docker.sh
+++ etc/docker.sh
@@ -15,6 +15,10 @@ IMAGE_TAG=alpine-fle-image
 build_alpine() {
     docker buildx create --name builder --bootstrap --use
 
+    # set up FLE creds on host.  don't download cryptd because we don't need it.
+    # TODO: once we map cwd onto the host, we don't need to build the image with the credential file present.
+    RUN_WITH_MONGOCRYPTD=true bash .evergreen/setup-fle.sh
+
     BASE_TAG=$LINUX_ARCH-alpine-base-node-$NODE_VERSION
     docker --debug buildx build --load --progress=plain \
         --platform linux/$LINUX_ARCH \
@@ -26,9 +30,6 @@ build_alpine() {
 }
 
 test_alpine() {
-    # set up FLE creds on host.  don't download cryptd because we don't need it.
-    RUN_WITH_MONGOCRYPTD=true bash .evergreen/setup-fle.sh
-
     # launch a mongocryptd on the host.
     ./mongodb/bin/mongocryptd --fork --port 3000 --pidfilepath $(pwd)/pid.file --logpath $(pwd)/logpath
     MONGOCRYPTD_URI='mongodb://localhost:3000'
