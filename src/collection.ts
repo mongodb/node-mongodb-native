876df2cf03706977e860b90af8a388f15fcbae85
Emily M Klassen <forivall@gmail.com>
2025-02-18 19:18:53 -0800

    docs(NODE-6765): update comments for findOneAndUpdate

--- src/collection.ts
+++ src/collection.ts
@@ -966,8 +966,12 @@ export class Collection<TSchema extends Document = Document> {
   /**
    * Find a document and update it in one atomic operation. Requires a write lock for the duration of the operation.
    *
+   * The value of `update` can be either:
+   * - UpdateFilter<TSchema> - A document that contains update operator expressions,
+   * - Document[] - an aggregation pipeline.
+   *
    * @param filter - The filter used to select the document to update
-   * @param update - Update operations to be performed on the document
+   * @param update - The modifications to apply
    * @param options - Optional settings for the command
    */
   async findOneAndUpdate(
