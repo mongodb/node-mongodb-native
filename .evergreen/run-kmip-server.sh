0db832503bcd7dec3cda764473844c7d3cf71a45
bailey <bailey.pearson@mongodb.com>
2025-02-21 10:10:29 -0700

    adjust KMS server ports to match shared tooling ports

--- .evergreen/run-kmip-server.sh
+++ .evergreen/run-kmip-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -o errexit  # Exit the script with error if any of the commands fail
+set -o errexit # Exit the script with error if any of the commands fail
 
 cd ${DRIVERS_TOOLS}/.evergreen/csfle
 . ./prepare-kmsvenv.sh
