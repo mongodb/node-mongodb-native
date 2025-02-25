8f861178f60a313159901d8c5769f105d81b7fd7
Durran Jordan <durran@gmail.com>
2025-02-25 18:41:41 +0100

    test(NODE6766): skip serverless tests

--- .evergreen/generate_evergreen_tasks.js
+++ .evergreen/generate_evergreen_tasks.js
@@ -807,16 +807,16 @@ BUILD_VARIANTS.push({
 });
 
 // special case for serverless testing
-BUILD_VARIANTS.push({
-  name: 'rhel8-test-serverless',
-  display_name: 'Serverless Test',
-  run_on: DEFAULT_OS,
-  expansions: {
-    NODE_LTS_VERSION: LOWEST_LTS,
-    NPM_VERSION: 9
-  },
-  tasks: ['serverless_task_group']
-});
+// BUILD_VARIANTS.push({
+//   name: 'rhel8-test-serverless',
+//   display_name: 'Serverless Test',
+//   run_on: DEFAULT_OS,
+//   expansions: {
+//     NODE_LTS_VERSION: LOWEST_LTS,
+//     NPM_VERSION: 9
+//   },
+//   tasks: ['serverless_task_group']
+// });
 
 BUILD_VARIANTS.push({
   name: 'rhel8-test-gcp-kms',
