a36087095ecaee9576728fc48e9e4e0aac449c4c
Neal Beeken <neal.beeken@mongodb.com>
2025-02-27 13:22:18 -0500

    chore: bump package

--- src/client-side-encryption/auto_encrypter.ts
+++ src/client-side-encryption/auto_encrypter.ts
@@ -239,7 +239,6 @@ export class AutoEncrypter {
     this._kmsProviders = options.kmsProviders || {};
 
     const mongoCryptOptions: MongoCryptOptions = {
-      //@ts-expect-error: not yet in the defs
       enableMultipleCollinfo: true,
       cryptoCallbacks
     };
--- test/integration/client-side-encryption/client_side_encryption.prose.test.js
+++ test/integration/client-side-encryption/client_side_encryption.prose.test.js
@@ -16,9 +16,6 @@ const {
 } = require('../../spec/client-side-encryption/external/external-schema.json');
 /* eslint-disable no-restricted-modules */
 const { ClientEncryption } = require('../../../src/client-side-encryption/client_encryption');
-const {
-  ClientSideEncryptionFilter
-} = require('../../tools/runner/filters/client_encryption_filter');
 const { getCSFLEKMSProviders } = require('../../csfle-kms-providers');
 const { AlpineTestConfiguration } = require('../../tools/runner/config');
 
