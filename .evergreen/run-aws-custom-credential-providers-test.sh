0be1f1f1eecec60525ab119c173dc2bc6f360b5b
Durran Jordan <durran@gmail.com>
2025-02-27 15:43:41 +0100

    test: setup fle first

--- .evergreen/run-aws-custom-credential-providers-test.sh
+++ .evergreen/run-aws-custom-credential-providers-test.sh
@@ -4,8 +4,8 @@ set -o errexit # Exit the script with error if any of the commands fail
 
 MONGODB_URI=${MONGODB_URI:-}
 
-source .evergreen/setup-mongodb-aws-auth-tests.sh
 source .evergreen/setup-fle.sh
+source .evergreen/setup-mongodb-aws-auth-tests.sh
 
 # load node.js environment
 source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
