694ddc87cceafbe7985d59f68a38f86c79327493
Neal Beeken <neal.beeken@mongodb.com>
2025-03-05 14:11:10 -0500

    chore: fix more

--- src/deps.ts
+++ src/deps.ts
@@ -203,7 +203,8 @@ export function getSocks(): SocksLib | { kModuleError: MongoMissingDependencyErr
   }
 }
 
-interface AWS4 {
+/** @internal */
+export interface AWS4 {
   /**
    * Created these inline types to better assert future usage of this API
    * @param options - options for request
@@ -244,9 +245,7 @@ interface AWS4 {
   };
 }
 
-export const aws4: AWS4 | { kModuleError: MongoMissingDependencyError } = loadAws4();
-
-function loadAws4() {
+export function loadAws4() {
   let aws4: AWS4 | { kModuleError: MongoMissingDependencyError };
   try {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
