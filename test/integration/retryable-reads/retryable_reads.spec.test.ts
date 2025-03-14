cd09d43528e574f3d63532ddc27303d6f687abbd
Bailey Pearson <bailey.pearson@mongodb.com>
2025-03-14 15:00:37 -0600

    ci: skip flaky tests in retryable reads and sdam suites (#4466)

--- test/integration/retryable-reads/retryable_reads.spec.test.ts
+++ test/integration/retryable-reads/retryable_reads.spec.test.ts
@@ -12,11 +12,17 @@ const UNIMPLEMENTED_APIS = [
   'listDatabaseObjects'
 ];
 
+const skippedTests = ['collection.listIndexes succeeds after retryable handshake network error'];
+
 describe('Retryable Reads (unified)', function () {
   runUnifiedSuite(loadSpecTests(path.join('retryable-reads', 'unified')), ({ description }) => {
     for (const apiName of UNIMPLEMENTED_APIS) {
       if (description.toLowerCase().includes(apiName.toLowerCase())) {
         return `The Node.js Driver does not support ${apiName}`;
+      }
+
+      if (skippedTests.includes(description)) {
+        return `TODO(NODE-6832): fix flaky retryable reads tests`;
       }
     }
     return false;
