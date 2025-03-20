3a3f061f04877d7f20c3c1e766ad8c2346c9dc22
Neal Beeken <neal.beeken@mongodb.com>
2025-03-20 10:26:28 -0400

    fix: logging results preview

--- .evergreen/perf-send.sh
+++ .evergreen/perf-send.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
 
 source $DRIVERS_TOOLS/.evergreen/init-node-and-npm-env.sh
 
 TARGET_FILE=$(realpath "${TARGET_FILE:-./test/benchmarks/driver_bench/results.json}")
+
+set -o xtrace
 
 node ./.evergreen/perf_send.mjs $TARGET_FILE
