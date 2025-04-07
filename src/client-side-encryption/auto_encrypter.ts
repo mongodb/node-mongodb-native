85124c25007ec4fa91cd429c147fe69ede396849
Bailey Pearson <bailey.pearson@mongodb.com>
2025-04-07 16:56:47 -0600

    test(NODE-6836): make broken mongocryptd tests run in CI  (#4497)

--- src/client-side-encryption/auto_encrypter.ts
+++ src/client-side-encryption/auto_encrypter.ts
@@ -365,14 +365,10 @@ export class AutoEncrypter {
       const client = await this._mongocryptdClient.connect();
       return client;
     } catch (error) {
-      const { message } = error;
-      if (message && (message.match(/timed out after/) || message.match(/ENOTFOUND/))) {
-        throw new MongoRuntimeError(
-          'Unable to connect to `mongocryptd`, please make sure it is running or in your PATH for auto-spawn',
-          { cause: error }
-        );
-      }
-      throw error;
+      throw new MongoRuntimeError(
+        'Unable to connect to `mongocryptd`, please make sure it is running or in your PATH for auto-spawn',
+        { cause: error }
+      );
     }
   }
 
