4ac4f9d7db82c29e76c68e28bbd9dc4d469200e9
Durran Jordan <durran@gmail.com>
2025-04-15 20:58:45 +0200

    test(NODE-6863): unskip flaky csot legacy timeout spec test (#4505)

--- test/tools/runner/flaky.ts
+++ test/tools/runner/flaky.ts
@@ -33,5 +33,6 @@ export const flakyTests = [
   'Retryable Writes (unified) retryable writes handshake failures collection.updateOne succeeds after retryable handshake network error',
   'Retryable Reads (unified) retryable reads handshake failures collection.aggregate succeeds after retryable handshake network error',
   'CSOT spec tests legacy timeouts behave correctly for retryable operations operation fails after two consecutive socket timeouts - aggregate on collection',
-  'Server Discovery and Monitoring Prose Tests Connection Pool Management ensure monitors properly create and unpause connection pools when they discover servers'
+  'Server Discovery and Monitoring Prose Tests Connection Pool Management ensure monitors properly create and unpause connection pools when they discover servers',
+  'CSOT spec tests legacy timeouts behave correctly for retryable operations operation succeeds after one socket timeout - aggregate on collection'
 ];
