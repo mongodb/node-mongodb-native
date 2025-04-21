60a8c665bf031c4d255477064f6fc04780f57f1d
Neal Beeken <neal.beeken@mongodb.com>
2025-04-21 11:57:46 -0400

    test: network error wrapping

--- test/integration/node-specific/convert_socket_errors.test.ts
+++ test/integration/node-specific/convert_socket_errors.test.ts
@@ -1,10 +1,42 @@
+import { Duplex } from 'node:stream';
+
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 
-import { ConnectionPool, type MongoClient, MongoNetworkError } from '../../mongodb';
+import { Connection, ConnectionPool, type MongoClient, MongoNetworkError, ns } from '../../mongodb';
 import { clearFailPoint, configureFailPoint } from '../../tools/utils';
 
 describe('Socket Errors', () => {
+  describe('when a socket emits an error', () => {
+    it('command throws a MongoNetworkError', async () => {
+      const socket = new Duplex();
+      // @ts-expect-error: not a real socket
+      const connection = new Connection(socket, {});
+      const testError = new Error('blah');
+      socket.emit('error', testError);
+      const commandRes = connection.command(ns('a.b'), { ping: 1 }, {});
+
+      const error = await commandRes.catch(error => error);
+      expect(error).to.be.instanceOf(MongoNetworkError);
+      expect(error.cause).to.equal(testError);
+    });
+  });
+
+  describe('when the sized message stream emits an error', () => {
+    it('command throws the same error', async () => {
+      const socket = new Duplex();
+      // @ts-expect-error: not a real socket
+      const connection = new Connection(socket, {});
+      const testError = new Error('blah');
+      // @ts-expect-error: private field
+      connection.messageStream.emit('error', testError);
+      const commandRes = connection.command(ns('a.b'), { ping: 1 }, {});
+
+      const error = await commandRes.catch(error => error);
+      expect(error).to.equal(testError);
+    });
+  });
+
   describe('when destroyed after write', () => {
     let client: MongoClient;
     let collection;
