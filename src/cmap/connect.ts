0204f4a2da120d798080139431281d947cc96077
Durran Jordan <durran@gmail.com>
2025-04-23 18:43:35 +0200

    fix: lint

--- src/cmap/connect.ts
+++ src/cmap/connect.ts
@@ -307,7 +307,7 @@ function parseConnectOptions(options: ConnectionOptions): SocketConnectOpts {
       (result as Document)[name] = options[name];
     }
   }
-result.keepAliveInitialDelay ??= 120000;
+  result.keepAliveInitialDelay ??= 120000;
   result.keepAlive = true;
   result.noDelay = options.noDelay ?? true;
 
