289f4fcd14c5171dca6ce93a7ba52d4a184c7c7c
Durran Jordan <durran@gmail.com>
2025-06-02 15:22:03 -0400

    fix: test

--- test/unit/cmap/auth/mongodb_oidc/automated_callback_workflow.test.ts
+++ test/unit/cmap/auth/mongodb_oidc/automated_callback_workflow.test.ts
@@ -2,22 +2,29 @@ import { expect } from 'chai';
 import * as sinon from 'sinon';
 
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
-import { AutomatedCallbackWorkflow } from '../../../../../src/cmap/auth/mongodb_oidc/automated_callback_workflow';
-// eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { callback } from '../../../../../src/cmap/auth/mongodb_oidc/gcp_machine_workflow';
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { TokenCache } from '../../../../../src/cmap/auth/mongodb_oidc/token_cache';
-import { Connection, MongoCredentials } from '../../../../mongodb';
+import {
+  AutomatedCallbackWorkflow,
+  CallbackWorkflow,
+  Connection,
+  MongoCredentials
+} from '../../../../mongodb';
 
 describe('AutomatedCallbackWorkflow', function () {
   describe('#execute', function () {
     context('when the cache has a token', function () {
+      const sandbox = sinon.createSandbox();
+
+      // See NODE-6801 and corresponding PR: https://github.com/mongodb/node-mongodb-native/pull/4438
+      // This is a test to ensure that we do not regress on the above issue. Do NOT remove this test.
       context('when the connection has no token', function () {
         const cache = new TokenCache();
-        const connection = sinon.createStubInstance(Connection);
-        const credentials = sinon.createStubInstance(MongoCredentials);
+        const connection = sandbox.createStubInstance(Connection);
+        const credentials = sandbox.createStubInstance(MongoCredentials);
+        sandbox.stub(CallbackWorkflow.prototype, 'finishAuthentication').resolves();
         const workflow = new AutomatedCallbackWorkflow(cache, callback);
-        sinon.stub(workflow, 'finishAuthentication').returns(Promise.resolve());
 
         beforeEach(function () {
           cache.put({ accessToken: 'test', expiresInSeconds: 7200 });
@@ -25,7 +32,7 @@ describe('AutomatedCallbackWorkflow', function () {
         });
 
         afterEach(function () {
-          sinon.restore();
+          sandbox.restore();
         });
 
         it('sets the token on the connection', async function () {
