2e06e53c1121449f4e9e6a898ab13dd6f4c0dd0b
bailey <bailey.pearson@mongodb.com>
2025-07-16 10:48:07 -0600

    comment test

--- test/integration/client-side-encryption/client_side_encryption.prose.20.mongocryptd_client.test.ts
+++ test/integration/client-side-encryption/client_side_encryption.prose.20.mongocryptd_client.test.ts
@@ -19,10 +19,22 @@ describe('20. Bypass creating mongocryptd client when shared library is loaded',
       this.skip();
     }
 
+    // Start a new thread (referred to as listenerThread)
+    // On listenerThread, create a TcpListener on 127.0.0.1 endpoint and port 27021. Start the listener and wait for establishing connections. If any connection is established, then signal about this to the main thread.
+    // Drivers MAY pass a different port if they expect their testing infrastructure to be using port 27021. Pass a port that should be free.
+    // In Node, we don't need to create a separate thread for the server.
     server = createServer({});
     server.listen(27021);
     server.on('connection', () => (hasConnection = true));
 
+    // Create a MongoClient configured with auto encryption (referred to as client_encrypted)
+    // Configure the required options. Use the local KMS provider as follows:
+    // { "local": { "key": <base64 decoding of LOCAL_MASTERKEY> } }
+    // Configure with the keyVaultNamespace set to keyvault.datakeys.
+    // Configure the following extraOptions:
+    // {
+    //   "mongocryptdURI": "mongodb://localhost:27021/?serverSelectionTimeoutMS=1000"
+    // }
     client = this.configuration.newClient(
       {},
       {
@@ -51,9 +63,18 @@ describe('20. Bypass creating mongocryptd client when shared library is loaded',
       }
     },
     async function () {
+      // Use client_encrypted to insert the document {"unencrypted": "test"} into db.coll.
       await client.db('db').collection('coll').insertOne({ unencrypted: 'test' });
+
+      // Expect no signal from listenerThread.
       expect(hasConnection).to.be.false;
 
+      // Note: this assertion is not in the spec test.  However, unlike other drivers, Node's client
+      // does not connect when instantiated.  So, we won't receive any TCP connections to the
+      // server unless the mongocryptd client is only instantiated.  This assertion captures the
+      // spirit of this test, causing it to fail if we do instantiate a client.  I left the
+      // TCP server in, although it isn't necessary for Node's test, just because its nice to have
+      // in case Node's client behavior ever changes.
       expect(client.autoEncrypter._mongocryptdClient).to.be.undefined;
     }
   );
