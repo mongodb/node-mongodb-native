330a330ef97862a72c1fb31c11be41bd3e465542
Durran Jordan <durran@gmail.com>
2025-09-17 14:36:33 +0200

    refactor: test

--- test/integration/auth/mongodb_aws.test.ts
+++ test/integration/auth/mongodb_aws.test.ts
@@ -18,6 +18,7 @@ import {
   type MongoDBNamespace,
   type MongoDBResponseConstructor,
   MongoMissingCredentialsError,
+  MongoMissingDependencyError,
   MongoServerError,
   setDifference
 } from '../../mongodb';
@@ -53,89 +54,6 @@ describe('MONGODB-AWS', function () {
     await client?.close();
   });
 
-  it('should authorize when successfully authenticated', async function () {
-    client = this.configuration.newClient(process.env.MONGODB_URI); // use the URI built by the test environment
-
-    const result = await client
-      .db('aws')
-      .collection('aws_test')
-      .estimatedDocumentCount()
-      .catch(error => error);
-
-    expect(result).to.not.be.instanceOf(MongoServerError);
-    expect(result).to.be.a('number');
-  });
-
-  describe('ConversationId', function () {
-    let commandStub: sinon.SinonStub<
-      [
-        ns: MongoDBNamespace,
-        command: Document,
-        options?: CommandOptions,
-        responseType?: MongoDBResponseConstructor
-      ],
-      Promise<any>
-    >;
-
-    let saslStartResult, saslContinue;
-
-    beforeEach(function () {
-      // spy on connection.command, filter for saslStart and saslContinue commands
-      commandStub = sinon.stub(Connection.prototype, 'command').callsFake(async function (
-        ns: MongoDBNamespace,
-        command: Document,
-        options: CommandOptions,
-        responseType?: MongoDBResponseConstructor
-      ) {
-        if (command.saslContinue != null) {
-          saslContinue = { ...command };
-        }
-
-        const result = await commandStub.wrappedMethod.call(
-          this,
-          ns,
-          command,
-          options,
-          responseType
-        );
-
-        if (command.saslStart != null) {
-          // Modify the result of the saslStart to check if the saslContinue uses it
-          result.conversationId = 999;
-          saslStartResult = { ...result };
-        }
-
-        return result;
-      });
-    });
-
-    afterEach(function () {
-      commandStub.restore();
-      sinon.restore();
-    });
-
-    it('should use conversationId returned by saslStart in saslContinue', async function () {
-      client = this.configuration.newClient(process.env.MONGODB_URI); // use the URI built by the test environment
-
-      const err = await client
-        .db('aws')
-        .collection('aws_test')
-        .estimatedDocumentCount()
-        .catch(e => e);
-
-      // Expecting the saslContinue to fail since we changed the conversationId
-      expect(err).to.be.instanceof(MongoServerError);
-      expect(err.message).to.match(/Mismatched conversation id/);
-
-      expect(saslStartResult).to.not.be.undefined;
-      expect(saslContinue).to.not.be.undefined;
-
-      expect(saslStartResult).to.have.property('conversationId', 999);
-
-      expect(saslContinue).to.have.property('conversationId').equal(saslStartResult.conversationId);
-    });
-  });
-
   context('when the AWS SDK is not present', function () {
     beforeEach(function () {
       if (awsSdkPresent) {
@@ -154,39 +72,22 @@ describe('MONGODB-AWS', function () {
           .estimatedDocumentCount()
           .catch(e => e);
 
-        expect(err).to.be.instanceof(MongoServerError);
+        expect(err).to.be.instanceof(MongoMissingDependencyError);
         expect(err.message).to.match(/credential-providers/);
       });
     });
   });
 
-  context('when user supplies a credentials provider', function () {
-    let providerCount = 0;
-
+  context('when the AWS SDK is present', function () {
     beforeEach(function () {
       if (!awsSdkPresent) {
-        this.skipReason = 'only relevant to AssumeRoleWithWebIdentity with SDK installed';
-        return this.skip();
-      }
-      // If we have a username the credentials have been set from the URI, options, or environment
-      // variables per the auth spec stated order.
-      if (client.options.credentials.username) {
-        this.skipReason = 'Credentials in the URI on env variables will not use custom provider.';
+        this.skipReason = 'Tests cases when the AWS SDK is installed.';
         return this.skip();
       }
     });
 
-    it('authenticates with a user provided credentials provider', async function () {
-      const credentialProvider = AWSTemporaryCredentialProvider.awsSDK;
-      const provider = async () => {
-        providerCount++;
-        return await credentialProvider.fromNodeProviderChain().apply();
-      };
-      client = this.configuration.newClient(process.env.MONGODB_URI, {
-        authMechanismProperties: {
-          AWS_CREDENTIAL_PROVIDER: provider
-        }
-      });
+    it('should authorize when successfully authenticated', async function () {
+      client = this.configuration.newClient(process.env.MONGODB_URI); // use the URI built by the test environment
 
       const result = await client
         .db('aws')
@@ -196,109 +97,216 @@ describe('MONGODB-AWS', function () {
 
       expect(result).to.not.be.instanceOf(MongoServerError);
       expect(result).to.be.a('number');
-      expect(providerCount).to.be.greaterThan(0);
-    });
-  });
-
-  it('should allow empty string in authMechanismProperties.AWS_SESSION_TOKEN to override AWS_SESSION_TOKEN environment variable', function () {
-    client = this.configuration.newClient(this.configuration.url(), {
-      authMechanismProperties: { AWS_SESSION_TOKEN: '' }
-    });
-    expect(client)
-      .to.have.nested.property('options.credentials.mechanismProperties.AWS_SESSION_TOKEN')
-      .that.equals('');
-  });
-
-  it('should store a MongoDBAWS provider instance per client', async function () {
-    client = this.configuration.newClient(process.env.MONGODB_URI);
-
-    await client
-      .db('aws')
-      .collection('aws_test')
-      .estimatedDocumentCount()
-      .catch(error => error);
-
-    expect(client).to.have.nested.property('s.authProviders');
-    const provider = client.s.authProviders.getOrCreateProvider('MONGODB-AWS');
-    expect(provider).to.be.instanceOf(MongoDBAWS);
-  });
-
-  describe('with missing aws token', () => {
-    let awsSessionToken: string | undefined;
-
-    beforeEach(() => {
-      awsSessionToken = process.env.AWS_SESSION_TOKEN;
-      delete process.env.AWS_SESSION_TOKEN;
     });
 
-    afterEach(() => {
-      if (awsSessionToken != null) {
-        process.env.AWS_SESSION_TOKEN = awsSessionToken;
-      }
+    describe('ConversationId', function () {
+      let commandStub: sinon.SinonStub<
+        [
+          ns: MongoDBNamespace,
+          command: Document,
+          options?: CommandOptions,
+          responseType?: MongoDBResponseConstructor
+        ],
+        Promise<any>
+      >;
+
+      let saslStartResult, saslContinue;
+
+      beforeEach(function () {
+        // spy on connection.command, filter for saslStart and saslContinue commands
+        commandStub = sinon.stub(Connection.prototype, 'command').callsFake(async function (
+          ns: MongoDBNamespace,
+          command: Document,
+          options: CommandOptions,
+          responseType?: MongoDBResponseConstructor
+        ) {
+          if (command.saslContinue != null) {
+            saslContinue = { ...command };
+          }
+
+          const result = await commandStub.wrappedMethod.call(
+            this,
+            ns,
+            command,
+            options,
+            responseType
+          );
+
+          if (command.saslStart != null) {
+            // Modify the result of the saslStart to check if the saslContinue uses it
+            result.conversationId = 999;
+            saslStartResult = { ...result };
+          }
+
+          return result;
+        });
+      });
+
+      afterEach(function () {
+        commandStub.restore();
+        sinon.restore();
+      });
+
+      it('should use conversationId returned by saslStart in saslContinue', async function () {
+        client = this.configuration.newClient(process.env.MONGODB_URI); // use the URI built by the test environment
+
+        const err = await client
+          .db('aws')
+          .collection('aws_test')
+          .estimatedDocumentCount()
+          .catch(e => e);
+
+        // Expecting the saslContinue to fail since we changed the conversationId
+        expect(err).to.be.instanceof(MongoServerError);
+        expect(err.message).to.match(/Mismatched conversation id/);
+
+        expect(saslStartResult).to.not.be.undefined;
+        expect(saslContinue).to.not.be.undefined;
+
+        expect(saslStartResult).to.have.property('conversationId', 999);
+
+        expect(saslContinue)
+          .to.have.property('conversationId')
+          .equal(saslStartResult.conversationId);
+      });
     });
 
-    it('should not throw an exception when aws token is missing', async function () {
+    context('when user supplies a credentials provider', function () {
+      let providerCount = 0;
+
+      beforeEach(function () {
+        // If we have a username the credentials have been set from the URI, options, or environment
+        // variables per the auth spec stated order.
+        if (client.options.credentials.username) {
+          this.skipReason = 'Credentials in the URI on env variables will not use custom provider.';
+          return this.skip();
+        }
+      });
+
+      it('authenticates with a user provided credentials provider', async function () {
+        const credentialProvider = AWSTemporaryCredentialProvider.awsSDK;
+        const provider = async () => {
+          providerCount++;
+          return await credentialProvider.fromNodeProviderChain().apply();
+        };
+        client = this.configuration.newClient(process.env.MONGODB_URI, {
+          authMechanismProperties: {
+            AWS_CREDENTIAL_PROVIDER: provider
+          }
+        });
+
+        const result = await client
+          .db('aws')
+          .collection('aws_test')
+          .estimatedDocumentCount()
+          .catch(error => error);
+
+        expect(result).to.not.be.instanceOf(MongoServerError);
+        expect(result).to.be.a('number');
+        expect(providerCount).to.be.greaterThan(0);
+      });
+    });
+
+    it('should allow empty string in authMechanismProperties.AWS_SESSION_TOKEN to override AWS_SESSION_TOKEN environment variable', function () {
+      client = this.configuration.newClient(this.configuration.url(), {
+        authMechanismProperties: { AWS_SESSION_TOKEN: '' }
+      });
+      expect(client)
+        .to.have.nested.property('options.credentials.mechanismProperties.AWS_SESSION_TOKEN')
+        .that.equals('');
+    });
+
+    it('should store a MongoDBAWS provider instance per client', async function () {
       client = this.configuration.newClient(process.env.MONGODB_URI);
 
-      const result = await client
+      await client
         .db('aws')
         .collection('aws_test')
         .estimatedDocumentCount()
         .catch(error => error);
 
-      // We check only for the MongoMissingCredentialsError
-      // and do check for the MongoServerError as the error or numeric result
-      // that can be returned depending on different types of environments
-      // getting credentials from different sources.
-      expect(result).to.not.be.instanceOf(MongoMissingCredentialsError);
+      expect(client).to.have.nested.property('s.authProviders');
+      const provider = client.s.authProviders.getOrCreateProvider('MONGODB-AWS');
+      expect(provider).to.be.instanceOf(MongoDBAWS);
     });
-  });
 
-  describe('EC2 with missing credentials', () => {
-    let client;
+    describe('with missing aws token', () => {
+      let awsSessionToken: string | undefined;
 
-    beforeEach(function () {
-      if (!process.env.IS_EC2) {
-        this.currentTest.skipReason = 'requires an AWS EC2 environment';
-        this.skip();
-      }
-      sinon.stub(http, 'request').callsFake(function (...args) {
-        // We pass in a legacy object that has the same properties as a URL
-        // but it is not an instanceof URL.
-        expect(args[0]).to.be.an('object');
-        if (typeof args[0] === 'object') {
-          args[0].hostname = 'www.example.com';
-          args[0].port = '81';
+      beforeEach(() => {
+        awsSessionToken = process.env.AWS_SESSION_TOKEN;
+        delete process.env.AWS_SESSION_TOKEN;
+      });
+
+      afterEach(() => {
+        if (awsSessionToken != null) {
+          process.env.AWS_SESSION_TOKEN = awsSessionToken;
         }
-        return http.request.wrappedMethod.apply(this, args);
+      });
+
+      it('should not throw an exception when aws token is missing', async function () {
+        client = this.configuration.newClient(process.env.MONGODB_URI);
+
+        const result = await client
+          .db('aws')
+          .collection('aws_test')
+          .estimatedDocumentCount()
+          .catch(error => error);
+
+        // We check only for the MongoMissingCredentialsError
+        // and do check for the MongoServerError as the error or numeric result
+        // that can be returned depending on different types of environments
+        // getting credentials from different sources.
+        expect(result).to.not.be.instanceOf(MongoMissingCredentialsError);
       });
     });
 
-    afterEach(async () => {
-      sinon.restore();
-      await client?.close();
-    });
+    describe('EC2 with missing credentials', () => {
+      let client;
 
-    it('should respect the default timeout of 10000ms', async function () {
-      const config = this.configuration;
-      client = config.newClient(process.env.MONGODB_URI, { authMechanism: 'MONGODB-AWS' }); // use the URI built by the test environment
-      const startTime = performance.now();
+      beforeEach(function () {
+        if (!process.env.IS_EC2) {
+          this.currentTest.skipReason = 'requires an AWS EC2 environment';
+          this.skip();
+        }
+        sinon.stub(http, 'request').callsFake(function (...args) {
+          // We pass in a legacy object that has the same properties as a URL
+          // but it is not an instanceof URL.
+          expect(args[0]).to.be.an('object');
+          if (typeof args[0] === 'object') {
+            args[0].hostname = 'www.example.com';
+            args[0].port = '81';
+          }
+          return http.request.wrappedMethod.apply(this, args);
+        });
+      });
 
-      const caughtError = await client
-        .db()
-        .command({ ping: 1 })
-        .catch(error => error);
+      afterEach(async () => {
+        sinon.restore();
+        await client?.close();
+      });
 
-      const endTime = performance.now();
-      const timeTaken = endTime - startTime;
-      expect(caughtError).to.be.instanceOf(MongoAWSError);
-      expect(caughtError)
-        .property('message')
-        .match(/(timed out after)|(Could not load credentials)/);
-      // Credentials provider from the SDK does not allow to configure the timeout
-      // and defaults to 2 seconds - so we ensure this timeout happens below 12s
-      // instead of the 10s-12s range previously.
-      expect(timeTaken).to.be.below(12000);
+      it('should respect the default timeout of 10000ms', async function () {
+        const config = this.configuration;
+        client = config.newClient(process.env.MONGODB_URI, { authMechanism: 'MONGODB-AWS' }); // use the URI built by the test environment
+        const startTime = performance.now();
+
+        const caughtError = await client
+          .db()
+          .command({ ping: 1 })
+          .catch(error => error);
+
+        const endTime = performance.now();
+        const timeTaken = endTime - startTime;
+        expect(caughtError).to.be.instanceOf(MongoAWSError);
+        expect(caughtError)
+          .property('message')
+          .match(/(timed out after)|(Could not load credentials)/);
+        // Credentials provider from the SDK does not allow to configure the timeout
+        // and defaults to 2 seconds - so we ensure this timeout happens below 12s
+        // instead of the 10s-12s range previously.
+        expect(timeTaken).to.be.below(12000);
+      });
     });
   });
 
@@ -477,7 +485,7 @@ describe('AWS KMS Credential Fetching', function () {
     });
     it('fetching AWS KMS credentials throws an error', async function () {
       const error = await refreshKMSCredentials({ aws: {} }).catch(e => e);
-      expect(error).to.be.instanceOf(MongoAWSError);
+      expect(error).to.be.instanceOf(MongoMissingDependencyError);
     });
   });
 
