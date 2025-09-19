a26fa7ccced50d76dd8729b3e7c7a14e49cd7dd0
bailey <bailey.pearson@mongodb.com>
2025-09-19 13:34:50 -0600

    fix CI (hopefully)
    - fixes lint of CI tooling
    - removes unnecessary json file in PR
    - upgrades DET to absolute latest
    - specifies useDefineForClassFields in TSConfig

--- .evergreen/ci_matrix_constants.js
+++ .evergreen/ci_matrix_constants.js
@@ -1,6 +1,6 @@
 const MONGODB_VERSIONS = ['latest', 'rapid', '8.0', '7.0', '6.0', '5.0', '4.4', '4.2'];
 const versions = [
-  { codeName: 'iron', versionNumber: "20.19.0", npmVersion: 'latest' },
+  { codeName: 'iron', versionNumber: '20.19.0', npmVersion: 'latest' },
   { codeName: 'jod', versionNumber: 22, npmVersion: 'latest' }
 ];
 const NODE_VERSIONS = versions.map(({ versionNumber }) => versionNumber).sort();
