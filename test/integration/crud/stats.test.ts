643acd4b72bbcd17a22cd80528e62c5e1747b749
bailey <bailey.pearson@mongodb.com>
2025-10-03 13:07:33 -0600

    refactor test to best practices

--- test/integration/crud/stats.test.ts
+++ test/integration/crud/stats.test.ts
@@ -1,11 +1,21 @@
 import { expect } from 'chai';
 
+import { type MongoClient } from '../../../src';
+
 describe('stats', function () {
-  it('correctly executes stats()', async function () {
-    const client = this.configuration.newClient();
+  let client: MongoClient;
+
+  beforeEach(async function () {
+    client = this.configuration.newClient();
     await client.connect();
+  });
+
+  afterEach(async function () {
+    await client.close();
+  });
+
+  it('correctly executes stats()', async function () {
     const stats = await client.db('foo').stats();
     expect(stats).not.to.be.null;
-    await client.close();
   });
 });
