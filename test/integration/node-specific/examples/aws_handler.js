f393d5aa74a68b0b4d69e87cffc709c2efde746b
Durran Jordan <durran@gmail.com>
2025-10-15 11:48:57 +0200

    Update test/integration/node-specific/examples/aws_handler.js
    Co-authored-by: Daria Pardue <daria.pardue@mongodb.com>

--- test/integration/node-specific/examples/aws_handler.js
+++ test/integration/node-specific/examples/aws_handler.js
@@ -1,7 +1,7 @@
 // begin lambda connection
 const { MongoClient } = require('mongodb');
 
-// Get the URI for the cluster and then uses AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+// Get the URI for the cluster and use AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
 // from the environment, then set the appropriate auth options. Note that MongoClient
 // now auto-connects so no need to store the connect() promise anywhere and reference it.
 const client = new MongoClient(process.env.MONGODB_URI, {
