13c05087e5975e932d21d24f42bf6033c5c28ed0
Sergey Zelenov <sergey.zelenov@mongodb.com>
2025-10-16 12:56:03 +0200

    test(NODE-7203): migrate `promote_buffers`

--- test/integration/node-specific/bson-options/promote_buffers.test.ts
+++ test/integration/node-specific/bson-options/promote_buffers.test.ts
@@ -1,182 +1,90 @@
 import { expect } from 'chai';
 
-import { assert as test, setupDatabase } from '../../shared';
+import { setupDatabase } from '../../shared';
 
 describe('Promote Buffers', function () {
   before(function () {
     return setupDatabase(this.configuration);
   });
 
-  it('should correctly honor promoteBuffers when creating an instance using Db', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: {
-      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-    },
+  it('should correctly honor promoteBuffers when creating an instance using Db', async function () {
+    const configuration = this.configuration;
+    const client = configuration.newClient(configuration.writeConcernMax(), {
+      maxPoolSize: 1,
+      promoteBuffers: true
+    });
 
-    test: function (done) {
-      const configuration = this.configuration;
-      const client = configuration.newClient(configuration.writeConcernMax(), {
-        maxPoolSize: 1,
-        promoteBuffers: true
-      });
+    const db = client.db(configuration.db);
+    await db.collection('shouldCorrectlyHonorPromoteBuffer1').insertOne({
+      doc: Buffer.alloc(256)
+    });
 
-      client.connect(function (err, client) {
-        const db = client.db(configuration.db);
-        db.collection('shouldCorrectlyHonorPromoteBuffer1').insert(
-          {
-            doc: Buffer.alloc(256)
-          },
-          function (err) {
-            expect(err).to.not.exist;
-
-            db.collection('shouldCorrectlyHonorPromoteBuffer1').findOne(function (err, doc) {
-              expect(err).to.not.exist;
-              test.ok(doc.doc instanceof Buffer);
-
-              client.close(done);
-            });
-          }
-        );
-      });
-    }
+    const doc = await db.collection('shouldCorrectlyHonorPromoteBuffer1').findOne();
+    expect(doc.doc).to.be.instanceof(Buffer);
+    await client.close();
   });
 
-  it('should correctly honor promoteBuffers when creating an instance using MongoClient', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: {
-      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-    },
+  it('should correctly honor promoteBuffers when creating an instance using MongoClient', async function () {
+    const configuration = this.configuration;
 
-    test: function (done) {
-      const configuration = this.configuration;
+    const client = configuration.newClient({}, { promoteBuffers: true });
+    const db = client.db(configuration.db);
 
-      const client = configuration.newClient({}, { promoteBuffers: true });
-      client.connect(function (err, client) {
-        const db = client.db(configuration.db);
+    await db.collection('shouldCorrectlyHonorPromoteBuffer2').insertOne({
+      doc: Buffer.alloc(256)
+    });
 
-        db.collection('shouldCorrectlyHonorPromoteBuffer2').insert(
-          {
-            doc: Buffer.alloc(256)
-          },
-          function (err) {
-            expect(err).to.not.exist;
-
-            db.collection('shouldCorrectlyHonorPromoteBuffer2').findOne(function (err, doc) {
-              expect(err).to.not.exist;
-              test.ok(doc.doc instanceof Buffer);
-
-              client.close(done);
-            });
-          }
-        );
-      });
-    }
+    const doc = await db.collection('shouldCorrectlyHonorPromoteBuffer2').findOne();
+    expect(doc.doc).to.be.instanceof(Buffer);
+    await client.close();
   });
 
-  it('should correctly honor promoteBuffers at cursor level', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: {
-      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-    },
+  it('should correctly honor promoteBuffers at cursor level', async function () {
+    const configuration = this.configuration;
 
-    test: function (done) {
-      const configuration = this.configuration;
+    const client = configuration.newClient({}, { promoteBuffers: true });
+    const db = client.db(configuration.db);
 
-      const client = configuration.newClient({}, { promoteBuffers: true });
-      client.connect(function (err, client) {
-        const db = client.db(configuration.db);
+    await db.collection('shouldCorrectlyHonorPromoteBuffer3').insertOne({
+      doc: Buffer.alloc(256)
+    });
 
-        db.collection('shouldCorrectlyHonorPromoteBuffer3').insert(
-          {
-            doc: Buffer.alloc(256)
-          },
-          function (err) {
-            expect(err).to.not.exist;
-
-            db.collection('shouldCorrectlyHonorPromoteBuffer3')
-              .find()
-              .next(function (err, doc) {
-                expect(err).to.not.exist;
-                test.ok(doc.doc instanceof Buffer);
-
-                client.close(done);
-              });
-          }
-        );
-      });
-    }
+    const doc = await db.collection('shouldCorrectlyHonorPromoteBuffer3').find().next();
+    expect(doc.doc).to.be.instanceof(Buffer);
+    await client.close();
   });
 
-  it('should correctly honor promoteBuffers at cursor find level', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: {
-      requires: { topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger'] }
-    },
+  it('should correctly honor promoteBuffers at cursor find level', async function () {
+    const configuration = this.configuration;
 
-    test: function (done) {
-      const configuration = this.configuration;
+    const client = configuration.newClient();
+    const db = client.db(configuration.db);
+    await db.collection('shouldCorrectlyHonorPromoteBuffer4').insertOne({
+      doc: Buffer.alloc(256)
+    });
 
-      const client = configuration.newClient();
-      client.connect(function (err, client) {
-        const db = client.db(configuration.db);
-        db.collection('shouldCorrectlyHonorPromoteBuffer4').insert(
-          {
-            doc: Buffer.alloc(256)
-          },
-          function (err) {
-            expect(err).to.not.exist;
-
-            db.collection('shouldCorrectlyHonorPromoteBuffer4')
-              .find({}, { promoteBuffers: true })
-              .next(function (err, doc) {
-                expect(err).to.not.exist;
-                test.ok(doc.doc instanceof Buffer);
-
-                client.close(done);
-              });
-          }
-        );
-      });
-    }
+    const doc = await db
+      .collection('shouldCorrectlyHonorPromoteBuffer4')
+      .find({}, { promoteBuffers: true })
+      .next();
+    expect(doc.doc).to.be.instanceof(Buffer);
+    await client.close();
   });
 
-  it('should correctly honor promoteBuffers at aggregate level', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: {
-      requires: {
-        topology: ['single', 'replicaset', 'sharded', 'ssl', 'heap', 'wiredtiger']
-      }
-    },
+  it('should correctly honor promoteBuffers at aggregate level', async function () {
+    const configuration = this.configuration;
 
-    test: function (done) {
-      const configuration = this.configuration;
+    const client = configuration.newClient();
+    const db = client.db(configuration.db);
+    await db.collection('shouldCorrectlyHonorPromoteBuffer5').insertOne({
+      doc: Buffer.alloc(256)
+    });
 
-      const client = configuration.newClient();
-      client.connect(function (err, client) {
-        const db = client.db(configuration.db);
-        db.collection('shouldCorrectlyHonorPromoteBuffer5').insert(
-          {
-            doc: Buffer.alloc(256)
-          },
-          function (err) {
-            expect(err).to.not.exist;
-
-            db.collection('shouldCorrectlyHonorPromoteBuffer5')
-              .aggregate([{ $match: {} }], { promoteBuffers: true })
-              .next(function (err, doc) {
-                expect(err).to.not.exist;
-                test.ok(doc.doc instanceof Buffer);
-
-                client.close(done);
-              });
-          }
-        );
-      });
-    }
+    const doc = await db
+      .collection('shouldCorrectlyHonorPromoteBuffer5')
+      .aggregate([{ $match: {} }], { promoteBuffers: true })
+      .next();
+    expect(doc.doc).to.be.instanceof(Buffer);
+    await client.close();
   });
 });
