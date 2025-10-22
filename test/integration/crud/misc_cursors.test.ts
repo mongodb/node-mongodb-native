aa78855b354803e16eadd848bb56d6703871448c
Pavel Safronov <pavel.safronov@gmail.com>
2025-10-22 14:04:44 -0700

    pr feedback

--- test/integration/crud/misc_cursors.test.ts
+++ test/integration/crud/misc_cursors.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { on, once } from 'events';
+import { once } from 'events';
 
 import { MongoClientClosedError } from '../../../src/error';
 import { type MongoClient } from '../../../src/mongo_client';
@@ -726,89 +726,6 @@ describe('Cursor', function () {
     }
   });
 
-  it('does not auto destroy streams', async function () {
-    const docs = [];
-
-    for (let i = 0; i < 10; i++) {
-      docs.push({ a: i + 1 });
-    }
-
-    const configuration = this.configuration;
-    await client.connect();
-
-    const db = client.db(configuration.db);
-    const collection = await db.createCollection('does_not_autodestroy_streams');
-
-    await collection.insertMany(docs, configuration.writeConcernMax());
-
-    const cursor = collection.find();
-    const stream = cursor.stream();
-    stream.on('close', () => {
-      expect.fail('extra close event must not be called');
-    });
-    stream.on('end', () => {
-      client.close();
-    });
-    stream.on('data', doc => {
-      expect(doc).to.exist;
-    });
-    stream.resume();
-    await once(stream, 'end');
-  });
-
-  it('immediately destroying a stream prevents the query from executing', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: {
-      requires: { topology: ['single', 'replicaset', 'sharded'] }
-    },
-
-    test: async function () {
-      const docs = [{ b: 2 }, { b: 3 }];
-      let i = 0,
-        doneCalled = 0;
-
-      const configuration = this.configuration;
-      await client.connect();
-
-      const db = client.db(configuration.db);
-      const collection = await db.createCollection(
-        'immediately_destroying_a_stream_prevents_the_query_from_executing'
-      );
-
-      // insert all docs
-      await collection.insertMany(docs, configuration.writeConcernMax());
-
-      const cursor = collection.find();
-      const stream = cursor.stream();
-
-      stream.on('data', function () {
-        i++;
-      });
-
-      function testDone() {
-        return err => {
-          ++doneCalled;
-
-          if (doneCalled === 1) {
-            expect(err).to.not.exist;
-            test.strictEqual(0, i);
-            test.strictEqual(true, cursor.closed);
-          }
-        };
-      }
-
-      cursor.once('close', testDone('close'));
-      stream.once('error', testDone('error'));
-      const promise = once(cursor, 'close');
-
-      stream.destroy();
-
-      await cursor.close();
-      await promise;
-    }
-  });
-
   it('removes session when cloning an find cursor', async function () {
     const collection = await client.db().collection('test');
 
@@ -831,77 +748,6 @@ describe('Cursor', function () {
 
     expect(cursor).to.have.property('session').not.to.be.null;
     expect(clonedCursor).to.have.property('session').to.be.null;
-  });
-
-  it('destroying a stream stops it', async function () {
-    const db = client.db();
-    await db.dropCollection('destroying_a_stream_stops_it');
-    const collection = await db.createCollection('destroying_a_stream_stops_it');
-
-    const docs = Array.from({ length: 10 }, (_, i) => ({ b: i + 1 }));
-
-    await collection.insertMany(docs);
-
-    const cursor = collection.find();
-    const stream = cursor.stream();
-
-    expect(cursor).property('closed', false);
-
-    const willClose = once(cursor, 'close');
-
-    const dataEvents = on(stream, 'data');
-
-    for (let i = 0; i < 5; i++) {
-      const {
-        value: [doc]
-      } = await dataEvents.next();
-      expect(doc).property('b', i + 1);
-    }
-
-    // After 5 successful data events, destroy stream
-    stream.destroy();
-
-    // We should get a close event on the stream and a close event on the cursor
-    // We should **not** get an 'error' or an 'end' event,
-    // the following will throw if either stream or cursor emitted an 'error' event
-    await Promise.race([
-      willClose,
-      sleep(100).then(() => Promise.reject(new Error('close event never emitted')))
-    ]);
-  });
-
-  it('Should not emit any events after close event emitted due to cursor killed', {
-    // Add a tag that our runner can trigger on
-    // in this case we are setting that node needs to be higher than 0.10.X to run
-    metadata: { requires: { topology: ['single', 'replicaset'] } },
-
-    test: async function () {
-      const configuration = this.configuration;
-      await client.connect();
-
-      const db = client.db(configuration.db);
-      const collection = db.collection('cursor_limit_skip_correctly');
-
-      // Insert x number of docs
-      const ordered = collection.initializeUnorderedBulkOp();
-
-      for (let i = 0; i < 100; i++) {
-        ordered.insert({ a: i });
-      }
-
-      await ordered.execute({ writeConcern: { w: 1 } });
-
-      // Let's attempt to skip and limit
-      const cursor = collection.find({}).batchSize(10);
-      const stream = cursor.stream();
-      stream.on('data', function () {
-        stream.destroy();
-      });
-
-      const onClose = once(cursor, 'close');
-      await cursor.close();
-      await onClose;
-    }
   });
 
   // NOTE: skipped for use of topology manager
