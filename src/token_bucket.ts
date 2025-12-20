d960f8acfaffaac4cd020871555afd2bcb292042
bailey <bailey.pearson@mongodb.com>
2025-12-20 11:00:07 -0700

    token bucket comments

--- src/token_bucket.ts
+++ src/token_bucket.ts
@@ -18,6 +18,18 @@ export class TokenBucket {
   }
 }
 
+/**
+ * @internal
+ * The amount to deposit on successful operations, as defined in the backpressure specification.
+ */
 export const TOKEN_REFRESH_RATE = 0.1;
-export const INITIAL_SIZE = 1000;
+/**
+ * @internal
+ * The initial size of the token bucket, as defined in the backpressure specification.
+ */
+export const INITIAL_TOKEN_BUCKET_SIZE = 1000;
+/**
+ * @internal
+ * The cost of a retry, as defined in the backpressure specification.
+ */
 export const RETRY_COST = 1;
