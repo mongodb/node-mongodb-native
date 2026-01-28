020b9d0e8b1383665e78542d198a8c92fe5cd985
bailey <bailey.pearson@mongodb.com>
2026-01-28 15:46:30 -0700

    unit tests and integration tests

--- test/unit/runtime_adapters.test.ts
+++ test/unit/runtime_adapters.test.ts
@@ -1,30 +1,31 @@
-import { expect } from "chai"
-import { MongoClient, OsAdapter } from "../../src"
+import { expect } from 'chai';
 import * as os from 'os';
 
+import { MongoClient, type OsAdapter } from '../../src';
+
 describe('Runtime Adapters tests', function () {
-    describe('`os`', function () {
-        describe('when no os adapter is provided', function () {
-            it(`defaults to Node's os module`, function () {
-                const client = new MongoClient('mongodb://localhost:27017');
+  describe('`os`', function () {
+    describe('when no os adapter is provided', function () {
+      it(`defaults to Node's os module`, function () {
+        const client = new MongoClient('mongodb://localhost:27017');
 
-                expect(client.options.runtime.os).to.equal(os);
-            })
-        })
+        expect(client.options.runtime.os).to.equal(os);
+      });
+    });
 
-        describe('when an os adapter is provided', function () {
-            it(`uses the user provided adapter`, function () {
-                const osAdapter: OsAdapter = {
-                    ...os
-                };
-                const client = new MongoClient('mongodb://localhost:27017', {
-                    runtimeAdapters: {
-                        os: osAdapter
-                    }
-                });
+    describe('when an os adapter is provided', function () {
+      it(`uses the user provided adapter`, function () {
+        const osAdapter: OsAdapter = {
+          ...os
+        };
+        const client = new MongoClient('mongodb://localhost:27017', {
+          runtimeAdapters: {
+            os: osAdapter
+          }
+        });
 
-                expect(client.options.runtime.os).to.equal(osAdapter);
-            })
-        })
-    })
-})
+        expect(client.options.runtime.os).to.equal(osAdapter);
+      });
+    });
+  });
+});
