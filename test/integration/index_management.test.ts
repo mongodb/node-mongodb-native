837f71a38910d18c3b55bfd11fc5288462caac0c
Pavel Safronov <pavel.safronov@gmail.com>
2026-02-24 15:54:55 -0800

    remove incorrect import

--- test/integration/index_management.test.ts
+++ test/integration/index_management.test.ts
@@ -7,7 +7,7 @@ import {
   type Db,
   type MongoClient,
   MongoServerError
-} from '../mongodb_runtime-testing';
+} from '../mongodb';
 import { type FailCommandFailPoint } from '../tools/utils';
 import { assert as test, filterForCommands, setupDatabase } from './shared';
 
