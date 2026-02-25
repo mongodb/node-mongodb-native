22c6031a673bef48597fb33041fe1122cc5ec549
Bailey Pearson <bailey.pearson@mongodb.com>
2026-02-25 12:00:46 -0700

    feat(NODE-7142): Exponential backoff and jitter in retry loops (#4871)
    Co-authored-by: Sergey Zelenov <sergey.zelenov@mongodb.com>
    Co-authored-by: Neal Beeken <neal.beeken@mongodb.com>

--- test/mongodb.ts
+++ test/mongodb.ts
@@ -128,6 +128,7 @@ export * from '../src/sdam/topology_description';
 export * from '../src/sessions';
 export * from '../src/sort';
 export * from '../src/timeout';
+export * from '../src/token_bucket';
 export * from '../src/transactions';
 export * from '../src/utils';
 export * from '../src/write_concern';
