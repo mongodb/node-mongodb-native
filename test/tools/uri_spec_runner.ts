098c0df92d47ded9163f0e7ee4083075d9f93c9b
Sergey Zelenov <sergey.zelenov@mongodb.com>
2026-02-27 17:12:02 +0100

    add adaptiveRetries into spec runner

--- test/tools/uri_spec_runner.ts
+++ test/tools/uri_spec_runner.ts
@@ -358,6 +358,7 @@ export function executeUriValidationTest(
       case 'serverSelectionTimeoutMS':
       case 'serverMonitoringMode':
       case 'socketTimeoutMS':
+      case 'adaptiveRetries':
       case 'retryWrites':
       case 'directConnection':
       case 'loadBalanced':
