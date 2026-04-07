49db2b15142d42fd0d7e8c1a21fbd183d084be43
Pavel Safronov <pavel.safronov@gmail.com>
2026-04-07 09:42:04 -0700

    revert unnecessary changes

--- test/integration/retryable-writes/retryable_writes.spec.prose.test.ts
+++ test/integration/retryable-writes/retryable_writes.spec.prose.test.ts
@@ -239,40 +239,38 @@ describe('Retryable Writes Spec Prose', () => {
      * Additionally, this test requires drivers to set a fail point after an insertOne operation but before the subsequent retry.
      * Drivers that are unable to set a failCommand after the CommandSucceededEvent SHOULD use mocking or write a unit test to cover the same sequence of events.
      *
-     * 1. Create a client with retryWrites=true.
+     * Create a client with retryWrites=true.
      *
-     * 2. Configure a fail point with error code `91` (ShutdownInProgress) with the `RetryableError` and
-     *     `SystemOverloadedError` error labels:
+     * Configure a fail point with error code 91 (ShutdownInProgress):
      * ```js
      * db.adminCommand({
      *   configureFailPoint: 'failCommand',
      *   mode: { times: 1 },
      *   data: {
+     *     writeConcernError: {
+     *       code: 91,
+     *       errorLabels: ['RetryableWriteError']
+     *     },
      *     failCommands: ['insert']
-     *     errorLabels: ['RetryableError', 'SystemOverloadedError'],
-     *     errorCode: 91,
      *   }
      * });
      * ```
-     *
-     * 3. Via the command monitoring CommandSucceededEvent, configure a fail point with error code `91` (ShutdownInProgress) and
-     *     the `NoWritesPerformed`, `RetryableError` and `SystemOverloadedError` labels:
+     * Via the command monitoring CommandSucceededEvent, configure a fail point with error code 10107 (NotWritablePrimary) and a NoWritesPerformed label:
      *
      * ```js
      * db.adminCommand({
      *   configureFailPoint: 'failCommand',
-     *   mode: 'alwaysOn',
+     *   mode: { times: 1 },
      *   data: {
+     *     errorCode: 10107,
+     *     errorLabels: ['RetryableWriteError', 'NoWritesPerformed'],
      *     failCommands: ['insert']
-     *     errorLabels: [ 'RetryableError', 'SystemOverloadedError', 'NoWritesPerformed'],
-     *     errorCode: 91,
      *   }
      * });
      * ```
+     * Drivers SHOULD only configure the 10107 fail point command if the the succeeded event is for the 91 error configured in step 2.
      *
-     * Drivers SHOULD configure the second fail point command only if the event is for the first error configured in step 2.
-     *
-     * 4. Attempt an insertOne operation on any record for any database and collection. For the resulting error, assert that the associated error code is 91.
+     * Attempt an insertOne operation on any record for any database and collection. For the resulting error, assert that the associated error code is 91.
      */
     it(
       'when a retry attempt fails with an error labeled NoWritesPerformed, drivers MUST return the original error',
@@ -281,8 +279,7 @@ describe('Retryable Writes Spec Prose', () => {
         const serverCommandStub = sinon.stub(Server.prototype, 'command');
         serverCommandStub.onCall(0).rejects(
           new MongoWriteConcernError({
-            // errorLabels: ['RetryableError', 'SystemOverloadedError'], // expected changes, tests fail
-            errorLabels: ['RetryableWriteError'], // original, tests pass
+            errorLabels: ['RetryableWriteError'],
             writeConcernError: { errmsg: 'ShutdownInProgress error', code: 91 },
             ok: 1
           })
@@ -290,10 +287,8 @@ describe('Retryable Writes Spec Prose', () => {
         serverCommandStub.onCall(1).returns(
           Promise.reject(
             new MongoWriteConcernError({
-              errorLabels: ['RetryableWriteError', 'NoWritesPerformed'], // original, tests pass
-              // errorLabels: ['RetryableError', 'SystemOverloadedError', 'NoWritesPerformed'], // expected changes, tests fail
-              writeConcernError: { errmsg: 'NotWritablePrimary error', code: 91 }, // my changes
-              ok: 1
+              errorLabels: ['RetryableWriteError', 'NoWritesPerformed'],
+              writeConcernError: { errmsg: 'NotWritablePrimary error', errorCode: 10107 }
             })
           )
         );
