d6d6570ad0d8cac0f44db424dc77297fd9d19cdd
Pavel Safronov <pavel.safronov@gmail.com>
2026-05-01 17:24:49 -0700

    really skip that test

--- test/integration/change-streams/change_streams.spec.test.ts
+++ test/integration/change-streams/change_streams.spec.test.ts
@@ -2,8 +2,11 @@ import * as path from 'path';
 
 import { loadSpecTests } from '../../spec';
 import { runUnifiedSuite } from '../../tools/unified-spec-runner/runner';
+import { config } from 'chai';
 
 // TODO: NODE-7559 - remove the mongodb version requirement once the spec tests are updated to be compatible with MongoDB 9.0
-describe('Change Streams Spec - Unified', { requires: { mongodb: '<9.0.0' } }, function () {
-  runUnifiedSuite(loadSpecTests(path.join('change-streams', 'unified')));
+describe('Change Streams Spec - Unified', function () {
+  it('should run, unless it should not', { requires: { mongodb: '<9.0' } }, function () {
+    runUnifiedSuite(loadSpecTests(path.join('change-streams', 'unified')));
+  });
 });
