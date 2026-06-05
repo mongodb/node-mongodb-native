a1be2a88ec100528a9737560ebdfb836ee3f03b0
Sean Milligan <sean.milligan@mongodb.com>
2026-06-05 14:41:22 -0700

    wip

--- test/manual/mocharc.js
+++ test/manual/mocharc.js
@@ -6,8 +6,8 @@ const [major] = process.versions.node.split('.');
 /** @type {import("mocha").MochaOptions} */
 module.exports = {
   require: [
-    'source-map-support/register',
     'ts-node/register',
+    'test/tools/runner/source_map.cjs',
     'test/tools/runner/throw_rejections.cjs',
     'test/tools/runner/chai_addons.ts'
   ],
---
+++ test/tools/runner/source_map.cjs
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('source-map-support').install({
+    hookRequire: true
+});
