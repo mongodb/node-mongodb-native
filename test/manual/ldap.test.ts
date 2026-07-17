62465ae15f56426b91ba6202b804e79683229b66
Johnathan Martell <286711055+johnmtll@users.noreply.github.com>
2026-07-17 15:46:56 -0400

    Revert "test: skips kerberos & ldap tests pending env var change"
    This reverts commit d9eedc19dcff5a419835d19af7692aee786c63bb.

--- test/manual/ldap.test.ts
+++ test/manual/ldap.test.ts
@@ -8,14 +8,6 @@ describe('LDAP', function () {
 
   const MONGODB_URI = `mongodb://${SASL_USER}:${SASL_PASS}@${SASL_HOST}?authMechanism=plain&authSource=$external`;
 
-  // Skipped while we await migration of the underlying environment variable to point at a
-  // compatible server.
-  beforeEach(function () {
-    this.currentTest.skipReason =
-      'Awaiting migration of the underlying env var to aim at a compatible server';
-    this.skip();
-  });
-
   it('Should correctly authenticate against ldap', async function () {
     const client = new MongoClient(MONGODB_URI);
     await client.connect();
